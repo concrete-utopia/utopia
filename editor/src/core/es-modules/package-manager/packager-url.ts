c75ec6f0978bd05c3aa001cd5a7a71385588e35c
Balint Gabor <127662+gbalint@users.noreply.github.com>
2020-06-02 18:26:10 +0200

    We need to url encode the package name in the packager request url

--- editor/src/core/es-modules/package-manager/packager-url.ts
+++ editor/src/core/es-modules/package-manager/packager-url.ts
@@ -1,7 +1,7 @@
 import { NpmDependency } from '../../shared/npm-dependency-types'
 
 export function getPackagerUrl(dep: NpmDependency) {
-  return `/v1/javascript/packager/${dep.name}/${dep.version}.json`
+  return `/v1/javascript/packager/${encodeURIComponent(dep.name)}/${dep.version}.json`
 }
 
 export function getJsDelivrListUrl(dep: NpmDependency) {
