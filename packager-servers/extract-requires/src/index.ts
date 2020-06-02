9a514841b65fe8bc386a2dd1f276c3fd7b2dcc3e
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-06-02 16:00:06 +0200

    making it work from the command line

--- packager-servers/extract-requires/src/index.ts
+++ packager-servers/extract-requires/src/index.ts
@@ -1,6 +1,17 @@
 import { resolveRequirePath } from './extract-require'
 import * as path from 'path'
+import * as fs from 'fs'
 
-const absoluteUrl = path.resolve('./test-folder/test-a/index.js')
+// usage: `npm start "input-file-path.js" "output-file-path.json"`
+
+if (process.argv.length !== 4) {
+  throw new Error('invalid number of parameters')
+}
+
+const targetFileParam = process.argv[2]
+const outputParam = process.argv[3]
+const absoluteUrl = path.resolve(targetFileParam)
 const result = resolveRequirePath(absoluteUrl)
-console.log('result', result)
+
+const outputPath = path.resolve(outputParam)
+fs.writeFileSync(outputPath, JSON.stringify(result, null, 2))
