3d364a0db702be958315f4845d5b0e8e12fdea5a
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-06-02 17:42:37 +0200

    let the script run if one or more of the requires fail

--- packager-servers/extract-requires/src/extract-require.ts
+++ packager-servers/extract-requires/src/extract-require.ts
@@ -3,8 +3,30 @@ import * as R from 'ramda'
 
 import { createRequire } from 'module'
 
+function monkeyPatchRequire(extensionsToMutate: NodeJS.RequireExtensions, fileExtension: string) {
+  console.log('mutating extensions', fileExtension)
+  const realRequire = extensionsToMutate[fileExtension]
+  extensionsToMutate[fileExtension] = (...args) => {
+    try {
+      const realResult = realRequire!(...args)
+      console.log('resolved module', args[1])
+      return realResult
+    } catch (e) {
+      console.log('failed to resolve module, returning mock')
+      return {}
+    }
+  }
+}
+
+function monkeyPatchExtensions(extensionsToMutate: NodeJS.RequireExtensions) {
+  Object.keys(extensionsToMutate).forEach((fileExtension) =>
+    monkeyPatchRequire(extensionsToMutate, fileExtension),
+  )
+}
+
 export function resolveRequirePath(absoluteFilePath: string) {
   const localRequire = createRequire(absoluteFilePath)
+  monkeyPatchExtensions(localRequire.extensions)
   const before = Object.keys(localRequire.cache)
   localRequire(absoluteFilePath)
   const after = Object.keys(localRequire.cache)
