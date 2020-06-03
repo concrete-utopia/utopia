c4ed79ad6b18a8e22e465524453f5183407242b8
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2020-06-03 13:44:34 +0100

    fix(NPM.hs tests) Commented out tests that are dependent on code that was commented out

--- server/test/Test/Utopia/Web/Packager/NPM.hs
+++ server/test/Test/Utopia/Web/Packager/NPM.hs
@@ -24,33 +24,33 @@ npmSpec = do
     it "should fail for a non-existent project" $ do
       semaphore <- newQSem 1
       withInstalledProject semaphore "non-existent-project-that-will-never-exist" "9.9.9.9.9.9" getNodeModulesSubDirectories `shouldThrow` anyIOException
-  describe "getModuleAndDependenciesFiles" $ do
-    it "should get a bunch of .js, .d.ts and package.json files" $ do
-      semaphore <- newQSem 1
-      result <- withInstalledProject semaphore "react" "16.13.1" $ getModuleAndDependenciesFiles "react"
-      let sortedFilenames = sort $ Map.keys result
-      sortedFilenames `shouldBe` [
-        "/node_modules/js-tokens/package.json",
-        "/node_modules/loose-envify/package.json",
-        "/node_modules/object-assign/index.js",
-        "/node_modules/object-assign/package.json",
-        "/node_modules/prop-types/checkPropTypes.js",
-        "/node_modules/prop-types/lib/ReactPropTypesSecret.js",
-        "/node_modules/prop-types/package.json",
-        "/node_modules/react-is/package.json",
-        "/node_modules/react/cjs/react.development.js",
-        "/node_modules/react/index.js",
-        "/node_modules/react/package.json" ]
-  describe "getRelevantFiles" $ do
-    it "should get a bunch of .d.ts and package.json files" $ do
-      semaphore <- newQSem 1
-      result <- withInstalledProject semaphore "react" "16.13.1" getRelevantFiles
-      let sortedFilenames = sort $ Map.keys result
-      sortedFilenames `shouldBe` [
-        "/node_modules/js-tokens/package.json",
-        "/node_modules/loose-envify/package.json",
-        "/node_modules/object-assign/package.json",
-        "/node_modules/prop-types/package.json",
-        "/node_modules/react-is/package.json",
-        "/node_modules/react/package.json" ]
+  -- describe "getModuleAndDependenciesFiles" $ do
+  --   it "should get a bunch of .js, .d.ts and package.json files" $ do
+  --     semaphore <- newQSem 1
+  --     result <- withInstalledProject semaphore "react" "16.13.1" $ getModuleAndDependenciesFiles "react"
+  --     let sortedFilenames = sort $ Map.keys result
+  --     sortedFilenames `shouldBe` [
+  --       "/node_modules/js-tokens/package.json",
+  --       "/node_modules/loose-envify/package.json",
+  --       "/node_modules/object-assign/index.js",
+  --       "/node_modules/object-assign/package.json",
+  --       "/node_modules/prop-types/checkPropTypes.js",
+  --       "/node_modules/prop-types/lib/ReactPropTypesSecret.js",
+  --       "/node_modules/prop-types/package.json",
+  --       "/node_modules/react-is/package.json",
+  --       "/node_modules/react/cjs/react.development.js",
+  --       "/node_modules/react/index.js",
+  --       "/node_modules/react/package.json" ]
+  -- describe "getRelevantFiles" $ do
+  --   it "should get a bunch of .d.ts and package.json files" $ do
+  --     semaphore <- newQSem 1
+  --     result <- withInstalledProject semaphore "react" "16.13.1" getRelevantFiles
+  --     let sortedFilenames = sort $ Map.keys result
+  --     sortedFilenames `shouldBe` [
+  --       "/node_modules/js-tokens/package.json",
+  --       "/node_modules/loose-envify/package.json",
+  --       "/node_modules/object-assign/package.json",
+  --       "/node_modules/prop-types/package.json",
+  --       "/node_modules/react-is/package.json",
+  --       "/node_modules/react/package.json" ]
 
