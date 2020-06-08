67ac1d6a6c0f29991ff728c540bf80856a01aa41
Malte Nuhn <malte.nuhn@gmail.com>
2020-06-08 10:08:57 +0100

    Update project-file-utils.ts

--- editor/src/core/model/project-file-utils.ts
+++ editor/src/core/model/project-file-utils.ts
@@ -321,7 +321,7 @@ export function fileTypeFromFileName(filename: string | null): ProjectFileType |
   if (filename == null) {
     return null
   }
-  if (filename.endsWith('.js')) {
+  if (filename.endsWith('src/app.js')) {
     return 'UI_JS_FILE'
   } else {
     const mimeType = mimeTypeLookup(filename)
