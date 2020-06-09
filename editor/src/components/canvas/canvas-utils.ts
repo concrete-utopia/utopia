8a7756f9ff7520b537ed920086d5fed936093e90
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-06-09 13:24:45 +0200

    Enable reparenting to all elements (#68)
    * enable reparenting to all elements
    
    * clarifying this function is only used for reparenting

--- editor/src/components/canvas/canvas-utils.ts
+++ editor/src/components/canvas/canvas-utils.ts
@@ -1298,18 +1298,14 @@ export function filterMultiSelectScenes(targets: Array<TemplatePath>): Array<Tem
   }
 }
 
-export function getTargetAtPosition(
+function getReparentTargetAtPosition(
   editorState: EditorState,
   position: CanvasPoint,
 ): TemplatePath | undefined {
   const allTargets = Canvas.getAllTargetsAtPoint(
     editorState,
     position,
-    [
-      TargetSearchType.ParentsOfSelected,
-      TargetSearchType.SiblingsOfSelected,
-      TargetSearchType.TopLevelElements,
-    ],
+    [TargetSearchType.All],
     false,
   )
   // filtering for non-selected views from alltargets
@@ -1328,7 +1324,7 @@ export function getReparentTarget(
   shouldReparent: boolean
   newParent: TemplatePath | null
 } {
-  const result = getTargetAtPosition(editorState, position)
+  const result = getReparentTargetAtPosition(editorState, position)
   const possibleNewParent = result == undefined ? null : result
   const currentParents = Utils.stripNulls(
     toReparent.map((view) => MetadataUtils.getParent(editorState.jsxMetadataKILLME, view)),
