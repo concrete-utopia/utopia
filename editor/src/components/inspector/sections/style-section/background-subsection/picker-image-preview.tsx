8d877bcf88d04f4edafbba9024b37571d5dcff1f
Malte Nuhn <malte.nuhn@gmail.com>
2020-06-17 15:40:07 +0100

    fix(imagepicker) - 'empty' state + guidance when empty

--- editor/src/components/inspector/sections/style-section/background-subsection/picker-image-preview.tsx
+++ editor/src/components/inspector/sections/style-section/background-subsection/picker-image-preview.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { colorTheme, FlexRow, Icn, UtopiaTheme } from '../../../../../uuiui'
+import { colorTheme, FlexRow, Icn, UtopiaTheme, SimpleFlexRow } from '../../../../../uuiui'
 import { betterReactMemo } from '../../../../../uuiui-deps'
 import { MetadataEditorModalPreviewHeight } from '../../../controls/color-picker'
 import { CSSURLFunctionBackgroundLayer } from '../../../common/css-utils'
@@ -33,7 +33,7 @@ export const PickerImagePreview = betterReactMemo(
               width: '100%',
               height: '100%',
               borderRadius: UtopiaTheme.inputBorderRadius,
-              backgroundColor: '#FFFFFF33',
+              backgroundColor: '#FFFFFF88',
               boxShadow: noImageProvided
                 ? undefined
                 : `0 0 0 1px ${colorTheme.warningForeground.value} inset`,
@@ -41,22 +41,26 @@ export const PickerImagePreview = betterReactMemo(
               color: noImageProvided ? undefined : colorTheme.warningForeground.value,
             }}
           >
-            {noImageProvided ? undefined : (
-              <Icn type='warningtriangle' color='orange' width={16} height={16} />
-            )}
-            <span style={{ paddingLeft: 4 }}>
-              {props.value.url === '' ? (
-                <div>
-                  Add an image URL <br />
-                  Local assets:<code>assets/image.png</code>
-                  <br />
-                  From the internet: <code>https://utopia.fm/smiangle.png</code>
+            <div>
+              {noImageProvided ? (
+                <div style={{ paddingLeft: 8, whiteSpace: 'normal' }}>
+                  <p>
+                    Add an image URL, e.g. <span style={{ fontWeight: 600 }}>assets/image.png</span>{' '}
+                    or <span style={{ fontWeight: 600 }}>https://utopia.fm/smiangle.png</span>
+                  </p>
+
+                  <p>
+                    NB You can add images by dropping them onto the canvas or into the file system,
+                    or with copy &amp; paste
+                  </p>
                 </div>
               ) : (
-                'The image did not load load.'
+                <SimpleFlexRow>
+                  <Icn type='warningtriangle' color='orange' width={16} height={16} />
+                  <span style={{ paddingLeft: 8 }}>Unable to load {props.value.url}</span>
+                </SimpleFlexRow>
               )}
-            </span>
-            <span>{props.value.url}</span>
+            </div>
           </FlexRow>
         ) : (
           <img
