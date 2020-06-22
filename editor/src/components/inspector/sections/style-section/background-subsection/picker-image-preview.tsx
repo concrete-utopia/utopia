bed744dd22881ed406f0cb965539645e8646b48b
Malte <malte.nuhn@gmail.com>
2020-06-22 13:09:44 +0100

    Fix/imagepicker (#121)
    * fix(image picker) - clarify warnings and troubleshooting help
    
    * fix(imagepicker) - 'empty' state + guidance when empty
    
    * Update picker-image-preview.tsx

--- editor/src/components/inspector/sections/style-section/background-subsection/picker-image-preview.tsx
+++ editor/src/components/inspector/sections/style-section/background-subsection/picker-image-preview.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { colorTheme, FlexRow, Icn, UtopiaTheme } from '../../../../../uuiui'
+import { colorTheme, FlexRow, Icn, UtopiaTheme, SimpleFlexRow } from '../../../../../uuiui'
 import { betterReactMemo } from '../../../../../uuiui-deps'
 import { MetadataEditorModalPreviewHeight } from '../../../controls/color-picker'
 import { CSSURLFunctionBackgroundLayer } from '../../../common/css-utils'
@@ -17,6 +17,8 @@ export const PickerImagePreview = betterReactMemo(
       setImageNotFound(true)
     }, [])
 
+    const imageURLempty = props.value.url === ''
+
     return (
       <div
         style={{
@@ -31,13 +33,34 @@ export const PickerImagePreview = betterReactMemo(
               width: '100%',
               height: '100%',
               borderRadius: UtopiaTheme.inputBorderRadius,
-              boxShadow: `0 0 0 1px ${colorTheme.warningForeground.value} inset`,
+              backgroundColor: '#FFFFFF88',
+              boxShadow: imageURLempty
+                ? undefined
+                : `0 0 0 1px ${colorTheme.warningForeground.value} inset`,
               justifyContent: 'center',
-              color: colorTheme.warningForeground.value,
+              color: imageURLempty ? undefined : colorTheme.warningForeground.value,
             }}
           >
-            <Icn type='warningtriangle' color='orange' width={16} height={16} />
-            <span style={{ paddingLeft: 4 }}>Image failed to load.</span>
+            <div>
+              {imageURLempty ? (
+                <div style={{ paddingLeft: 8, whiteSpace: 'normal' }}>
+                  <p>
+                    Add an image URL, e.g. <span style={{ fontWeight: 600 }}>assets/image.png</span>{' '}
+                    or <span style={{ fontWeight: 600 }}>https://utopia.fm/smiangle.png</span>
+                  </p>
+
+                  <p>
+                    NB You can add images by dropping them onto the canvas or into the file system,
+                    or with copy &amp; paste
+                  </p>
+                </div>
+              ) : (
+                <SimpleFlexRow>
+                  <Icn type='warningtriangle' color='orange' width={16} height={16} />
+                  <span style={{ paddingLeft: 8 }}>Unable to load {props.value.url}</span>
+                </SimpleFlexRow>
+              )}
+            </div>
           </FlexRow>
         ) : (
           <img
