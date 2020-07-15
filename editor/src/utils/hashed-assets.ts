dfab84d2c577d2fd10e4da354470b2ef2200883b
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2020-07-15 15:29:26 +0100

    Faster startup (#263)
    * fix(Editor) Inject editor css files when starting the editor to improve load
    
    * fix(Editor) Pulled out css required for initial loading screen
    
    * fix(Editor) Call triggerHashedAssetsUpdate at the very start rather than blocking initial render with it
    
    * actually still block the editor first render while triggerHashedAssetsUpdate is happening
    
    * fix(Editor) Remove unnecessary dependencies from the default project
    
    * fixing typo
    
    * fix(tests) avoid calling fetch in hashedAssetsUpdate during tests
    
    * Update editor/src/templates/editor-entry-point.tsx
    
    Co-authored-by: Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>

--- editor/src/utils/hashed-assets.ts
+++ editor/src/utils/hashed-assets.ts
@@ -1,21 +1,26 @@
 import { HEADERS, MODE } from '../common/server'
 import { BASE_URL } from '../common/env-vars'
+import { isBrowserEnvironment } from '../core/shared/utils'
 
 const HASHED_ASSETS_ENDPOINT = BASE_URL + 'hashed-assets.json'
 
 export let HASHED_ASSETS_MAPPINGS: { [key: string]: string } = {}
 
 export function triggerHashedAssetsUpdate(): Promise<void> {
-  return fetch(HASHED_ASSETS_ENDPOINT, {
-    method: 'GET',
-    credentials: 'include',
-    headers: HEADERS,
-    mode: MODE,
-  }).then((response) => {
-    response.json().then((mappingsJSON) => {
-      HASHED_ASSETS_MAPPINGS = mappingsJSON
+  if (isBrowserEnvironment) {
+    return fetch(HASHED_ASSETS_ENDPOINT, {
+      method: 'GET',
+      credentials: 'include',
+      headers: HEADERS,
+      mode: MODE,
+    }).then((response) => {
+      response.json().then((mappingsJSON) => {
+        HASHED_ASSETS_MAPPINGS = mappingsJSON
+      })
     })
-  })
+  } else {
+    return Promise.resolve()
+  }
 }
 
 export function getPossiblyHashedURL(url: string): string {
