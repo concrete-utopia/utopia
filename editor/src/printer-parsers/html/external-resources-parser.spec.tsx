26f990cf7cea8aeaca461eed7fc76d187820047e
Alec Molloy <alecmolloy@users.noreply.github.com>
2020-07-24 10:23:55 -0700

    Printer/onSubmitValue for external resources (#318)
    * parse external resources
    
    * Update editor/package.json
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
    
    * add tests and fix things
    
    * Update external-resources-parser.tsx
    
    * Update left-pane.tsx
    
    * printer and updater for external resources
    
    * make things better for tests
    
    * kinda fix tests
    
    * remove duplicate action
    
    * Update external-resources-parser.tsx
    
    * Update action-utils.ts
    
    * Update actions.ts
    
    * a simpler test for external resource updating
    
    * Update test-utils.ts
    
    * default to public/index.html if no package json entry is found
    
    * fix up tests for real
    
    * Update external-resources-parser.spec.tsx
    
    * clean up external resources hook
    
    * Update external-resources-parser.spec.tsx
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>

--- editor/src/printer-parsers/html/external-resources-parser.spec.tsx
+++ editor/src/printer-parsers/html/external-resources-parser.spec.tsx
@@ -1,4 +1,13 @@
-import { parseLinkTags, getGeneratedExternalLinkText } from './external-resources-parser'
+import { previewHtml } from '../../core/model/new-project-files'
+import { isRight } from '../../core/shared/either'
+import {
+  externalResources,
+  genericExternalResource,
+  getGeneratedExternalLinkText,
+  googleFontsResource,
+  parseLinkTags,
+  updateHTMLExternalResourcesLinks,
+} from './external-resources-parser'
 
 const testFile = `
 <!DOCTYPE html>
@@ -11,6 +20,9 @@ const testFile = `
   <link href="http://utopia.com/styles.css" rel="stylesheet">
   <!-- Random extra comment that doesn't belong -->
   <link href="//utopia.com/styles.css" rel="stylesheet">
+  <link href="/favicon.ico" rel="icon">
+  <link href="apple-icon-114.png" rel="apple-touch-icon-precomposed" sizes="114x114" type="image/png">
+  <link rel="preload" href="myFont.woff2" as="font" type="font/woff2" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Comfortaa&text=Hello%20World" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Comfortaa&text=%c2%a1Hola!" rel="stylesheet">
   <!-- End Generated Utopia External Links -->
@@ -35,53 +47,107 @@ describe('external-resources-parser', () => {
         <link href=\\"http://utopia.com/styles.css\\" rel=\\"stylesheet\\">
         <!-- Random extra comment that doesn't belong -->
         <link href=\\"//utopia.com/styles.css\\" rel=\\"stylesheet\\">
+        <link href=\\"/favicon.ico\\" rel=\\"icon\\">
+        <link href=\\"apple-icon-114.png\\" rel=\\"apple-touch-icon-precomposed\\" sizes=\\"114x114\\" type=\\"image/png\\">
+        <link rel=\\"preload\\" href=\\"myFont.woff2\\" as=\\"font\\" type=\\"font/woff2\\" crossorigin=\\"anonymous\\">
         <link href=\\"https://fonts.googleapis.com/css2?family=Comfortaa&text=Hello%20World\\" rel=\\"stylesheet\\">
         <link href=\\"https://fonts.googleapis.com/css2?family=Comfortaa&text=%c2%a1Hola!\\" rel=\\"stylesheet\\">",
       }
     `)
 
-    const parsedLinkTags = parseLinkTags(parsedLinkTagsText.value)
-    expect(parsedLinkTags).toMatchInlineSnapshot(`
+    if (isRight(parsedLinkTagsText)) {
+      const parsedLinkTags = parseLinkTags(parsedLinkTagsText.value)
+      expect(parsedLinkTags).toMatchInlineSnapshot(`
+        Object {
+          "type": "RIGHT",
+          "value": Object {
+            "genericExternalResources": Array [
+              Object {
+                "href": "https://utopia.com/styles.css",
+                "rel": "stylesheet",
+                "type": "generic-external-resource",
+              },
+              Object {
+                "href": "http://utopia.com/styles.css",
+                "rel": "stylesheet",
+                "type": "generic-external-resource",
+              },
+              Object {
+                "href": "//utopia.com/styles.css",
+                "rel": "stylesheet",
+                "type": "generic-external-resource",
+              },
+              Object {
+                "href": "/favicon.ico",
+                "rel": "icon",
+                "type": "generic-external-resource",
+              },
+              Object {
+                "href": "apple-icon-114.png",
+                "rel": "apple-touch-icon-precomposed",
+                "type": "generic-external-resource",
+              },
+              Object {
+                "href": "myFont.woff2",
+                "rel": "preload",
+                "type": "generic-external-resource",
+              },
+            ],
+            "googleFontsResources": Array [
+              Object {
+                "fontFamily": "Red Rose",
+                "fontStyleParams": undefined,
+                "type": "google-fonts-resource",
+              },
+              Object {
+                "fontFamily": "Red Rose",
+                "fontStyleParams": ":wght@400",
+                "type": "google-fonts-resource",
+              },
+              Object {
+                "fontFamily": "Comfortaa",
+                "fontStyleParams": undefined,
+                "type": "google-fonts-resource",
+              },
+              Object {
+                "fontFamily": "Comfortaa",
+                "fontStyleParams": undefined,
+                "type": "google-fonts-resource",
+              },
+            ],
+          },
+        }
+      `)
+    }
+  })
+})
+
+describe('updates external resources', () => {
+  it('adds new resources', () => {
+    const updated = updateHTMLExternalResourcesLinks(
+      previewHtml,
+      externalResources(
+        [genericExternalResource('https://utopia.com/stylesheet.css', 'stylesheet')],
+        [googleFontsResource('Roboto')],
+      ),
+    )
+    expect(updated).toMatchInlineSnapshot(`
       Object {
         "type": "RIGHT",
-        "value": Object {
-          "genericExternalResources": Array [
-            Object {
-              "href": "https://utopia.com/styles.css",
-              "type": "generic-external-resource",
-            },
-            Object {
-              "href": "http://utopia.com/styles.css",
-              "type": "generic-external-resource",
-            },
-            Object {
-              "href": "//utopia.com/styles.css",
-              "type": "generic-external-resource",
-            },
-          ],
-          "googleFontsResources": Array [
-            Object {
-              "fontFamily": "Red Rose",
-              "fontStyleParams": undefined,
-              "type": "google-fonts-resource",
-            },
-            Object {
-              "fontFamily": "Red Rose",
-              "fontStyleParams": ":wght@400",
-              "type": "google-fonts-resource",
-            },
-            Object {
-              "fontFamily": "Comfortaa",
-              "fontStyleParams": undefined,
-              "type": "google-fonts-resource",
-            },
-            Object {
-              "fontFamily": "Comfortaa",
-              "fontStyleParams": undefined,
-              "type": "google-fonts-resource",
-            },
-          ],
-        },
+        "value": "<!DOCTYPE html>
+      <html lang=\\"en\\">
+        <head>
+          <meta charset=\\"utf-8\\">
+          <title>Utopia React App</title>
+          <!-- Begin Generated Utopia External Links -->
+          <link href=\\"https://utopia.com/stylesheet.css\\" rel=\\"stylesheet\\">
+          <link href=\\"https://fonts.googleapis.com/css2?family=Roboto\\" rel=\\"stylesheet\\">
+          <!-- End Generated Utopia External Links -->
+        </head>
+        <body>
+          <div id=\\"root\\"></div>
+        </body>
+      </html>",
       }
     `)
   })
