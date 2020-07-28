3bfbb09ec38b69fce3c1f83dd8d0a1403f5fb9e4
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2020-07-28 09:59:04 +0200

    Feature/canvas dynamic scene resize (#333)
    * feature(scene) resizing a dynamic scene resizes the rootview instead
    
    * fix(tests) changed createFakeMetadataForParseSuccess to not use unmapScene anymore
    
    * fix(editor) removing unused import
    
    * feature(scene) added tests for resizing a dynamic scene
    
    * fix(tests) fix scenepath for createFakeMetadataForParseSuccess

--- editor/src/components/canvas/canvas-utils-scene.spec.browser.ts
+++ editor/src/components/canvas/canvas-utils-scene.spec.browser.ts
@@ -3,6 +3,7 @@ import {
   renderTestEditorWithCode,
   TestScenePath,
   makeTestProjectCodeWithSnippet,
+  getTestParseSuccess,
 } from './ui-jsx-test-utils' // IMPORTANT - THIS IMPORT MUST ALWAYS COME FIRST
 import { fireEvent } from '@testing-library/react'
 import { act } from 'react-dom/test-utils'
@@ -11,6 +12,9 @@ import * as Prettier from 'prettier'
 import * as TP from '../../core/shared/template-path'
 
 import { PrettierConfig } from '../../core/workers/parser-printer/prettier-utils'
+import { createFakeMetadataForParseSuccess } from '../../utils/test-utils'
+import { determineElementsToOperateOnForDragging } from './controls/select-mode/move-utils'
+import { BakedInStoryboardUID } from '../../core/model/scene-utils'
 
 describe('moving a scene/rootview on the canvas', () => {
   it('dragging a dynamic scene’s root view sets the scene position', async () => {
@@ -227,5 +231,102 @@ describe('moving a scene/rootview on the canvas', () => {
         </View>
       `),
     )
+  })
+  it('resizing a dynamic scene changes size of the rootview', async () => {
+    const testUID = 'scene-0'
+    const rootViewUID = 'abcd'
+    const testCode = Prettier.format(
+      `/** @jsx jsx */
+        import * as React from 'react'
+        import { Scene, Storyboard, View, jsx } from 'utopia-api'
+        export var App = (props) => {
+          return (
+            <View
+              style={{ width: 375, height: 812 }}
+              layout={{ layoutSystem: 'pinSystem' }}
+              data-uid={'${rootViewUID}'}
+            >
+              <View
+                style={{ backgroundColor: '#0091FFAA', left: 50, top: 50, width: 200, height: 200 }}
+                layout={{ layoutSystem: 'pinSystem' }}
+                data-uid={'bbb'}
+              />
+            </View>
+          )
+        }
+        export var storyboard = (props) => {
+          return (
+            <Storyboard data-uid={'${BakedInStoryboardUID}'}>
+              <Scene
+                style={{ position: 'absolute' }}
+                component={App}
+                data-uid={'${testUID}'}
+              />
+            </Storyboard>
+          )
+        }`,
+      PrettierConfig,
+    )
+    const parseSuccess = getTestParseSuccess(testCode)
+    const testMetadata = createFakeMetadataForParseSuccess(parseSuccess)
+    const selectedScene = TP.scenePath([BakedInStoryboardUID, testUID])
+    const rootViewPath = TP.instancePath(selectedScene, [rootViewUID])
+    const elementsToResize = determineElementsToOperateOnForDragging(
+      [selectedScene],
+      testMetadata,
+      false,
+      false,
+    )
+
+    expect(elementsToResize).toEqual([rootViewPath])
+  })
+  it('resizing a static scene changes size of the scene, not the rootview', async () => {
+    const testUID = 'scene-0'
+    const rootViewUID = 'abcd'
+    const testCode = Prettier.format(
+      `/** @jsx jsx */
+        import * as React from 'react'
+        import { Scene, Storyboard, View, jsx } from 'utopia-api'
+        export var App = (props) => {
+          return (
+            <View
+              style={{ width: '100%', height: '100%' }}
+              layout={{ layoutSystem: 'pinSystem' }}
+              data-uid={'${rootViewUID}'}
+            >
+              <View
+                style={{ backgroundColor: '#0091FFAA', left: 50, top: 50, width: 200, height: 200 }}
+                layout={{ layoutSystem: 'pinSystem' }}
+                data-uid={'bbb'}
+              />
+            </View>
+          )
+        }
+        export var storyboard = (props) => {
+          return (
+            <Storyboard data-uid={'${BakedInStoryboardUID}'}>
+              <Scene
+                style={{ position: 'absolute', width: 100, height: 100 }}
+                component={App}
+                static
+                data-uid={'${testUID}'}
+              />
+            </Storyboard>
+          )
+        }`,
+      PrettierConfig,
+    )
+    const parseSuccess = getTestParseSuccess(testCode)
+    const testMetadata = createFakeMetadataForParseSuccess(parseSuccess)
+    const selectedScene = TP.scenePath([BakedInStoryboardUID, testUID])
+    const rootViewPath = TP.instancePath(selectedScene, [rootViewUID])
+    const elementsToResize = determineElementsToOperateOnForDragging(
+      [selectedScene],
+      testMetadata,
+      false,
+      false,
+    )
+
+    expect(elementsToResize).toEqual([rootViewPath])
   })
 })
