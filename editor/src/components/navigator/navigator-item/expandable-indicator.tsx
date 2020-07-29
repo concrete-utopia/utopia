15eba84cccdbbb8520a4917dabbcb30b1bc7e767
Alec Molloy <alecmolloy@users.noreply.github.com>
2020-07-29 15:10:46 -0700

    Update expandable-indicator.tsx

--- editor/src/components/navigator/navigator-item/expandable-indicator.tsx
+++ editor/src/components/navigator/navigator-item/expandable-indicator.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { Icn, Icons } from '../../../uuiui'
 
 export const ExpansionArrowWidth = 8
 export const ExpansionArrowHeight = 8
@@ -11,28 +12,13 @@ interface ExpandableIndicatorProps {
   onClick?: (e: any) => void
 }
 
-export const ExpandableIndicator: React.StatelessComponent<ExpandableIndicatorProps> = (props) => (
-  <svg
-    className={'row-expansion ' + (props.visible ? 'expansive' : 'not-expansive')}
-    style={{
-      transform: props.collapsed ? 'rotate(-90deg)' : '',
-      transformOrigin: 'center center',
-      transition: 'transform .05s',
-      padding: '7px 4px',
-    }}
-    width={ExpansionArrowWidth}
-    height={ExpansionArrowHeight}
-    strokeLinecap='round'
-    shapeRendering='geometricPrecision'
-    onMouseDown={props.onMouseDown}
-    onClick={props.onClick}
-  >
-    <polyline
-      className='expandable-indicator'
-      strokeWidth={props.visible ? 1 : 0}
-      stroke={props.selected ? 'white' : '#666'}
-      fill='transparent'
-      points='2,3 5,6 8,3'
+export const ExpandableIndicator: React.StatelessComponent<ExpandableIndicatorProps> = (props) =>
+  props.visible ? (
+    <Icn
+      category='semantic'
+      type={`expansionarrow-${props.collapsed ? 'right' : 'down'}`}
+      color={props.selected ? 'white' : 'black'}
+      onMouseDown={props.onMouseDown}
+      onClick={props.onClick}
     />
-  </svg>
-)
+  ) : null
