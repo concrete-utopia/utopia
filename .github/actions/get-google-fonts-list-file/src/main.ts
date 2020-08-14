11ff6820f42422171ea90c6131d6abb56097a521
Alec Molloy <alecmolloy@users.noreply.github.com>
2020-08-14 13:19:52 -0700

    Update main.ts

--- .github/actions/get-google-fonts-list-file/src/main.ts
+++ .github/actions/get-google-fonts-list-file/src/main.ts
@@ -19,37 +19,41 @@ async function run(): Promise<void> {
     .then(response => {
       response
         .json()
-        .then((responseData: {items: GoogleFontsList}) => {
-          const data = responseData.items.map(datum => ({
-            type: 'google-fonts-typeface',
-            family: datum.family,
-            variants: datum.variants
-          }))
-          if (!(data.length > 0)) {
-            core.setFailed(`Data: ${JSON.stringify(data)} is empty`)
+        .then((responseData: {items: GoogleFontsList; error: string}) => {
+          if (responseData.error != null) {
+            core.setFailed(responseData.error)
           } else {
-            core.setOutput('google-fonts-list-length', data.length)
+            const data = responseData.items.map(datum => ({
+              type: 'google-fonts-typeface',
+              family: datum.family,
+              variants: datum.variants
+            }))
+            if (!(data.length > 0)) {
+              core.setFailed(`Data: ${JSON.stringify(data)} is empty`)
+            } else {
+              core.setOutput('google-fonts-list-length', data.length)
 
-            const dataJSONString = JSON.stringify(data)
-            const uglyFile = `import { GoogleFontsTypefaceMetadata } from '../src/components/navigator/external-resources/google-fonts-utils'
+              const dataJSONString = JSON.stringify(data)
+              const uglyFile = `import { GoogleFontsTypefaceMetadata } from '../src/components/navigator/external-resources/google-fonts-utils'
 
 /** This is auto-generated using this workflow action: https://github.com/concrete-utopia/get-google-fonts-list-file */
 export const googleFontsList: Array<GoogleFontsTypefaceMetadata> = ${dataJSONString}`
-            const prettyFile = prettier.format(uglyFile, {
-              parser: 'typescript',
-              plugins: [parserTypescript],
-              printWidth: 100,
-              trailingComma: 'all',
-              tabWidth: 2,
-              semi: false,
-              singleQuote: true,
-              quoteProps: 'as-needed',
-              bracketSpacing: true,
-              jsxSingleQuote: true,
-              jsxBracketSameLine: false,
-              arrowParens: 'always'
-            })
-            core.setOutput('google-fonts-file', prettyFile)
+              const prettyFile = prettier.format(uglyFile, {
+                parser: 'typescript',
+                plugins: [parserTypescript],
+                printWidth: 100,
+                trailingComma: 'all',
+                tabWidth: 2,
+                semi: false,
+                singleQuote: true,
+                quoteProps: 'as-needed',
+                bracketSpacing: true,
+                jsxSingleQuote: true,
+                jsxBracketSameLine: false,
+                arrowParens: 'always'
+              })
+              core.setOutput('google-fonts-file', prettyFile)
+            }
           }
         })
         .catch(error => {
