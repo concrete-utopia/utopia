060cacc3df60bc13998c1826a74057e27e109396
Alec Molloy <alecmolloy@users.noreply.github.com>
2020-08-14 08:58:23 -0700

    move things into utopia

---
+++ .github/actions/get-google-fonts-list-file/__tests__/main.test.ts
@@ -0,0 +1,15 @@
+import * as process from 'process'
+import * as cp from 'child_process'
+import * as path from 'path'
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_MILLISECONDS'] = '500'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  expect(
+    cp.execSync(`node ${ip}`, options).toString().includes('google-fonts-file')
+  ).toBeTruthy()
+})
---
+++ .github/actions/get-google-fonts-list-file/src/main.ts
@@ -0,0 +1,45 @@
+import * as core from '@actions/core'
+import fetch from 'node-fetch'
+interface GoogleFontsFontMetadata {
+  family: string
+  variants: Array<string>
+}
+type GoogleFontsList = Array<GoogleFontsFontMetadata>
+
+const GOOGLE_WEB_FONTS_KEY = 'AIzaSyBffJtCo2vL68hdQKH3IYjo0ELFAAGYNW4'
+const GoogleWebFontsURL = `https://www.googleapis.com/webfonts/v1/webfonts?key=${GOOGLE_WEB_FONTS_KEY}`
+
+async function run(): Promise<void> {
+  fetch(GoogleWebFontsURL)
+    .then(response => {
+      response
+        .json()
+        .then((responseData: {items: GoogleFontsList}) => {
+          const data = responseData.items.map(datum => ({
+            type: 'google-fonts-typeface',
+            family: datum.family,
+            variants: datum.variants
+          }))
+          if (!(data.length > 0)) {
+            core.setFailed(`Data: ${JSON.stringify(data)} is empty`)
+          } else {
+            core.setOutput('google-fonts-list-length', data.length)
+
+            const dataJSONString = JSON.stringify(data)
+            const file = `import { GoogleFontsTypefaceMetadata } from '../src/components/navigator/external-resources/google-fonts-utils'
+
+/** This is auto-generated using this workflow action: https://github.com/concrete-utopia/get-google-fonts-list-file */
+export const googleFontsList: Array<GoogleFontsTypefaceMetadata> = ${dataJSONString}`
+            core.setOutput('google-fonts-file', file)
+          }
+        })
+        .catch(error => {
+          core.setFailed(error.message)
+        })
+    })
+    .catch(error => {
+      core.setFailed(error.message)
+    })
+}
+
+run()
