b072b57158c21e425c64ed2abcf0d415d6b14998
Alec Molloy <alecmolloy@users.noreply.github.com>
2020-08-17 15:58:52 -0700

    Update google-fonts-resources-list-search.tsx

--- editor/src/components/navigator/external-resources/google-fonts-resources-list-search.tsx
+++ editor/src/components/navigator/external-resources/google-fonts-resources-list-search.tsx
@@ -115,10 +115,10 @@ export const GoogleFontsResourcesListSearch = betterReactMemo<GoogleFontsResourc
         children: Utils.stripNulls(
           googleFontsList.map((fontDatum) => {
             const linkedVariants = linkedResources.find(
-              (resource) => resource.fontFamily === fontDatum.family,
+              (resource) => resource.fontFamily === fontDatum.name,
             )
             return fontFamilyData(
-              fontDatum.family,
+              fontDatum.name,
               fontDatum.variants.map((variant) => {
                 const isDownloaded =
                   (linkedVariants?.variants.findIndex(
@@ -127,7 +127,7 @@ export const GoogleFontsResourcesListSearch = betterReactMemo<GoogleFontsResourc
                       linkedVariant.webFontWeight === variant.webFontWeight,
                   ) ?? -1) >= 0
                 return fontVariantData(
-                  webFontFamilyVariant(fontDatum.family, variant),
+                  webFontFamilyVariant(fontDatum.name, variant),
                   isDownloaded,
                   pushNewFontFamilyVariant,
                   removeFontFamilyVariant,
