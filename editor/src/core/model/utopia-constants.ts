fe66f77587be984376d7bd177b92500f0ba92391
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2020-08-25 10:30:55 +0100

    chore(pr) Added file missing from previous commit.

---
+++ editor/src/core/model/utopia-constants.ts
@@ -0,0 +1,13 @@
+export const UTOPIA_UID_KEY = 'data-uid'
+export const UTOPIA_LABEL_KEY = 'data-label'
+export const UTOPIA_ORIGINAL_ID_KEY = 'data-utopia-original-uid'
+export const UTOPIA_DO_NOT_TRAVERSE_KEY = 'data-utopia-do-not-traverse'
+export const UTOPIA_SCENE_ID_KEY = 'data-utopia-scene-id'
+
+export const UtopiaKeys: Array<string> = [
+  UTOPIA_UID_KEY,
+  UTOPIA_LABEL_KEY,
+  UTOPIA_ORIGINAL_ID_KEY,
+  UTOPIA_DO_NOT_TRAVERSE_KEY,
+  UTOPIA_SCENE_ID_KEY,
+]
