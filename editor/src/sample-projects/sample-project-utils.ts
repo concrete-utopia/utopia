6c7e748f60148a1e51ec94c11d824c63d5a9fbb6
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2020-09-08 15:50:16 +0100

    spike(editor) Eager evaluation test.

--- editor/src/sample-projects/sample-project-utils.ts
+++ editor/src/sample-projects/sample-project-utils.ts
@@ -13,9 +13,28 @@ import {
   getSamplePreviewHTMLFile,
 } from '../core/model/new-project-files'
 import { codeFile, directory } from '../core/model/project-file-utils'
+import { ProjectContents } from '../core/shared/project-file-types'
 import { getSampleComponentsFile, getUiBuilderUIJSFile } from './ui-builder-ui-js-file'
 
 export const UI_BUILDER_PROJECT_ID = 'UI-BUILDER'
+
+let generatedFiles: ProjectContents = {}
+for (let index = 0; index < 2000; index++) {
+  const path = `/src/code${index}.js`
+  const code = `/** @jsx jsx */
+import * as React from 'react'
+import { Scene, Storyboard, jsx } from 'utopia-api'
+console.log('${path} executed')
+export var Component${index} = (props) => {
+  return (
+    <div
+      style={{ width: '100%', height: '100%', backgroundColor: '#FFFFFF' }}
+      layout={{ layoutSystem: 'pinSystem' }}
+    />
+  )
+}`
+  generatedFiles[path] = codeFile(code, null)
+}
 
 export function defaultProject(): PersistentModel {
   const selectedTab: EditorTab = releaseNotesTab()
@@ -24,6 +43,7 @@ export function defaultProject(): PersistentModel {
     appID: null,
     projectVersion: CURRENT_PROJECT_VERSION,
     projectContents: {
+      ...generatedFiles,
       '/package.json': codeFile(JSON.stringify(DefaultPackageJson, null, 2), null),
       '/src': directory(),
       '/src/app.js': getDefaultUIJsFile(),
