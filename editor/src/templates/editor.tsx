5e78c00ac0c561801d4c357a467a97a33420e8e3
Eniko Demeter <enidemi@users.noreply.github.com>
2020-09-28 17:25:33 +0200

    draw outlines using rectangles

--- editor/src/templates/editor.tsx
+++ editor/src/templates/editor.tsx
@@ -52,6 +52,7 @@ import {
 } from '../components/editor/store/editor-state'
 import {
   EditorStateContext,
+  useEditorState,
   UtopiaStoreAPI,
   UtopiaStoreHook,
 } from '../components/editor/store/store-hook'
@@ -78,6 +79,7 @@ import {
 } from '../components/canvas/ui-jsx-canvas'
 import { isLeft } from '../core/shared/either'
 import { importZippedGitProject, isProjectImportSuccess } from '../core/model/project-import'
+import * as THREE from 'three'
 
 if (PROBABLY_ELECTRON) {
   let { webFrame } = requireElectron()
@@ -364,41 +366,95 @@ export class Editor {
   }
 }
 
-import { Canvas, useFrame } from 'react-three-fiber'
-function Box(props: any) {
+import { Canvas, useFrame, useThree } from 'react-three-fiber'
+import { MetadataUtils } from '../core/model/element-metadata-utils'
+export const OutlineControlR3f = (props: any) => {
   // This reference will give us direct access to the mesh
   const mesh = React.useRef()
   // Set up state for the hovered and active state
   const [hovered, setHover] = React.useState(false)
   const [active, setActive] = React.useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(
-    () =>
-      (((mesh as any).current as any).rotation.x = ((mesh as any)
-        .current as any).rotation.y += 0.01),
-  )
+  // // Rotate mesh every frame, this is outside of React without overhead
+  // useFrame(
+  //   () =>
+  //     (((mesh as any).current as any).rotation.x = ((mesh as any)
+  //       .current as any).rotation.y += 0.01),
+  // )
+  // const { viewport } = useThree()
+
+  const outlineSize = 1
   return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHover(true)}
-      onPointerOut={(e) => setHover(false)}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+    <>
+      <mesh
+        position={[props.frame.x, props.frame.y, 0]}
+        ref={mesh}
+        scale={[1, 1, 1]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry args={[props.frame.width, props.frame.height, 0]} />
+        <meshStandardMaterial color={hovered ? 'black' : 'blue'} />
+      </mesh>
+      <mesh
+        position={[props.frame.x, props.frame.y, 0]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry args={[outlineSize, props.frame.height, 0]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+      <mesh
+        position={[props.frame.x, props.frame.y, 0]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry args={[props.frame.width, outlineSize, 0]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+      <mesh
+        position={[props.frame.x + props.frame.width, props.frame.y, 0]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry args={[outlineSize, props.frame.height, 0]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+      <mesh
+        position={[props.frame.x, props.frame.y + props.frame.height, 0]}
+        onClick={(e) => setActive(!active)}
+        onPointerOver={(e) => setHover(true)}
+        onPointerOut={(e) => setHover(false)}
+      >
+        <boxBufferGeometry args={[props.frame.width, outlineSize, 0]} />
+        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      </mesh>
+    </>
   )
 }
-function App() {
+export const App = () => {
+  const { componentMetadata } = useEditorState((store) => ({
+    componentMetadata: store.editor.jsxMetadataKILLME,
+  }))
+  const allElements = MetadataUtils.getAllPaths(componentMetadata)
   return (
-    <Canvas colorManagement>
+    <Canvas style={{ width: 1000, height: 500 }} colorManagement>
       <ambientLight intensity={0.2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      {allElements.map((path, i) => {
+        const frame = MetadataUtils.getFrameInCanvasCoords(path, componentMetadata)
+        const updatedFrame = {
+          x: frame?.x || 0,
+          y: frame?.y || 0,
+          width: frame?.width || 0,
+          height: frame?.height || 0,
+        }
+        return <OutlineControlR3f key={i} path={path} frame={updatedFrame} />
+      })}
     </Canvas>
   )
 }
