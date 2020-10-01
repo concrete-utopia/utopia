e7e6accc030be467fa3b202275e9fadf8f1d7405
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2020-10-01 13:11:05 +0100

    fix(editor) Include support for tagged versions

--- editor/src/components/editor/npm-dependency/npm-dependency.spec.ts
+++ editor/src/components/editor/npm-dependency/npm-dependency.spec.ts
@@ -1,4 +1,9 @@
-import { findMatchingVersion, packageNotFound, versionLookupSuccess } from './npm-dependency'
+import {
+  findMatchingVersion,
+  getVersionType,
+  packageNotFound,
+  versionLookupSuccess,
+} from './npm-dependency'
 
 require('jest-fetch-mock').enableMocks()
 
@@ -45,5 +50,51 @@ describe('Attempting to match a version for an invalid package', () => {
     const version = '1.0.0'
     const result = await findMatchingVersion('', version)
     expect(result).toEqual(packageNotFound)
+  })
+})
+
+describe('Checking the version type of a dependency', () => {
+  // The below examples are (mostly) taken from https://docs.npmjs.com/files/package.json#dependencies
+
+  it('Returns GITHUB for github repos', () => {
+    expect(getVersionType('expressjs/express')).toEqual('GITHUB')
+    expect(getVersionType('mochajs/mocha#4727d357ea')).toEqual('GITHUB')
+    expect(getVersionType('user/repo#feature/branch')).toEqual('GITHUB')
+  })
+
+  it('Returns LOCAL for local files', () => {
+    expect(getVersionType('../foo/bar')).toEqual('LOCAL')
+    expect(getVersionType('~/foo/bar')).toEqual('LOCAL')
+    expect(getVersionType('./foo/bar')).toEqual('LOCAL')
+    expect(getVersionType('/foo/bar')).toEqual('LOCAL')
+    expect(getVersionType('file:../foo/bar')).toEqual('LOCAL')
+  })
+
+  it('Returns SEMVER for semver versions', () => {
+    expect(getVersionType('1.0.0 - 2.9999.9999')).toEqual('SEMVER')
+    expect(getVersionType('>=1.0.2 <2.1.2')).toEqual('SEMVER')
+    expect(getVersionType('>1.0.2 <=2.3.4')).toEqual('SEMVER')
+    expect(getVersionType('2.0.1')).toEqual('SEMVER')
+    expect(getVersionType('<1.0.0 || >=2.3.1 <2.4.5 || >=2.5.2 <3.0.0')).toEqual('SEMVER')
+    expect(getVersionType('~1.2')).toEqual('SEMVER')
+    expect(getVersionType('~1.2.3')).toEqual('SEMVER')
+    expect(getVersionType('2.x')).toEqual('SEMVER')
+    expect(getVersionType('3.3.x')).toEqual('SEMVER')
+  })
+
+  it('Returns TAG for tagged versions', () => {
+    expect(getVersionType('beta')).toEqual('TAG')
+    expect(getVersionType('experimental')).toEqual('TAG')
+    expect(getVersionType('latest')).toEqual('TAG')
+    expect(getVersionType('next')).toEqual('TAG')
+    expect(getVersionType('unstable')).toEqual('TAG')
+  })
+
+  it('Returns URL for URLs', () => {
+    expect(getVersionType('http://asdf.com/asdf.tar.gz')).toEqual('URL')
+    expect(getVersionType('git+ssh://git@github.com:npm/cli.git#v1.0.27')).toEqual('URL')
+    expect(getVersionType('git+ssh://git@github.com:npm/cli#semver:^5.0')).toEqual('URL')
+    expect(getVersionType('git+https://isaacs@github.com/npm/cli.git')).toEqual('URL')
+    expect(getVersionType('git://github.com/npm/cli.git#v1.0.27')).toEqual('URL')
   })
 })
