3ea8d5acd567ee7d7c0251bcee5ffe072ebd2f37
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-10-13 13:58:57 +0200

    only collect computedStyles metadata for the selected views (#609)
    * only collect computedStyles metadata for the selected views
    
    * slightly improved comment about rendering an empty inspector
    
    * cleanup in getComputedStyle

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -116,7 +116,7 @@ describe('React Render Count Tests - ', () => {
     )
 
     const renderCountAfter = renderResult.getNumberOfRenders()
-    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(630) // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toBeLessThan(640)
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(740) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(750)
   })
 })
