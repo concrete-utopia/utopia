dc11c23fd610ce714cd58c8e6e1454433c386ea5
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2020-11-09 17:46:22 +0000

    fix(tests) Fixed snapshots and performance test affected by changes

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -60,7 +60,7 @@ describe('React Render Count Tests - ', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     expect(renderCountAfter - renderCountBefore).toBeGreaterThan(565) // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toBeLessThan(570)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(575)
   })
 
   it('Changing the selected view', async () => {
