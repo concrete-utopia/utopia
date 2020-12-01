9703602f0f88d9fd4411fd2a7fb8d1cf8aedc299
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-12-01 11:15:25 +0100

    clicking on a perf test html

--- editor/puppeteer-test.js
+++ editor/puppeteer-test.js
@@ -1,14 +1,19 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
+const path = require('path')
 
 puppeteerStart = async function () {
   const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--enable-thread-instruction-count'],
   })
   const page = await browser.newPage()
-  await page.tracing.start({ path: 'trace.json' })
-  await page.goto('https://google.com/')
-  await page.tracing.stop()
+  await page.goto(`file:${path.join(__dirname, 'perftest.html')}`)
+  const [button] = await page.$x("//div[contains(., 'expensive computation')]")
+  if (button) {
+    await page.tracing.start({ path: 'trace.json' })
+    await button.click()
+    await page.tracing.stop()
+  }
   await browser.close()
   let traceData = fs.readFileSync('trace.json').toString()
   // console.log(traceData);
