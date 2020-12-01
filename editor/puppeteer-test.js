bccc28858f889269b2ca65efd5673b37398f0f38
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-12-01 15:23:25 +0100

    frame time test

--- editor/puppeteer-test.js
+++ editor/puppeteer-test.js
@@ -12,6 +12,7 @@ puppeteerStart = async function () {
   if (button) {
     await page.tracing.start({ path: 'trace.json' })
     await button.click()
+    await page.waitForSelector('#done', { visible: true })
     await page.tracing.stop()
   }
   await browser.close()
@@ -21,8 +22,36 @@ puppeteerStart = async function () {
   const beginEvent = performanceMeasureEvents.find((e) => e.ph === 'b')
   const endEvent = performanceMeasureEvents.find((e) => e.ph === 'e')
   const time = (endEvent.ts - beginEvent.ts) / 1000
-  console.info('time!', time)
-  console.info(`::set-output name=perf-result::${time}ms`)
+
+  const frameTimeEvents = traceJson.traceEvents.filter((e) => e.name.startsWith('animation_frame_'))
+  const frameStarts = new Map()
+  const frameEnds = new Map()
+
+  frameTimeEvents.forEach((fte) => {
+    const frameID = fte.name.split('animation_frame_')[1]
+    if (fte.ph === 'b') {
+      frameStarts.set(frameID, fte.ts)
+    }
+    if (fte.ph === 'e') {
+      frameEnds.set(frameID, fte.ts)
+    }
+  })
+
+  let frameTimes = []
+  let totalFrameTimes = 0
+  frameEnds.forEach((_, i) => {
+    const frameTime = (frameEnds.get(i) - frameStarts.get(i)) / 1000
+    frameTimes.push(frameTime)
+    totalFrameTimes += frameTime
+  })
+
+  const frameAvg = totalFrameTimes / frameTimes.length
+
+  console.info(
+    `::set-output name=perf-result::"${time}ms – average frame length: ${frameAvg} – frame times: [${frameTimes.join(
+      ',',
+    )}]"`,
+  )
 }
 
 puppeteerStart()
