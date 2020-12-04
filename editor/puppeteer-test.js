ba5252e9892aaae0d44d1d25d8fcc0865fb7102f
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-12-04 12:07:56 +0100

    Update puppeteer-test.js

--- editor/puppeteer-test.js
+++ editor/puppeteer-test.js
@@ -76,11 +76,9 @@ puppeteerStart = async function () {
   )
 }
 
-try {
-  puppeteerStart()
-} catch (e) {
+await puppeteerStart().catch((e) => {
   console.info(
     `::set-output name=perf-result::"There was an error with Puppeteer: ${e.name} – ${e.message}"`,
   )
   return
-}
+})
