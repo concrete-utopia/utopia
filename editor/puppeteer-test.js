9eee4dbbb8e3fb1123cc708309efcc5ab86d6881
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2020-12-04 11:56:28 +0100

    try to run comment on job failure

--- editor/puppeteer-test.js
+++ editor/puppeteer-test.js
@@ -31,56 +31,56 @@ function consoleDoneMessage(page) {
 }
 
 puppeteerStart = async function () {
-  try {
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--enable-thread-instruction-count'],
-      headless: true,
-    })
-    const page = await browser.newPage()
-    // page.on('console', (message) =>
-    //   console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`),
-    // )
-    await page.goto(`https://utopia.pizza/project/5596ecdd/?branch_name=feature/perf-test-button`)
-    await page.waitForXPath("//a[contains(., 'P S')]")
-    const [button] = await page.$x("//a[contains(., 'P S')]")
-    await button.click()
-    await consoleDoneMessage(page)
-    await page.tracing.start({ path: 'trace.json' })
-    await button.click()
-    await consoleDoneMessage(page)
-    await page.tracing.stop()
-    await browser.close()
-    let traceData = fs.readFileSync('trace.json').toString()
-    const traceJson = JSON.parse(traceData)
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--enable-thread-instruction-count'],
+    headless: true,
+  })
+  const page = await browser.newPage()
+  // page.on('console', (message) =>
+  //   console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`),
+  // )
+  await page.goto(`https://utopia.pizza/project/5596ecdd/?branch_name=feature/perf-test-button`)
+  await page.waitForXPath("//a[contains(., 'P S')]")
+  const [button] = await page.$x("//a[contains(., 'P S')]")
+  await button.click()
+  await consoleDoneMessage(page)
+  await page.tracing.start({ path: 'trace.json' })
+  await button.click()
+  await consoleDoneMessage(page)
+  await page.tracing.stop()
+  await browser.close()
+  let traceData = fs.readFileSync('trace.json').toString()
+  const traceJson = JSON.parse(traceData)
 
-    const frameTimeEvents = traceJson.traceEvents.filter((e) => e.name.startsWith('scroll_step_'))
-    let frameTimes = []
-    let lastFrameTimestamp = null
-    let totalFrameTimes = 0
-    frameTimeEvents.forEach((fte) => {
-      const frameID = fte.name.split('scroll_step_')[1] - 1
-      const frameTimestamp = fte.ts
-      if (lastFrameTimestamp != null) {
-        const frameDelta = (frameTimestamp - lastFrameTimestamp) / 1000
-        frameTimes[frameID] = frameDelta
-        totalFrameTimes += frameDelta
-      }
-      lastFrameTimestamp = frameTimestamp
-    })
+  const frameTimeEvents = traceJson.traceEvents.filter((e) => e.name.startsWith('scroll_step_'))
+  let frameTimes = []
+  let lastFrameTimestamp = null
+  let totalFrameTimes = 0
+  frameTimeEvents.forEach((fte) => {
+    const frameID = fte.name.split('scroll_step_')[1] - 1
+    const frameTimestamp = fte.ts
+    if (lastFrameTimestamp != null) {
+      const frameDelta = (frameTimestamp - lastFrameTimestamp) / 1000
+      frameTimes[frameID] = frameDelta
+      totalFrameTimes += frameDelta
+    }
+    lastFrameTimestamp = frameTimestamp
+  })
 
-    const frameAvg = totalFrameTimes / frameTimes.length
+  const frameAvg = totalFrameTimes / frameTimes.length
 
-    console.info(
-      `::set-output name=perf-result::"${totalFrameTimes}ms – average frame length: ${frameAvg} – frame times: [${frameTimes.join(
-        ',',
-      )}]"`,
-    )
-  } catch (e) {
-    console.info(
-      `::set-output name=perf-result::"There was an error with Puppeteer: ${e.name} – ${e.message}"`,
-    )
-    return
-  }
+  console.info(
+    `::set-output name=perf-result::"${totalFrameTimes}ms – average frame length: ${frameAvg} – frame times: [${frameTimes.join(
+      ',',
+    )}]"`,
+  )
 }
 
-puppeteerStart()
+try {
+  puppeteerStart()
+} catch (e) {
+  console.info(
+    `::set-output name=perf-result::"There was an error with Puppeteer: ${e.name} – ${e.message}"`,
+  )
+  return
+}
