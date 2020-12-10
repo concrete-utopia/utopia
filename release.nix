ce6d570fe0b0a336302c051831d2d0ee756566c4
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2020-12-10 13:52:03 +0000

    chore(editor) Switch to master and remove cabal-plan customisation.

--- release.nix
+++ release.nix
@@ -6,16 +6,12 @@ let
   };
 
   pkgs = import (builtins.fetchTarball {
-    name = "nixos-20.09";
-    url = https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz;
+    name = "nixos-master";
+    url = https://github.com/NixOS/nixpkgs/archive/1f65211aa37adfc5a28c4849bbe73dcb2d2d1e2b.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+    sha256 = "07i4kbxpn0pvqnyvpvxp310ylj5yan9hp89gk3y38qfxcz39ik71";
   }) { inherit config; };
-  
-  haskell-lib = pkgs.haskell.lib;
-  custom-cabal-plan = haskell-lib.addExtraLibraries (haskell-lib.appendConfigureFlag pkgs.haskellPackages.cabal-plan "-flicense-report") [pkgs.haskellPackages.zlib pkgs.haskellPackages.tar]; 
 in
   { 
     pkgs = pkgs;
-    custom-cabal-plan = custom-cabal-plan;
   }
