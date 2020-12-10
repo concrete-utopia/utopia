d5cac5647c25049d83707b8b33cfa612aefb2a3c
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2020-12-10 11:19:43 +0000

    chore(project) Updated the nix-pkgs and package.yaml for the server

--- release.nix
+++ release.nix
@@ -6,10 +6,10 @@ let
   };
 
   pkgs = import (builtins.fetchTarball {
-    name = "nixos-19.09";
-    url = https://github.com/NixOS/nixpkgs/archive/19.09.tar.gz;
+    name = "nixos-20.09";
+    url = https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+    sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
   }) { inherit config; };
   
   haskell-lib = pkgs.haskell.lib;
