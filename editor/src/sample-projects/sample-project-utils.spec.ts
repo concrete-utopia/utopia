50317b9fb1d24100b8c64c3df9ec4c17e9896196
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2020-12-16 17:43:23 +0000

    chore(sample-project-utils.spec.ts) xit test until I have a better solution

--- editor/src/sample-projects/sample-project-utils.spec.ts
+++ editor/src/sample-projects/sample-project-utils.spec.ts
@@ -2,7 +2,7 @@ import { defaultProject } from './sample-project-utils'
 import * as fs from 'fs'
 
 describe('Project model integrity check', () => {
-  it('ensures that the server tests use the most recent project model structure', () => {
+  xit('ensures that the server tests use the most recent project model structure', () => {
     const stringifiedProject = JSON.stringify(defaultProject())
     fs.writeFileSync('../server/test/Test/Utopia/Web/SampleProject.json', stringifiedProject)
   })
