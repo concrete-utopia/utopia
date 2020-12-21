a68e5ec8c16597f15d655011380353fc62e4170e
OmarDaSilva <omargalvaosilva@gmail.com>
2020-12-21 17:37:30 +0000

    Added value cut off

--- performance-test/src/puppeteer-test.ts
+++ performance-test/src/puppeteer-test.ts
@@ -170,7 +170,7 @@ async function createTestPng(
       title: {
         text:
           'Xaxis: Frame Times (ms) - red:60fps, black:30fps, green:15fps, yellow:7.5fps - Scrolling Test (n=' +
-          n +
+          valueOutsideCutoff(frameTimesArray) +
           ' Results not shown)',
         font: {
           family: 'Courier New, monospace',
