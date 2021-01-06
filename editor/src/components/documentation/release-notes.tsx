d4467cf0a7a4bdb9f33332fbe3803547d881a749
Eniko Demeter <enidemi@users.noreply.github.com>
2021-01-06 14:33:24 +0100

    fix welcome tab

--- editor/src/components/documentation/release-notes.tsx
+++ editor/src/components/documentation/release-notes.tsx
@@ -3,33 +3,50 @@ import { jsx } from '@emotion/react'
 import styled from '@emotion/styled'
 
 import * as React from 'react'
-import { colorTheme } from '../../uuiui'
+import { colorTheme, SimpleFlexColumn, SimpleFlexRow, UtopiaTheme } from '../../uuiui'
+import { FileTabs } from '../filebrowser/file-tabs'
 import { H1, H2, PrettyKeys, EM, CalloutPrimary, A } from './documentation-components'
 
 import { GettingStarted } from './getting-started'
 
 export function ReleaseNotesContent() {
   return (
-    <div
-      css={{
-        label: 'ReleaseNotesContainer',
-        backgroundColor: colorTheme.emphasizedBackground.value,
-        fontSize: '16px',
-        lineHeight: '26px',
-        paddingBottom: 18,
-        width: '100%',
-        cursor: 'text',
-        userSelect: 'text',
-        WebkitUserSelect: 'text',
-        overflow: 'scroll',
-        whiteSpace: 'pre-wrap',
-        '&  *': {
+    <SimpleFlexColumn>
+      <SimpleFlexRow
+        className='tabRail'
+        style={{
+          minHeight: 30,
+          height: 30,
+          borderBottom: `1px solid ${UtopiaTheme.color.subduedBorder.value}`,
+          alignItems: 'stretch',
+          justifyContent: 'stretch',
+          backgroundColor: 'transparent',
+          overflowX: 'hidden',
+        }}
+      >
+        <FileTabs />
+      </SimpleFlexRow>
+      <div
+        css={{
+          label: 'ReleaseNotesContainer',
+          backgroundColor: colorTheme.emphasizedBackground.value,
+          fontSize: '16px',
+          lineHeight: '26px',
+          paddingBottom: 18,
+          width: '100%',
+          cursor: 'text',
           userSelect: 'text',
           WebkitUserSelect: 'text',
-        },
-      }}
-    >
-      <GettingStarted />
-    </div>
+          overflow: 'scroll',
+          whiteSpace: 'pre-wrap',
+          '&  *': {
+            userSelect: 'text',
+            WebkitUserSelect: 'text',
+          },
+        }}
+      >
+        <GettingStarted />
+      </div>
+    </SimpleFlexColumn>
   )
 }
