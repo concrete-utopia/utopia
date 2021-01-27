d57ef2058e9dda87f98b0f408295f5c8d02f5634
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-01-27 20:17:38 +0100

    adding the first inspector test using the new test IDs and the real computedStyle

---
+++ editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser.tsx
@@ -0,0 +1,68 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import { BakedInStoryboardUID } from '../../../core/model/scene-utils'
+import * as TP from '../../../core/shared/template-path'
+import {
+  makeTestProjectCodeWithSnippet,
+  renderTestEditorWithCode,
+  TestScenePath,
+} from '../../canvas/ui-jsx.test-utils'
+import { selectComponents } from '../../editor/actions/action-creators'
+
+describe('inspector tests with real metadata', () => {
+  it('element with TLWH', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(`
+        <div
+          style={{ ...props.style, position: 'absolute', backgroundColor: '#FFFFFF' }}
+          data-uid={'aaa'}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              backgroundColor: '#DDDDDD',
+              position: 'fixed',
+              padding: 20,
+              paddingLeft: 15,
+              left: 55,
+              top: 98,
+              width: 266,
+              height: 124,
+            }}
+            data-uid={'bbb'}
+          ></div>
+        </div>
+      `),
+    )
+
+    await renderResult.dispatch(
+      [selectComponents([TP.instancePath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const metadata = renderResult.getEditorState().editor.jsxMetadataKILLME.elements[
+      'utopia-storyboard-uid/scene-aaa:aaa/bbb'
+    ]
+
+    const flexPaddingTopControl = (await renderResult.renderedDOM.findByTestId(
+      'flexPadding-T',
+    )) as HTMLInputElement
+    const flexPaddingLeftControl = (await renderResult.renderedDOM.findByTestId(
+      'flexPadding-L',
+    )) as HTMLInputElement
+
+    // Padding top is coming from the shorthand `padding` value.
+    expect(metadata.computedStyle?.['paddingTop']).toMatchInlineSnapshot(`"20px"`)
+    expect(flexPaddingTopControl.value).toMatchInlineSnapshot(`"20"`)
+    expect(
+      flexPaddingTopControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"detected"`)
+
+    // Padding left is coming from the `paddingLeft` value.
+    expect(metadata.computedStyle?.['paddingLeft']).toMatchInlineSnapshot(`"15px"`)
+    expect(flexPaddingLeftControl.value).toMatchInlineSnapshot(`"15"`)
+    expect(
+      flexPaddingLeftControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+  })
+})
