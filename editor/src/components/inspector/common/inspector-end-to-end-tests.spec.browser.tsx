d8c21358bab3c47f5fb3c0a354d26e048a89b715
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-02-01 17:54:42 +0100

    Fix/inspector layout pin displayed values (#899)
    * tests(inspector) layout section tests from TLBRWH controlstatus and value
    
    * fix(inspector) pin-controls use pointInfo.value
    
    * fix(inspector) remove frame from PinsLayoutNumberControl props

--- editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser.tsx
+++ editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser.tsx
@@ -13,4 +13,256 @@ describe('inspector tests with real metadata', () => {
   it('placeholder', () => {
     // the tests will come here
   })
+  it('TLWH layout controls', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(`
+        <div
+          style={{ ...props.style, position: 'absolute', backgroundColor: '#FFFFFF' }}
+          data-uid={'aaa'}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              backgroundColor: '#DDDDDD',
+              left: 55,
+              top: 98,
+              width: 266,
+              height: 124,
+            }}
+            data-uid={'bbb'}
+          ></div>
+        </div>
+      `),
+    )
+
+    await renderResult.dispatch(
+      [selectComponents([TP.instancePath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const metadata = renderResult.getEditorState().editor.jsxMetadataKILLME.elements[
+      'utopia-storyboard-uid/scene-aaa:aaa/bbb'
+    ]
+
+    const widthControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Width-number-input',
+    )) as HTMLInputElement
+    const heightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Height-number-input',
+    )) as HTMLInputElement
+    const topControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedTop-number-input',
+    )) as HTMLInputElement
+    const leftControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedLeft-number-input',
+    )) as HTMLInputElement
+    const bottomControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedBottom-number-input',
+    )) as HTMLInputElement
+    const rightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedRight-number-input',
+    )) as HTMLInputElement
+
+    expect(metadata.computedStyle?.['width']).toMatchInlineSnapshot(`"266px"`)
+    expect(widthControl.value).toMatchInlineSnapshot(`"266"`)
+    expect(
+      widthControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['height']).toMatchInlineSnapshot(`"124px"`)
+    expect(heightControl.value).toMatchInlineSnapshot(`"124"`)
+    expect(
+      heightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['top']).toMatchInlineSnapshot(`"98px"`)
+    expect(topControl.value).toMatchInlineSnapshot(`"98"`)
+    expect(
+      topControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['left']).toMatchInlineSnapshot(`"55px"`)
+    expect(leftControl.value).toMatchInlineSnapshot(`"55"`)
+    expect(
+      leftControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(bottomControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      bottomControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+
+    expect(rightControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      rightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+  })
+  it('TLBR layout controls', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(`
+        <div
+          style={{ ...props.style, position: 'absolute', backgroundColor: '#FFFFFF' }}
+          data-uid={'aaa'}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              backgroundColor: '#DDDDDD',
+              left: 55,
+              top: 98,
+              bottom: 200,
+              right: 10,
+            }}
+            data-uid={'bbb'}
+          ></div>
+        </div>
+      `),
+    )
+
+    await renderResult.dispatch(
+      [selectComponents([TP.instancePath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const metadata = renderResult.getEditorState().editor.jsxMetadataKILLME.elements[
+      'utopia-storyboard-uid/scene-aaa:aaa/bbb'
+    ]
+
+    const widthControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Width-number-input',
+    )) as HTMLInputElement
+    const heightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Height-number-input',
+    )) as HTMLInputElement
+    const topControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedTop-number-input',
+    )) as HTMLInputElement
+    const leftControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedLeft-number-input',
+    )) as HTMLInputElement
+    const bottomControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedBottom-number-input',
+    )) as HTMLInputElement
+    const rightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedRight-number-input',
+    )) as HTMLInputElement
+
+    expect(widthControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      widthControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+
+    expect(heightControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      heightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+
+    expect(metadata.computedStyle?.['top']).toMatchInlineSnapshot(`"98px"`)
+    expect(topControl.value).toMatchInlineSnapshot(`"98"`)
+    expect(
+      topControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['left']).toMatchInlineSnapshot(`"55px"`)
+    expect(leftControl.value).toMatchInlineSnapshot(`"55"`)
+    expect(
+      leftControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['bottom']).toMatchInlineSnapshot(`"200px"`)
+    expect(bottomControl.value).toMatchInlineSnapshot(`"200"`)
+    expect(
+      bottomControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['right']).toMatchInlineSnapshot(`"10px"`)
+    expect(rightControl.value).toMatchInlineSnapshot(`"10"`)
+    expect(
+      rightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+  })
+  it('WHBR layout controls', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(`
+        <div
+          style={{ ...props.style, position: 'absolute', backgroundColor: '#FFFFFF' }}
+          data-uid={'aaa'}
+        >
+          <div
+            style={{
+              position: 'absolute',
+              backgroundColor: '#DDDDDD',
+              width: 203,
+              height: 102,
+              bottom: 200,
+              right: 10,
+            }}
+            data-uid={'bbb'}
+          ></div>
+        </div>
+      `),
+    )
+
+    await renderResult.dispatch(
+      [selectComponents([TP.instancePath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const metadata = renderResult.getEditorState().editor.jsxMetadataKILLME.elements[
+      'utopia-storyboard-uid/scene-aaa:aaa/bbb'
+    ]
+
+    const widthControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Width-number-input',
+    )) as HTMLInputElement
+    const heightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-Height-number-input',
+    )) as HTMLInputElement
+    const topControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedTop-number-input',
+    )) as HTMLInputElement
+    const leftControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedLeft-number-input',
+    )) as HTMLInputElement
+    const bottomControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedBottom-number-input',
+    )) as HTMLInputElement
+    const rightControl = (await renderResult.renderedDOM.findByTestId(
+      'position-PinnedRight-number-input',
+    )) as HTMLInputElement
+
+    expect(metadata.computedStyle?.['width']).toMatchInlineSnapshot(`"203px"`)
+    expect(widthControl.value).toMatchInlineSnapshot(`"203"`)
+    expect(
+      widthControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['height']).toMatchInlineSnapshot(`"102px"`)
+    expect(heightControl.value).toMatchInlineSnapshot(`"102"`)
+    expect(
+      heightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(topControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      topControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+
+    expect(leftControl.value).toMatchInlineSnapshot(`""`)
+    expect(
+      leftControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"unset"`)
+
+    expect(metadata.computedStyle?.['bottom']).toMatchInlineSnapshot(`"200px"`)
+    expect(bottomControl.value).toMatchInlineSnapshot(`"200"`)
+    expect(
+      bottomControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+
+    expect(metadata.computedStyle?.['right']).toMatchInlineSnapshot(`"10px"`)
+    expect(rightControl.value).toMatchInlineSnapshot(`"10"`)
+    expect(
+      rightControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
+    ).toMatchInlineSnapshot(`"simple"`)
+  })
 })
