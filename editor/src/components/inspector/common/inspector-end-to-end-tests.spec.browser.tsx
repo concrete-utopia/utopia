7718510b615d0d400bce1fb0cbd44ba31752259b
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-02-01 10:31:17 +0100

    creating dummy placeholder file for inspector metadata tests

---
+++ editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { render } from '@testing-library/react'
+import { BakedInStoryboardUID } from '../../../core/model/scene-utils'
+import * as TP from '../../../core/shared/template-path'
+import {
+  makeTestProjectCodeWithSnippet,
+  renderTestEditorWithCode,
+  TestScenePath,
+} from '../../canvas/ui-jsx.test-utils'
+import { selectComponents } from '../../editor/actions/action-creators'
+
+describe('inspector tests with real metadata', () => {
+  // the tests will come here
+})
