ae64fc03a46b59d468480211de5b01e0727f192c
Eniko Demeter <enidemi@users.noreply.github.com>
2021-02-17 15:48:04 +0100

    feature(inspector-hooks) padding shorthand-longhand hook

---
+++ editor/src/components/inspector/common/longhand-shorthand-hooks.spec.ts
@@ -0,0 +1,205 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { ComputedStyle, StyleAttributeMetadata } from '../../../core/shared/element-template'
+import { ParsedPropertiesKeys } from './css-utils'
+import { useInspectorInfoLonghandShorthand } from './longhand-shorthand-hooks'
+import { stylePropPathMappingFn } from './property-path-hooks'
+import {
+  getPropsForStyleProp,
+  makeInspectorHookContextProvider,
+} from './property-path-hooks.test-utils'
+
+describe('useInspectorInfo: padding shorthand and longhands', () => {
+  function getPaddingHookResult<P extends ParsedPropertiesKeys>(
+    longhand: P,
+    shorthand: P,
+    styleObjectExpressions: Array<string>,
+    spiedProps: Array<any>,
+    computedStyles: Array<ComputedStyle>,
+    attributeMetadatas: Array<StyleAttributeMetadata>,
+  ) {
+    const props = styleObjectExpressions.map(
+      (styleExpression) => getPropsForStyleProp(styleExpression, ['myStyleOuter', 'myStyleInner'])!,
+    )
+
+    const contextProvider = makeInspectorHookContextProvider(
+      [],
+      props,
+      ['myStyleOuter', 'myStyleInner'],
+      spiedProps,
+      computedStyles,
+      attributeMetadatas,
+    )
+
+    const { result } = renderHook(
+      () =>
+        useInspectorInfoLonghandShorthand(
+          longhand,
+          shorthand,
+          stylePropPathMappingFn as any, // ¯\_(ツ)_/¯
+        ),
+      {
+        wrapper: contextProvider,
+      },
+    )
+    return result.current
+  }
+
+  it('paddingLeft', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 }`],
+      [{ paddingLeft: 5 }],
+      [{ paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 5 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft']])
+  })
+
+  it('paddingLeft, padding', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5, padding: 15 }`],
+      [{ paddingLeft: 5, padding: 15 }],
+      [{ paddingTop: '15px', paddingRight: '15px', paddingBottom: '15px', paddingLeft: '15px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 15 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft', 'padding']])
+  })
+
+  it('padding, paddingLeft', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ padding: 15, paddingLeft: 5 }`],
+      [{ padding: 15, paddingLeft: 5 }],
+      [{ paddingTop: '15px', paddingRight: '15px', paddingBottom: '15px', paddingLeft: '5px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 5 })
+    expect(hookResult.orderedPropKeys).toEqual([['padding', 'paddingLeft']])
+  })
+
+  it('paddingLeft, padding, paddingRight', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5, padding: 15, paddingRight: 20 }`], // paddingRight is irrelevant, we are making sure it doesn't affect anything
+      [{ paddingLeft: 5, padding: 15, paddingRight: 20 }],
+      [{ paddingTop: '15px', paddingRight: '20px', paddingBottom: '15px', paddingLeft: '15px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 15 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft', 'padding']])
+  })
+
+  it('paddingLeft controlled', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 + 5 }`],
+      [{ paddingLeft: 10 }],
+      [{ paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '10px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 10 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft']])
+    expect(hookResult.controlStatus).toEqual('controlled')
+  })
+
+  it('paddingLeft, padding controlled', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5, padding: 15 + 5 }`],
+      [{ paddingLeft: 5, padding: 20 }],
+      [{ paddingTop: '20px', paddingRight: '20px', paddingBottom: '20px', paddingLeft: '20px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 20 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft', 'padding']])
+    expect(hookResult.controlStatus).toEqual('controlled')
+  })
+
+  it('padding controlled, paddingLeft', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ padding: 15 + 5, paddingLeft: 5 }`],
+      [{ padding: 20, paddingLeft: 5 }],
+      [{ paddingTop: '20px', paddingRight: '20px', paddingBottom: '20px', paddingLeft: '5px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 5 })
+    expect(hookResult.orderedPropKeys).toEqual([['padding', 'paddingLeft']])
+    expect(hookResult.controlStatus).toEqual('set')
+  })
+
+  it('paddingLeft controlled, padding', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 + 5, padding: 15 }`],
+      [{ paddingLeft: 10, padding: 15 }],
+      [{ paddingTop: '15px', paddingRight: '15px', paddingBottom: '15px', paddingLeft: '15px' }],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 15 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft', 'padding']])
+    expect(hookResult.controlStatus).toEqual('set')
+  })
+
+  it('multiselect: [paddingLeft], [paddingLeft] identical', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 }`, `{ paddingLeft: 5 }`],
+      [{ paddingLeft: 5 }, { paddingLeft: 5 }],
+      [
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+      ],
+      [],
+    )
+    expect(hookResult.value).toEqual({ unit: 'px', value: 5 })
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft'], ['paddingLeft']])
+    expect(hookResult.controlStatus).toEqual('multiselect-identical-set')
+  })
+
+  it('multiselect: [paddingLeft], [padding, paddingLeft] if not identical, return unoverwritable', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 }`, `{ padding: 5, paddingLeft: 5 }`],
+      [{ paddingLeft: 5 }, { paddingLeft: 5 }],
+      [
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+      ],
+      [],
+    )
+    expect(hookResult.value).toEqual(undefined)
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft'], ['padding', 'paddingLeft']])
+    expect(hookResult.controlStatus).toEqual('multiselect-unoverwritable')
+  })
+
+  it('multiselect: [paddingLeft], [paddingLeft, padding] if not identical, return unoverwritable', () => {
+    const hookResult = getPaddingHookResult(
+      'paddingLeft',
+      'padding',
+      [`{ paddingLeft: 5 }`, `{ paddingLeft: 5, padding: 5 }`],
+      [{ paddingLeft: 5 }, { paddingLeft: 5 }],
+      [
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+        { paddingTop: '0px', paddingRight: '0px', paddingBottom: '0px', paddingLeft: '5px' },
+      ],
+      [],
+    )
+    expect(hookResult.value).toEqual(undefined)
+    expect(hookResult.orderedPropKeys).toEqual([['paddingLeft'], ['paddingLeft', 'padding']])
+    expect(hookResult.controlStatus).toEqual('multiselect-unoverwritable')
+  })
+})
---
+++ editor/src/components/inspector/common/longhand-shorthand-hooks.ts
@@ -0,0 +1,129 @@
+import { last } from '../../../core/shared/array-utils'
+import { arrayEquals } from '../../../core/shared/utils'
+import { getControlStatusFromPropertyStatus, PropertyStatus } from '../../../uuiui-deps'
+import { ParsedProperties, ParsedPropertiesKeys } from './css-utils'
+import {
+  InspectorInfo,
+  ParsedValues,
+  PathMappingFn,
+  useInspectorInfoNoDefaults,
+} from './property-path-hooks'
+
+function getShadowedLonghandShorthandValue<
+  LonghandKey extends ParsedPropertiesKeys,
+  ShorthandKey extends ParsedPropertiesKeys
+>(
+  longhand: LonghandKey,
+  shorthand: ShorthandKey,
+  longhandPropertyStatus: PropertyStatus,
+  shorthandPropertyStatus: PropertyStatus,
+  values: Partial<ParsedValues<LonghandKey | ShorthandKey>>,
+  orderedPropKeys: (LonghandKey | ShorthandKey)[][], // multiselect
+): { value: ParsedProperties[LonghandKey] | undefined; propertyStatus: PropertyStatus } {
+  const allPropKeysEqual = orderedPropKeys.every((propKeys) => {
+    return arrayEquals(propKeys, orderedPropKeys[0])
+  })
+  if (!allPropKeysEqual) {
+    return {
+      value: undefined,
+      propertyStatus: longhandPropertyStatus,
+    }
+  }
+
+  const propKeys = orderedPropKeys[0] ?? []
+  const lastKey = last(propKeys)
+  if (lastKey == null) {
+    return {
+      value: undefined,
+      propertyStatus: longhandPropertyStatus,
+    }
+  } else {
+    if (lastKey === longhand) {
+      return {
+        value: values[lastKey],
+        propertyStatus: longhandPropertyStatus,
+      }
+    } else {
+      // Important: we ASSUME that the transformed shorthand value is an object,
+      // where the keys are the longhand keys and the values are the individual longhand values
+      const shorthandValue = values[shorthand] as any
+      if (longhand in shorthandValue) {
+        return {
+          value: shorthandValue?.[longhand] as ParsedProperties[LonghandKey] | undefined,
+          propertyStatus: shorthandPropertyStatus,
+        }
+      } else {
+        throw new Error(
+          `We didn't find longhand key ${longhand} in resolved shorthand value for shorthand ${shorthand}`,
+        )
+      }
+    }
+  }
+}
+
+export function useInspectorInfoLonghandShorthand<
+  LonghandKey extends ParsedPropertiesKeys,
+  ShorthandKey extends ParsedPropertiesKeys
+>(
+  longhand: LonghandKey,
+  shorthand: ShorthandKey,
+  pathMappingFn: PathMappingFn<LonghandKey | ShorthandKey>,
+): Omit<
+  InspectorInfo<ParsedProperties[LonghandKey] | undefined, LonghandKey | ShorthandKey>,
+  'useSubmitValueFactory'
+> {
+  let inspectorInfo = useInspectorInfoNoDefaults(
+    [longhand, shorthand],
+    (v) => v,
+    () => {
+      return null as any
+    },
+    pathMappingFn,
+  )
+
+  const longhandPropertyStatus = useInspectorInfoNoDefaults(
+    [longhand],
+    (v) => v,
+    () => {
+      return null as any
+    },
+    pathMappingFn,
+  ).propertyStatus
+  const shorthandPropertyStatus = useInspectorInfoNoDefaults(
+    [shorthand],
+    (v) => v,
+    () => {
+      return null as any
+    },
+    pathMappingFn,
+  ).propertyStatus
+  const { value, propertyStatus } = getShadowedLonghandShorthandValue(
+    longhand,
+    shorthand,
+    longhandPropertyStatus,
+    shorthandPropertyStatus,
+    inspectorInfo.value,
+    inspectorInfo.orderedPropKeys,
+  )
+
+  const onSubmitValue = (
+    newTransformedValues: ParsedProperties[LonghandKey] | undefined,
+    transient?: boolean | undefined,
+  ) => {}
+  const onTransientSubmitValue = (
+    newTransformedValues: ParsedProperties[LonghandKey] | undefined,
+  ) => onSubmitValue(newTransformedValues, true)
+
+  const onUnsetValues = () => {}
+
+  return {
+    value: value,
+    controlStatus: getControlStatusFromPropertyStatus(propertyStatus),
+    propertyStatus: propertyStatus,
+    controlStyles: inspectorInfo.controlStyles,
+    orderedPropKeys: inspectorInfo.orderedPropKeys,
+    onSubmitValue: onSubmitValue,
+    onTransientSubmitValue: onTransientSubmitValue,
+    onUnsetValues: onUnsetValues,
+  }
+}
