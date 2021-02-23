1c5a78521f6c74a80d4beba82f68a5c00c54ef6c
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2021-02-23 12:45:17 +0000

    chore(utopia-vscode) Drop old stores to save filling up local storage

--- utopia-vscode-common/src/fs/fs-core.ts
+++ utopia-vscode-common/src/fs/fs-core.ts
@@ -2,17 +2,31 @@ import * as localforage from 'localforage'
 import { stripTrailingSlash } from '../path-utils'
 import { FSNode } from './fs-types'
 
+let storesLastAccessedStore: LocalForage // There is no way to request a list of existing stores, so we have to explicitly track them
+
 let store: LocalForage
+let thisStoreName: string
 
 export async function initializeStore(
   storeName: string,
   driver: string = localforage.INDEXEDDB,
 ): Promise<void> {
+  thisStoreName = storeName
+
   store = localforage.createInstance({
     name: 'utopia',
     storeName: storeName,
     driver: driver,
   })
+
+  storesLastAccessedStore = localforage.createInstance({
+    name: 'utopia',
+    storeName: 'AllStoresLastAccessedTSs',
+    driver: localforage.INDEXEDDB,
+  })
+
+  updateLastAccessed()
+  dropOldStores()
 }
 
 export async function keys(): Promise<string[]> {
@@ -29,4 +43,40 @@ export async function setItem(path: string, value: FSNode): Promise<FSNode> {
 
 export async function removeItem(path: string): Promise<void> {
   return store.removeItem(stripTrailingSlash(path))
+}
+
+const ONE_HOUR = 1000 * 60 * 60 
+const ONE_DAY = ONE_HOUR * 24
+
+async function updateLastAccessed(): Promise<void> {
+  await storesLastAccessedStore.setItem(thisStoreName, Date.now())
+  setTimeout(updateLastAccessed, ONE_HOUR)
+}
+
+export async function checkLastAccessed(): Promise<number> {
+  const lastAccessedFile = await storesLastAccessedStore.getItem<number>(thisStoreName)
+  return lastAccessedFile ?? 0
+}
+
+async function dropOldStores(): Promise<void> {
+  const now = Date.now()
+  const allStores = await storesLastAccessedStore.keys()
+  const allStoresWithLastAccessedTS = await Promise.all(allStores.map(async k => {
+    const ts = await storesLastAccessedStore.getItem<number>(k)
+    return {
+      store: k,
+      ts: ts ?? now
+    }
+  }))
+  const storesToDrop = allStoresWithLastAccessedTS.filter(v => (now - v.ts) > ONE_DAY)
+  if (storesToDrop.length > 0) {
+    const storeNamesToDrop = storesToDrop.map(v => v.store)
+    console.log(`Dropping stores ${JSON.stringify(storeNamesToDrop)}`)
+    storeNamesToDrop.forEach(storeName => {
+      localforage.dropInstance({
+        name: 'utopia',
+        storeName: storeName
+      }).then(() => console.log(`Dropped ${storeName}`))
+    })
+  }
 }
