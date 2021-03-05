05c325c347f3b1e2bbbe422682615f926a2c174c
Eniko Demeter <enidemi@users.noreply.github.com>
2021-03-05 14:45:26 +0100

    better react memo

--- editor/src/components/navigator/navigator-item/item-preview.tsx
+++ editor/src/components/navigator/navigator-item/item-preview.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { TemplatePath } from '../../../core/shared/project-file-types'
 import { IcnProps, Icn } from '../../../uuiui'
+import { betterReactMemo } from '../../../uuiui-deps'
 import { useNameAndIcon } from '../../inspector/common/name-and-icon-hook'
 
 interface ItemPreviewProps {
@@ -8,18 +9,21 @@ interface ItemPreviewProps {
   color: IcnProps['color']
 }
 
-export const ItemPreview: React.FunctionComponent<ItemPreviewProps> = (props) => {
-  // preview depends on three things:
-  // 1 - role
-  // 2 - if it's empty or not
-  // 3 - if it's a component or not
-  // 4 - if it's a placeholder or not
-  // 5 if it's generated or not
-  const iconProps = useNameAndIcon(props.path).iconProps
+export const ItemPreview: React.FunctionComponent<ItemPreviewProps> = betterReactMemo(
+  'ItemPreview',
+  (props) => {
+    // preview depends on three things:
+    // 1 - role
+    // 2 - if it's empty or not
+    // 3 - if it's a component or not
+    // 4 - if it's a placeholder or not
+    // 5 if it's generated or not
+    const iconProps = useNameAndIcon(props.path).iconProps
 
-  return (
-    <div className='w20 h20 flex justify-center items-center relative'>
-      <Icn {...iconProps} color={props.color} />
-    </div>
-  )
-}
+    return (
+      <div className='w20 h20 flex justify-center items-center relative'>
+        <Icn {...iconProps} color={props.color} />
+      </div>
+    )
+  },
+)
