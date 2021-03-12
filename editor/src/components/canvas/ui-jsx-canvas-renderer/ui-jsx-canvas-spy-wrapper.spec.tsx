28a606bd8ddabd5e659cff0eca93f3f0cd0c6c61
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-03-12 15:56:16 +0100

    Update ui-jsx-canvas-spy-wrapper.spec.tsx

--- editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-spy-wrapper.spec.tsx
+++ editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-spy-wrapper.spec.tsx
@@ -8,37 +8,57 @@ import { objectMap } from '../../../core/shared/object-utils'
 import * as TP from '../../../core/shared/template-path'
 import { renderTestEditorWithCode } from '../ui-jsx.test-utils'
 
+const exampleProject = `/** @jsx jsx */
+import * as React from 'react'
+import { Scene, Storyboard, jsx } from 'utopia-api'
+import { View } from 'utopia-api'
+
+const Button = (props) => {
+  return <div data-uid='button-root'>{props.children}</div>
+}
+const Card = () => {
+  return (
+    <Button data-uid='button-instance' >
+      <div data-uid='hi-element'>hi!</div>
+    </Button>
+  )
+}
+export var App = (props) => {
+  return (
+    <div
+      data-uid='app-root'
+      style={{ width: '100%', height: '100%', backgroundColor: '#FFFFFF', position: 'relative' }}
+    >
+      <div data-uid='inner-div'>
+        <Card
+          data-uid='card-instance'
+          style={{
+            backgroundColor: '#0091FFAA',
+            position: 'absolute',
+            left: 70,
+            top: 387,
+            width: 193,
+            height: 221,
+          }}
+        />
+      </div>
+    </div>
+  )
+}
+export var storyboard = (
+  <Storyboard data-uid='storyboard'>
+    <Scene
+      data-uid='scene'
+      component={App}
+      props={{}}
+      style={{ position: 'absolute', left: 0, top: 0, width: 375, height: 812 }}
+    />
+  </Storyboard>
+)`
+
 describe('Spy Wrapper Tests', () => {
   it('a simple component in a regular scene', async () => {
-    const { getEditorState } = await renderTestEditorWithCode(`
-    /** @jsx jsx */
-    import * as React from 'react'
-    import { Scene, Storyboard, jsx } from 'utopia-api'
-    import { View } from 'utopia-api'
-
-    export var App = (props) => {
-      return (
-        <div
-          data-uid='app-root'
-          style={{ width: '100%', height: '100%', backgroundColor: '#FFFFFF', position: 'relative' }}
-        >
-          <div data-uid='inner-div'>
-
-          </div>
-        </div>
-      )
-    }
-    export var storyboard = (
-      <Storyboard data-uid='storyboard-uid'>
-        <Scene
-          data-uid='scene-uid'
-          component={App}
-          props={{}}
-          style={{ position: 'absolute', left: 0, top: 0, width: 375, height: 812 }}
-        />
-      </Storyboard>
-    )
-  `)
+    const { getEditorState } = await renderTestEditorWithCode(exampleProject)
 
     const spiedMetadata = getEditorState().editor.spyMetadataKILLME
     const sanitizedSpyData = objectMap((elementMetadata, key) => {
@@ -57,33 +77,41 @@ describe('Spy Wrapper Tests', () => {
 
     expect(sanitizedSpyData).toMatchInlineSnapshot(`
       Object {
-        ":storyboard-uid": Object {
+        ":storyboard": Object {
           "children": Array [
-            ":storyboard-uid/scene-uid",
+            ":storyboard/scene",
           ],
           "name": "Storyboard",
-          "templatePathAsKey": ":storyboard-uid",
-          "templatePathAsReportedBySpy": ":storyboard-uid",
+          "templatePathAsKey": ":storyboard",
+          "templatePathAsReportedBySpy": ":storyboard",
         },
-        ":storyboard-uid/scene-uid": Object {
+        ":storyboard/scene": Object {
           "children": Array [],
           "name": "Scene",
-          "templatePathAsKey": ":storyboard-uid/scene-uid",
-          "templatePathAsReportedBySpy": ":storyboard-uid/scene-uid",
+          "templatePathAsKey": ":storyboard/scene",
+          "templatePathAsReportedBySpy": ":storyboard/scene",
         },
-        "storyboard-uid/scene-uid:app-root": Object {
+        "storyboard/scene:app-root": Object {
           "children": Array [
-            "storyboard-uid/scene-uid:app-root/inner-div",
+            "storyboard/scene:app-root/inner-div",
           ],
           "name": "div",
-          "templatePathAsKey": "storyboard-uid/scene-uid:app-root",
-          "templatePathAsReportedBySpy": "storyboard-uid/scene-uid:app-root",
+          "templatePathAsKey": "storyboard/scene:app-root",
+          "templatePathAsReportedBySpy": "storyboard/scene:app-root",
         },
-        "storyboard-uid/scene-uid:app-root/inner-div": Object {
-          "children": Array [],
+        "storyboard/scene:app-root/inner-div": Object {
+          "children": Array [
+            "storyboard/scene:app-root/inner-div/card-instance",
+          ],
           "name": "div",
-          "templatePathAsKey": "storyboard-uid/scene-uid:app-root/inner-div",
-          "templatePathAsReportedBySpy": "storyboard-uid/scene-uid:app-root/inner-div",
+          "templatePathAsKey": "storyboard/scene:app-root/inner-div",
+          "templatePathAsReportedBySpy": "storyboard/scene:app-root/inner-div",
+        },
+        "storyboard/scene:app-root/inner-div/card-instance": Object {
+          "children": Array [],
+          "name": "Card",
+          "templatePathAsKey": "storyboard/scene:app-root/inner-div/card-instance",
+          "templatePathAsReportedBySpy": "storyboard/scene:app-root/inner-div/card-instance",
         },
       }
     `)
