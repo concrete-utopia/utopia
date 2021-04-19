aec4371e3fd0ca96da75a0b4f8148d649a59cf19
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-04-19 15:53:35 +0200

    feature(ui) togglebutton added

---
+++ editor/src/uuiui/toggle-button.tsx
@@ -0,0 +1,45 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/react'
+import * as React from 'react'
+import { colorTheme } from './styles/theme'
+
+interface ToggleButtonProps {
+  value: boolean
+  srcOn: string
+  srcOff: string
+  onToggleValue: () => void
+  className?: string
+}
+
+export const ToggleButton: React.FunctionComponent<ToggleButtonProps> = (props) => {
+  const { value, onToggleValue, srcOn, srcOff, className } = props
+  return (
+    <button
+      className={className}
+      css={{
+        width: 22,
+        height: 22,
+        backgroundColor: value ? colorTheme.toggleButtonBackground.value : 'transparent',
+        backgroundSize: 18,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        outline: 'none',
+        border: 'none',
+        backgroundImage: value ? `url(${srcOn})` : `url(${srcOff})`,
+        '&:focus-within': {
+          border: `1px solid ${colorTheme.toggleButtonHoverBorder.value}`,
+          outline: 'none',
+        },
+        '&:hover': {
+          backgroundColor: colorTheme.toggleButtonHoverBackground.value,
+          border: `1px solid ${colorTheme.toggleButtonHoverBorder.value}`,
+        },
+        '&:active': {
+          transform: 'translateY(1px)',
+          backgroundImage: value ? `url(${srcOff})` : `url(${srcOn})`,
+        },
+      }}
+      onClick={onToggleValue}
+    />
+  )
+}
