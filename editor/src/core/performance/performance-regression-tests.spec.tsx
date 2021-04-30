87f4e46e58f9e6c974d3b4336885ba9c2fb1c312
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-04-30 10:34:05 +0200

    update render count

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -59,8 +59,8 @@ describe('React Render Count Tests - ', () => {
     )
 
     const renderCountAfter = renderResult.getNumberOfRenders()
-    expect(renderCountAfter - renderCountBefore).toBeGreaterThan(310) // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toBeLessThan(320)
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThan(670) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(680)
   })
 
   it('Changing the selected view', async () => {
