704af766fe72cfa42c00a2014d165fa2ed9de9b3
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-05-06 14:06:08 +0200

    fix(canvas) unwrap group update dom metadata

--- editor/src/components/editor/actions/actions.ts
+++ editor/src/components/editor/actions/actions.ts
@@ -2033,12 +2033,13 @@ export const UPDATE_FNS = {
           editor,
           'forward',
         )
+        let newSelection: ElementPath[] = []
         const withChildrenMoved = children.reduce((working, child) => {
           const childFrame = MetadataUtils.getFrameInCanvasCoords(
             child.elementPath,
             editor.jsxMetadata,
           )
-          return editorMoveTemplate(
+          const result = editorMoveTemplate(
             child.elementPath,
             child.elementPath,
             childFrame,
@@ -2047,11 +2048,22 @@ export const UPDATE_FNS = {
             parentFrame,
             working,
             null,
-          ).editor
+          )
+          if (result.newPath != null) {
+            newSelection.push(result.newPath)
+          }
+          return result.editor
         }, editor)
         const withViewDeleted = deleteElements([action.target], withChildrenMoved)
 
-        return withViewDeleted
+        return {
+          ...withViewDeleted,
+          selectedViews: newSelection,
+          canvas: {
+            ...withViewDeleted.canvas,
+            mountCount: editor.canvas.mountCount + 1,
+          },
+        }
       },
       dispatch,
     )
