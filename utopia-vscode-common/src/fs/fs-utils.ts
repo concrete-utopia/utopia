concrete-utopia/utopia

commit 8d271bd16b12943d55db167c31a4c099df11f4ae

fix(editor) Rename/move folder corrects imports as it goes. (#1240)
- Fixes #1222.
- Fix an issue in `zipContentsTree(Async)` to ensure that it will continue deeper into a hierarchy if the left hand side does not exist.
- Improved early exiting in `zipContentsTreeAsync`.
- Added a test case for `UPDATE_FILE_PATH` to double check what happens to the files and their imports.
- `replaceFilePath` now works through the imports of the project contents updating them if they resolve to ones that have been updated.
- `applyProjectContentChanges` now split into `collateProjectChanges` that determines what all the changes are that need to be made and `applyProjectChanges` which applies each of those in turn. The `ProjectChange` type represents those changes.
- `applyVSCodeChanges` is now chained off of the previous invocation to ensure that multiple calls aren't running simultaneously.
- `editorDispatch` checks if an `UPDATE_FROM_WORKER` action has been triggered and if there has already been a save, to ensure that updates from that action are saved instead of losing them should the user reload.
- `alreadySaved` added to `EditorStore` to indicate if the project has been saved.
- Allow directories to be renamed in the file browser.
- Implemented `importStatementFromImportDetails` as a way to produce an `ImportStatement` from a module name and `ImportDetails`, which constructs the `rawCode` value, allowing these statements to be updated.
- Removed `projectID` from `writeProjectFile` and `writeProjectContents`.
- Make `/src/index.js` be the parsed version when used in tests.
- Make sure that `targetsForOperation` returns elements in the order they need to be to be deleted.
- `directoryOfPath` doesn't include the trailing slash.
- `ensureDirectoryExists` runs down the ancestors specifically in order rather than creating each one "simultaneously".
- `deletePath` deletes the targets one at a time.

utopia-vscode-common/src/fs/fs-utils.ts
@@ -40,7 +40,11 @@ const existingFileError = (path: string) => handleError(eexist(path))
 const isDirectoryError = (path: string) => handleError(eisdir(path))
 const isNotDirectoryError = (path: string) => handleError(enotdir(path))
 
-export async function initializeFS(storeName: string, user: FSUser, driver: string = INDEXEDDB): Promise<void> {
+export async function initializeFS(
+  storeName: string,
+  user: FSUser,
+  driver: string = INDEXEDDB,
+): Promise<void> {
   fsUser = user
   await initializeStore(storeName, driver)
   await simpleCreateDirectoryIfMissing('/')
@@ -98,11 +102,13 @@ export async function readFileUnsavedContent(path: string): Promise<Uint8Array |
   return fileNode.unsavedContent
 }
 
-export async function readFileAsUTF8(path: string): Promise<{content: string, unsavedContent: string | null}> {
+export async function readFileAsUTF8(
+  path: string,
+): Promise<{ content: string; unsavedContent: string | null }> {
   const { content, unsavedContent } = await getFile(path)
   return {
     content: decoder.decode(content),
-    unsavedContent: unsavedContent == null ? null : decoder.decode(unsavedContent)
+    unsavedContent: unsavedContent == null ? null : decoder.decode(unsavedContent),
   }
 }
 
@@ -140,7 +146,10 @@ export async function getDescendentPaths(path: string): Promise<string[]> {
 async function targetsForOperation(path: string, recursive: boolean): Promise<string[]> {
   if (recursive) {
     const allDescendents = await getDescendentPaths(path)
-    return [path, ...allDescendents]
+    let result = [path, ...allDescendents]
+    result.sort()
+    result.reverse()
+    return result
   } else {
     return [path]
   }
@@ -149,7 +158,7 @@ async function targetsForOperation(path: string, recursive: boolean): Promise<st
 function directoryOfPath(path: string): string | null {
   const target = path.endsWith('/') ? path.slice(0, -1) : path
   const lastSlashIndex = target.lastIndexOf('/')
-  return lastSlashIndex >= 0 ? path.slice(0, lastSlashIndex + 1) : null
+  return lastSlashIndex >= 0 ? path.slice(0, lastSlashIndex) : null
 }
 
 function filenameOfPath(path: string): string {
@@ -242,10 +251,16 @@ async function simpleCreateDirectoryIfMissing(path: string): Promise<void> {
 
 export async function ensureDirectoryExists(path: string): Promise<void> {
   const allPaths = allPathsUpToPath(path)
-  await Promise.all(allPaths.map(simpleCreateDirectoryIfMissing))
+  for (const pathToCreate of allPaths) {
+    await simpleCreateDirectoryIfMissing(pathToCreate)
+  }
 }
 
-export async function writeFile(path: string, content: Uint8Array, unsavedContent: Uint8Array | null): Promise<void> {
+export async function writeFile(
+  path: string,
+  content: Uint8Array,
+  unsavedContent: Uint8Array | null,
+): Promise<void> {
   const parent = await getParent(path)
   const maybeExistingFile = await getItem(path)
   if (maybeExistingFile != null && isDirectory(maybeExistingFile)) {
@@ -254,7 +269,8 @@ export async function writeFile(path: string, content: Uint8Array, unsavedConten
 
   const now = Date.now()
   const fileCTime = maybeExistingFile == null ? now : maybeExistingFile.ctime
-  const lastSavedTime = unsavedContent == null || maybeExistingFile == null ? now : maybeExistingFile.lastSavedTime
+  const lastSavedTime =
+    unsavedContent == null || maybeExistingFile == null ? now : maybeExistingFile.lastSavedTime
   const fileToWrite = fsFile(content, unsavedContent, fileCTime, now, lastSavedTime, fsUser)
   await setItem(path, fileToWrite)
   if (parent != null) {
@@ -266,20 +282,37 @@ export async function writeFileSavedContent(path: string, content: Uint8Array): 
   return writeFile(path, content, null)
 }
 
-export async function writeFileUnsavedContent(path: string, unsavedContent: Uint8Array): Promise<void> {
+export async function writeFileUnsavedContent(
+  path: string,
+  unsavedContent: Uint8Array,
+): Promise<void> {
   const savedContent = await readFileSavedContent(path)
   return writeFile(path, savedContent, unsavedContent)
 }
 
-export async function writeFileAsUTF8(path: string, content: string, unsavedContent: string | null): Promise<void> {
-  return writeFile(path, encoder.encode(content), unsavedContent == null ? null : encoder.encode(unsavedContent))
+export async function writeFileAsUTF8(
+  path: string,
+  content: string,
+  unsavedContent: string | null,
+): Promise<void> {
+  return writeFile(
+    path,
+    encoder.encode(content),
+    unsavedContent == null ? null : encoder.encode(unsavedContent),
+  )
 }
 
-export async function writeFileSavedContentAsUTF8(path: string, savedContent: string): Promise<void> {
+export async function writeFileSavedContentAsUTF8(
+  path: string,
+  savedContent: string,
+): Promise<void> {
   return writeFileAsUTF8(path, savedContent, null)
 }
 
-export async function writeFileUnsavedContentAsUTF8(path: string, unsavedContent: string): Promise<void> {
+export async function writeFileUnsavedContentAsUTF8(
+  path: string,
+  unsavedContent: string,
+): Promise<void> {
   return writeFileUnsavedContent(path, encoder.encode(unsavedContent))
 }
 
@@ -331,7 +364,9 @@ export async function deletePath(path: string, recursive: boolean): Promise<stri
 
   // Really this should fail if recursive isn't set to true when trying to delete a
   // non-empty directory, but for some reason VSCode doesn't provide an error suitable for that
-  await Promise.all(targets.map((p) => removeItem(p)))
+  for (const target of targets) {
+    await removeItem(target)
+  }
 
   if (parent != null) {
     await markModified(parent)

utopia-vscode-extension/src/extension.ts
@@ -122,6 +122,14 @@ function watchForChangesFromVSCode(context: vscode.ExtensionContext, projectID: 
         }
       }
     }),
+    vscode.workspace.onDidOpenTextDocument((document) => {
+      if (isUtopiaDocument(document)) {
+        const path = fromUtopiaURI(document.uri)
+        if (!incomingFileChanges.has(path)) {
+          updateDirtyContent(document.uri)
+        }
+      }
+    }),
   )
 }
 

editor/src/components/assets.ts
@@ -283,12 +283,12 @@ export function zipContentsTree(
       throw new Error(`Invalid state of both elements being false reached.`)
     } else {
       const shouldContinueDeeper = onElement(fullPath, firstContents, secondContents)
-      if (
-        isProjectContentDirectory(firstContents) &&
-        isProjectContentDirectory(secondContents) &&
-        shouldContinueDeeper
-      ) {
-        zipContentsTree(firstContents.children, secondContents.children, onElement)
+      if (isProjectContentDirectory(secondContents) && shouldContinueDeeper) {
+        zipContentsTree(
+          isProjectContentDirectory(firstContents) ? firstContents.children : {},
+          secondContents.children,
+          onElement,
+        )
       }
     }
   }
@@ -303,25 +303,29 @@ export async function zipContentsTreeAsync(
     secondContents: ProjectContentsTree | null,
   ) => Promise<boolean>,
 ): Promise<void> {
-  let keys: Set<string> = emptySet()
-  Object.keys(firstTree).forEach(keys.add, keys)
-  Object.keys(secondTree).forEach(keys.add, keys)
-  for (const key of keys) {
-    const firstContents = propOrNull(key, firstTree)
-    const secondContents = propOrNull(key, secondTree)
-    const fullPath = firstContents?.fullPath ?? secondContents?.fullPath
-    if (fullPath == null) {
-      throw new Error(`Invalid state of both elements being false reached.`)
-    } else {
-      // eslint-disable-next-line no-await-in-loop
-      const shouldContinueDeeper = await onElement(fullPath, firstContents, secondContents)
-      if (
-        isProjectContentDirectory(firstContents) &&
-        isProjectContentDirectory(secondContents) &&
-        shouldContinueDeeper
-      ) {
+  if (firstTree == secondTree) {
+    return Promise.resolve()
+  } else {
+    let keys: Set<string> = emptySet()
+    Object.keys(firstTree).forEach(keys.add, keys)
+    Object.keys(secondTree).forEach(keys.add, keys)
+    for (const key of keys) {
+      const firstContents = propOrNull(key, firstTree)
+      const secondContents = propOrNull(key, secondTree)
+      const fullPath = firstContents?.fullPath ?? secondContents?.fullPath
+      if (fullPath == null) {
+        throw new Error(`Invalid state of both elements being false reached.`)
+      } else {
         // eslint-disable-next-line no-await-in-loop
-        await zipContentsTreeAsync(firstContents.children, secondContents.children, onElement)
+        const shouldContinueDeeper = await onElement(fullPath, firstContents, secondContents)
+        if (isProjectContentDirectory(secondContents) && shouldContinueDeeper) {
+          // eslint-disable-next-line no-await-in-loop
+          await zipContentsTreeAsync(
+            isProjectContentDirectory(firstContents) ? firstContents.children : {},
+            secondContents.children,
+            onElement,
+          )
+        }
       }
     }
   }

editor/src/components/canvas/dom-walker.spec.browser.tsx
@@ -71,6 +71,7 @@ async function renderTestEditorWithCode(appUiJsFileCode: string) {
       new FakeWatchdogWorker(),
     ),
     dispatch: dispatch,
+    alreadySaved: false,
   }
 
   const storeHook = create<EditorStore>((set) => initialEditorStore)

editor/src/components/canvas/ui-jsx.test-utils.tsx
@@ -160,6 +160,7 @@ export async function renderTestEditorWithModel(
       new FakeWatchdogWorker(),
     ),
     dispatch: asyncTestDispatch,
+    alreadySaved: false,
   }
 
   const storeHook = create<EditorStore>((set) => initialEditorStore)

editor/src/components/editor/actions/actions.spec.ts
@@ -64,9 +64,16 @@ import {
   getOpenUIJSFile,
   PersistentModel,
   StoryboardFilePath,
+  defaultUserState,
+  editorModelFromPersistentModel,
 } from '../store/editor-state'
 import { editorMoveTemplate, UPDATE_FNS } from './actions'
-import { setCanvasFrames, setProp_UNSAFE, switchLayoutSystem } from './action-creators'
+import {
+  setCanvasFrames,
+  setProp_UNSAFE,
+  switchLayoutSystem,
+  updateFilePath,
+} from './action-creators'
 import { getLayoutPropertyOr } from '../../../core/layout/getLayoutProperty'
 import {
   ScenePathForTestUiJsFile,
@@ -84,9 +91,15 @@ import { TestScenePath } from '../../canvas/ui-jsx.test-utils'
 import { NO_OP } from '../../../core/shared/utils'
 import { CURRENT_PROJECT_VERSION } from './migrations/migrations'
 import { generateCodeResultCache } from '../../custom-code/code-file'
-import { contentsToTree, getContentsTreeFileFromString } from '../../assets'
+import {
+  contentsToTree,
+  getContentsTreeFileFromString,
+  treeToContents,
+  walkContentsTreeForParseSuccess,
+} from '../../assets'
 import { emptyComments } from '../../../core/workers/parser-printer/parser-printer-comments'
 import { getUtopiaJSXComponentsFromSuccess } from '../../../core/model/project-file-utils'
+import { complexDefaultProject } from '../../../sample-projects/sample-project-utils'
 const chaiExpect = Chai.expect
 
 function storyboardComponent(numberOfScenes: number): UtopiaJSXComponent {
@@ -1026,5 +1039,44 @@ describe('LOAD', () => {
     ) as TextFile).fileContents
     expect(isParseSuccess(newSecondFileContents.parsed)).toBeTruthy()
     expect(newSecondFileContents.code).toEqual(initialFileContents.code)
+  })
+})
+
+describe('UPDATE_FILE_PATH', () => {
+  it('updates the files in a directory and imports related to it', () => {
+    const project = complexDefaultProject()
+    const editorState = editorModelFromPersistentModel(project, NO_OP)
+    const actualResult = UPDATE_FNS.UPDATE_FILE_PATH(
+      updateFilePath('/src', '/src2'),
+      editorState,
+      defaultUserState,
+      NO_OP,
+    )
+    let filesAndTheirImports: { [filename: string]: Array<string> } = {}
+    walkContentsTreeForParseSuccess(actualResult.projectContents, (fullPath, success) => {
+      filesAndTheirImports[fullPath] = Object.keys(success.imports).sort()
+    })
+    expect(filesAndTheirImports).toMatchInlineSnapshot(`
+      Object {
+        "/src2/app.js": Array [
+          "/src2/card.js",
+          "react",
+        ],
+        "/src2/card.js": Array [
+          "react",
+          "utopia-api",
+        ],
+        "/src2/index.js": Array [
+          "./app.js",
+          "react",
+          "react-dom",
+        ],
+        "/utopia/storyboard.js": Array [
+          "/src2/app.js",
+          "react",
+          "utopia-api",
+        ],
+      }
+    `)
   })
 })

editor/src/components/editor/actions/actions.ts
@@ -65,6 +65,7 @@ import {
   deleteJSXAttribute,
   setJSXAttributesAttribute,
   emptyJsxMetadata,
+  isImportStatement,
 } from '../../../core/shared/element-template'
 import {
   generateUidWithExistingComponents,
@@ -105,6 +106,7 @@ import {
   applyToAllUIJSFiles,
   updateFileContents,
   applyUtopiaJSXComponentsChanges,
+  saveTextFileContents,
 } from '../../../core/model/project-file-utils'
 import {
   Either,
@@ -144,6 +146,8 @@ import {
   unparsed,
   ParseSuccess,
   importAlias,
+  Imports,
+  importStatementFromImportDetails,
 } from '../../../core/shared/project-file-types'
 import {
   addImport,
@@ -497,6 +501,8 @@ import Meta from 'antd/lib/card/Meta'
 import utils from '../../../utils/utils'
 import { defaultConfig } from 'utopia-vscode-common'
 import { getTargetParentForPaste } from '../../../utils/clipboard'
+import { absolutePathFromRelativePath } from '../../../utils/path-utils'
+import { resolveModule } from '../../../core/es-modules/package-manager/module-resolution'
 
 function applyUpdateToJSXElement(
   element: JSXElement,
@@ -1190,29 +1196,119 @@ function replaceFilePath(
     ...projectContents,
   }
   let updatedFiles: Array<{ oldPath: string; newPath: string }> = []
-  Utils.fastForEach(Object.keys(projectContents), (key) => {
-    if (oldFolderRegex.test(key)) {
-      const projectFile = projectContents[key]
-      const newFilePath = key.replace(oldPath, newPath)
+  Utils.fastForEach(Object.keys(projectContents), (filename) => {
+    if (oldFolderRegex.test(filename)) {
+      const projectFile = projectContents[filename]
+      const newFilePath = filename.replace(oldPath, newPath)
       const fileType = isDirectory(projectFile) ? 'DIRECTORY' : fileTypeFromFileName(newFilePath)
       if (fileType == null) {
         // Can't identify the file type.
-        error = `Can't rename ${key} to ${newFilePath}.`
+        error = `Can't rename ${filename} to ${newFilePath}.`
       } else {
         const updatedProjectFile = switchToFileType(projectFile, fileType)
         if (updatedProjectFile == null) {
           // Appears this file can't validly be changed.
-          error = `Can't rename ${key} to ${newFilePath}.`
+          error = `Can't rename ${filename} to ${newFilePath}.`
         } else {
           // Remove the old file.
-          delete updatedProjectContents[key]
+          delete updatedProjectContents[filename]
           updatedProjectContents[newFilePath] = updatedProjectFile
-          updatedFiles.push({ oldPath: key, newPath: newFilePath })
+          updatedFiles.push({ oldPath: filename, newPath: newFilePath })
         }
       }
     }
   })
 
+  // Correct any imports in files that have changed because of the above file movements.
+  Utils.fastForEach(Object.keys(updatedProjectContents), (filename) => {
+    const projectFile = updatedProjectContents[filename]
+    // Only for successfully parsed text files, with some protection for files that are yet to be parsed.
+    if (
+      isTextFile(projectFile) &&
+      isParseSuccess(projectFile.fileContents.parsed) &&
+      projectFile.fileContents.revisionsState !== RevisionsState.CodeAhead
+    ) {
+      let updatedParseResult: ParseSuccess = projectFile.fileContents.parsed
+      fastForEach(updatedFiles, (updatedFile) => {
+        fastForEach(Object.keys(updatedParseResult.imports), (importSource) => {
+          // Only do this for import sources that look like file paths.
+          if (importSource.startsWith('.') || importSource.startsWith('/')) {
+            const resolveResult = resolveModule(projectContentsTree, {}, filename, importSource)
+
+            if (
+              resolveResult.type === 'RESOLVE_SUCCESS' &&
+              resolveResult.success.path === updatedFile.oldPath
+            ) {
+              // Create new absolute import path and shift the import in this file to represent that.
+              const importFromParse = updatedParseResult.imports[importSource]
+              let updatedImports: Imports = {
+                ...updatedParseResult.imports,
+              }
+              delete updatedImports[importSource]
+              // If an absolute path was used before, use the updated absolute path.
+              const newImportPath = importSource.startsWith('/')
+                ? updatedFile.newPath
+                : getFilePathToImport(updatedFile.newPath, filename)
+              updatedImports[newImportPath] = importFromParse
+
+              // Update the parse result to be incorporated later.
+              updatedParseResult = {
+                ...updatedParseResult,
+                imports: updatedImports,
+              }
+            }
+          }
+        })
+
+        // Update the top level element import statements.
+        const updatedTopLevelElements = updatedParseResult.topLevelElements.map(
+          (topLevelElement) => {
+            if (isImportStatement(topLevelElement)) {
+              const resolveResult = resolveModule(
+                projectContentsTree,
+                {},
+                filename,
+                topLevelElement.module,
+              )
+              if (
+                resolveResult.type === 'RESOLVE_SUCCESS' &&
+                resolveResult.success.path === updatedFile.oldPath
+              ) {
+                // If an absolute path was used before, use the updated absolute path.
+                const newImportPath = topLevelElement.module.startsWith('/')
+                  ? updatedFile.newPath
+                  : getFilePathToImport(updatedFile.newPath, filename)
+                const importDefinition = forceNotNull(
+                  'Import should exist.',
+                  updatedParseResult.imports[newImportPath],
+                )
+                return importStatementFromImportDetails(newImportPath, importDefinition)
+              } else {
+                return topLevelElement
+              }
+            } else {
+              return topLevelElement
+            }
+          },
+        )
+
+        updatedParseResult = {
+          ...updatedParseResult,
+          topLevelElements: updatedTopLevelElements,
+        }
+      })
+
+      updatedProjectContents[filename] = saveTextFileContents(
+        projectFile,
+        textFileContents(
+          projectFile.fileContents.code,
+          updatedParseResult,
+          RevisionsState.ParsedAhead,
+        ),
+        projectFile.lastSavedContents == null,
+      )
+    }
+  })
   // Check if we discovered an error.
   if (error == null) {
     return {

commit 7a9c51c5f27ce84e73984946df3717c2301cf33e

fix(vscode) Prevent VSCode from grabbing focus when selecting elements. - Fixes #1246. - Fixed regression caused by #1240, where all the child paths would be   filtered out and messages would not exit the mailbox. `directoryOfPath`   was rewritten (and renaming it to `getParentPath`) and then ensuring   the various call sites were aligned. - Passed the `preserveFocus` option when invoking the `vscode.open` command.

utopia-vscode-common/src/fs/fs-utils.ts
@@ -1,5 +1,5 @@
 import { INDEXEDDB } from 'localforage'
-import { appendToPath } from '../path-utils'
+import { appendToPath, stripLeadingSlash, stripTrailingSlash } from '../path-utils'
 import { getItem, initializeStore, keys, removeItem, setItem } from './fs-core'
 import {
   FSError,
@@ -155,10 +155,14 @@ async function targetsForOperation(path: string, recursive: boolean): Promise<st
   }
 }
 
-function directoryOfPath(path: string): string | null {
-  const target = path.endsWith('/') ? path.slice(0, -1) : path
-  const lastSlashIndex = target.lastIndexOf('/')
-  return lastSlashIndex >= 0 ? path.slice(0, lastSlashIndex) : null
+function getParentPath(path: string): string | null {
+  const withoutLeadingOrTrailingSlash = stripLeadingSlash(stripTrailingSlash(path))
+  const pathElems = withoutLeadingOrTrailingSlash.split('/')
+  if (pathElems.length <= 1) {
+    return null
+  } else {
+    return `/${pathElems.slice(0, -1).join('/')}`
+  }
 }
 
 function filenameOfPath(path: string): string {
@@ -169,8 +173,8 @@ function filenameOfPath(path: string): string {
 
 export async function childPaths(path: string): Promise<string[]> {
   const allDescendents = await getDescendentPaths(path)
-  const pathAsDir = path.endsWith('/') ? path : `${path}/`
-  return allDescendents.filter((k) => directoryOfPath(k) === pathAsDir)
+  const pathAsDir = stripTrailingSlash(path)
+  return allDescendents.filter((k) => getParentPath(k) === pathAsDir)
 }
 
 async function getDirectory(path: string): Promise<FSDirectory> {
@@ -184,7 +188,7 @@ async function getDirectory(path: string): Promise<FSDirectory> {
 
 async function getParent(path: string): Promise<FSNodeWithPath | null> {
   // null signifies we're already at the root
-  const parentPath = directoryOfPath(path)
+  const parentPath = getParentPath(path)
   if (parentPath == null) {
     return null
   } else {
@@ -237,7 +241,7 @@ async function simpleCreateDirectoryIfMissing(path: string): Promise<void> {
 
     // Attempt to mark the parent as modified, but don't fail if it doesn't exist
     // since it might not have been created yet
-    const parentPath = directoryOfPath(path)
+    const parentPath = getParentPath(path)
     if (parentPath != null) {
       const parentNode = await getItem(parentPath)
       if (parentNode != null) {

utopia-vscode-extension/src/extension.ts
@@ -278,7 +278,7 @@ async function openFile(fileUri: vscode.Uri, retries: number = 5): Promise<boole
   const filePath = fromUtopiaURI(fileUri)
   const fileExists = await exists(filePath)
   if (fileExists) {
-    await vscode.commands.executeCommand('vscode.open', fileUri)
+    await vscode.commands.executeCommand('vscode.open', fileUri, { preserveFocus: true })
     return true
   } else {
     // Just in case the message is processed before the file has been written to the FS