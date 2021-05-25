0a978a30c89fbc66d890157801b5848acce8f760
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-05-25 12:16:41 +0200

    create snapshots for errors

--- editor/src/components/canvas/ui-jsx-canvas-errors.spec.tsx
+++ editor/src/components/canvas/ui-jsx-canvas-errors.spec.tsx
@@ -29,7 +29,17 @@ describe('UiJsxCanvas errors', () => {
     `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": undefined,
+          "lineNumber": undefined,
+          "message": "MyCard is not defined",
+          "name": "ReferenceError",
+          "originalCode": undefined,
+        },
+      ]
+    `)
   })
 })
 
@@ -62,7 +72,53 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 2,
+          "lineNumber": 4,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "import * as React from \\"react\\"",
+              "highlight": false,
+              "lineNumber": 1,
+            },
+            ScriptLine {
+              "content": "import { View, Storyboard, Scene } from 'utopia-api'",
+              "highlight": false,
+              "lineNumber": 2,
+            },
+            ScriptLine {
+              "content": "const MyComp = (props) => <div data-uid={'bbb'}>Utopia</div>",
+              "highlight": false,
+              "lineNumber": 3,
+            },
+            ScriptLine {
+              "content": "a.a // 16,1 this shall throw an error!",
+              "highlight": true,
+              "lineNumber": 4,
+            },
+            ScriptLine {
+              "content": "export var App = (props) => {",
+              "highlight": false,
+              "lineNumber": 5,
+            },
+            ScriptLine {
+              "content": "  return (<MyComp data-uid={'aaa'}/>)",
+              "highlight": false,
+              "lineNumber": 6,
+            },
+            ScriptLine {
+              "content": "}",
+              "highlight": false,
+              "lineNumber": 7,
+            },
+          ],
+        },
+      ]
+    `)
   })
   it('an arbitrary jsx child has correct source map', () => {
     const canvasErrors = testCanvasErrorInline(
@@ -96,7 +152,38 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 13,
+          "lineNumber": 1,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "",
+              "highlight": true,
+              "lineNumber": 1,
+            },
+            ScriptLine {
+              "content": "import * as React from \\"react\\"",
+              "highlight": false,
+              "lineNumber": 2,
+            },
+            ScriptLine {
+              "content": "import { View, Storyboard, Scene } from 'utopia-api'",
+              "highlight": false,
+              "lineNumber": 3,
+            },
+            ScriptLine {
+              "content": "const MyComp = (props) => <div data-uid={'bbb'}>Utopia</div>",
+              "highlight": false,
+              "lineNumber": 4,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('an arbitrary jsx child has correct source map even if the entire expression is broken', () => {
@@ -132,7 +219,38 @@ export var App = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 3,
+          "lineNumber": 1,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "",
+              "highlight": true,
+              "lineNumber": 1,
+            },
+            ScriptLine {
+              "content": "import * as React from \\"react\\"",
+              "highlight": false,
+              "lineNumber": 2,
+            },
+            ScriptLine {
+              "content": "import { View, Storyboard, Scene } from 'utopia-api'",
+              "highlight": false,
+              "lineNumber": 3,
+            },
+            ScriptLine {
+              "content": "const MyComp = (props) => <div data-uid={'bbb'}>Utopia</div>",
+              "highlight": false,
+              "lineNumber": 4,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('an arbitrary jsx attribute has correct source map', () => {
@@ -168,7 +286,53 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 57,
+          "lineNumber": 8,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "",
+              "highlight": false,
+              "lineNumber": 5,
+            },
+            ScriptLine {
+              "content": "export var App = (props) => {",
+              "highlight": false,
+              "lineNumber": 6,
+            },
+            ScriptLine {
+              "content": "  return (",
+              "highlight": false,
+              "lineNumber": 7,
+            },
+            ScriptLine {
+              "content": "    <MyComp data-uid={'aaa'} someAttribute={'hello' + a.a /* 19,55 */}>",
+              "highlight": true,
+              "lineNumber": 8,
+            },
+            ScriptLine {
+              "content": "      hello!",
+              "highlight": false,
+              "lineNumber": 9,
+            },
+            ScriptLine {
+              "content": "    </MyComp>",
+              "highlight": false,
+              "lineNumber": 10,
+            },
+            ScriptLine {
+              "content": "  )",
+              "highlight": false,
+              "lineNumber": 11,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('an arbitrary jsx attribute has correct source map even if the entire expression is broken', () => {
@@ -204,7 +368,53 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 47,
+          "lineNumber": 8,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "",
+              "highlight": false,
+              "lineNumber": 5,
+            },
+            ScriptLine {
+              "content": "export var App = (props) => {",
+              "highlight": false,
+              "lineNumber": 6,
+            },
+            ScriptLine {
+              "content": "  return (",
+              "highlight": false,
+              "lineNumber": 7,
+            },
+            ScriptLine {
+              "content": "    <MyComp data-uid={'aaa'} someAttribute={a.a /* 19,45 */}>",
+              "highlight": true,
+              "lineNumber": 8,
+            },
+            ScriptLine {
+              "content": "      hello!",
+              "highlight": false,
+              "lineNumber": 9,
+            },
+            ScriptLine {
+              "content": "    </MyComp>",
+              "highlight": false,
+              "lineNumber": 10,
+            },
+            ScriptLine {
+              "content": "  )",
+              "highlight": false,
+              "lineNumber": 11,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('arbitrary at the top of a component has correct source map', () => {
@@ -241,7 +451,53 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 10,
+          "lineNumber": 7,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "const MyComp = (props) => <div data-uid={'bbb'}>Utopia</div>",
+              "highlight": false,
+              "lineNumber": 4,
+            },
+            ScriptLine {
+              "content": "",
+              "highlight": false,
+              "lineNumber": 5,
+            },
+            ScriptLine {
+              "content": "export var App = (props) => {",
+              "highlight": false,
+              "lineNumber": 6,
+            },
+            ScriptLine {
+              "content": "  '5' + a.a // 18,9",
+              "highlight": true,
+              "lineNumber": 7,
+            },
+            ScriptLine {
+              "content": "  return (",
+              "highlight": false,
+              "lineNumber": 8,
+            },
+            ScriptLine {
+              "content": "    <MyComp data-uid={'aaa'}>",
+              "highlight": false,
+              "lineNumber": 9,
+            },
+            ScriptLine {
+              "content": "      hello!",
+              "highlight": false,
+              "lineNumber": 10,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('arbitrary at the top of a component has correct source map even if the entire expression is broken', () => {
@@ -278,7 +534,53 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": 4,
+          "lineNumber": 7,
+          "message": "a is not defined",
+          "name": "ReferenceError",
+          "originalCode": Array [
+            ScriptLine {
+              "content": "const MyComp = (props) => <div data-uid={'bbb'}>Utopia</div>",
+              "highlight": false,
+              "lineNumber": 4,
+            },
+            ScriptLine {
+              "content": "",
+              "highlight": false,
+              "lineNumber": 5,
+            },
+            ScriptLine {
+              "content": "export var App = (props) => {",
+              "highlight": false,
+              "lineNumber": 6,
+            },
+            ScriptLine {
+              "content": "  a.a // 18,3",
+              "highlight": true,
+              "lineNumber": 7,
+            },
+            ScriptLine {
+              "content": "  return (",
+              "highlight": false,
+              "lineNumber": 8,
+            },
+            ScriptLine {
+              "content": "    <MyComp data-uid={'aaa'}>",
+              "highlight": false,
+              "lineNumber": 9,
+            },
+            ScriptLine {
+              "content": "      hello!",
+              "highlight": false,
+              "lineNumber": 10,
+            },
+          ],
+        },
+      ]
+    `)
   })
 
   it('React.useEffect at the root fails usefully', () => {
@@ -307,7 +609,14 @@ export var ${BakedInStoryboardVariableName} = (props) => {
 }
 `,
     )
-    expect(result).toMatchInlineSnapshot()
+    expect(result).toMatchInlineSnapshot(`
+      "<div
+        id=\\"canvas-container\\"
+        style=\\"all: initial; position: absolute;\\"
+        data-utopia-valid-paths=\\"\\"
+      ></div>
+      "
+    `)
   })
 
   it('handles an undefined component gracefully', () => {
@@ -336,7 +645,17 @@ export var ${BakedInStoryboardVariableName} = (props) => {
       `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": undefined,
+          "lineNumber": undefined,
+          "message": "Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.",
+          "name": "Error",
+          "originalCode": undefined,
+        },
+      ]
+    `)
   })
 
   it('handles an non-existent component gracefully', () => {
@@ -364,6 +683,16 @@ export var ${BakedInStoryboardVariableName} = (props) => {
       `,
       {},
     )
-    expect(canvasErrors).toMatchInlineSnapshot()
+    expect(canvasErrors).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "columnNumber": undefined,
+          "lineNumber": undefined,
+          "message": "Element type is invalid: expected a string (for built-in components) or a class/function (for composite components) but got: undefined. You likely forgot to export your component from the file it's defined in, or you might have mixed up default and named imports.",
+          "name": "Error",
+          "originalCode": undefined,
+        },
+      ]
+    `)
   })
 })
