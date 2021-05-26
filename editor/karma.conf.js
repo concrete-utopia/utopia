88c3d934be75701e85ac14cab3d5e2ede4a79a73
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-05-26 15:43:30 +0200

    using older karma

--- editor/karma.conf.js
+++ editor/karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['webpack', 'mocha'],
+    frameworks: ['mocha'],
     webpack: webpackConfig,
 
     // list of files / patterns to load in the browser
