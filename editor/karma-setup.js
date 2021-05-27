ec48bbc004c0fb914d7cc00036a392487a78887c
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-05-27 14:26:48 +0200

    fix missing browserFS

--- editor/karma-setup.js
+++ editor/karma-setup.js
@@ -1,4 +1,13 @@
 // // The matchers API
 import expect from 'expect'
+import * as BrowserFS from 'browserfs'
+
+// eslint react plugin uses this
+BrowserFS.configure({ fs: 'InMemory', options: {} }, (e) => {
+  if (e) {
+    throw e
+  }
+})
+window.BrowserFS = BrowserFS
 
 window.expect = expect
