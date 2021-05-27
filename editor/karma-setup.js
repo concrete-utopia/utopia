44ef5bdd765f555c6f6010545a5a3f807ceb506c
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-05-27 12:42:39 +0200

    using expect

--- editor/karma-setup.js
+++ editor/karma-setup.js
@@ -1,14 +1,4 @@
-// // the jest.fn() API
-import jest from 'jest-mock'
 // // The matchers API
 import expect from 'expect'
 
-// // Add missing Jest functions
-window.test = window.it
-window.test.each = (inputs) => (testName, test) =>
-  inputs.forEach((args) => window.it(testName, () => test(...args)))
-window.test.todo = function () {
-  return undefined
-}
-window.jest = jest
 window.expect = expect
--- editor/src/components/canvas/ui-jsx-canvas-errors.spec.browser2.tsx
+++ editor/src/components/canvas/ui-jsx-canvas-errors.spec.browser2.tsx
@@ -1,12 +1,10 @@
 import { BakedInStoryboardUID, BakedInStoryboardVariableName } from '../../core/model/scene-utils'
 import { act, render } from '@testing-library/react'
-import * as chai from 'chai'
 import * as React from 'react'
-var expect = chai.expect
 
 describe('UiJsxCanvas errors', () => {
   it('handles a component that is not imported by throwing a ReferenceError', () => {
     const renderResult = render(<div style={{ width: 50, height: 12 }}>{BakedInStoryboardUID}</div>)
-    expect(renderResult.baseElement.nodeName).equal('BODY')
+    expect(renderResult.baseElement.nodeName).toEqual('BODY')
   })
 })
