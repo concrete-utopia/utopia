d6eac71a1d309489f4bea45aefbe3fc67719054c
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-05-27 15:38:35 +0200

    hmmmm

--- editor/karma.conf.js
+++ editor/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     // Here I'm including all of the the Jest tests which are all under the __tests__ directory.
     // You may need to tweak this patter to find your test files/
-    // files: ['./karma-setup.js', 'src/.*\\.spec\\.(jsx?|tsx?)$'],
+    // files: ['./karma-setup.js', './src/**/*.spec.browser2.{ts,tsx}'],
     files: [
       './karma-setup.js',
       './src/components/canvas/ui-jsx-canvas-errors.spec.browser2.tsx',
