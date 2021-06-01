743b47862392bbf39cf7df9360e9cc00137f64c1
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-06-01 12:52:47 +0200

    writing an end to end test to wrap it all up

--- editor/src/core/workers/parser-printer/parser-printer-uids.spec.ts
+++ editor/src/core/workers/parser-printer/parser-printer-uids.spec.ts
@@ -1,6 +1,6 @@
 import { MajesticBrokerTestCaseCode } from '../../../test-cases/majestic-broker'
 import { getAllUniqueUids } from '../../model/element-template-utils'
-import { getComponentsFromTopLevelElements } from '../../model/project-file-utils'
+import { directory, getComponentsFromTopLevelElements } from '../../model/project-file-utils'
 import { uniq } from '../../shared/array-utils'
 import {
   isJSXArbitraryBlock,
@@ -14,18 +14,29 @@ import {
 } from '../../shared/element-template'
 import {
   foldParsedTextFile,
+  ProjectContents,
   RevisionsState,
   TextFile,
   textFile,
   textFileContents,
+  unparsed,
 } from '../../shared/project-file-types'
 import { lintAndParse, parseCode, printCode, printCodeOptions } from './parser-printer'
 import { emptyComments } from './parser-printer-comments'
 import { testParseCode } from './parser-printer.test-utils'
 import { applyPrettier } from 'utopia-vscode-common'
-import { addFileToProjectContents, ProjectContentTreeRoot } from '../../../components/assets'
-import { StoryboardFilePath } from '../../../components/editor/store/editor-state'
+import {
+  addFileToProjectContents,
+  contentsToTree,
+  ProjectContentTreeRoot,
+} from '../../../components/assets'
+import {
+  DefaultPackageJson,
+  StoryboardFilePath,
+} from '../../../components/editor/store/editor-state'
 import { emptySet } from '../../shared/set-utils'
+import { createCodeFile } from '../../../components/custom-code/code-file.test-utils'
+import { renderTestEditorWithProjectContent } from '../../../components/canvas/ui-jsx.test-utils'
 
 function addCodeFileToProjectContents(
   projectContents: ProjectContentTreeRoot,
@@ -131,6 +142,75 @@ describe('parseCode', () => {
 
     const uniqueIDs = getAllUniqueUids(projectContents, 'Unique IDs failure.')
     expect(uniq(uniqueIDs).length).toEqual(6)
+  })
+
+  it('can successfully load a multifile project with duplicated UIDs', async () => {
+    let projectContents: ProjectContents = {
+      '/package.json': textFile(
+        textFileContents(
+          JSON.stringify(DefaultPackageJson, null, 2),
+          unparsed,
+          RevisionsState.BothMatch,
+        ),
+        null,
+        0,
+      ),
+      '/src': directory(),
+      '/utopia': directory(),
+      [StoryboardFilePath]: createCodeFile(
+        StoryboardFilePath,
+        `
+  import * as React from 'react'
+  import { Scene, Storyboard } from 'utopia-api'
+  import { App } from '/src/app.js'
+
+  export var storyboard = (
+    <Storyboard data-uid='storyboard'>
+      <Scene
+        data-uid='scene'
+        style={{ position: 'absolute', left: 0, top: 0, width: 375, height: 812 }}
+      >
+        <App data-uid='app' />
+      </Scene>
+    </Storyboard>
+  )`,
+      ),
+      '/src/app.js': createCodeFile(
+        '/src/app.js',
+        `import * as React from 'react'
+        import { Card } from './card'
+
+        export const App = (props) => {
+          return (
+            <div data-uid='duplicated'>
+              <div data-uid='duplicated'>Hello World!</div>
+              <Card data-uid='aaa' />
+            </div>
+          )
+        }
+        `,
+      ),
+      '/src/card.js': createCodeFile(
+        '/src/app.js',
+        `import * as React from 'react'
+
+        export const Card = (props) => {
+          return (
+            <div data-uid='duplicated'>
+              <div data-uid='duplicated2'>Hello World!</div>
+              <div data-uid='aab'></div>
+            </div>
+          )
+        } `,
+      ),
+    }
+    const renderResult = await renderTestEditorWithProjectContent(contentsToTree(projectContents))
+
+    const uniqueIDs = getAllUniqueUids(
+      renderResult.getEditorState().editor.projectContents,
+      'Unique IDs failure.',
+    )
+    expect(uniq(uniqueIDs).length).toEqual(9)
   })
 })
 
