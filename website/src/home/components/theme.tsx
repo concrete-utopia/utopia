e30d5bb467ea6a6833afd522a47675e6b11e1ab6
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-06-11 14:55:36 +0200

    delete the website folder

--- website/src/home/components/theme.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsx jsx */
-import React from 'react'
-
-export const colors = {
-  primary: '#007AFF',
-  default: 'black',
-}
--- website/src/missing-types/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare module 'facepaint'
-declare module 'can-autoplay'
