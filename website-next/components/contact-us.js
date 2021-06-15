a00fd97cb5b111032a19ff34e9407900963c2003
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-06-15 14:34:59 +0200

    contact us section layout switched

--- website-next/components/contact-us.js
+++ website-next/components/contact-us.js
@@ -2,22 +2,26 @@ import { BasicEmailSignup } from './email-signup'
 
 
 const navigation = [
-  { name: 'Create a Project', href: '/project' },
-  { name: 'Discord', href: 'https://discord.gg/dSWs79MY' },
-  { name: 'Github', href: 'https://github.com/concrete-utopia/utopia' },
+  { name: 'Play with Utopia', href: '/project' },
+  { name: 'Join our Discord', href: 'https://discord.gg/dSWs79MY' },
+  { name: 'Check us on Github', href: 'https://github.com/concrete-utopia/utopia' },
 ]
 
 export const ContactUs = props => (
-  <div className='flex justify-between items-center'>
-    <BasicEmailSignup />
-    {navigation.map((item) => (
-      <a
-        key={item.name}
-        href={item.href}
-        className='block px-3 py-2 rounded-md text-body hover:text-gray-900 hover:bg-gray-50'
-      >
-        {item.name}
-      </a>
-    ))}
-  </div>
+  <>
+    <div className='pb-6 font-body'>
+      <BasicEmailSignup />
+    </div>
+    <div className='font-body text-lg flex justify-center items-center'>
+      {navigation.map((item) => (
+        <a
+          key={item.name}
+          href={item.href}
+          className='block px-6 py-2 rounded-md text-body hover:text-gray-900 hover:bg-gray-50'
+        >
+          {item.name}
+        </a>
+      ))}
+    </div>
+  </>
 )
