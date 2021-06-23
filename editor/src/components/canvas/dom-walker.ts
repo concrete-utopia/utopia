2efda2fcad69d1f5cd27e4e3e035582fe5333df7
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-06-23 15:29:21 +0200

    remove console logs

--- editor/src/components/canvas/dom-walker.ts
+++ editor/src/components/canvas/dom-walker.ts
@@ -432,7 +432,6 @@ export function useDomWalker(
 
   const fireThrottledCallback = useThrottledCallback(() => {
     if (containerRef.current != null) {
-      // console.log('DOM_WALKER_START')
       if (LogDomWalkerPerformance) {
         performance.mark('DOM_WALKER_START')
       }
@@ -476,7 +475,6 @@ export function useDomWalker(
       // Fragments will appear as multiple separate entries with duplicate UIDs, so we need to handle those
       const fixedMetadata = mergeFragmentMetadata(metadata)
 
-      // console.log('DOM_WALKER_FINISH')
       props.onDomReport(fixedMetadata, cachedPaths)
     }
   })
