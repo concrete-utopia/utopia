8b5885349c1fda0f375de45ce2271a09c531d161
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2021-06-23 14:01:28 +0100

    fix(canvas) Leave jsx text blocks as they are when rendering the canvas (#1420)

--- editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-component-renderer.tsx
+++ editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-component-renderer.tsx
@@ -167,7 +167,7 @@ export function createComponentRendererComponent(params: {
       } else {
         const ownElementPath = EP.appendNewElementPath(instancePath, getUtopiaID(element))
 
-        return renderCoreElement(
+        const renderedCoreElement = renderCoreElement(
           element,
           ownElementPath,
           mutableContext.rootScope,
@@ -188,6 +188,12 @@ export function createComponentRendererComponent(params: {
           code,
           highlightBounds,
         )
+
+        if (typeof renderedCoreElement === 'string' || typeof renderedCoreElement === 'number') {
+          return <>{renderedCoreElement}</>
+        } else {
+          return renderedCoreElement
+        }
       }
     }
 
