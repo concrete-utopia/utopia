3f11053dc0e7baef036d820a30b19e66c28dd43f
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2021-07-08 12:55:57 +0200

    fixes for the PR

--- editor/src/components/canvas/controls/classname-select.tsx
+++ editor/src/components/canvas/controls/classname-select.tsx
@@ -13,6 +13,7 @@ import WindowedSelect, {
   ValueContainerProps,
 } from 'react-windowed-select'
 import type { StylesConfig } from 'react-select'
+import type { Option } from 'react-select/src/filters'
 import chroma from 'chroma-js'
 
 import * as EditorActions from '../../editor/actions/action-creators'
@@ -36,16 +37,21 @@ const TailWindOptions = TailWindList.classNames.map((className, index) => ({
   value: className,
 }))
 
-const DropdownIndicator = (props: IndicatorProps<TailWindOption, true>) => (
-  <components.DropdownIndicator {...props}>
-    <span> ↓ </span>
-  </components.DropdownIndicator>
+const DropdownIndicator = betterReactMemo(
+  'DropdownIndicator',
+  (props: IndicatorProps<TailWindOption, true>) => (
+    <components.DropdownIndicator {...props}>
+      <span> ↓ </span>
+    </components.DropdownIndicator>
+  ),
 )
 
 const ClearIndicator = () => null
 const IndicatorSeparator = () => null
 
-const NoOptionsMessage = (props: any) => <span {...props}>No results found</span>
+const NoOptionsMessage = betterReactMemo('NoOptionsMessage', (props: any) => (
+  <span {...props}>No results found</span>
+))
 
 const AngledStripe = styled.div({
   width: 5,
@@ -64,51 +70,58 @@ const getColorForCategory = (category: string) => {
   } else return 'pink'
 }
 
-const MultiValueContainer = (props: MultiValueProps<TailWindOption>) => {
-  const getStriped = (data: TailWindOption) => {
-    const categories = (data?.categories as Array<string>) ?? []
-    if (categories.length > 0) {
-      return categories.map((category, index) => (
-        <AngledStripe
-          key={data?.label ?? index}
-          style={{ backgroundColor: getColorForCategory(category) }}
-        />
-      ))
-    } else {
-      return null
-    }
-  }
+const MultiValueContainer = betterReactMemo(
+  'MultiValueContainer',
+  (props: MultiValueProps<TailWindOption>) => {
+    const { data } = props
+    const stripes: jsx.JSX.Element[] = React.useMemo(() => {
+      const categories = (data?.categories as Array<string>) ?? []
+      if (categories.length > 0) {
+        return categories.map((category, index) => (
+          <AngledStripe
+            key={data?.label ?? index}
+            style={{ backgroundColor: getColorForCategory(category) }}
+          />
+        ))
+      } else {
+        return []
+      }
+    }, [data])
 
-  return (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: 'black',
-      }}
-    >
-      <components.MultiValueContainer {...props} />
+    return (
       <div
         style={{
           display: 'flex',
-          height: 16,
-          paddingRight: 4,
-          paddingLeft: 2,
+          alignItems: 'center',
+          backgroundColor: 'black',
         }}
       >
-        {getStriped(props.data)}
+        <components.MultiValueContainer {...props} />
+        <div
+          style={{
+            display: 'flex',
+            height: 16,
+            paddingRight: 4,
+            paddingLeft: 2,
+          }}
+        >
+          {stripes}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  },
+)
 
-const ValueContainer = (props: ValueContainerProps<TailWindOption, true>) => {
-  return (
-    <div style={{ overflowX: 'scroll', flex: 1 }}>
-      <components.ValueContainer {...props} />
-    </div>
-  )
-}
+const ValueContainer = betterReactMemo(
+  'ValueContainer',
+  (props: ValueContainerProps<TailWindOption, true>) => {
+    return (
+      <div style={{ overflowX: 'scroll', flex: 1 }}>
+        <components.ValueContainer {...props} />
+      </div>
+    )
+  },
+)
 
 export const ClassNameSelect: React.FunctionComponent = betterReactMemo('ClassNameSelect', () => {
   const theme = useColorTheme()
@@ -157,129 +170,136 @@ export const ClassNameSelect: React.FunctionComponent = betterReactMemo('ClassNa
           value: name,
         }))
 
-  const colourStyles: StylesConfig = {
-    container: (styles: React.CSSProperties) => ({
-      // the outermost element. It contains the popup menu, so don't set a height on it!
-      // shouldn't contain any sizing
-      ...styles,
-      width: '100%',
-    }),
-    control: (styles) => ({
-      // need to remove styles here, since that implicitly sets a height of 38
-      // ...styles,
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      background: 'transparent',
-      outline: 'none',
-      ':focus-within': {
-        outline: 'none',
-        border: 'none',
-      },
-    }),
-    valueContainer: () => ({
-      // the container for added options (tags) and input
-      // sibling to indicatorsContainer
-      // default styles mess with layout, so ignore them
-      display: 'flex',
-      alignItems: 'center',
-      gap: 4,
-      paddingLeft: 4,
-      paddingRight: 4,
-      paddingTop: 0,
-      paddingBottom: 0,
-      maxWidth: 0,
-    }),
-
-    multiValue: () => {
-      return {
-        cursor: 'pointer',
+  const colourStyles: StylesConfig = React.useMemo(
+    () => ({
+      container: (styles: React.CSSProperties) => ({
+        // the outermost element. It contains the popup menu, so don't set a height on it!
+        // shouldn't contain any sizing
+        ...styles,
+        width: '100%',
+      }),
+      control: (styles) => ({
+        // need to remove styles here, since that implicitly sets a height of 38
+        // ...styles,
         display: 'flex',
         alignItems: 'center',
-        height: 18,
-        backgroundColor: '#191818',
-      }
-    },
-    multiValueLabel: () => ({
-      fontSize: 10,
-      padding: '2px 4px',
-      color: 'white',
-    }),
-    multiValueRemove: (styles: React.CSSProperties, { data }) => ({
-      width: 11,
-      display: 'flex',
-      paddingTop: 2,
-      opacity: 0.4,
-      color: data.color,
-      ':hover': {
-        opacity: 1,
-        backgroundColor: data.color,
-        color: 'white',
-      },
-    }),
-    input: () => {
-      return {
-        fontSize: 11,
-        color: theme.fg1.value,
-        letterSpacing: 0.3,
+        justifyContent: 'space-between',
         background: 'transparent',
-        display: 'flex',
-        alignItems: 'center',
-      }
-    },
-    indicatorsContainer: (styles) => ({
-      ...styles,
-      height: 20,
-    }),
-    option: (styles: React.CSSProperties, { data, isDisabled, isFocused, isSelected }) => {
-      // a single entry in the options list
-      const categories = data?.categories ?? []
-
-      let optionColor = chroma('black')
-      if (categories.length === 1) {
-        optionColor = chroma(theme.primary.value)
-      }
-
-      let backgroundColor: string | undefined = undefined
-      if (isFocused) {
-        backgroundColor = optionColor.alpha(0.1).css()
-      } else if (isSelected) {
-        backgroundColor = optionColor.css()
-      } else if (isDisabled) {
-        backgroundColor = undefined
-      }
-
-      let activeBackgroundColor =
-        !isDisabled && (isSelected ? data.color : optionColor.alpha(0.3).css())
-
-      let color: string = optionColor.css()
-      if (isSelected) {
-        if (chroma.contrast(optionColor, 'white') > 2) {
-          color = 'white'
-        } else {
-          color = 'black'
-        }
-      } else if (isDisabled) {
-        color = '#ccc'
-      }
-
-      return {
-        minHeight: 27,
-        display: 'flex',
-        alignItems: 'center',
-        paddingLeft: 8,
-        paddingRight: 8,
-        backgroundColor: backgroundColor,
-        color: color,
-        cursor: isDisabled ? 'not-allowed' : 'default',
-
-        ':active': {
-          ...(styles as any)[':active'],
-          backgroundColor: activeBackgroundColor,
+        outline: 'none',
+        ':focus-within': {
+          outline: 'none',
+          border: 'none',
         },
-      }
-    },
-  }
+      }),
+      valueContainer: () => ({
+        // the container for added options (tags) and input
+        // sibling to indicatorsContainer
+        // default styles mess with layout, so ignore them
+        display: 'flex',
+        alignItems: 'center',
+        gap: 4,
+        paddingLeft: 4,
+        paddingRight: 4,
+        paddingTop: 0,
+        paddingBottom: 0,
+        maxWidth: 0,
+      }),
+
+      multiValue: () => {
+        return {
+          cursor: 'pointer',
+          display: 'flex',
+          alignItems: 'center',
+          height: 18,
+          backgroundColor: '#191818',
+        }
+      },
+      multiValueLabel: () => ({
+        fontSize: 10,
+        padding: '2px 4px',
+        color: 'white',
+      }),
+      multiValueRemove: (styles: React.CSSProperties, { data }) => ({
+        width: 11,
+        display: 'flex',
+        paddingTop: 2,
+        opacity: 0.4,
+        color: data.color,
+        ':hover': {
+          opacity: 1,
+          backgroundColor: data.color,
+          color: 'white',
+        },
+      }),
+      input: () => {
+        return {
+          fontSize: 11,
+          color: theme.fg1.value,
+          letterSpacing: 0.3,
+          background: 'transparent',
+          display: 'flex',
+          alignItems: 'center',
+        }
+      },
+      indicatorsContainer: (styles) => ({
+        ...styles,
+        height: 20,
+      }),
+      option: (styles: React.CSSProperties, { data, isDisabled, isFocused, isSelected }) => {
+        // a single entry in the options list
+        const categories = data?.categories ?? []
+
+        let optionColor = chroma('black')
+        if (categories.length === 1) {
+          optionColor = chroma(theme.primary.value)
+        }
+
+        let backgroundColor: string | undefined = undefined
+        if (isFocused) {
+          backgroundColor = optionColor.alpha(0.1).css()
+        } else if (isSelected) {
+          backgroundColor = optionColor.css()
+        } else if (isDisabled) {
+          backgroundColor = undefined
+        }
+
+        let activeBackgroundColor =
+          !isDisabled && (isSelected ? data.color : optionColor.alpha(0.3).css())
+
+        let color: string = optionColor.css()
+        if (isSelected) {
+          if (chroma.contrast(optionColor, 'white') > 2) {
+            color = 'white'
+          } else {
+            color = 'black'
+          }
+        } else if (isDisabled) {
+          color = '#ccc'
+        }
+
+        return {
+          minHeight: 27,
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: 8,
+          paddingRight: 8,
+          backgroundColor: backgroundColor,
+          color: color,
+          cursor: isDisabled ? 'not-allowed' : 'default',
+
+          ':active': {
+            ...(styles as any)[':active'],
+            backgroundColor: activeBackgroundColor,
+          },
+        }
+      },
+    }),
+    [theme],
+  )
+
+  const filterOption = React.useCallback((option: Option, rawInput: string) => {
+    return createFilter({ ignoreAccents: false })(option, rawInput)
+  }, [])
 
   return (
     <div
@@ -295,7 +315,7 @@ export const ClassNameSelect: React.FunctionComponent = betterReactMemo('ClassNa
       }}
     >
       <WindowedSelect
-        filterOption={createFilter({ ignoreAccents: false })}
+        filterOption={filterOption}
         options={TailWindOptions}
         onChange={onChange}
         value={selectedValues}
