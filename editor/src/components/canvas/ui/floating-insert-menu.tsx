772cb6810e82da5b36f49fdd9e826200da9f4906
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-07-12 19:28:00 +0200

    Wrap in.. menu react select (#1486)
    * adding floating wrap in... overlay bascics
    
    * making the canvas clickable again
    
    * don't double-offset the ReactErrorOverlay
    
    * persisting the EditorMode we are in (for local development)
    
    * WRAP_IN_PICKER to actually open picker
    
    * populating with dummy data
    
    * adding group labels
    
    * HeadlessStringInput gains onEscape and onSubmitValue
    
    * using HeadlessStringInput, hiding insertion controls
    
    * hover color
    
    * Update floating-insert-menu.tsx
    
    * almost working wrap select
    
    * using the right imports object for insertion
    
    * listen to escape and enter keys in the floating insert popup
    
    * a flat list, to prepare for indexed access
    
    * move highlight to index-based (sad!)
    
    * key-based insertable item highlight
    
    * just a splash of memo
    
    * add back list filtering
    
    * removing group, ungroup->unwrap
    
    * wrap element now wraps in view again, no picker
    
    * wrap in (with picker): G, wrap in View: cmd + G
    
    * prevent default for shortcuts which found a relevant handler
    
    * apply a bit of theme
    
    * Update floating-insert-menu.tsx
    
    * Update performance-regression-tests.spec.tsx
    
    * Update floating-insert-menu.tsx
    
    * use importDetails() factory function
    
    * first version  of react-select as the floating insert menu
    
    * add a slight padding to the groups, move the group label to inline to save vertical space
    
    * memoize floating menu
    
    * removing unused components
    
    * adding // TODO Themes
    
    * create nice memoized themed componentSelectorStyles
    
    * sigh
    
    * Update floating-insert-menu.tsx
    
    * fixing todo, making memoized onChange
    
    * adding even more // ...styles,

--- editor/src/components/canvas/ui/floating-insert-menu.tsx
+++ editor/src/components/canvas/ui/floating-insert-menu.tsx
@@ -1,18 +1,12 @@
 /** @jsx jsx */
 import * as React from 'react'
 import { jsx } from '@emotion/react'
+import Select, { StylesConfig, ValueType } from 'react-select'
 
 import { betterReactMemo } from '../../../uuiui-deps'
 import { useEditorState, useRefEditorState } from '../../editor/store/store-hook'
 
-import styled from '@emotion/styled'
-import {
-  FlexColumn,
-  FlexRow,
-  HeadlessStringInput,
-  OnClickOutsideHOC,
-  useColorTheme,
-} from '../../../uuiui'
+import { FlexColumn, OnClickOutsideHOC, useColorTheme } from '../../../uuiui'
 import { usePossiblyResolvedPackageDependencies } from '../../editor/npm-dependency/npm-dependency'
 import {
   getComponentGroups,
@@ -31,40 +25,47 @@ import {
 import { emptyComments } from '../../../core/workers/parser-printer/parser-printer-comments'
 import { useHandleCloseOnESCOrEnter } from '../../inspector/common/inspector-utils'
 
-function useFocusOnMount<T extends HTMLElement>(): React.RefObject<T> {
-  const ref = React.useRef<T>(null)
-  React.useEffect(() => {
-    // eslint-disable-next-line no-unused-expressions
-    ref.current?.focus()
-  }, [ref])
-  return ref
-}
-
-type InsertMenuItem = InsertableComponent & {
+type InsertMenuItemValue = InsertableComponent & {
   source: InsertableComponentGroupType | null
   key: string
 }
 
-type InsertableComponentFlatList = Array<InsertMenuItem>
+type InsertMenuItem = {
+  label: string
+  value: InsertMenuItemValue
+}
+
+type InsertMenuItemGroup = {
+  label: string
+  options: Array<InsertMenuItem>
+}
+
+type InsertableComponentFlatList = Array<InsertMenuItemGroup>
 
 function convertInsertableComponentsToFlatList(
   insertableComponents: InsertableComponentGroup[],
 ): InsertableComponentFlatList {
   return insertableComponents.flatMap((componentGroup) => {
-    return componentGroup.insertableComponents.map(
-      (insertableComponent, index): InsertMenuItem => {
-        const source = index === 0 ? componentGroup.source : null
-        return {
-          ...insertableComponent,
-          key: `${getInsertableGroupLabel(componentGroup.source)}-${insertableComponent.name}`,
-          source: source,
-        }
-      },
-    )
+    return {
+      label: getInsertableGroupLabel(componentGroup.source),
+      options: componentGroup.insertableComponents.map(
+        (insertableComponent, index): InsertMenuItem => {
+          const source = index === 0 ? componentGroup.source : null
+          return {
+            label: insertableComponent.name,
+            value: {
+              ...insertableComponent,
+              key: `${getInsertableGroupLabel(componentGroup.source)}-${insertableComponent.name}`,
+              source: source,
+            },
+          }
+        },
+      ),
+    }
   })
 }
 
-function useGetInsertableComponents(filterString: string): InsertableComponentFlatList {
+function useGetInsertableComponents(): InsertableComponentFlatList {
   const dependencies = usePossiblyResolvedPackageDependencies()
 
   const { packageStatus, propertyControlsInfo, projectContents, fullPath } = useEditorState(
@@ -91,65 +92,155 @@ function useGetInsertableComponents(filterString: string): InsertableComponentFl
           dependencies,
           fullPath,
         ),
-      ).filter((insertableComponent) =>
-        insertableComponent.name.toLowerCase().includes(filterString.toLowerCase()),
       )
     }
-  }, [packageStatus, propertyControlsInfo, projectContents, dependencies, fullPath, filterString])
+  }, [packageStatus, propertyControlsInfo, projectContents, dependencies, fullPath])
 
   return insertableComponents
 }
 
-export const ListItem: React.FunctionComponent<{
-  highlighted: boolean
-  insertableComponent: InsertMenuItem
-  onClick: (insertableComponent: InsertMenuItem) => void
-  onMouseOver: (insertableComponent: InsertMenuItem) => void
-}> = betterReactMemo(
-  'ListItem',
-  ({ onClick, onMouseOver, insertableComponent, highlighted, ...props }) => {
-    const colorTheme = useColorTheme()
-    const handleClick = React.useCallback(() => {
-      onClick(insertableComponent)
-    }, [insertableComponent, onClick])
+function useComponentSelectorStyles(): StylesConfig {
+  const colorTheme = useColorTheme()
+  // componentSelectorStyles will only be recreated if the theme changes, otherwise we re-use the same object
+  return React.useMemo(
+    () => ({
+      container: (styles) => ({
+        // the outermost element. It contains the popup menu,  so don't set a height on it!
+        // shouldn't contain any sizing
+        // ...styles,
+        flexGrow: 1,
+        display: 'flex',
+        flexDirection: 'column',
+      }),
+      control: (styles) => ({
+        // need to remove styles here, since that implicitly sets a height of 38
+        // ...styles,
+        display: 'flex',
+        background: 'transparent',
+        outline: 'none',
+        ':focus-within': {
+          outline: 'none',
+          border: 'none',
+        },
+      }),
+      valueContainer: (styles) => ({
+        // the container for added options (tags) and input
+        // sibling to indicatorsContainer
+        // default styles mess with layout, so ignore them
+        // ...styles,
+        display: 'flex',
+        position: 'relative',
+        flexGrow: 1,
+        flexShrink: 0,
+        overflowX: 'scroll',
+        alignItems: 'center',
+        gap: 4,
+        paddingLeft: 4,
+        paddingRight: 4,
+        paddingTop: 0,
+        paddingBottom: 0,
+      }),
+      indicatorsContainer: (styles) => ({
+        // ...styles,
+        display: 'none',
+      }),
 
-    const handleMouseOver = React.useCallback(() => {
-      onMouseOver(insertableComponent)
-    }, [insertableComponent, onMouseOver])
-
-    return (
-      <div
-        onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        css={{
-          flex: '0 0 25px',
+      multiValue: (styles, { data }) => {
+        return {
+          // ...styles,
+          cursor: 'pointer',
           display: 'flex',
           alignItems: 'center',
-          borderRadius: 2,
-          paddingLeft: 4,
-          paddingRight: 4,
-          background: highlighted ? colorTheme.contextMenuHighlightBackground.value : 'transparent',
-          color: highlighted
-            ? colorTheme.contextMenuHighlightForeground.value
-            : colorTheme.contextMenuForeground.value,
-        }}
-        {...props}
-      />
-    )
-  },
-)
+        }
+      },
+      multiValueLabel: (styles, { data }) => ({
+        // ...styles,
+        fontSize: 10,
+        padding: '2px 4px',
+      }),
+      multiValueRemove: (styles, { data }) => ({
+        // ...styles,
+        width: 11,
+        display: 'flex',
+        paddingTop: 2,
+        opacity: 0.4,
+        color: data.color,
+        ':hover': {
+          opacity: 1,
+          backgroundColor: data.color,
+        },
+      }),
+      menu: (styles) => {
+        // the outer shell
+        return {
+          // ...styles,
+          boxShadow: 'none',
+          borderRadius: 0,
+          background: 'transparent',
+          overflowY: 'scroll',
+          flex: 1,
+        }
+      },
+      menuList: (styles) => {
+        // the list wrapper
+        return {
+          // ...styles,
+          position: 'relative',
+          maxHeight: 300,
+          padding: 4,
+          overflowY: 'scroll',
+        }
+      },
+      input: (styles) => {
+        return {
+          ...styles,
+          color: 'black',
+          fontSize: 11,
+          flexGrow: 1,
+          letterSpacing: 0.3,
+          background: 'transparent',
+          display: 'flex',
+          alignItems: 'center',
+        }
+      },
+      option: (styles, { data, isDisabled, isFocused, isSelected }) => {
+        // a single entry in the options list
 
-export const Subdued = styled.div({
-  color: 'hsl(0,0%,70%)',
-})
-
-const showInsertionOptions = false
-
-export var FloatingMenu = () => {
+        return {
+          ...styles,
+          height: 25,
+          display: 'flex',
+          alignItems: 'center',
+          paddingLeft: 8,
+          paddingRight: 8,
+          cursor: isDisabled ? 'not-allowed' : 'default',
+        }
+      },
+      group: () => {
+        return {
+          // ...styles,
+          paddingTop: 6,
+        }
+      },
+      groupHeading: (styles) => {
+        return {
+          // ...styles,
+          color: colorTheme.fg7.value,
+          height: 25,
+          right: 8,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+        }
+      },
+    }),
+    [colorTheme],
+  )
+}
+export var FloatingMenu = betterReactMemo('FloatingMenu', () => {
   const colorTheme = useColorTheme()
-  const [highlightedKey, setHighlightedKey] = React.useState<string | null>(null)
+  const componentSelectorStyles = useComponentSelectorStyles()
   const dispatch = useEditorState((store) => store.dispatch, 'FloatingMenu dispatch')
-  // TODO move onClickOutside to here as well?
   useHandleCloseOnESCOrEnter(
     React.useCallback(
       (key: 'Escape' | 'Enter') => {
@@ -158,47 +249,41 @@ export var FloatingMenu = () => {
       [dispatch],
     ),
   )
-  const inputRef = useFocusOnMount<HTMLInputElement>()
   const projectContentsRef = useRefEditorState((store) => store.editor.projectContents)
   const selectedViewsref = useRefEditorState((store) => store.editor.selectedViews)
-  const [filterString, setFilterString] = React.useState('')
-  const insertableComponents = useGetInsertableComponents(filterString)
-  const onFilterInput = React.useCallback((event: React.FormEvent<HTMLInputElement>) => {
-    setFilterString(event.currentTarget.value)
-  }, [])
+  const insertableComponents = useGetInsertableComponents()
 
-  const onClickElement = React.useCallback(
-    (insertableComponent: InsertableComponent) => {
-      const newUID = generateUidWithExistingComponents(projectContentsRef.current)
-      const newElement = jsxElement(
-        insertableComponent.element.name,
-        newUID,
-        setJSXAttributesAttribute(
-          insertableComponent.element.props,
-          'data-uid',
-          jsxAttributeValue(newUID, emptyComments),
-        ),
-        insertableComponent.element.children,
-      )
-      dispatch([
-        wrapInView(selectedViewsref.current, {
-          element: newElement,
-          importsToAdd: insertableComponent.importsToAdd,
-        }),
-        closeFloatingInsertMenu(),
-      ])
+  const onChange = React.useCallback(
+    (value: ValueType<InsertMenuItem>) => {
+      if (value != null && !Array.isArray(value)) {
+        const insertableComponent = (value as InsertMenuItem).value
+        const newUID = generateUidWithExistingComponents(projectContentsRef.current)
+        const newElement = jsxElement(
+          insertableComponent.element.name,
+          newUID,
+          setJSXAttributesAttribute(
+            insertableComponent.element.props,
+            'data-uid',
+            jsxAttributeValue(newUID, emptyComments),
+          ),
+          insertableComponent.element.children,
+        )
+        dispatch([
+          wrapInView(selectedViewsref.current, {
+            element: newElement,
+            importsToAdd: insertableComponent.importsToAdd,
+          }),
+          closeFloatingInsertMenu(),
+        ])
+      }
     },
     [dispatch, projectContentsRef, selectedViewsref],
   )
 
-  const onMouseOverElement = React.useCallback((insertableComponent: InsertMenuItem) => {
-    setHighlightedKey(insertableComponent.key)
-  }, [])
-
   return (
     <div
       style={{
-        backgroundColor: '#fefefe',
+        backgroundColor: '#fefefe', // TODO Theme
         position: 'relative',
         margin: 20,
         fontSize: 11,
@@ -206,101 +291,43 @@ export var FloatingMenu = () => {
     >
       <FlexColumn
         style={{
-          border: '1px solid hsl(0,0%,93%)',
-          borderRadius: 3,
-          background: colorTheme.neutralBackground.value,
-          boxShadow: '1px 1px 3px #00000022',
-          width: 220,
+          width: 280,
+          background: 'hsl(0,0%,96%)', // TODO Theme
+          border: '1px solid hsl(0,0%,93%)', // TODO Theme
+          borderRadius: 2,
+          minHeight: 300,
           overflow: 'hidden',
-          height: 300,
+          boxShadow: '0px 0px 4px 1px hsla(0,0%,30%,10%)', // TODO Theme
         }}
       >
-        <FlexRow
+        <div
           style={{
-            paddingLeft: 8,
-            minHeight: 34,
-            color: colorTheme.primary.value,
-            fontWeight: 600,
-          }}
-        >
-          Wrap in...
-        </FlexRow>
-        <FlexRow style={{ minHeight: 34 }}>
-          <HeadlessStringInput
-            ref={inputRef}
-            style={{
-              border: 'none',
-              height: 22,
-              paddingLeft: 4,
-              background: colorTheme.secondaryBackground.value,
-              flexGrow: 1,
-            }}
-            onInput={onFilterInput}
-            placeholder='Type to filter'
-          />
-        </FlexRow>
-        <FlexColumn
-          style={{
-            minHeight: 80,
-            overflowY: 'scroll',
+            display: 'flex',
             paddingLeft: 8,
             paddingRight: 8,
-            flexGrow: 1,
+            height: 34,
+            alignItems: 'center',
           }}
         >
-          {insertableComponents.map((insertableComponent, index) => {
-            return (
-              <React.Fragment key={insertableComponent.key}>
-                {insertableComponent.source != null ? (
-                  <Subdued>{getInsertableGroupLabel(insertableComponent.source)}</Subdued>
-                ) : null}
-                <ListItem
-                  key={insertableComponent.key}
-                  onClick={onClickElement}
-                  onMouseOver={onMouseOverElement}
-                  insertableComponent={insertableComponent}
-                  highlighted={highlightedKey === insertableComponent.key}
-                >
-                  {insertableComponent.name}
-                </ListItem>
-              </React.Fragment>
-            )
-          })}
-        </FlexColumn>
-        {showInsertionOptions ? (
-          <FlexColumn
-            style={{
-              borderTop: '1px solid hsl(0,0%,93%)',
-              minHeight: 48,
-              paddingTop: 4,
-              paddingLeft: 8,
-              paddingRight: 8,
-            }}
-          >
-            <Subdued
-              style={{
-                lineHeight: 1.3,
-                fontFamily: 'Inter',
-                fontSize: 10,
-              }}
-            >
-              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent:
-              'center'
-            </Subdued>
-            <FlexRow style={{ height: 34, gap: 8, padding: 0 }}>
-              <input type='checkbox' />
-              <label htmlFor='withContent'>Add content</label>
-            </FlexRow>
-            <FlexRow style={{ height: 34, gap: 8, padding: 0 }}>
-              <input type='checkbox' />
-              <label htmlFor='withContent'>Fixed dimensions</label>
-            </FlexRow>
-          </FlexColumn>
-        ) : null}
+          <b>Wrap In...</b>
+        </div>
+
+        <Select
+          autoFocus
+          isMulti={false}
+          controlShouldRenderValue={false}
+          hideSelectedOptions={false}
+          menuIsOpen
+          onChange={onChange}
+          options={insertableComponents}
+          placeholder='Search...'
+          styles={componentSelectorStyles}
+          tabSelectsValue={false}
+        />
       </FlexColumn>
     </div>
   )
-}
+})
 
 interface FloatingInsertMenuProps {}
 
