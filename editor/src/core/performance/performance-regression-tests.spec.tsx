c694fa5c37f2e6b4230efdb664ded6274c60a88a
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2021-07-30 16:16:32 +0100

    Stop removing functions from recorded props (#1634)
    * fix(editor) Testing out the removal of makeCanvasElementPropsSafe
    
    * fix(tests) Fixing another broken snapshot
    
    * fix(Inspector) Simplifying the event handler section of the inspector and adding more render count tests
    
    * chore(tests) Remove children from metadata in snapshot tests
    
    * fix(Inspector) use a React.useMemo and forEachRight

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -3,6 +3,7 @@ import {
   renderTestEditorWithCode,
   getPrintedUiJsCode,
   TestScenePath,
+  makeTestProjectCodeWithComponentInnards,
 } from '../../components/canvas/ui-jsx.test-utils'
 import { selectComponents, setProp_UNSAFE } from '../../components/editor/actions/action-creators'
 import * as EP from '../shared/element-path'
@@ -11,7 +12,7 @@ import { jsxAttributeValue } from '../shared/element-template'
 import { emptyComments } from '../workers/parser-printer/parser-printer-comments'
 
 describe('React Render Count Tests - ', () => {
-  it('Clicking on opacity slider', async () => {
+  it('Clicking on opacity slider with a simple project', async () => {
     const renderResult = await renderTestEditorWithCode(
       makeTestProjectCodeWithSnippet(`
       <View style={{ ...props.style }} data-uid='aaa'>
@@ -58,11 +59,70 @@ describe('React Render Count Tests - ', () => {
     )
 
     const renderCountAfter = renderResult.getNumberOfRenders()
-    expect(renderCountAfter - renderCountBefore).toBeGreaterThan(500) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThan(545) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(555)
+  })
+
+  it('Clicking on opacity slider with a less simple project', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithComponentInnards(`
+      const [i, setI] = React.useState(0)
+      return (
+        <View style={{ ...props.style }} data-uid='aaa'>
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 1, left: 52, top: 61, width: 256, height: 202 }}
+            data-uid='bbb'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+        </View>
+      )
+      `),
+    )
+    await renderResult.dispatch(
+      [selectComponents([EP.appendNewElementPath(TestScenePath, ['aaa'])], false)],
+      false,
+    )
+    expect(renderResult.renderedDOM.getByText('Opacity')).toBeDefined()
+
+    await renderResult.dispatch(
+      [selectComponents([EP.appendNewElementPath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const renderCountBefore = renderResult.getNumberOfRenders()
+
+    await renderResult.dispatch(
+      [
+        setProp_UNSAFE(
+          EP.appendNewElementPath(TestScenePath, ['aaa', 'bbb']),
+          PP.create(['style', 'opacity']),
+          jsxAttributeValue(0.3, emptyComments),
+        ),
+      ],
+      true,
+    )
+
+    expect(getPrintedUiJsCode(renderResult.getEditorState())).toEqual(
+      makeTestProjectCodeWithComponentInnards(`
+      const [i, setI] = React.useState(0)
+      return (
+        <View style={{ ...props.style }} data-uid='aaa'>
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 0.3, left: 52, top: 61, width: 256, height: 202 }}
+            data-uid='bbb'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+        </View>
+      )
+      `),
+    )
+
+    const renderCountAfter = renderResult.getNumberOfRenders()
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThan(550) // if this breaks, GREAT NEWS but update the test please :)
     expect(renderCountAfter - renderCountBefore).toBeLessThan(560)
   })
 
-  it('Changing the selected view', async () => {
+  it('Changing the selected view with a simple project', async () => {
     const renderResult = await renderTestEditorWithCode(
       makeTestProjectCodeWithSnippet(`
       <View style={{ ...props.style }} data-uid='aaa'>
@@ -111,7 +171,70 @@ describe('React Render Count Tests - ', () => {
     )
 
     const renderCountAfter = renderResult.getNumberOfRenders()
-    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(470) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(500) // if this breaks, GREAT NEWS but update the test please :)
     expect(renderCountAfter - renderCountBefore).toBeLessThan(510)
+  })
+
+  it('Changing the selected view with a less simple project', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithComponentInnards(`
+      const [i, setI] = React.useState(0)
+      return (
+        <View style={{ ...props.style }} data-uid='aaa'>
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 1, left: 52, top: 61, width: 256, height: 202 }}
+            data-uid='bbb'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 1, left: 152, top: 161, width: 256, height: 202 }}
+            data-uid='ccc'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+        </View>
+      )
+      `),
+    )
+    await renderResult.dispatch(
+      [selectComponents([EP.appendNewElementPath(TestScenePath, ['aaa'])], false)],
+      false,
+    )
+    expect(renderResult.renderedDOM.getByText('Opacity')).toBeDefined()
+
+    await renderResult.dispatch(
+      [selectComponents([EP.appendNewElementPath(TestScenePath, ['aaa', 'bbb'])], false)],
+      false,
+    )
+
+    const renderCountBefore = renderResult.getNumberOfRenders()
+
+    await renderResult.dispatch(
+      [selectComponents([EP.appendNewElementPath(TestScenePath, ['aaa', 'ccc'])], false)],
+      false,
+    )
+
+    expect(getPrintedUiJsCode(renderResult.getEditorState())).toEqual(
+      makeTestProjectCodeWithComponentInnards(`
+      const [i, setI] = React.useState(0)
+      return (
+        <View style={{ ...props.style }} data-uid='aaa'>
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 1, left: 52, top: 61, width: 256, height: 202 }}
+            data-uid='bbb'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+          <View
+            style={{ backgroundColor: '#DDDDDD', position: 'absolute', opacity: 1, left: 152, top: 161, width: 256, height: 202 }}
+            data-uid='ccc'
+            onMouseDown={() => setI((current) => current + 1)}
+          />
+        </View>
+      )
+      `),
+    )
+
+    const renderCountAfter = renderResult.getNumberOfRenders()
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(510) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(520)
   })
 })
