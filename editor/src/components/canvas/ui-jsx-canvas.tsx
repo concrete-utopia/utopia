a4e3fac9e6c08d4afa719d2f76b970da9c6ea848
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-08-24 15:22:38 +0200

    oops accidental log

--- editor/src/components/canvas/ui-jsx-canvas.tsx
+++ editor/src/components/canvas/ui-jsx-canvas.tsx
@@ -82,7 +82,6 @@ import { getParseSuccessOrTransientForFilePath, getValidElementPaths } from './c
 import { NO_OP } from '../../core/shared/utils'
 import { useTwind } from '../../core/tailwind/tailwind'
 import { atomWithPubSub, usePubSubAtomReadOnly } from '../../core/shared/atom-with-pub-sub'
-import { reduxDevtoolsLogMessage } from '../../core/shared/redux-devtools'
 
 applyUIDMonkeyPatch()
 
@@ -265,7 +264,6 @@ function useClearSpyMetadataOnRemount(
 export const UiJsxCanvas = betterReactMemo(
   'UiJsxCanvas',
   React.forwardRef<HTMLDivElement, UiJsxCanvasPropsWithErrorCallback>((props, ref) => {
-    reduxDevtoolsLogMessage('Canvas Render')
     const {
       offset,
       scale,
