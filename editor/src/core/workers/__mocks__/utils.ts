fe443443f7ee7116826a3650ae3249bccff90ff6
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-09-09 10:31:49 +0200

    Update utils.ts

--- editor/src/core/workers/__mocks__/utils.ts
+++ editor/src/core/workers/__mocks__/utils.ts
@@ -1,3 +1,7 @@
+/**
+ * We are mocking core/workers/utils.ts because they use es6 stuff `import.meta.url` for webpack. That is not compatible with Node.js (Jest).
+ */
+
 export function createTsWorker() {
   return null
 }
