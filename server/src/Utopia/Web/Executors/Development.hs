f98d9b9f475587969cf9cf51d41ea23d844b17c7
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2021-09-14 10:48:50 +0100

    Load an icon from the local dev server, not utopia.app. (#1813)
    - Fixes #1812.
    - Fixes `dummyUser` to take a root from, which is passed in
      `localAuthCodeCheck`.

--- server/src/Utopia/Web/Executors/Development.hs
+++ server/src/Utopia/Web/Executors/Development.hs
@@ -91,12 +91,12 @@ handleAuthCodeResponse action response = do
 localAuthURL :: Text
 localAuthURL = "/authenticate?code=logmein&state=shrugemoji"
 
-dummyUser :: UserDetails
-dummyUser = UserDetails { userDetailsUserId  = "1"
-                        , userDetailsEmail   = Just "team@utopia.app"
-                        , userDetailsName    = Just "Utopian Worker #296"
-                        , userDetailsPicture = Just "https://utopia.app/editor/avatars/utopino3.png"
-                        }
+dummyUser :: Text -> UserDetails
+dummyUser cdnRoot = UserDetails { userDetailsUserId  = "1"
+                                , userDetailsEmail   = Just "team@utopia.app"
+                                , userDetailsName    = Just "Utopian Worker #296"
+                                , userDetailsPicture = Just (cdnRoot <> "/editor/avatars/utopino3.png")
+                                }
 
 {-|
   Fallback for validating the authentication code in the case where Auth0 isn't setup locally.
@@ -106,7 +106,9 @@ localAuthCodeCheck "logmein" action = do
   sessionStore <- fmap _sessionState ask
   pool <- fmap _projectPool ask
   metrics <- fmap _databaseMetrics ask
-  successfulAuthCheck metrics pool sessionStore action dummyUser
+  portOfServer <- fmap _serverPort ask
+  let cdnRoot = "http://localhost:" <> show portOfServer
+  successfulAuthCheck metrics pool sessionStore action $ dummyUser cdnRoot
 localAuthCodeCheck _ action = do
   return $ action Nothing
 
