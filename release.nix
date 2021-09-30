8017db72f4f8742a64d34eb973ea5ce8e8fb5430
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2021-09-30 07:50:18 +0100

    chore(pr) Try a very recent commit. (#1871)

--- release.nix
+++ release.nix
@@ -7,9 +7,9 @@ let
 
   pkgs = import (builtins.fetchTarball {
     name = "nixos-master";
-    url = https://github.com/NixOS/nixpkgs/archive/6120ac5cd201f6cb593d1b80e861be0342495be9.tar.gz;
+    url = https://github.com/NixOS/nixpkgs/archive/62126f8c155d58f7836b2c3873a40e4ad00cf46e.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "04mrjxr1qsdcgcryx7yy72cgcw14c0770gfcgzrdfpnvmjdgbi9i";
+    sha256 = "0vzbm8hfc8nqp8nkw2riphgz1bbrrrx0m6717a2yyzxpg0na26wr";
   }) { inherit config; };
 in
   { 
