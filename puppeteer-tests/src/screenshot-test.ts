8bf591de3475bade8f65aab2ffda0c8118bc19ce
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2021-10-06 15:37:22 +0100

    fix(puppeteer-tests) Fixed a silly gitignore mistake

---
+++ puppeteer-tests/src/screenshot-test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+require('dotenv').config({ path: 'src/.env' })
+import { v4 as UUID } from 'uuid'
+import { initialiseTests, setupBrowser, uploadPNGtoAWS } from './utils'
+
+const PROJECT_ID = process.env.PROJECT_ID ?? 'cca5fbff'
+const BASE_EDITOR_URL = process.env.BASE_EDITOR_URL ?? `https://utopia.pizza`
+const EDITOR_URL = process.env.EDITOR_URL ?? `${BASE_EDITOR_URL}/project/${PROJECT_ID}`
+
+async function takeScreenshot() {
+  const { page, browser } = await setupBrowser(EDITOR_URL)
+  try {
+    await initialiseTests(page)
+
+    const imageFileName = `./app-screenshot-${UUID()}.png`
+    console.info(`Capturing screenshot with file name ${imageFileName}`)
+    await page.screenshot({
+      path: imageFileName,
+    })
+
+    const uploadResult = (await uploadPNGtoAWS(imageFileName)) ?? ''
+
+    console.info(`Screenshot captured`)
+    console.info(`::set-output name=screenshot:: ${uploadResult}`)
+  } catch (e) {
+    const errorMessage = `"There was an error with Puppeteer: ${e.name} – ${e.message}"`
+    console.info(`::set-output name=screenshot::${errorMessage}`)
+  } finally {
+    await browser.close()
+  }
+}
+
+takeScreenshot()
