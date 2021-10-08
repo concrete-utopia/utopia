638e5ca40edf4814b207ee77cc7116ee7bd212ae
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2021-10-08 15:53:23 +0100

    fix(inspector) Trim a few of the property control parser errors. (#1900)
    - For various parsers in `property-controls-parser.ts`, `css-utils.ts`
      and `value-parser-utils.ts` trim the length of their failure messages.

--- editor/src/utils/value-parser-utils.spec.ts
+++ editor/src/utils/value-parser-utils.spec.ts
@@ -3,12 +3,12 @@ import { left, right } from '../core/shared/either'
 
 describe('parseBoolean', () => {
   it('returns a left for something which is not a boolean', () => {
-    expect(parseBoolean(9)).toEqual(left(descriptionParseError('Value is not a boolean.')))
-    expect(parseBoolean('hat')).toEqual(left(descriptionParseError('Value is not a boolean.')))
-    expect(parseBoolean({})).toEqual(left(descriptionParseError('Value is not a boolean.')))
-    expect(parseBoolean([])).toEqual(left(descriptionParseError('Value is not a boolean.')))
-    expect(parseBoolean(null)).toEqual(left(descriptionParseError('Value is not a boolean.')))
-    expect(parseBoolean(undefined)).toEqual(left(descriptionParseError('Value is not a boolean.')))
+    expect(parseBoolean(9)).toEqual(left(descriptionParseError('Not a boolean.')))
+    expect(parseBoolean('hat')).toEqual(left(descriptionParseError('Not a boolean.')))
+    expect(parseBoolean({})).toEqual(left(descriptionParseError('Not a boolean.')))
+    expect(parseBoolean([])).toEqual(left(descriptionParseError('Not a boolean.')))
+    expect(parseBoolean(null)).toEqual(left(descriptionParseError('Not a boolean.')))
+    expect(parseBoolean(undefined)).toEqual(left(descriptionParseError('Not a boolean.')))
   })
   it('returns a right for something which is a boolean', () => {
     expect(parseBoolean(true)).toEqual(right(true))
@@ -18,12 +18,12 @@ describe('parseBoolean', () => {
 
 describe('parseNumber', () => {
   it('returns a left for something which is not a number', () => {
-    expect(parseNumber('hat')).toEqual(left(descriptionParseError('Value is not a number.')))
-    expect(parseNumber(true)).toEqual(left(descriptionParseError('Value is not a number.')))
-    expect(parseNumber({})).toEqual(left(descriptionParseError('Value is not a number.')))
-    expect(parseNumber([])).toEqual(left(descriptionParseError('Value is not a number.')))
-    expect(parseNumber(null)).toEqual(left(descriptionParseError('Value is not a number.')))
-    expect(parseNumber(undefined)).toEqual(left(descriptionParseError('Value is not a number.')))
+    expect(parseNumber('hat')).toEqual(left(descriptionParseError('Not a number.')))
+    expect(parseNumber(true)).toEqual(left(descriptionParseError('Not a number.')))
+    expect(parseNumber({})).toEqual(left(descriptionParseError('Not a number.')))
+    expect(parseNumber([])).toEqual(left(descriptionParseError('Not a number.')))
+    expect(parseNumber(null)).toEqual(left(descriptionParseError('Not a number.')))
+    expect(parseNumber(undefined)).toEqual(left(descriptionParseError('Not a number.')))
   })
   it('returns a right for something which is a number', () => {
     expect(parseNumber(1)).toEqual(right(1))
@@ -36,12 +36,12 @@ describe('parseNumber', () => {
 
 describe('parseString', () => {
   it('returns a left for something which is not a string', () => {
-    expect(parseString(9)).toEqual(left(descriptionParseError('Value is not a string.')))
-    expect(parseString(true)).toEqual(left(descriptionParseError('Value is not a string.')))
-    expect(parseString({})).toEqual(left(descriptionParseError('Value is not a string.')))
-    expect(parseString([])).toEqual(left(descriptionParseError('Value is not a string.')))
-    expect(parseString(null)).toEqual(left(descriptionParseError('Value is not a string.')))
-    expect(parseString(undefined)).toEqual(left(descriptionParseError('Value is not a string.')))
+    expect(parseString(9)).toEqual(left(descriptionParseError('Not a string.')))
+    expect(parseString(true)).toEqual(left(descriptionParseError('Not a string.')))
+    expect(parseString({})).toEqual(left(descriptionParseError('Not a string.')))
+    expect(parseString([])).toEqual(left(descriptionParseError('Not a string.')))
+    expect(parseString(null)).toEqual(left(descriptionParseError('Not a string.')))
+    expect(parseString(undefined)).toEqual(left(descriptionParseError('Not a string.')))
   })
   it('returns a right for something which is a string', () => {
     expect(parseString('')).toEqual(right(''))
