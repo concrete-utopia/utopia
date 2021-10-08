a330d2aff1fb6572cec5d29ba8299691fe9ec16a
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-10-08 12:04:41 +0200

    update render count

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -241,7 +241,7 @@ describe('React Render Count Tests - ', () => {
     )
 
     const renderCountAfter = renderResult.getNumberOfRenders()
-    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(2485) // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toBeLessThan(2495)
+    expect(renderCountAfter - renderCountBefore).toBeGreaterThanOrEqual(2490) // if this breaks, GREAT NEWS but update the test please :)
+    expect(renderCountAfter - renderCountBefore).toBeLessThan(2505)
   })
 })
