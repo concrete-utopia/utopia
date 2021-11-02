4b8f65f14d2e8acfa079bb483afd6bd61b16c7b8
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2021-11-02 16:42:07 +0100

    cleaner type

--- editor/src/core/third-party/third-party-controls.ts
+++ editor/src/core/third-party/third-party-controls.ts
@@ -1,11 +1,10 @@
 import type { PropertyControls } from 'utopia-api'
+import type { PropertyControlsInfo } from '../../components/custom-code/code-file'
 import { AntdControls } from '../property-controls/third-party-property-controls/antd-controls'
 import { ReactThreeFiberControls } from '../property-controls/third-party-property-controls/react-three-fiber-controls'
 import { UtopiaApiControls } from './utopia-api-controls'
 
-export const DefaultThirdPartyControlDefinitions: {
-  [packageName: string]: { [componentName: string]: PropertyControls }
-} = {
+export const DefaultThirdPartyControlDefinitions: PropertyControlsInfo = {
   '@react-three/fiber': ReactThreeFiberControls,
   antd: AntdControls,
   'utopia-api': UtopiaApiControls,
