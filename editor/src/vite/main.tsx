4052e37155332312dda89f67078d1620bf209a86
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-01-03 12:09:56 +0100

    remove canvas-only entry point

--- editor/src/vite/main.tsx
+++ editor/src/vite/main.tsx
@@ -1,11 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-// import { EditorRoot } from './editor-fast-entry'
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <EditorRoot />
-//   </React.StrictMode>,
-//   document.getElementById('utopia-editor-root'),
-// )
 import '../templates/editor-entry-point'
