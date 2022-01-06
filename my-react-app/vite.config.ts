aa9e80e4ad481fce2c633ae5b083222e73476063
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2022-01-06 11:31:32 +0100

    hahahahahahaha

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -1,7 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  root: './src/',
+  base: '/editor/',
+  define: {
+    global: '{}',
+    module: '{}',
+    exports: '{}',
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#big-list-of-options
+      input: {
+        index: resolve(__dirname, 'src/main.tsx'),
+      },
+    },
+  },
 })
