220a2993b90f6a96863af3a5a9f16f387cf4b015
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-01-10 12:15:43 +0100

    latest vite-rollup config

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -3,45 +3,57 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { injectHtml } from 'vite-plugin-html'
 
-export default defineConfig({
-  plugins: [react(), injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } })],
-  root: './src/vite',
-  publicDir: '../../resources/editor',
-  server: {
-    port: 8088,
-    host: '0.0.0.0',
-    fs: {
-      allow: [
-        '../..',
-        '../../../website-next/node_modules/',
-        '../../../website-next/components/common/',
-        '../../../utopia-api/',
-        '../../../utopia-vscode-common/',
-      ],
-    },
-  },
-  base: '/editor/',
-  define: {
-    global: '{}',
-    module: '{}',
-    exports: '{}',
-    'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
-    'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
-    'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
-    'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
-    'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
-    'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
-    'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
-  },
-  build: {
-    target: 'esnext',
-    outDir: resolve(__dirname, 'lib2/editor'),
-    rollupOptions: {
-      // https://rollupjs.org/guide/en/#big-list-of-options
-      input: {
-        index: resolve(__dirname, 'src/vite/main.tsx'),
-        'vscode-outer': resolve(__dirname, 'src/vite/vscode-outer/vscode-editor-outer-iframe.tsx'),
+// import replace from '@rollup/plugin-replace'
+
+export default defineConfig(({ mode }) => {
+  const isDevEnv = mode === 'development'
+  return {
+    plugins: [
+      isDevEnv && react(),
+      injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } }),
+    ],
+    root: './src/vite',
+    publicDir: '../../resources/editor',
+    server: {
+      port: 8088,
+      host: '0.0.0.0',
+      fs: {
+        allow: [
+          '../..',
+          '../../../website-next/node_modules/',
+          '../../../website-next/components/common/',
+          '../../../utopia-api/',
+          '../../../utopia-vscode-common/',
+        ],
       },
     },
-  },
+    base: '/editor/',
+    define: {
+      'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
+      'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
+      'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
+      'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
+      'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
+      'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
+      'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
+    },
+    build: {
+      target: 'esnext',
+      outDir: resolve(__dirname, 'lib2/editor'),
+      commonjsOptions: {},
+      sourcemap: true,
+      compact: true,
+      minify: 'terser',
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        plugins: [
+          // replace({ 'process.env.NODE_ENV': 'production' }),
+        ],
+        input: {
+          index: resolve(__dirname, 'src/vite/index.html'),
+          'vscode-outer': resolve(__dirname, 'src/vite/vscode-outer/index.html'),
+        },
+      },
+    },
+  }
 })
