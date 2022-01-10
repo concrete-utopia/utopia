44dad05e0192119c624d2eed6f94ea642f621acd
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2022-01-10 12:29:07 +0000

    chore(tests) Use fake timers in tests rather than async delays

--- editor/src/core/shared/timed-cache-map.spec.ts
+++ editor/src/core/shared/timed-cache-map.spec.ts
@@ -1,9 +1,10 @@
-import { delay } from '../../utils/utils.test-utils'
 import { TimedCacheMap } from './timed-cache-map'
 
+jest.useFakeTimers()
+
 describe('Adding values to a TimedCacheMap', () => {
-  it('Retains all values if none have gone stale', async () => {
-    const timeToClean = 20
+  it('Retains all values if none have gone stale', () => {
+    const timeToClean = 200
     const timeToStale = 60000
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
     const input = [
@@ -15,13 +16,13 @@ describe('Adding values to a TimedCacheMap', () => {
       cache.set(k, v)
     })
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     const result = Array.from(cache.entries())
     expect(result).toEqual(input)
   })
 
-  it('Removes all stale values', async () => {
+  it('Removes all stale values', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
@@ -34,38 +35,38 @@ describe('Adding values to a TimedCacheMap', () => {
       cache.set(k, v)
     })
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // All still there
     const result1 = Array.from(cache.entries())
     expect(result1).toEqual(input)
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // Ensure one specific value remains
     cache.get('b')
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // All others have gone
     const result2 = Array.from(cache.entries())
     expect(result2).toEqual([['b', 'second']])
   })
 
-  it('Calling cache.get() retains a value', async () => {
+  it('Calling cache.get() retains a value', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
     cache.set('a', 'first')
     cache.set('b', 'second')
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // Still there
     const result1 = cache.get('a')
     expect(result1).toEqual('first')
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // Still there after the other value has gone stale
     const result2 = cache.get('a')
@@ -75,19 +76,19 @@ describe('Adding values to a TimedCacheMap', () => {
     expect(expectedMissing).toBeUndefined()
   })
 
-  it('Calling cache.set() retains a value', async () => {
+  it('Calling cache.set() retains a value', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
     cache.set('a', 'first')
     cache.set('b', 'second')
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // Update the value
     cache.set('a', 'third')
 
-    await delay(timeToClean + 10)
+    jest.advanceTimersByTime(timeToClean + 10)
 
     // Still there after the other value has gone stale
     const result = cache.get('a')
@@ -119,7 +120,7 @@ describe('Adding values to a TimedCacheMap', () => {
     expect(result2).toEqual([])
   })
 
-  it('Calling cache.entries() behaves as expected', async () => {
+  it('Calling cache.entries() behaves as expected', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
@@ -135,13 +136,13 @@ describe('Adding values to a TimedCacheMap', () => {
     const result1 = Array.from(cache.entries())
     expect(result1).toEqual(input)
 
-    await delay(timeToStale + 10)
+    jest.advanceTimersByTime(timeToStale + 10)
 
     const result2 = Array.from(cache.entries())
     expect(result2).toEqual([])
   })
 
-  it('Calling cache.keys() behaves as expected', async () => {
+  it('Calling cache.keys() behaves as expected', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
@@ -157,13 +158,13 @@ describe('Adding values to a TimedCacheMap', () => {
     const result1 = Array.from(cache.keys())
     expect(result1).toEqual(['a', 'b', 'c'])
 
-    await delay(timeToStale + 10)
+    jest.advanceTimersByTime(timeToStale + 10)
 
     const result2 = Array.from(cache.keys())
     expect(result2).toEqual([])
   })
 
-  it('Calling cache.values() behaves as expected', async () => {
+  it('Calling cache.values() behaves as expected', () => {
     const timeToClean = 20
     const timeToStale = timeToClean * 2
     let cache = new TimedCacheMap<string, string>(timeToStale, timeToClean)
@@ -179,7 +180,7 @@ describe('Adding values to a TimedCacheMap', () => {
     const result1 = Array.from(cache.values())
     expect(result1).toEqual(['first', 'second', 'third'])
 
-    await delay(timeToStale + 10)
+    jest.advanceTimersByTime(timeToStale + 10)
 
     const result2 = Array.from(cache.values())
     expect(result2).toEqual([])
