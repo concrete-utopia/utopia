2cc18e224c3cae402d182ca819919ed7d742435a
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-01-14 14:58:44 +0100

    removing rollup plugins and config

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -1,94 +1,39 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { injectHtml } from 'vite-plugin-html'
-import { visualizer } from 'rollup-plugin-visualizer'
-import worker from 'rollup-plugin-workers'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import inject from '@rollup/plugin-inject'
 
-export default defineConfig(({ mode }) => {
-  function createManualChunks(id, { getModuleInfo }) {
-    if (id.includes('node_modules')) {
-      const mod = getModuleInfo(id)
-      if (id.includes('@babel/standalone')) {
-        return 'babel'
-      }
-      if (id.includes('eslint')) {
-        // console.log('getModuleInfo(id)', id, mod?.importers)
-        return 'eslint'
-      }
-      return 'vendor'
-      // } else if (id.includes('uuiui')) {
-      //   return 'uuiui'
-    }
-  }
-
-  const isDevEnv = mode === 'development'
-  return {
-    plugins: [
-      isDevEnv && react(),
-      injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } }),
-    ],
-    root: './src/vite',
-    publicDir: '../../resources/editor',
-    server: {
-      port: 8088,
-      host: '0.0.0.0',
-      fs: {
-        allow: [
-          '../..',
-          '../../../website-next/node_modules/',
-          '../../../website-next/components/common/',
-          '../../../utopia-api/',
-          '../../../utopia-vscode-common/',
-        ],
-      },
+export default defineConfig({
+  plugins: [react(), injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } })],
+  root: './src/vite',
+  publicDir: '../../resources/editor',
+  server: {
+    port: 8088,
+    host: '0.0.0.0',
+    fs: {
+      allow: [
+        '../..',
+        '../../../website-next/node_modules/',
+        '../../../website-next/components/common/',
+        '../../../utopia-api/',
+        '../../../utopia-vscode-common/',
+      ],
     },
-    base: '/editor/',
-    define: {
-      'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
-      'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
-      'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
-      'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
-      'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
-      'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
-      'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
-    },
-    optimizeDeps: {
-      // This is a workaround for an apparent issue in the vite react plugin, which leads to
-      // "react/jsx-runtime" potentially being an undiscoverable dependency, meaning it won't be
-      // optimised, leading to a runtime error that "module is not defined"
-      // https://github.com/vitejs/vite/issues/6215
-      include: ['react/jsx-runtime'],
-    },
-    build: {
-      target: 'esnext',
-      outDir: resolve(__dirname, 'lib'),
-      commonjsOptions: {},
-      sourcemap: true,
-      compact: true,
-      minify: 'terser',
-      rollupOptions: {
-        plugins: [
-          inject({
-            modules: { Buffer: ['buffer', 'Buffer'] },
-          }),
-          nodeResolve({
-            moduleDirectories: module.paths,
-          }),
-          worker(),
-          // visualizer(),
-          // replace({ 'process.env.NODE_ENV': 'production' }),
-        ],
-        input: {
-          index: resolve(__dirname, 'src/vite/index.html'),
-          'vscode-outer': resolve(__dirname, 'src/vite/vscode-outer/index.html'),
-        },
-        // output: {
-        //   manualChunks: createManualChunks,
-        // },
-      },
-    },
-  }
+  },
+  base: '/editor/',
+  define: {
+    'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
+    'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
+    'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
+    'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
+    'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
+    'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
+    'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
+  },
+  optimizeDeps: {
+    // This is a workaround for an apparent issue in the vite react plugin, which leads to
+    // "react/jsx-runtime" potentially being an undiscoverable dependency, meaning it won't be
+    // optimised, leading to a runtime error that "module is not defined"
+    // https://github.com/vitejs/vite/issues/6215
+    include: ['react/jsx-runtime'],
+  },
 })
