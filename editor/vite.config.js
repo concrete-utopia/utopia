94742dffdb0d9710d6983d84ba951c7cec94dfe2
Rheeseyb <1044774+Rheeseyb@users.noreply.github.com>
2022-01-17 12:06:33 +0000

    chore(vite) Restoring the current rollup config, without breaking dev

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -1,39 +1,66 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { injectHtml } from 'vite-plugin-html'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
-export default defineConfig({
-  plugins: [react(), injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } })],
-  root: './src/vite',
-  publicDir: '../../resources/editor',
-  server: {
-    port: 8088,
-    host: '0.0.0.0',
-    fs: {
-      allow: [
-        '../..',
-        '../../../website-next/node_modules/',
-        '../../../website-next/components/common/',
-        '../../../utopia-api/',
-        '../../../utopia-vscode-common/',
-      ],
+export default defineConfig(({ mode }) => {
+  const isDevEnv = mode === 'development'
+  return {
+    plugins: [
+      isDevEnv && react(),
+      injectHtml({ data: { UTOPIA_SHA: process.env.REACT_APP_COMMIT_HASH } }),
+    ],
+    root: './src/vite',
+    publicDir: '../../resources/editor',
+    server: {
+      port: 8088,
+      host: '0.0.0.0',
+      fs: {
+        allow: [
+          '../..',
+          '../../../website-next/node_modules/',
+          '../../../website-next/components/common/',
+          '../../../utopia-api/',
+          '../../../utopia-vscode-common/',
+        ],
+      },
     },
-  },
-  base: '/editor/',
-  define: {
-    'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
-    'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
-    'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
-    'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
-    'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
-    'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
-    'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
-  },
-  optimizeDeps: {
-    // This is a workaround for an apparent issue in the vite react plugin, which leads to
-    // "react/jsx-runtime" potentially being an undiscoverable dependency, meaning it won't be
-    // optimised, leading to a runtime error that "module is not defined"
-    // https://github.com/vitejs/vite/issues/6215
-    include: ['react/jsx-runtime'],
-  },
+    base: '/editor/',
+    define: {
+      'process.env.NODE_ENV': `"${process.env.APP_ENV}"`,
+      'process.env.REACT_APP_ENVIRONMENT_CONFIG': `"${process.env.REACT_APP_ENVIRONMENT_CONFIG}"`,
+      'process.env.REACT_APP_AUTH0_REDIRECT_URI': `"${process.env.REACT_APP_AUTH0_REDIRECT_URI}"`,
+      'process.env.REACT_APP_AUTH0_CLIENT_ID': `"${process.env.REACT_APP_AUTH0_CLIENT_ID}"`,
+      'process.env.REACT_APP_AUTH0_ENDPOINT': `"${process.env.REACT_APP_AUTH0_ENDPOINT}"`,
+      'process.env.GOOGLE_WEB_FONTS_KEY': `"${process.env.GOOGLE_WEB_FONTS_KEY}"`,
+      'process.env.REACT_APP_COMMIT_HASH': `"${process.env.REACT_APP_COMMIT_HASH}"`,
+    },
+    optimizeDeps: {
+      // This is a workaround for an apparent issue in the vite react plugin, which leads to
+      // "react/jsx-runtime" potentially being an undiscoverable dependency, meaning it won't be
+      // optimised, leading to a runtime error that "module is not defined"
+      // https://github.com/vitejs/vite/issues/6215
+      include: ['react/jsx-runtime'],
+    },
+    build: {
+      target: 'esnext',
+      outDir: resolve(__dirname, 'lib'),
+      commonjsOptions: {},
+      sourcemap: true,
+      compact: true,
+      minify: 'terser',
+      rollupOptions: {
+        plugins: [
+          nodeResolve({
+            moduleDirectories: module.paths,
+          }),
+        ],
+        input: {
+          index: resolve(__dirname, 'src/vite/index.html'),
+          'vscode-outer': resolve(__dirname, 'src/vite/vscode-outer/index.html'),
+        },
+      },
+    },
+  }
 })
