a8461e85bcb674ea5be98afa4b125b8b708dfea6
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-02-03 21:30:08 +0100

    Set pinned nixpkgs to newer version

--- release.nix
+++ release.nix
@@ -7,9 +7,9 @@ let
 
   pkgs = import (builtins.fetchTarball {
     name = "nixos-master";
-    url = https://github.com/NixOS/nixpkgs/archive/f513127ffe5e3f5225d814933d554488c9f33c5c.tar.gz;
+    url = https://github.com/NixOS/nixpkgs/archive/43e3b6af08f29c4447a6073e3d5b86a4f45dd420.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "18l6qn7plavxhw6h1xpg7h1szjc0bvqdy1wfzhc2zqw9j8890a0j";
+    sha256 = "11hhdifv8nn56qakw9n37k7vcrlp0ra77dzvbklr9q5ygf0mh75h";
   }) { inherit config; };
 in
   { 
