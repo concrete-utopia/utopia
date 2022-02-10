04c956eca8a9c727dff92cff823dc3cf3554c89c
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-02-10 17:32:07 +0100

    show the selection outline

--- editor/src/components/canvas/controls/selection-outline-control2.tsx
+++ editor/src/components/canvas/controls/selection-outline-control2.tsx
@@ -1,5 +1,43 @@
 import React from 'react'
+import * as EP from '../../../core/shared/element-path'
+import { windowPoint } from '../../../core/shared/math-utils'
+import { useEditorState } from '../../editor/store/store-hook'
+import { windowToCanvasCoordinates } from '../dom-lookup'
 
-export const SelectionOutlineControl2 = () => {
-  return <div>hello</div>
-}
+export const SelectionOutlineControl2 = React.memo(() => {
+  const selectedElements = useEditorState(
+    (store) => store.editor.selectedViews,
+    'SelectionOutlineControl2',
+  )
+  const canvasOffset = useEditorState(
+    (store) => store.editor.canvas.roundedCanvasOffset,
+    'SelectionOutlineControl2 canvasOffset',
+  )
+  if (selectedElements.length === 1) {
+    const target = selectedElements[0]
+    const htmlElement = document.querySelector(`*[data-paths~="${EP.toString(target)}"]`)
+    const frame = htmlElement?.getBoundingClientRect()
+
+    if (frame == null) {
+      return null
+    }
+    const frameInCanvasCoords = windowToCanvasCoordinates(
+      1,
+      canvasOffset,
+      windowPoint({ x: frame.x, y: frame.y }),
+    )
+    return (
+      <div
+        style={{
+          position: 'absolute',
+          left: frameInCanvasCoords.canvasPositionRounded.x + canvasOffset.x,
+          top: frameInCanvasCoords.canvasPositionRounded.y + canvasOffset.y,
+          width: frame.width,
+          height: frame.height,
+          backgroundColor: 'hotpink',
+        }}
+      ></div>
+    )
+  }
+  return null
+})
