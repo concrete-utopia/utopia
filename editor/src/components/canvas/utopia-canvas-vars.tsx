42b6d59339e6064ffbb224dd48b638e40301212e
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2022-02-21 19:07:40 +0100

    using a useSelectorWithCallback in UtopiaCanvasVarStyleTag

--- editor/src/components/canvas/utopia-canvas-vars.tsx
+++ editor/src/components/canvas/utopia-canvas-vars.tsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import { CanvasScale, CanvasScrollOffset } from '../../utils/global-positions'
-import { useEditorState } from '../editor/store/store-hook'
+import { useSelectorWithCallback } from '../editor/store/store-hook'
 
 export const UtopiaCanvasVarStyleTag = React.memo(() => {
-  const roundedCanvasOffset = useEditorState(
-    (store) => store.editor.canvas.roundedCanvasOffset,
-    'DesignPanelRoot roundedCanvasOffset',
-  )
-  const canvasScale = useEditorState(
-    (store) => store.editor.canvas.scale,
-    'DesignPanelRoot roundedCanvasOffset',
+  const styleTagRef = React.useRef<HTMLStyleElement>(null)
+
+  useSelectorWithCallback(
+    (store) => ({
+      roundedCanvasOffset: store.editor.canvas.roundedCanvasOffset,
+      canvasScale: store.editor.canvas.scale,
+    }),
+    ({ roundedCanvasOffset, canvasScale }) => {
+      // To make sure we are changing these global variables at the exact same time we are changing the css vars, we are mutating all of them here, at the same time
+      CanvasScrollOffset.x = roundedCanvasOffset.x
+      CanvasScrollOffset.y = roundedCanvasOffset.y
+      CanvasScale.current = canvasScale
+      if (styleTagRef.current != null) {
+        styleTagRef.current.innerHTML = `
+        .utopia-canvas-var-container {
+          --utopia-canvas-offset-x: ${roundedCanvasOffset.x}px;
+          --utopia-canvas-offset-y: ${roundedCanvasOffset.y}px;
+        }
+      `
+      }
+    },
+    true,
   )
 
-  CanvasScrollOffset.x = roundedCanvasOffset.x
-  CanvasScrollOffset.y = roundedCanvasOffset.y
-  CanvasScale.current = canvasScale
-  return (
-    <style>{`
-  .utopia-canvas-var-container {
-    --utopia-canvas-offset-x: ${roundedCanvasOffset.x}px;
-    --utopia-canvas-offset-y: ${roundedCanvasOffset.y}px;
-  }
-`}</style>
-  )
+  return <style ref={styleTagRef} />
 })
