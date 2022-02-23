2b874a69f09044ded3c884c31b1e74eb5a4206b7
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-02-23 17:56:19 +0100

    Feature/canvas flex resize control (#2104)
    * feature(canvas) new flex resize controls
    
    * update test
    
    * update render count test
    
    * fix target property
    
    * minor change for multiselect resize

--- editor/src/components/canvas/controls/new-canvas-controls.tsx
+++ editor/src/components/canvas/controls/new-canvas-controls.tsx
@@ -59,6 +59,7 @@ import { LayoutTargetableProp } from '../../../core/layout/layout-helpers-new'
 import { getDragStateStart } from '../canvas-utils'
 import { AbsoluteResizeControl } from './select-mode/absolute-resize-control'
 import { OutlineControl } from './select-mode/simple-outline-control'
+import { FlexResizeControl } from './select-mode/flex-resize-control'
 
 export const CanvasControlsContainerID = 'new-canvas-controls-container'
 
@@ -411,6 +412,10 @@ const NewCanvasControlsInner = (props: NewCanvasControlsInnerProps) => {
       {when(
         isFeatureEnabled('Canvas Absolute Resize Controls'),
         <AbsoluteResizeControl localSelectedElements={localSelectedViews} />,
+      )}
+      {when(
+        isFeatureEnabled('Canvas Absolute Resize Controls'),
+        <FlexResizeControl localSelectedElements={localSelectedViews} />,
       )}
     </div>
   )
