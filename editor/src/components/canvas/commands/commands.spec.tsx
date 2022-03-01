a770d7af51fb28bd14d63c96f4946731ecc59e0c
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-03-01 15:43:46 +0100

    AdjustNumberProperty command test

--- editor/src/components/canvas/commands/commands.spec.tsx
+++ editor/src/components/canvas/commands/commands.spec.tsx
@@ -1,10 +1,13 @@
 import { createBuiltInDependenciesList } from '../../../core/es-modules/package-manager/built-in-dependencies-list'
 import * as EP from '../../../core/shared/element-path'
+import { getNumberPropertyFromProps } from '../../../core/shared/jsx-attributes'
 import { complexDefaultProjectPreParsed } from '../../../sample-projects/sample-project-utils.test-utils'
 import { selectComponents, setFocusedElement } from '../../editor/actions/action-creators'
+import { withUnderlyingTargetFromEditorState } from '../../editor/store/editor-state'
+import { stylePropPathMappingFn } from '../../inspector/common/property-path-hooks'
 import { renderTestEditorWithModel } from '../ui-jsx.test-utils'
-import { runWildcardPatch } from './command-runners'
-import { applyStatePatches, wildcardPatch } from './commands'
+import { runAdjustNumberProperty, runWildcardPatch } from './command-runners'
+import { adjustNumberProperty, applyStatePatches, wildcardPatch } from './commands'
 
 describe('wildcardPatch', () => {
   it('works for a basic pinned element', async () => {
@@ -34,5 +37,64 @@ describe('wildcardPatch', () => {
       [result.editorStatePatch],
     )
     expect(patchedEditor.selectedViews).toEqual([])
+  })
+})
+
+describe('adjustNumberProperty', () => {
+  it('works for left style prop', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      complexDefaultProjectPreParsed(),
+      'dont-await-first-dom-report',
+      createBuiltInDependenciesList(null),
+    )
+
+    const cardInstancePath = EP.elementPath([
+      ['storyboard-entity', 'scene-1-entity', 'app-entity'],
+      ['app-outer-div', 'card-instance'],
+    ])
+
+    const originalEditorState = renderResult.getEditorState().editor
+
+    const originalLeftStyleProp = withUnderlyingTargetFromEditorState(
+      cardInstancePath,
+      originalEditorState,
+      null,
+      (success, element, underlyingTarget, underlyingFilePath) => {
+        return getNumberPropertyFromProps(element.props, stylePropPathMappingFn('left', ['style']))
+      },
+    )!
+
+    const delta = 10
+
+    const adjustNumberPropertyCommand = adjustNumberProperty(
+      'permanent',
+      cardInstancePath,
+      stylePropPathMappingFn('left', ['style']),
+      delta,
+      true,
+    )
+
+    const result = runAdjustNumberProperty(
+      renderResult.getEditorState().editor,
+      [],
+      adjustNumberPropertyCommand,
+    )
+
+    const patchedEditor = applyStatePatches(
+      renderResult.getEditorState().editor,
+      renderResult.getEditorState().editor,
+      [result.editorStatePatch],
+    )
+
+    const updatedLeftStyleProp = withUnderlyingTargetFromEditorState(
+      cardInstancePath,
+      patchedEditor,
+      null,
+      (success, element, underlyingTarget, underlyingFilePath) => {
+        return getNumberPropertyFromProps(element.props, stylePropPathMappingFn('left', ['style']))
+      },
+    )
+
+    expect(updatedLeftStyleProp).toEqual(originalLeftStyleProp + delta)
   })
 })
