90621822664122ef329eb0c5a8b897a0b6fa07a1
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2022-03-03 16:20:14 +0000

    performance(canvas) Use a tree to hold parent-child relationships. (#2132)
    - Added `buildTree`, which takes a bunch of element paths and produces
      a n-way tree from the paths so that children of a target can be identified
      quickly.
    - Implemented `printTree` which takes the tree produced by `buildTree` and
      returns a string formatted for testing purposes.
    - Added `forEachChildOfTarget` to call a callback for each child path
      of a given path.
    - Simplified some functions like `getRootViews`, `getChildrenPaths`,
      `getImmediateChildrenPaths` and `getStorboardMetadata`.
    - `getAllPathsIncludingUnfurledFocusedComponents` and `getFramesInCanvasContext`
      now use the new tree to identify the children of targets.

--- editor/src/components/canvas/canvas.ts
+++ editor/src/components/canvas/canvas.ts
@@ -16,6 +16,8 @@ import { EditorAction } from '../editor/action-types'
 import * as EditorActions from '../editor/actions/action-creators'
 import { DerivedState, EditorState } from '../editor/store/editor-state'
 import * as EP from '../../core/shared/element-path'
+import { buildTree, forEachChildOfTarget } from '../../core/shared/element-path-tree'
+import { objectValues } from '../../core/shared/object-utils'
 
 export enum TargetSearchType {
   ParentsOfSelected = 'ParentsOfSelected',
@@ -47,6 +49,10 @@ const Canvas = {
     metadata: ElementInstanceMetadataMap,
     useBoundingFrames: boolean,
   ): Array<FrameWithPath> {
+    // Note: This will not necessarily be representative of the structured ordering in
+    // the code that produced these elements.
+    const projectTree = buildTree(objectValues(metadata).map((m) => m.elementPath))
+
     function recurseChildren(
       component: ElementInstanceMetadata,
     ): { boundingRect: CanvasRectangle | null; frames: Array<FrameWithPath> } {
@@ -60,13 +66,20 @@ const Canvas = {
 
       const overflows = MetadataUtils.overflows(component)
       const includeClippedNext = useBoundingFrames && overflows
-      const {
-        children,
-        unfurledComponents,
-      } = MetadataUtils.getAllChildrenElementsIncludingUnfurledFocusedComponents(
-        component.elementPath,
-        metadata,
-      )
+
+      let children: Array<ElementInstanceMetadata> = []
+      let unfurledComponents: Array<ElementInstanceMetadata> = []
+      forEachChildOfTarget(projectTree, component.elementPath, (childPath) => {
+        const childMetadata = MetadataUtils.findElementByElementPath(metadata, childPath)
+        if (childMetadata != null) {
+          if (EP.isRootElementOfInstance(childPath)) {
+            unfurledComponents.push(childMetadata)
+          } else {
+            children.push(childMetadata)
+          }
+        }
+      })
+
       const childFrames = children.map((child) => {
         const recurseResults = recurseChildren(child)
         const rectToBoundWith = includeClippedNext ? recurseResults.boundingRect : globalFrame
