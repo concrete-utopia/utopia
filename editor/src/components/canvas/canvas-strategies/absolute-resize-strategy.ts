ae492ba432d3bfd509df28b502803efb786fab7a
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-03-10 16:38:06 +0100

    resize commands

--- editor/src/components/canvas/canvas-strategies/absolute-resize-strategy.ts
+++ editor/src/components/canvas/canvas-strategies/absolute-resize-strategy.ts
@@ -1,8 +1,23 @@
+import { isHorizontalPoint } from 'utopia-api/core'
+import { getLayoutProperty } from '../../../core/layout/getLayoutProperty'
+import { framePointForPinnedProp } from '../../../core/layout/layout-helpers-new'
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
+import { mapDropNulls } from '../../../core/shared/array-utils'
+import { isRight, right } from '../../../core/shared/either'
 import { JSXElement } from '../../../core/shared/element-template'
+import { CanvasRectangle, CanvasVector } from '../../../core/shared/math-utils'
+import { ElementPath } from '../../../core/shared/project-file-types'
 import { withUnderlyingTarget } from '../../editor/store/editor-state'
+import { stylePropPathMappingFn } from '../../inspector/common/property-path-hooks'
+import { EdgePosition } from '../canvas-types'
+import {
+  AdjustCssLengthProperty,
+  adjustCssLengthProperty,
+} from '../commands/adjust-css-length-command'
 import { wildcardPatch } from '../commands/wildcard-patch-command'
 import { CanvasStrategy } from './canvas-strategy-types'
+
+type AbsolutePin = 'left' | 'top' | 'right' | 'bottom' | 'width' | 'height'
 
 export const absoluteResizeStrategy: CanvasStrategy = {
   id: 'ABSOLUTE_RESIZE',
@@ -18,7 +33,9 @@ export const absoluteResizeStrategy: CanvasStrategy = {
       return false
     }
   },
-  controlsToRender: [], // Uses existing hooks in select-mode-hooks.tsx
+  controlsToRender: [
+    // { control: AbsoluteResizeControl, key: 'absolute-resize-control', show: 'always-visible' }, // TODO IT HAS PROPS
+  ],
   fitness: (canvasState, interactionState, sessionState) => {
     return absoluteResizeStrategy.isApplicable(
       canvasState,
@@ -33,9 +50,43 @@ export const absoluteResizeStrategy: CanvasStrategy = {
   apply: (canvasState, interactionState, sessionState) => {
     if (
       interactionState.interactionData.type === 'DRAG' &&
-      interactionState.interactionData.drag != null
+      interactionState.interactionData.drag != null &&
+      interactionState.activeControl.type === 'RESIZE_HANDLE'
     ) {
+      const drag = interactionState.interactionData.drag
+      const edgePosition = interactionState.activeControl.edgePosition
+
+      const commandsForSelectedElements = canvasState.selectedElements.flatMap(
+        (selectedElement) => {
+          const element: JSXElement | null = withUnderlyingTarget(
+            selectedElement,
+            canvasState.projectContents,
+            {},
+            canvasState.openFile,
+            null,
+            (_, e) => e,
+          )
+          const elementParentBounds =
+            MetadataUtils.findElementByElementPath(
+              sessionState.startingMetadata, // TODO should this be using the current metadata?
+              selectedElement,
+            )?.specialSizeMeasurements.immediateParentBounds ?? null
+
+          if (element == null) {
+            return []
+          }
+
+          return createResizeCommands(
+            element,
+            selectedElement,
+            edgePosition,
+            drag,
+            elementParentBounds,
+          )
+        },
+      )
       return [
+        ...commandsForSelectedElements,
         wildcardPatch('transient', {
           highlightedViews: {
             $set: [],
@@ -46,4 +97,58 @@ export const absoluteResizeStrategy: CanvasStrategy = {
     // Fallback for when the checks above are not satisfied.
     return []
   },
+}
+
+function pinsForEdgePosition(edgePosition: EdgePosition): AbsolutePin[] {
+  let horizontalPins: AbsolutePin[] = []
+  let verticalPins: AbsolutePin[] = []
+
+  if (edgePosition.x === 0) {
+    horizontalPins = ['left', 'width']
+  } else if (edgePosition.x === 1) {
+    horizontalPins = ['right', 'width']
+  }
+
+  if (edgePosition.y === 0) {
+    verticalPins = ['top', 'height']
+  } else if (edgePosition.y === 1) {
+    verticalPins = ['bottom', 'height']
+  }
+
+  return [...horizontalPins, ...verticalPins]
+}
+
+function createResizeCommands(
+  element: JSXElement,
+  selectedElement: ElementPath,
+  edgePosition: EdgePosition,
+  drag: CanvasVector,
+  elementParentBounds: CanvasRectangle | null,
+): AdjustCssLengthProperty[] {
+  const pins = pinsForEdgePosition(edgePosition)
+  return mapDropNulls((pin) => {
+    const horizontal = isHorizontalPoint(
+      // TODO avoid using the loaded FramePoint enum
+      framePointForPinnedProp(pin),
+    )
+    const negative =
+      pin === 'right' ||
+      pin === 'bottom' ||
+      (pin === 'width' && edgePosition.x === 0) ||
+      (pin === 'height' && edgePosition.y === 0)
+    const value = getLayoutProperty(pin, right(element.props), ['style'])
+    if (isRight(value) && value.value != null) {
+      // TODO what to do about missing properties?
+      return adjustCssLengthProperty(
+        'permanent',
+        selectedElement,
+        stylePropPathMappingFn(pin, ['style']),
+        (horizontal ? drag.x : drag.y) * (negative ? -1 : 1),
+        horizontal ? elementParentBounds?.width : elementParentBounds?.height,
+        true,
+      )
+    } else {
+      return null
+    }
+  }, pins)
 }
