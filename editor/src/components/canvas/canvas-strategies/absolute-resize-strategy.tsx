342eb1ff65807479059184ed5f6abd7ff901dcab
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2022-03-15 19:52:43 +0100

    v0 of using snapPoint in resize

--- editor/src/components/canvas/canvas-strategies/absolute-resize-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/absolute-resize-strategy.tsx
@@ -4,18 +4,26 @@ import { framePointForPinnedProp } from '../../../core/layout/layout-helpers-new
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import { mapDropNulls } from '../../../core/shared/array-utils'
 import { isRight, right } from '../../../core/shared/either'
-import { JSXElement } from '../../../core/shared/element-template'
-import { CanvasRectangle, CanvasVector } from '../../../core/shared/math-utils'
+import { ElementInstanceMetadataMap, JSXElement } from '../../../core/shared/element-template'
+import {
+  CanvasPoint,
+  CanvasRectangle,
+  CanvasVector,
+  offsetPoint,
+  vectorDifference,
+} from '../../../core/shared/math-utils'
 import { ElementPath } from '../../../core/shared/project-file-types'
 import { withUnderlyingTarget } from '../../editor/store/editor-state'
 import { stylePropPathMappingFn } from '../../inspector/common/property-path-hooks'
 import { EdgePosition } from '../canvas-types'
+import { snapPoint } from '../canvas-utils'
 import {
   AdjustCssLengthProperty,
   adjustCssLengthProperty,
 } from '../commands/adjust-css-length-command'
 import { updateHighlightedViews } from '../commands/update-highlighted-views-command'
 import { AbsoluteResizeControl } from '../controls/select-mode/absolute-resize-control'
+import { GuidelineWithSnappingVector } from '../guideline'
 import { CanvasStrategy } from './canvas-strategy-types'
 
 type AbsolutePin = 'left' | 'top' | 'right' | 'bottom' | 'width' | 'height'
@@ -56,6 +64,18 @@ export const absoluteResizeStrategy: CanvasStrategy = {
     ) {
       const drag = interactionState.interactionData.drag
       const edgePosition = interactionState.activeControl.edgePosition
+      const snappedDrag = snapDrag(
+        canvasState.selectedElements,
+        sessionState.startingMetadata,
+        canvasState.scale,
+        interactionState.interactionData.dragStart,
+        drag,
+        true,
+        false,
+        null as any,
+        null as any,
+        edgePosition,
+      )
 
       const commandsForSelectedElements = canvasState.selectedElements.flatMap(
         (selectedElement) => {
@@ -81,7 +101,7 @@ export const absoluteResizeStrategy: CanvasStrategy = {
             element,
             selectedElement,
             edgePosition,
-            drag,
+            snappedDrag.snappedDragVector,
             elementParentBounds,
           )
         },
@@ -145,4 +165,38 @@ function createResizeCommands(
       return null
     }
   }, pins)
+}
+
+function snapDrag(
+  selectedViews: Array<ElementPath>,
+  jsxMetadata: ElementInstanceMetadataMap,
+  canvasScale: number,
+  dragStart: CanvasPoint,
+  drag: CanvasVector,
+  enableSnapping: boolean,
+  keepAspectRatio: boolean,
+  diagonalA: CanvasPoint,
+  diagonalB: CanvasPoint,
+  resizingFromPosition: EdgePosition | null,
+): {
+  snappedDragVector: CanvasPoint
+  guidelinesWithSnappingVector: Array<GuidelineWithSnappingVector>
+} {
+  const pointToSnap: CanvasPoint = offsetPoint(dragStart, drag)
+  const { snappedPointOnCanvas, guidelinesWithSnappingVector } = snapPoint(
+    selectedViews,
+    jsxMetadata,
+    canvasScale,
+    pointToSnap,
+    enableSnapping,
+    keepAspectRatio,
+    diagonalA,
+    diagonalB,
+    resizingFromPosition,
+  )
+  const snappedDragVector = vectorDifference(dragStart, snappedPointOnCanvas)
+  return {
+    snappedDragVector: snappedDragVector,
+    guidelinesWithSnappingVector: guidelinesWithSnappingVector,
+  }
 }
