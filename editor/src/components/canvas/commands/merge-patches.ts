51bf314c33c59f539f356c98cc3b6406b15229d3
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-03-18 14:45:32 +0100

    Make sure there are no unsupported commands in the patches

--- editor/src/components/canvas/commands/merge-patches.ts
+++ editor/src/components/canvas/commands/merge-patches.ts
@@ -1,5 +1,16 @@
 import { Spec } from 'immutability-helper'
 
+const SupportedCommands = ['$set']
+
+function isCommand(key: string) {
+  return key.startsWith('$')
+}
+
+function isSupportedCommand(key: string) {
+  SupportedCommands.indexOf(key) > -1
+}
+
+// TODO: modify Spec type so it only supports the $set command
 export function mergePatches<T>(patches: Array<Spec<T>>) {
   let modifiedPaths: Set<string> = new Set()
   let compressedPatches: Array<Spec<T>> = []
@@ -34,6 +45,9 @@ function getPathsInner<T, K extends keyof Spec<T>>(
     return
   }
   keys.forEach((key) => {
+    if (isCommand(key) && !isSupportedCommand) {
+      throw new Error(`${key} is an unsupported immutability-helper command`)
+    }
     if (key === '$set') {
       paths.push(prefix)
     } else {
