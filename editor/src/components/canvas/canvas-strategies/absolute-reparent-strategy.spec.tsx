084b1cd678b9e2e681d45043d56a8f4976266db3
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-03-22 18:23:05 +0100

    fix test data after master merge

--- editor/src/components/canvas/canvas-strategies/absolute-reparent-strategy.spec.tsx
+++ editor/src/components/canvas/canvas-strategies/absolute-reparent-strategy.spec.tsx
@@ -52,7 +52,7 @@ function reparentElement(
       currentStrategy: null as any, // the strategy does not use this
       currentStrategyFitness: null as any, // the strategy does not use this
       currentStrategyCommands: null as any, // the strategy does not use this
-      accumulatedCommands: null as any, // the strategy does not use this
+      accumulatedPatches: null as any, // the strategy does not use this
       commandDescriptions: null as any, // the strategy does not use this
       sortedApplicableStrategies: null as any, // the strategy does not use this
       startingMetadata: {
