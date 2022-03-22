747645a9ca2df2f03a5e56cf4d2d991578f1ad07
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-03-22 11:47:49 +0100

    Store merged patches instead of accumulated commands (#2165)
    * Merge patches coming from commands
    
    * Add object check
    
    * Make sure there are no unsupported commands in the patches
    
    * Add all necessary commands to accumulatedPatches
    
    * Fix test build
    
    * Update test snapshots
    
    * Remove accumulatedcommands
    
    * Fix state patching
    
    * Fix: do not include current commands in accumulated patches
    
    * Add unit tests
    
    * Rename variable

--- editor/src/components/canvas/canvas-strategies/absolute-resize-delta-strategy.spec.tsx
+++ editor/src/components/canvas/canvas-strategies/absolute-resize-delta-strategy.spec.tsx
@@ -33,7 +33,7 @@ function resizeElement(editor: EditorState, edgePosition: EdgePosition): EditorS
       currentStrategy: null as any, // the strategy does not use this
       currentStrategyFitness: null as any, // the strategy does not use this
       currentStrategyCommands: null as any, // the strategy does not use this
-      accumulatedCommands: null as any, // the strategy does not use this
+      accumulatedPatches: null as any, // the strategy does not use this
       commandDescriptions: null as any, // the strategy does not use this
       sortedApplicableStrategies: null as any, // the strategy does not use this
       startingMetadata: {
@@ -45,7 +45,7 @@ function resizeElement(editor: EditorState, edgePosition: EdgePosition): EditorS
       },
     } as StrategyState,
   )
-  return foldAndApplyCommands(editor, editor, strategyResult, 'permanent').editorState
+  return foldAndApplyCommands(editor, editor, [], [], strategyResult, 'permanent').editorState
 }
 
 function createTestEditorAndResizeElement(
