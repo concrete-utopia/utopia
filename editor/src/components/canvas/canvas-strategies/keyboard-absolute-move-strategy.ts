e309ce4f0ffac03c8eadce4bcb3b3b12b8fc6b2d
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-03-25 18:46:47 +0100

    Enable keyboard move for multiselect

--- editor/src/components/canvas/canvas-strategies/keyboard-absolute-move-strategy.ts
+++ editor/src/components/canvas/canvas-strategies/keyboard-absolute-move-strategy.ts
@@ -10,13 +10,12 @@ export const keyboardAbsoluteMoveStrategy: CanvasStrategy = {
   id: 'KEYBOARD_ABSOLUTE_MOVE',
   name: 'Keyboard absolute Move',
   isApplicable: (canvasState, _interactionState, metadata) => {
-    if (canvasState.selectedElements.length === 1) {
-      const elementMetadata = MetadataUtils.findElementByElementPath(
-        metadata,
-        canvasState.selectedElements[0],
-      )
+    if (canvasState.selectedElements.length > 0) {
+      return canvasState.selectedElements.every((element) => {
+        const elementMetadata = MetadataUtils.findElementByElementPath(metadata, element)
 
-      return elementMetadata?.specialSizeMeasurements.position === 'absolute'
+        return elementMetadata?.specialSizeMeasurements.position === 'absolute'
+      })
     } else {
       return false
     }
