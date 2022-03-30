18ee99ac576d40ed791453f878cf9b4b73f44187
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-03-30 18:56:58 +0200

    Remove unused imports

--- editor/src/components/canvas/canvas-strategies/shared-absolute-resize-strategy-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/shared-absolute-resize-strategy-helpers.ts
@@ -1,30 +1,18 @@
 import { isHorizontalPoint } from 'utopia-api/core'
 import { getLayoutProperty } from '../../../core/layout/getLayoutProperty'
 import { framePointForPinnedProp } from '../../../core/layout/layout-helpers-new'
-import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import { mapDropNulls } from '../../../core/shared/array-utils'
 import { isRight, right } from '../../../core/shared/either'
-import { ElementInstanceMetadataMap, JSXElement } from '../../../core/shared/element-template'
-import { CanvasRectangle, CanvasVector, offsetPoint } from '../../../core/shared/math-utils'
+import { JSXElement } from '../../../core/shared/element-template'
+import { CanvasRectangle, CanvasVector } from '../../../core/shared/math-utils'
 import { ElementPath } from '../../../core/shared/project-file-types'
-import { withUnderlyingTarget } from '../../editor/store/editor-state'
 import { stylePropPathMappingFn } from '../../inspector/common/property-path-hooks'
 import { EdgePosition } from '../canvas-types'
 import {
   AdjustCssLengthProperty,
   adjustCssLengthProperty,
 } from '../commands/adjust-css-length-command'
-import { setSnappingGuidelines } from '../commands/set-snapping-guidelines-command'
-import { updateHighlightedViews } from '../commands/update-highlighted-views-command'
-import { AbsoluteResizeControl } from '../controls/select-mode/absolute-resize-control'
 import { AbsolutePin } from './absolute-resize-helpers'
-import { GuidelineWithSnappingVector } from '../guideline'
-import { CanvasStrategy } from './canvas-strategy-types'
-import {
-  getMultiselectBounds,
-  resizeBoundingBox,
-  runLegacyAbsoluteResizeSnapping,
-} from './shared-absolute-move-strategy-helpers'
 
 export function createResizeCommands(
   element: JSXElement,
