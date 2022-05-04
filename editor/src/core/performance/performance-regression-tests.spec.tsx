7e6f669e6bd8c15edfc22458365e36a29b01a5b0
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-05-04 14:10:35 +0200

    fix(inspector) fix highlight effect (#2222)
    * fix(inspector) mouseover effect and missing contextmenu
    
    * update render count tests

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -64,7 +64,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`357`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`361`)
   })
 
   it('Clicking on opacity slider with a less simple project', async () => {
@@ -124,7 +124,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`417`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`421`)
   })
 
   it('Changing the selected view with a simple project', async () => {
@@ -178,7 +178,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`2350`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`2373`)
   })
 
   it('Changing the selected view with a less simple project', async () => {
@@ -242,6 +242,6 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`2503`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`2526`)
   })
 })
