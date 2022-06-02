7c0a343df38a2a46760ccf41e5f30ddf89e9ee8a
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-06-02 11:51:49 +0200

    experiment feature swithc is always on

--- editor/src/utils/feature-switches.ts
+++ editor/src/utils/feature-switches.ts
@@ -65,6 +65,9 @@ fastForEach(AllFeatureNames, (name) => {
 })
 
 export function isFeatureEnabled(featureName: FeatureName): boolean {
+  if (featureName === 'Canvas Strategies') {
+    return true
+  }
   return FeatureSwitches[featureName] ?? false
 }
 
