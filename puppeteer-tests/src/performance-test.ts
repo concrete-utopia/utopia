1afe24e23e333608cbba436634953101f016c531
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2022-06-13 16:19:07 +0100

    feat(tests) Use a fixed version for comparing performance tests

--- puppeteer-tests/src/performance-test.ts
+++ puppeteer-tests/src/performance-test.ts
@@ -10,7 +10,8 @@ const moveFile = require('move-file')
 
 const BRANCH_NAME = process.env.BRANCH_NAME ? `?branch_name=${process.env.BRANCH_NAME}` : ''
 const STAGING_EDITOR_URL = process.env.EDITOR_URL ?? `https://utopia.pizza/p${BRANCH_NAME}`
-const MASTER_EDITOR_URL = process.env.MASTER_EDITOR_URL ?? `https://utopia.pizza/p`
+const MASTER_EDITOR_URL =
+  process.env.MASTER_EDITOR_URL ?? `https://utopia.pizza/p?branch_name=performance-test-target`
 
 export function wait(timeout: number): Promise<void> {
   return new Promise((resolve) => {
