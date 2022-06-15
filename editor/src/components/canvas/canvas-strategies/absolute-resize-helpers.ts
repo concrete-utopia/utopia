bf62a4ec56d0e2e83f1938f06ef45a7d5aedce0e
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2022-06-15 16:40:28 +0200

    cleaning this up a bit

--- editor/src/components/canvas/canvas-strategies/absolute-resize-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/absolute-resize-helpers.ts
@@ -5,7 +5,7 @@ export const verticalPins: Array<AbsolutePin> = ['top', 'height', 'bottom']
 
 export function hasAtLeastTwoPinsPerSide(props: { [key: string]: any }): boolean {
   return (
-    horizontalPins.filter((pin) => props['style']?.[pin] != null).length >= 2 &&
-    verticalPins.filter((pin) => props['style']?.[pin] != null).length >= 2
+    horizontalPins.filter((pin) => props.style?.[pin] != null).length >= 2 &&
+    verticalPins.filter((pin) => props.style?.[pin] != null).length >= 2
   )
 }
