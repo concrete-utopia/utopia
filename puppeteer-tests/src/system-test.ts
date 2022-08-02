7163d20013c5bdf3704f0815fd884f367a934bad
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2022-08-02 11:06:27 +0100

    fix(tests) Use the screenshot project

--- puppeteer-tests/src/system-test.ts
+++ puppeteer-tests/src/system-test.ts
@@ -4,8 +4,10 @@ import * as puppeteer from 'puppeteer'
 import type { PageEventObject } from 'puppeteer'
 import { ONE_MINUTE_IN_MS, setupBrowser, timeLimitPromise } from './utils'
 
+const PROJECT_ID = process.env.PROJECT_ID ?? '258a98d6'
 const BRANCH_NAME = process.env.BRANCH_NAME ? `?branch_name=${process.env.BRANCH_NAME}` : ''
-const STAGING_EDITOR_URL = process.env.EDITOR_URL ?? `https://utopia.pizza/p${BRANCH_NAME}`
+const STAGING_EDITOR_URL =
+  process.env.EDITOR_URL ?? `https://utopia.pizza/p/${PROJECT_ID}${BRANCH_NAME}`
 
 async function clickOnce(
   page: puppeteer.Page,
