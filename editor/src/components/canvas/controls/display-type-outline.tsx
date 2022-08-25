53aebce1344aab916ff6ddaf336919a693dcfc77
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-08-25 16:00:04 +0200

    better outlines

--- editor/src/components/canvas/controls/display-type-outline.tsx
+++ editor/src/components/canvas/controls/display-type-outline.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import { mapDropNulls } from '../../../core/shared/array-utils'
+import * as EP from '../../../core/shared/element-path'
 import { useColorTheme } from '../../../uuiui'
 import { useEditorState } from '../../editor/store/store-hook'
+import { CanvasFrameAndTarget } from '../canvas-types'
 import { CanvasOffsetWrapper } from './canvas-offset-wrapper'
 export const DisplayTypeOutline = () => {
   const colorTheme = useColorTheme()
   const outlineColor = colorTheme.primary.value
 
-  const siblingFramesWithSameDisplayType = useEditorState((store) => {
+  const siblingFramesWithSameDisplayType: CanvasFrameAndTarget[] = useEditorState((store) => {
     if (store.editor.selectedViews.length === 1) {
       const path = store.editor.selectedViews[0]
       const targetDisplayType = MetadataUtils.findElementByElementPath(
@@ -17,8 +19,15 @@ export const DisplayTypeOutline = () => {
       )?.specialSizeMeasurements.display
       const siblings = MetadataUtils.getSiblings(store.editor.jsxMetadata, path)
       const frames = mapDropNulls((sibling) => {
-        if (sibling.specialSizeMeasurements.display === targetDisplayType) {
-          return MetadataUtils.getFrameInCanvasCoords(sibling.elementPath, store.editor.jsxMetadata)
+        const frame = MetadataUtils.getFrameInCanvasCoords(
+          sibling.elementPath,
+          store.editor.jsxMetadata,
+        )
+        if (sibling.specialSizeMeasurements.display === targetDisplayType && frame != null) {
+          return {
+            frame: frame,
+            target: sibling.elementPath,
+          }
         } else {
           return null
         }
@@ -31,17 +40,17 @@ export const DisplayTypeOutline = () => {
 
   return siblingFramesWithSameDisplayType.length === 0 ? null : (
     <CanvasOffsetWrapper>
-      {siblingFramesWithSameDisplayType.map((frame, i) => {
+      {siblingFramesWithSameDisplayType.map((frameAndTarget, i) => {
         return (
           <div
-            key={i}
+            key={EP.toString(frameAndTarget.target)}
             style={{
               position: 'absolute',
               boxSizing: 'border-box',
-              left: frame.x,
-              top: frame.y,
-              width: frame.width,
-              height: frame.height,
+              left: frameAndTarget.frame.x,
+              top: frameAndTarget.frame.y,
+              width: frameAndTarget.frame.width,
+              height: frameAndTarget.frame.height,
               pointerEvents: 'none',
               backgroundImage: `linear-gradient(135deg, ${outlineColor} 2.5%, rgba(255,255,255,0) 2.5%, rgba(255,255,255,0) 50%, ${outlineColor} 50%, ${outlineColor} 52%, rgba(255,255,255,0) 52%, rgba(255,255,255,0) 100%)`,
               backgroundSize: `20px 20px`,
