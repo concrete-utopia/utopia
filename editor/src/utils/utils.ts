1413772d4ea33a864dc1c66ad1daadb588cc7545
Berci Kormendy <korber314@duck.com>
2022-09-07 16:31:44 +0200

    deduplicateBy

--- editor/src/utils/utils.ts
+++ editor/src/utils/utils.ts
@@ -924,6 +924,19 @@ export function isOptionsType<T extends OptionTypeBase>(
   return Array.isArray(value)
 }
 
+function deduplicateBy<T>(key: (t: T) => string, ts: Array<T>): Array<T> {
+  const seen = new Set<string>()
+  const results: Array<T> = []
+  for (const t of ts) {
+    const k = key(t)
+    if (!seen.has(k)) {
+      results.push(t)
+    }
+    seen.add(k)
+  }
+  return results
+}
+
 export default {
   generateUUID: generateUUID,
   assert: assert,
@@ -1085,4 +1098,5 @@ export default {
   processErrorWithSourceMap: processErrorWithSourceMap,
   findLastIndex: findLastIndex,
   timeLimitPromise: timeLimitPromise,
+  deduplicateBy: deduplicateBy,
 }
