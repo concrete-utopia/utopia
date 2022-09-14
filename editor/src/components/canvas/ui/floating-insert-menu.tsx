da915d103181e7620c754c54a633ca4611a94f41
Berci Kormendy <korber314@duck.com>
2022-09-14 14:58:25 +0200

    Revert "use custom component"
    This reverts commit 51aec1a1b47df6a9537f4bada1a7083664b50393.

--- editor/src/components/canvas/ui/floating-insert-menu.tsx
+++ editor/src/components/canvas/ui/floating-insert-menu.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 import { jsx } from '@emotion/react'
 import { CSSObject } from '@emotion/serialize'
 import WindowedSelect, {
+  ActionMeta,
   InputActionMeta,
-  OptionProps,
   StylesConfig,
   ValueType,
 } from 'react-windowed-select'
 
-import { getControlStyles, Utils } from '../../../uuiui-deps'
+import { getControlStyles } from '../../../uuiui-deps'
 import { useEditorState, useRefEditorState } from '../../editor/store/store-hook'
 
 import {
@@ -67,7 +67,6 @@ type InsertMenuItemValue = InsertableComponent & {
 
 type InsertMenuItem = {
   label: string
-  source: string | null
   value: InsertMenuItemValue
 }
 
@@ -89,7 +88,6 @@ function convertInsertableComponentsToFlatList(
           const source = index === 0 ? componentGroup.source : null
           return {
             label: insertableComponent.name,
-            source: Utils.optionalMap(getInsertableGroupLabel, source),
             value: {
               ...insertableComponent,
               key: `${getInsertableGroupLabel(componentGroup.source)}-${insertableComponent.name}`,
@@ -299,7 +297,13 @@ function useComponentSelectorStyles(): StylesConfig<InsertMenuItem, false> {
       groupHeading: (styles): CSSObject => {
         return {
           // ...styles,
-          display: 'none',
+          color: colorTheme.fg7.value,
+          height: 25,
+          right: 12,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          pointerEvents: 'none',
         }
       },
     }),
@@ -311,48 +315,6 @@ interface CheckboxRowProps {
   id: string
   checked: boolean
   onChange: (value: boolean) => void
-}
-
-const CustomOption = (props: OptionProps<InsertMenuItem, false>) => {
-  const { innerRef, innerProps, isDisabled, isFocused, label, data } = props
-  const colorTheme = useColorTheme()
-  return (
-    <div
-      ref={innerRef}
-      {...innerProps}
-      style={{
-        boxSizing: 'border-box',
-        height: UtopiaTheme.layout.rowHeight.smaller,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingLeft: 4,
-        paddingRight: 4,
-        cursor: isDisabled ? 'not-allowed' : 'default',
-        color: isFocused ? colorTheme.inverted.fg0.value : colorTheme.fg0.value,
-        backgroundColor: isFocused ? colorTheme.primary.value : 'transparent',
-        borderRadius: UtopiaTheme.inputBorderRadius,
-      }}
-    >
-      <div style={{ paddingRight: 2 }}>{label}</div>
-      <div
-        style={{
-          color: colorTheme.fg7.value,
-          height: UtopiaTheme.layout.rowHeight.smaller,
-          right: 12,
-          display: 'flex',
-          alignItems: 'center',
-          pointerEvents: 'none',
-          overflow: 'hidden',
-        }}
-      >
-        <div style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>
-          {data.source ?? ''}
-        </div>
-        <span style={{ minWidth: 0 }}></span>
-      </div>
-    </div>
-  )
 }
 
 const CheckboxRow = React.memo<React.PropsWithChildren<CheckboxRowProps>>(
@@ -625,7 +587,6 @@ export var FloatingMenu = React.memo(() => {
           placeholder='Search...'
           styles={componentSelectorStyles}
           tabSelectsValue={false}
-          components={{ Option: CustomOption }}
         />
         {showInsertionControls ? (
           <FlexColumn>
