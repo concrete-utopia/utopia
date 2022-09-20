9f679ea977d6e286da2e8068d2f1e6515f787fcc
Balint Gabor <127662+gbalint@users.noreply.github.com>
2022-09-20 15:57:17 +0200

    Draw to insert (#2556)
    * First commit
    
    * Some cleanup
    
    * Fixes
    
    * First commit
    
    * Remove outdated comment
    
    * Make it work only from the corner
    
    * Fix setcursor commands
    
    * 1 is enough for fitness
    
    * Make it work on the sides
    
    * Better fake metadata for flex
    
    * Extract fake metadata creation
    
    * Add tests
    
    * Remove unused parameter
    
    * Fix build
    
    * fix: adapt strategy names into functions
    
    * Fix build
    
    * simplify
    
    * Fix test comments and add new tests
    
    Co-authored-by: Berci Kormendy <korber314@duck.com>

--- editor/src/components/canvas/canvas-strategies/canvas-strategies.tsx
+++ editor/src/components/canvas/canvas-strategies/canvas-strategies.tsx
@@ -38,9 +38,10 @@ import {
 } from './flow-reorder-strategy'
 import { isInsertMode } from '../../editor/editor-modes'
 import { dragToInsertStrategy } from './drag-to-insert-strategy'
-import { CSSCursor } from '../../../uuiui-deps'
 import { StateSelector } from 'zustand'
 import { NonResizableControl } from '../controls/select-mode/non-resizable-control'
+import { drawToInsertStrategy } from './draw-to-insert-strategy'
+import { flexResizeBasicStrategy } from './flex-resize-basic-strategy'
 import { optionalMap } from '../../../core/shared/optional-utils'
 
 export const RegisteredCanvasStrategies: Array<CanvasStrategy> = [
@@ -56,9 +57,11 @@ export const RegisteredCanvasStrategies: Array<CanvasStrategy> = [
   convertToAbsoluteAndMoveStrategy,
   absoluteReparentToFlexStrategy,
   dragToInsertStrategy,
+  drawToInsertStrategy,
   flowReorderAutoConversionStrategy,
   flowReorderNoConversionStrategy,
   flowReorderSameTypeOnlyStrategy,
+  flexResizeBasicStrategy,
 ]
 
 export function pickCanvasStateFromEditorState(
@@ -333,6 +336,7 @@ export function isResizableStrategy(canvasStrategy: CanvasStrategy): boolean {
   switch (canvasStrategy.id) {
     case 'ABSOLUTE_RESIZE_BOUNDING_BOX':
     case 'KEYBOARD_ABSOLUTE_RESIZE':
+    case 'FLEX_RESIZE_BASIC':
       return true
     default:
       return false
