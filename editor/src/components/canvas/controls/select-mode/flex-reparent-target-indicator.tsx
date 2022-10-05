7f09ae5faba1fbd70fbfb5876d2d9f199240103e
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-10-05 12:49:37 +0200

    mouseleave resets size

--- editor/src/components/canvas/controls/select-mode/flex-reparent-target-indicator.tsx
+++ editor/src/components/canvas/controls/select-mode/flex-reparent-target-indicator.tsx
@@ -19,6 +19,9 @@ export const FlexReparentTargetIndicator = React.memo(() => {
   const mouseover = React.useCallback(() => {
     setMouseOverSizeOffset(4)
   }, [setMouseOverSizeOffset])
+  const mouseLeave = React.useCallback(() => {
+    setMouseOverSizeOffset(0)
+  }, [setMouseOverSizeOffset])
 
   return (
     <CanvasOffsetWrapper>
@@ -26,6 +29,7 @@ export const FlexReparentTargetIndicator = React.memo(() => {
         {reparentTargetLines.map((line, i) => (
           <div
             onMouseOver={isHoverInteraction ? mouseover : Utils.NO_OP}
+            onMouseLeave={isHoverInteraction ? mouseLeave : Utils.NO_OP}
             data-testid={`flex-reparent-indicator-${i}`}
             key={i}
             style={{
