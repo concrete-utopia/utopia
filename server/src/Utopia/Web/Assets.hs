6ce5d362b43da80be0cad410ee9279d2297f59f9
Sean Parsons <git@futurenotfound.com>
2022-10-13 11:08:45 +0100

    feature(github) Load assets and images when loading a project from Github. - Changed `ImageFile.hash` to be an `Int` instead of `Integer` to make   it more amenable to being hashed. - `getBranchContent` now includes the project ID in a query parameter. - Created some utility functions mostly fronted by `blobEntryTypeFromFilename`   which determine if a blob should fall into one of 3 categories:   Text, Image or Asset. - `getRecursiveGitTreeAsContent` now takes an instance of `SaveAsset`   and passes it into `projectContentFromGitEntry` so that images and   assets can be uploaded while constructing the project contents. - For images, the `JuicyPixls` library is used to grab the dimensions   of the image so that it can be included in the metadata.

--- server/src/Utopia/Web/Assets.hs
+++ server/src/Utopia/Web/Assets.hs
@@ -9,6 +9,7 @@ import           Control.Lens
 import           Control.Monad.Fail
 import           Control.Monad.Trans.AWS
 import qualified Data.ByteString.Lazy    as BL
+import qualified Data.ByteString    as B
 import           Data.Generics.Sum
 import           Data.String
 import           Data.Text
@@ -22,6 +23,8 @@ import           Protolude               hiding (intercalate)
 import           System.Directory
 import           System.Environment
 import           System.FilePath
+import Network.Mime
+import Data.Map.Strict as M
 
 data AWSResources = AWSResources
                   { _awsEnv :: Env
@@ -38,6 +41,8 @@ assetContentsFromLoadAssetResult AssetNotFound            = fail "Asset not foun
 assetContentsFromLoadAssetResult (AssetLoaded content _)  = pure content
 
 type LoadAsset = [Text] -> Maybe Text -> IO LoadAssetResult
+
+type SaveAsset = Text -> [Text] -> BL.ByteString -> IO ()
 
 type LoadThumbnail = Text -> Maybe Text -> IO LoadAssetResult
 
@@ -196,3 +201,35 @@ saveProjectThumbnailToS3 :: AWSResources -> Text -> BL.ByteString -> IO ()
 saveProjectThumbnailToS3 resources projectID contents = runInAWS resources $ do
   let putObjectRequest = putObject (_bucket resources) (projectIDToThumbnailObjectKey projectID) (toBody contents)
   void $ send putObjectRequest
+
+data BlobEntryType = TextEntryType
+                   | ImageEntryType
+                   | AssetEntryType
+                   deriving (Eq, Ord, Show)
+
+expandedMimeMap :: MimeMap
+expandedMimeMap = defaultMimeMap <> M.fromList
+  [ ("ts", "application/typescript")
+  , ("tsx" , "application/typescript")
+  , ("jsx", "application/javascript")
+  ]
+
+looksLikeJavaScript :: MimeType -> Bool
+looksLikeJavaScript mimeType = B.isSuffixOf "/javascript" mimeType || B.isSuffixOf "/typescript" mimeType
+
+looksLikeText :: MimeType -> Bool
+looksLikeText mimeType = looksLikeJavaScript mimeType || B.isPrefixOf "text/" mimeType || B.isPrefixOf "application/json" mimeType
+
+looksLikeImage :: MimeType -> Bool
+looksLikeImage mimeType = B.isPrefixOf "image/" mimeType
+
+mimeLookup :: FileName -> MimeType
+mimeLookup = mimeByExt expandedMimeMap defaultMimeType
+
+blobEntryTypeFromFilename :: FileName -> BlobEntryType
+blobEntryTypeFromFilename filename =
+  let mimeType = mimeLookup filename
+      possiblyText = if looksLikeText mimeType then Just TextEntryType else Nothing
+      possiblyImage = if looksLikeImage mimeType then Just ImageEntryType else Nothing
+  in  fromMaybe AssetEntryType (possiblyText <|> possiblyImage)
+
