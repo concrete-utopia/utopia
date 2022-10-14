41a77088f4be9bc2b336bc59dcbeab9c6d02d401
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2022-10-14 17:12:48 +0200

    adding a test

--- editor/src/components/canvas/canvas-strategies/strategies/forced-absolute-reparent-strategies.spec.browser2.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/forced-absolute-reparent-strategies.spec.browser2.tsx
@@ -3,9 +3,14 @@ import {
   BakedInStoryboardVariableName,
 } from '../../../../core/model/scene-utils'
 import { windowPoint, WindowPoint } from '../../../../core/shared/math-utils'
-import { cmdModifier, Modifiers } from '../../../../utils/modifiers'
+import { cmdModifier, emptyModifiers, Modifiers } from '../../../../utils/modifiers'
+import { wait } from '../../../../utils/utils.test-utils'
 import { CanvasControlsContainerID } from '../../controls/new-canvas-controls'
-import { mouseClickAtPoint, mouseDragFromPointWithDelta } from '../../event-helpers.test-utils'
+import {
+  mouseClickAtPoint,
+  mouseDragFromPointWithDelta,
+  pressKey,
+} from '../../event-helpers.test-utils'
 import {
   EditorRenderResult,
   formatTestProjectCode,
@@ -14,7 +19,7 @@ import {
   TestAppUID,
   TestSceneUID,
 } from '../../ui-jsx.test-utils'
-import { MetaCanvasStrategy } from '../canvas-strategies'
+import { MetaCanvasStrategy, RegisteredCanvasStrategies } from '../canvas-strategies'
 import { CustomStrategyState, InteractionCanvasState } from '../canvas-strategy-types'
 import { InteractionSession } from '../interaction-state'
 import { reparentMetaStrategy } from './reparent-metastrategy'
@@ -24,6 +29,7 @@ async function dragElement(
   targetTestId: string,
   dragDelta: WindowPoint,
   modifiers: Modifiers,
+  midDragCallback?: () => void,
 ): Promise<void> {
   const targetElement = await renderResult.renderedDOM.getByTestId(targetTestId)
   const targetElementBounds = targetElement.getBoundingClientRect()
@@ -37,6 +43,7 @@ async function dragElement(
   mouseClickAtPoint(canvasControlsLayer, startPoint, { modifiers: cmdModifier })
   mouseDragFromPointWithDelta(canvasControlsLayer, startPoint, dragDelta, {
     modifiers: modifiers,
+    midDragCallback: midDragCallback,
   })
 }
 
@@ -281,6 +288,128 @@ describe('Forced Absolute Reparent Strategies', () => {
   </div>`),
     )
   })
+
+  it('Absolute to forced absolute can be accessed by using the strategy selector', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(defaultTestCode),
+      'await-first-dom-report',
+      RegisteredCanvasStrategies,
+    )
+
+    const absoluteChild = await renderResult.renderedDOM.findByTestId('absolutechild')
+    const absoluteChildRect = absoluteChild.getBoundingClientRect()
+    const absoluteChildCenter = {
+      x: absoluteChildRect.x + absoluteChildRect.width / 2,
+      y: absoluteChildRect.y + absoluteChildRect.height / 2,
+    }
+    const secondFlexChild = await renderResult.renderedDOM.findByTestId('flexchild2')
+    const secondFlexChildRect = secondFlexChild.getBoundingClientRect()
+    const secondFlexChildCenter = {
+      x: secondFlexChildRect.x + secondFlexChildRect.width / 2,
+      y: secondFlexChildRect.y + secondFlexChildRect.height / 2,
+    }
+
+    await renderResult.getDispatchFollowUpActionsFinished()
+
+    const dragDelta = windowPoint({
+      x: secondFlexChildCenter.x - absoluteChildCenter.x,
+      y: secondFlexChildCenter.y - absoluteChildCenter.y,
+    })
+
+    await dragElement(
+      renderResult,
+      'absolutechild',
+      dragDelta,
+      cmdModifier,
+      function midDragCallback() {
+        pressKey('3')
+      },
+    )
+
+    await renderResult.getDispatchFollowUpActionsFinished()
+
+    expect(getPrintedUiJsCode(renderResult.getEditorState())).toEqual(
+      makeTestProjectCodeWithSnippet(`
+  <div
+    style={{
+      position: 'absolute',
+      width: 700,
+      height: 600,
+    }}
+    data-uid='container'
+    data-testid='container'
+  >
+    <div
+      style={{
+        width: 250,
+        height: 500,
+        backgroundColor: 'orange',
+      }}
+      data-uid='staticparent'
+      data-testid='staticparent'
+    />
+    <div
+      style={{
+        position: 'absolute',
+        width: 250,
+        height: 500,
+        left: 250,
+        top: 0,
+        backgroundColor: 'lightblue',
+      }}
+      data-uid='absoluteparent'
+      data-testid='absoluteparent'
+    />
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        position: 'absolute',
+        width: 250,
+        height: 500,
+        left: 500,
+        top: 0,
+        backgroundColor: 'lightgreen',
+      }}
+      data-uid='flexparent'
+      data-testid='flexparent'
+    >
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'teal',
+        }}
+        data-uid='flexchild1'
+        data-testid='flexchild1'
+      />
+      <div
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'red',
+        }}
+        data-uid='flexchild2'
+        data-testid='flexchild2'
+      >
+        <div
+          style={{
+            position: 'absolute',
+            left: 100,
+            top: 0,
+            width: 100,
+            height: 100,
+            backgroundColor: 'yellow',
+          }}
+          data-uid='absolutechild'
+          data-testid='absolutechild'
+        />
+      </div>
+    </div>
+  </div>`),
+    )
+  })
+
   it('Absolute to forced absolute is not the default choice', async () => {
     const renderResult = await renderTestEditorWithCode(
       makeTestProjectCodeWithSnippet(defaultTestCode),
