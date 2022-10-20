06af637cb2ed388f955409fb0a0c858c635759ff
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2022-10-20 17:55:20 +0200

    remove portal from html, add css

--- editor/src/components/canvas/controls/select-mode/cursor-overlay.tsx
+++ editor/src/components/canvas/controls/select-mode/cursor-overlay.tsx
@@ -4,7 +4,6 @@ import { cursorForMissingReparentedItems } from '../../canvas-strategies/strateg
 import { CSSCursor } from '../../canvas-types'
 import { getCursorFromDragState } from '../../canvas-utils'
 import { useDelayedEditorState } from '../../canvas-strategies/canvas-strategies'
-import { EditorID } from '../../../../core/shared/utils'
 
 export function getCursorFromEditor(editorState: EditorState): CSSCursor | null {
   const forMissingReparentedItems = cursorForMissingReparentedItems(
@@ -22,17 +21,12 @@ export const CursorOverlay = React.memo(() => {
   })
 
   React.useEffect(() => {
-    const editor = document.getElementById(EditorID)
     if (cursor != null) {
       document.body.classList.add('customCursor')
       document.body.style.cursor = cursor
     } else {
       document.body.classList.remove('customCursor')
       document.body.style.cursor = 'unset'
-    }
-
-    if (editor != null) {
-      editor.style.pointerEvents = 'initial'
     }
   }, [cursor])
 
