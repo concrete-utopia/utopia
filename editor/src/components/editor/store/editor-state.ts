9f8869a824c39a7a253decb0fce9c59e7d9e3a75
thetylerwolf <laptop@laptops-mbp.lan>
2022-11-02 12:24:03 +0100

    fix to editor-state.ts

--- editor/src/components/editor/store/editor-state.ts
+++ editor/src/components/editor/store/editor-state.ts
@@ -1,39 +1,80 @@
 import * as json5 from 'json5'
-import { MetadataUtils } from '../../../core/model/element-metadata-utils'
+import { findJSXElementAtPath, MetadataUtils } from '../../../core/model/element-metadata-utils'
 import {
-  findJSXElementAtStaticPath,
-  getUtopiaID,
-  insertJSXElementChild,
-  removeJSXElementChild,
-  transformJSXComponentAtPath,
-} from '../../../core/model/element-template-utils'
-import {
-  applyToAllUIJSFiles,
-  applyUtopiaJSXComponentsChanges,
-  getHighlightBoundsForProject,
-  getHighlightBoundsFromParseResult,
-  getUtopiaJSXComponentsFromSuccess,
-  saveTextFileContents,
-} from '../../../core/model/project-file-utils'
-import {
-  BakedInStoryboardVariableName,
-  getStoryboardElementPath,
-} from '../../../core/model/scene-utils'
-import { Either, isRight, left, mapEither, right } from '../../../core/shared/either'
-import {
+  ElementInstanceMetadata,
   ElementInstanceMetadataMap,
-  emptyJsxMetadata,
   getElementsByUIDFromTopLevelElements,
-  isJSXElement,
   isUtopiaJSXComponent,
-  JSXAttribute,
   JSXElement,
   JSXElementChild,
   TopLevelElement,
   UtopiaJSXComponent,
+  isJSXElement,
+  emptyJsxMetadata,
+  JSXAttribute,
   walkElements,
 } from '../../../core/shared/element-template'
+import {
+  insertJSXElementChild,
+  removeJSXElementChild,
+  transformJSXComponentAtPath,
+  getUtopiaID,
+  findJSXElementAtStaticPath,
+} from '../../../core/model/element-template-utils'
+import {
+  correctProjectContentsPath,
+  getOrDefaultScenes,
+  getUtopiaJSXComponentsFromSuccess,
+  saveTextFileContents,
+  getHighlightBoundsFromParseResult,
+  updateFileContents,
+  getHighlightBoundsForProject,
+  applyUtopiaJSXComponentsChanges,
+  applyToAllUIJSFiles,
+} from '../../../core/model/project-file-utils'
 import type { ErrorMessage, ErrorMessageSeverity } from '../../../core/shared/error-messages'
+import type { PackageStatus, PackageStatusMap } from '../../../core/shared/npm-dependency-types'
+import {
+  Imports,
+  ParseSuccess,
+  ProjectFile,
+  RevisionsState,
+  ElementPath,
+  TextFile,
+  isTextFile,
+  StaticElementPath,
+  NodeModules,
+  foldParsedTextFile,
+  textFileContents,
+  isParseSuccess,
+  codeFile,
+  isParseFailure,
+  isParsedTextFile,
+  HighlightBoundsForUids,
+  HighlightBoundsWithFile,
+  PropertyPath,
+  HighlightBoundsWithFileForUids,
+  parseSuccess,
+} from '../../../core/shared/project-file-types'
+import { diagnosticToErrorMessage } from '../../../core/workers/ts/ts-utils'
+import {
+  ExportsInfo,
+  MultiFileBuildResult,
+  UtopiaTsWorkers,
+} from '../../../core/workers/common/worker-types'
+import {
+  bimapEither,
+  Either,
+  foldEither,
+  isLeft,
+  isRight,
+  left,
+  mapEither,
+  right,
+} from '../../../core/shared/either'
+import { KeysPressed } from '../../../utils/keyboard'
+import { keepDeepReferenceEqualityIfPossible } from '../../../utils/react-performance'
+import Utils, { IndexPosition } from '../../../utils/utils'
 import {
   CanvasPoint,
   CanvasRectangle,
@@ -41,39 +82,14 @@ import {
   LocalRectangle,
   WindowPoint,
 } from '../../../core/shared/math-utils'
-import type { PackageStatus, PackageStatusMap } from '../../../core/shared/npm-dependency-types'
-import {
-  codeFile,
-  ElementPath,
-  foldParsedTextFile,
-  HighlightBoundsForUids,
-  HighlightBoundsWithFile,
-  HighlightBoundsWithFileForUids,
-  Imports,
-  isParsedTextFile,
-  isParseFailure,
-  isParseSuccess,
-  isTextFile,
-  NodeModules,
-  ParseSuccess,
-  parseSuccess,
-  ProjectFile,
-  RevisionsState,
-  StaticElementPath,
-  TextFile,
-  textFileContents,
-} from '../../../core/shared/project-file-types'
-import {
-  ExportsInfo,
-  MultiFileBuildResult,
-  UtopiaTsWorkers,
-} from '../../../core/workers/common/worker-types'
-import { KeysPressed } from '../../../utils/keyboard'
-import Utils, { IndexPosition } from '../../../utils/utils'
 import {
   addFileToProjectContents,
+  ensureDirectoriesExist,
+  getContentsTreeFileFromElements,
   getContentsTreeFileFromString,
   ProjectContentTreeRoot,
+  transformContentsTree,
+  walkContentsTree,
 } from '../../assets'
 import {
   CanvasFrameAndTarget,
@@ -87,6 +103,7 @@ import {
   getParseSuccessOrTransientForFilePath,
   produceCanvasTransientState,
 } from '../../canvas/canvas-utils'
+import { CursorPosition } from '../../code-editor/code-editor-utils'
 import { EditorPanel } from '../../common/actions/index'
 import {
   CodeResultCache,
@@ -96,40 +113,46 @@ import {
   PropertyControlsInfo,
   ResolveFn,
 } from '../../custom-code/code-file'
-import { FontSettings } from '../../inspector/common/css-utils'
-import { EditorDispatch, LoginState, ProjectListing } from '../action-types'
-import { CURRENT_PROJECT_VERSION } from '../actions/migrations/migrations'
 import { convertModeToSavedMode, EditorModes, Mode, PersistedMode } from '../editor-modes'
+import { FontSettings } from '../../inspector/common/css-utils'
+import { DebugDispatch, EditorDispatch, LoginState, ProjectListing } from '../action-types'
+import { CURRENT_PROJECT_VERSION } from '../actions/migrations/migrations'
 import { StateHistory } from '../history'
+import {
+  BakedInStoryboardVariableName,
+  getStoryboardElementPath,
+} from '../../../core/model/scene-utils'
 
 import {
+  toUid,
+  toString,
   dynamicPathToStaticPath,
   staticElementPath,
-  toString,
-  toUid,
 } from '../../../core/shared/element-path'
 
-import * as friendlyWords from 'friendly-words'
-import { defaultConfig, UtopiaVSCodeConfig } from 'utopia-vscode-common'
-import { loginNotYetKnown } from '../../../common/user'
-import * as EP from '../../../core/shared/element-path'
-import { forceNotNull } from '../../../core/shared/optional-utils'
-import { addToComplexMap, ComplexMap, emptyComplexMap } from '../../../utils/map'
 import { Notice } from '../../common/notice'
+import { emptyComplexMap, ComplexMap, addToComplexMap } from '../../../utils/map'
+import * as friendlyWords from 'friendly-words'
+import { fastForEach } from '../../../core/shared/utils'
 import { ShortcutConfiguration } from '../shortcut-definitions'
+import { loginNotYetKnown, notLoggedIn } from '../../../common/user'
+import { immediatelyResolvableDependenciesWithEditorRequirements } from '../npm-dependency/npm-dependency'
 import {
   DerivedStateKeepDeepEquality,
   ElementInstanceMetadataMapKeepDeepEquality,
 } from './store-deep-equality-instances'
+import { forceNotNull } from '../../../core/shared/optional-utils'
+import * as EP from '../../../core/shared/element-path'
+import { defaultConfig, UtopiaVSCodeConfig } from 'utopia-vscode-common'
 
 import * as OPI from 'object-path-immutable'
 import { MapLike } from 'typescript'
+import { pick } from '../../../core/shared/object-utils'
 import { LayoutTargetableProp } from '../../../core/layout/layout-helpers-new'
 import { atomWithPubSub } from '../../../core/shared/atom-with-pub-sub'
-import { pick } from '../../../core/shared/object-utils'
 
-import { Spec } from 'immutability-helper'
 import { v4 as UUID } from 'uuid'
+import { PersistenceMachine } from '../persistence/persistence'
 import type { BuiltInDependencies } from '../../../core/es-modules/package-manager/built-in-dependencies-list'
 import { UTOPIA_LABEL_KEY } from '../../../core/model/utopia-constants'
 import { FileResult } from '../../../core/shared/file-utils'
@@ -137,10 +160,14 @@ import { GithubFileStatus } from '../../../core/shared/github'
 import { memoize } from '../../../core/shared/memoize'
 import { emptySet } from '../../../core/shared/set-utils'
 import { DefaultThirdPartyControlDefinitions } from '../../../core/third-party/third-party-controls'
-import { MouseButtonsPressed } from '../../../utils/mouse'
+import { Spec } from 'immutability-helper'
+import { memoize } from '../../../core/shared/memoize'
 import { InteractionSession, StrategyState } from '../../canvas/canvas-strategies/interaction-state'
 import { GuidelineWithSnappingVectorAndPointsOfRelevance } from '../../canvas/guideline'
-import { PersistenceMachine } from '../persistence/persistence'
+import { MouseButtonsPressed } from '../../../utils/mouse'
+import { emptySet } from '../../../core/shared/set-utils'
+import { UTOPIA_LABEL_KEY } from '../../../core/model/utopia-constants'
+import { FileResult } from '../../../core/shared/file-utils'
 
 const ObjectPathImmutable: any = OPI
 
@@ -158,7 +185,6 @@ export const LeftPaneDefaultWidth = 260
 export const MenuBarWidth = 44
 
 const DefaultNavigatorWidth = 280
-
 export const NavigatorWidthAtom = atomWithPubSub({
   key: 'NavigatorWidthAtom',
   defaultValue: DefaultNavigatorWidth,
@@ -230,7 +256,8 @@ export interface UserState extends UserConfiguration {
 export type GithubOperation =
   | { name: 'commish' }
   | { name: 'listBranches' }
-  | { name: 'loadBranch'; branchName: string }
+  | { name: 'loadBranch'; branchName: string; githubRepo: GithubRepo }
+  | { name: 'loadRepositories' }
 
 export function githubOperationPrettyName(op: GithubOperation): string {
   switch (op.name) {
@@ -240,6 +267,8 @@ export function githubOperationPrettyName(op: GithubOperation): string {
       return 'Listing branches'
     case 'loadBranch':
       return 'Loading branch'
+    case 'loadRepositories':
+      return 'Loading Repositories'
     default:
       const _exhaustiveCheck: never = op
       return 'Unknown operation' // this should never happen
@@ -249,12 +278,23 @@ export function githubOperationPrettyName(op: GithubOperation): string {
 export function isGithubLoadingBranch(
   operations: Array<GithubOperation>,
   branchName: string,
+  repo: GithubRepo | null,
 ): boolean {
-  return operations.some((o) => o.name === 'loadBranch' && o.branchName === branchName)
+  return operations.some(
+    (o) =>
+      o.name === 'loadBranch' &&
+      o.branchName === branchName &&
+      o.githubRepo.owner === repo?.owner &&
+      o.githubRepo.repository === repo?.repository,
+  )
 }
 
 export function isGithubCommishing(operations: Array<GithubOperation>): boolean {
   return operations.some((o) => o.name === 'commish')
+}
+
+export function isGithubLoadingRepositories(operations: Array<GithubOperation>): boolean {
+  return operations.some((operation) => operation.name === 'loadRepositories')
 }
 
 export const defaultUserState: UserState = {
