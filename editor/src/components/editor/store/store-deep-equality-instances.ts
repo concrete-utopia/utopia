d1861d68713ae472615ff0fb9e68174e66e2ccc0
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2022-11-09 09:57:12 +0100

    Detect upstream changes and potential conflicts (#2779)
    * detect upstream changes and potential conflicts
    
    * use when
    
    * update data on reset
    
    * don't show branch UI if the branch is not set
    
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>

--- editor/src/components/editor/store/store-deep-equality-instances.ts
+++ editor/src/components/editor/store/store-deep-equality-instances.ts
@@ -452,7 +452,9 @@ import {
   textResult,
 } from '../../../core/shared/file-utils'
 import {
+  emptyGithubFileChanges,
   GithubBranch,
+  GithubFileChanges,
   GithubFileStatus,
   repositoryEntry,
   RepositoryEntry,
@@ -3241,11 +3243,26 @@ export const ProjectGithubSettingsKeepDeepEquality: KeepDeepEqualityCall<Project
     projectGithubSettings,
   )
 
-export const GithubDataKeepDeepEquality: KeepDeepEqualityCall<GithubData> = combine2EqualityCalls(
+export const GithubFileChangesKeepDeepEquality: KeepDeepEqualityCall<GithubFileChanges> =
+  combine3EqualityCalls(
+    (settings) => settings.modified,
+    arrayDeepEquality(StringKeepDeepEquality),
+    (settings) => settings.untracked,
+    arrayDeepEquality(StringKeepDeepEquality),
+    (settings) => settings.deleted,
+    arrayDeepEquality(StringKeepDeepEquality),
+    emptyGithubFileChanges,
+  )
+
+export const GithubDataKeepDeepEquality: KeepDeepEqualityCall<GithubData> = combine4EqualityCalls(
   (data) => data.branches,
   arrayDeepEquality(GithubBranchKeepDeepEquality),
   (data) => data.publicRepositories,
   arrayDeepEquality(RepositoryEntryKeepDeepEquality),
+  (data) => data.lastUpdatedAt,
+  NullableNumberKeepDeepEquality,
+  (data) => data.upstreamChanges,
+  nullableDeepEquality(GithubFileChangesKeepDeepEquality),
   emptyGithubData,
 )
 
