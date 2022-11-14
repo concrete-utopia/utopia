cbdc56bfbb436d949e3c218c5f8979501a620877
Federico Ruggi <federico.ruggi@shopify.com>
2022-11-14 18:44:50 +0100

    just do the boring thing and just set true when launching the fetch

--- editor/src/core/shared/dependencies.ts
+++ editor/src/core/shared/dependencies.ts
@@ -64,10 +64,6 @@ export async function refreshDependencies(
 
     const depsToFetch = newDeps.concat(updatedDeps)
 
-    if (depsToFetch.length > 0) {
-      dispatch([setRefreshingDependencies(true)], 'everyone')
-    }
-
     const fetchNodeModulesResult = await fetchNodeModules(depsToFetch, builtInDependencies)
 
     const loadedPackagesStatus = createLoadedPackageStatusMapFromDependencies(
@@ -86,6 +82,7 @@ export async function refreshDependencies(
     return updatedNodeModulesFiles
   }
 
+  dispatch([setRefreshingDependencies(true)], 'everyone')
   return doRefresh().finally(() => {
     dispatch([setRefreshingDependencies(false)], 'everyone')
   })
