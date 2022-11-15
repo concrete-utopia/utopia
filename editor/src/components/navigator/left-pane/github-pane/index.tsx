0a32bfc19a11265c2f3eb97b9ac2779ca7b677ba
Federico Ruggi <federico.ruggi@shopify.com>
2022-11-15 12:55:13 +0100

    update chevron up

--- editor/src/components/navigator/left-pane/github-pane/index.tsx
+++ editor/src/components/navigator/left-pane/github-pane/index.tsx
@@ -120,9 +120,9 @@ const ChevronUpIcon = () => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
-      width='12px'
-      height='12px'
-      viewBox='0 0 24 24'
+      width='11px'
+      height='11px'
+      viewBox='11 0 2 22'
       fill='none'
       stroke='currentColor'
       strokeWidth='2'
