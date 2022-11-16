db7079b31bf3d5f699e455e385db86d3c8fddea4
thetylerwolf <laptop@laptops-mbp.lan>
2022-11-16 16:22:24 +0100

    removed spurious comments

--- editor/src/uuiui/styles/css-vars.tsx
+++ editor/src/uuiui/styles/css-vars.tsx
@@ -1,8 +1,4 @@
-// TODO: pass in colorTheme to updateCssVars from a hook and remove the dependency here:
-import {
-  useColorThemeVariables,
-  // utopiaThemeCssValues
-} from './theme'
+import { useColorThemeVariables } from './theme'
 import React from 'react'
 
 export const ColorThemeStyleComponent = React.memo(() => {
@@ -16,10 +12,6 @@ export const ColorThemeStyleComponent = React.memo(() => {
         const [variable, value] = entry
         return `${variable}:${value};`
       })}
-      {/* {Object.entries(utopiaThemeCssValues).map((entry) => {
-        const [variable, value] = entry
-        return `${variable}:${value};`
-      })} */}
       {'}'}
     </style>
   )
