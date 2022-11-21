e1c6d0a97aadf87a88bc937a4c04c10fb91f49c0
Federico Ruggi <federico.ruggi@shopify.com>
2022-11-21 15:29:50 +0100

    use ??

--- editor/src/components/navigator/left-pane/github-pane/index.tsx
+++ editor/src/components/navigator/left-pane/github-pane/index.tsx
@@ -608,7 +608,7 @@ const LocalChangesBlock = () => {
           <StringInput
             testId='commit-message-input'
             placeholder='Commit message'
-            value={commitMessage || ''}
+            value={commitMessage ?? ''}
             onChange={updateCommitMessage}
           />
           {when(
@@ -879,7 +879,7 @@ const BranchNotLoadedBlock = () => {
                 <StringInput
                   testId='commit-message-input'
                   placeholder='Commit message'
-                  value={commitMessage || ''}
+                  value={commitMessage ?? ''}
                   onChange={updateCommitMessage}
                 />
                 <Button
