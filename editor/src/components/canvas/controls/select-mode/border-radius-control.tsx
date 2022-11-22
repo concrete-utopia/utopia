3ee7cc35660e69cd251796147f75f099fc5cc5b8
Bertalan Körmendy <korber314@gmail.com>
2022-11-22 13:24:55 +0100

    Make border radius handles appear instantly on mouseover (#2906)
    * feature
    
    * cleanup
    
    * `includes` -> `some` + `pathsEqual`
    
    Co-authored-by: Berci Kormendy <korber314@duck.com>

--- editor/src/components/canvas/controls/select-mode/border-radius-control.tsx
+++ editor/src/components/canvas/controls/select-mode/border-radius-control.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as EP from '../../../../core/shared/element-path'
 import { CanvasVector, Size, windowPoint } from '../../../../core/shared/math-utils'
 import { ElementPath } from '../../../../core/shared/project-file-types'
 import { Modifier } from '../../../../utils/modifiers'
@@ -66,20 +67,7 @@ export const BorderRadiusControl = controlForStrategyMemoized<BorderRadiusContro
 
   const colorTheme = useColorTheme()
 
-  const timeoutRef = React.useRef<NodeJS.Timeout | null>(null)
-  const [backgroundShown, setBackgroundShown] = React.useState<boolean>(false)
-  React.useEffect(() => {
-    const timeoutHandle = timeoutRef.current
-    if (timeoutHandle != null) {
-      clearTimeout(timeoutHandle)
-    }
-
-    if (hoveredViews.includes(selectedElement)) {
-      timeoutRef.current = setTimeout(() => setBackgroundShown(true), 200)
-    } else {
-      setBackgroundShown(false)
-    }
-  }, [hoveredViews, selectedElement])
+  const backgroundShown = hoveredViews.some((p) => EP.pathsEqual(p, selectedElement))
 
   const controlRef = useBoundingBox([selectedElement], (ref, boundingBox) => {
     if (isZeroSizedElement(boundingBox)) {
