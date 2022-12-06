f39d8467cb4d18f3841b1fc6ee39eb0162c2aac4
Malte <malte.nuhn@gmail.com>
2022-12-06 15:47:00 +0100

    ux(github) - only show github settings to logged-in users (#2963)
    * only show github settings to logged-in users
    
    * just to be on the safe side here
    
    Co-authored-by: Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>

--- editor/src/components/navigator/left-pane/github-pane/index.tsx
+++ editor/src/components/navigator/left-pane/github-pane/index.tsx
@@ -14,6 +14,7 @@ import {
   updateProjectWithBranchContent,
   useGithubFileChanges,
 } from '../../../../core/shared/github'
+import { User } from '../../../../uuiui-deps'
 import { startGithubAuthentication } from '../../../../utils/github-auth'
 import { unless, when } from '../../../../utils/react-conditionals'
 import {
@@ -27,6 +28,7 @@ import {
   SectionTitleRow,
   StringInput,
   Title,
+  UtopiaTheme,
 } from '../../../../uuiui'
 import * as EditorActions from '../../../editor/actions/action-creators'
 import {
@@ -951,8 +953,12 @@ const PullRequestBlock = () => {
 export const GithubPane = React.memo(() => {
   const githubUser = useEditorState(
     (store) => store.editor.githubData.githubUserDetails,
-    'Github user details',
+    'GithubPane githubUser',
   )
+  const isLoggedIn = useEditorState((store) => {
+    return User.isLoggedIn(store.userState.loginState)
+  }, 'GithubPane isLoggedIn')
+
   const openGithubProfile = React.useCallback(() => {
     if (githubUser != null) {
       window.open(githubUser.htmlURL, '_blank')
@@ -981,17 +987,31 @@ export const GithubPane = React.memo(() => {
             </Button>,
           )}
         </SectionTitleRow>
+        {unless(
+          isLoggedIn,
+          <FlexRow
+            style={{
+              paddingLeft: UtopiaTheme.layout.rowHorizontalPadding,
+              paddingRight: UtopiaTheme.layout.rowHorizontalPadding,
+            }}
+          >
+            <p>You need to be signed into Utopia to use the Github integration</p>
+          </FlexRow>,
+        )}
       </Section>
-      <Section style={{ padding: '10px' }}>
-        <AccountBlock />
-        <RepositoryBlock />
-        <BranchBlock />
-        <BranchNotLoadedBlock />
-        <RemoteChangesBlock />
-        <LocalChangesBlock />
-        <PullRequestBlock />
-        <PullRequestButton />
-      </Section>
+      {when(
+        isLoggedIn,
+        <Section style={{ padding: '10px' }}>
+          <AccountBlock />
+          <RepositoryBlock />
+          <BranchBlock />
+          <BranchNotLoadedBlock />
+          <RemoteChangesBlock />
+          <LocalChangesBlock />
+          <PullRequestBlock />
+          <PullRequestButton />
+        </Section>,
+      )}
     </>
   )
 })
