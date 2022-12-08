1ccb9dac56d3c3386828b0bbf23d82a984eb561b
Berci Kormendy <korber314@gmail.com>
2022-12-08 11:11:13 +0100

    basic inspector strategy

--- editor/src/components/inspector/inspector-strategies.tsx
+++ editor/src/components/inspector/inspector-strategies.tsx
@@ -1,8 +1,44 @@
-import { ElementInstanceMetadataMap } from '../../core/shared/element-template'
+import { MetadataUtils } from '../../core/model/element-metadata-utils'
+import {
+  ElementInstanceMetadataMap,
+  emptyComments,
+  jsxAttributeValue,
+} from '../../core/shared/element-template'
 import { ElementPath } from '../../core/shared/project-file-types'
+import * as PP from '../../core/shared/property-path'
 import { EditorAction } from '../editor/action-types'
+import { setProperty } from '../editor/actions/action-creators'
+import { FlexAlignment, FlexJustifyContent } from './inspector-common'
 
 export type InspectorStrategy = (
   metadata: ElementInstanceMetadataMap,
   selectedElementPaths: Array<ElementPath>,
-) => Array<EditorAction>
+) => Array<EditorAction> | null
+
+export const setFlexAlignJustifyContentStrategies = (
+  flexAlignment: FlexAlignment,
+  justifyContent: FlexJustifyContent,
+): Array<InspectorStrategy> => [
+  (metadata, elementPaths) => {
+    const elements = elementPaths.filter((e) =>
+      MetadataUtils.isFlexLayoutedContainer(MetadataUtils.findElementByElementPath(metadata, e)),
+    )
+
+    if (elements.length === 0) {
+      return null
+    }
+
+    return elements.flatMap((path) => [
+      setProperty(
+        path,
+        PP.create(['style', 'alignItems']),
+        jsxAttributeValue(flexAlignment, emptyComments),
+      ),
+      setProperty(
+        path,
+        PP.create(['style', 'justifyContent']),
+        jsxAttributeValue(justifyContent, emptyComments),
+      ),
+    ])
+  },
+]
