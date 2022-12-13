a9badb1b17aee6d5eb74e6d5283b9f1ec7c0a56d
Berci Kormendy <korber314@gmail.com>
2022-12-13 12:43:00 +0100

    wwwip

--- editor/src/components/inspector/fill-hug-fixed-control.tsx
+++ editor/src/components/inspector/fill-hug-fixed-control.tsx
@@ -1,25 +1,28 @@
 import React from 'react'
-import { MetadataUtils } from '../../core/model/element-metadata-utils'
+import { getLayoutProperty } from '../../core/layout/getLayoutProperty'
+import { getSimpleAttributeAtPath, MetadataUtils } from '../../core/model/element-metadata-utils'
 import { stripNulls } from '../../core/shared/array-utils'
-import { isStoryboardChild } from '../../core/shared/element-path'
-import { ElementInstanceMetadataMap } from '../../core/shared/element-template'
+import { defaultEither, foldEither, isLeft, isRight, right } from '../../core/shared/either'
+import { ElementInstanceMetadataMap, isJSXElement } from '../../core/shared/element-template'
 import { optionalMap } from '../../core/shared/optional-utils'
 import { ElementPath } from '../../core/shared/project-file-types'
+import { create } from '../../core/shared/property-path'
 import { assertNever, NO_OP } from '../../core/shared/utils'
-import { FlexRow, PopupList, SimpleNumberInput } from '../../uuiui'
+import { PopupList, SimpleNumberInput } from '../../uuiui'
 import { getControlStyles, SelectOption } from '../../uuiui-deps'
-import { useEditorState } from '../editor/store/store-hook'
+import { useEditorState, useRefEditorState } from '../editor/store/store-hook'
+import { CSSNumber, cssNumber, parseCSSLengthPercent } from './common/css-utils'
 import { metadataSelector, selectedViewsSelector } from './inpector-selectors'
+import { fillContainerApplicable, hugContentsApplicable } from './inspector-common'
+import {
+  InspectorStrategy,
+  runStrategies,
+  setPropFillStrategies,
+  setPropFixedStrategies,
+  setPropHugStrategies,
+} from './inspector-strategies'
 
 const controlId = (segment: 'width' | 'height') => `hug-fixed-fill-${segment}`
-
-const hugContentsApplicable = (
-  metadata: ElementInstanceMetadataMap,
-  elementPath: ElementPath,
-): boolean => MetadataUtils.getChildrenPaths(metadata, elementPath).length > 0
-
-const fillContainerApplicable = (elementPath: ElementPath): boolean =>
-  !isStoryboardChild(elementPath)
 
 type FixedHugFill = 'fixed' | 'hug' | 'fill'
 
@@ -59,12 +62,31 @@ const FillHugFixedControlOptions = ({
   ])
 
 function detectFillHugFixedState(
+  property: 'width' | 'height',
   metadata: ElementInstanceMetadataMap,
   elementPath: ElementPath | null,
 ): FixedHugFill | null {
   const element = MetadataUtils.findElementByElementPath(metadata, elementPath)
-  if (element == null) {
+  if (element == null || isLeft(element.element) || !isJSXElement(element.element.value)) {
     return null
+  }
+
+  const prop = defaultEither(
+    null,
+    getSimpleAttributeAtPath(right(element.element.value.props), create(['style', 'width'])),
+  )
+
+  if (prop == null || prop === 'min-content') {
+    return 'hug'
+  }
+
+  if (prop === '100%') {
+    return 'fill'
+  }
+
+  const parsed = parseCSSLengthPercent(prop)
+  if (isRight(parsed)) {
+    return 'fixed'
   }
 
   return null
@@ -86,13 +108,46 @@ export const FillHugFixedControl = React.memo<FillHugFixedControlProps>((props) 
     })
   }, 'FillHugFixedControl options')
 
-  const currentValue = useEditorState(
+  const dispatch = useEditorState((store) => store.dispatch, 'FillHugFixedControl dispatch')
+  const metadataRef = useRefEditorState(metadataSelector)
+  const selectedViewsRef = useRefEditorState(selectedViewsSelector)
+
+  const widthCurrentValue = useEditorState(
     (store) =>
       detectFillHugFixedState(
+        'width',
         metadataSelector(store),
         selectedViewsSelector(store).at(0) ?? null,
       ) ?? undefined,
     'FillHugFixedControl currentValue',
+  )
+
+  const heightCurrentValue = useEditorState(
+    (store) =>
+      detectFillHugFixedState(
+        'height',
+        metadataSelector(store),
+        selectedViewsSelector(store).at(0) ?? null,
+      ) ?? undefined,
+    'FillHugFixedControl currentValue',
+  )
+
+  const onSubmitHeight = React.useCallback(
+    ({ value: anyValue }: SelectOption) => {
+      const value = anyValue as FixedHugFill
+      const strategy = strategyForMode(cssNumber(42, 'px'), 'height', value)
+      runStrategies(dispatch, metadataRef.current, selectedViewsRef.current, strategy)
+    },
+    [dispatch, metadataRef, selectedViewsRef],
+  )
+
+  const onSubmitWidth = React.useCallback(
+    ({ value: anyValue }: SelectOption) => {
+      const value = anyValue as FixedHugFill
+      const strategy = strategyForMode(cssNumber(42, 'px'), 'width', value)
+      runStrategies(dispatch, metadataRef.current, selectedViewsRef.current, strategy)
+    },
+    [dispatch, metadataRef, selectedViewsRef],
   )
 
   const controlStylesRef = React.useRef(getControlStyles('simple'))
@@ -123,13 +178,13 @@ export const FillHugFixedControl = React.memo<FillHugFixedControlProps>((props) 
         stepSize={1}
         minimum={0}
         maximum={Infinity}
-        labelInner={'px'}
+        labelInner={'W'}
         defaultUnitToHide={'px'}
         focusOnMount={false}
       />
       <SimpleNumberInput
-        id={controlId('width')}
-        testId={controlId('width')}
+        id={controlId('height')}
+        testId={controlId('height')}
         value={4}
         onSubmitValue={NO_OP}
         onTransientSubmitValue={NO_OP}
@@ -139,24 +194,41 @@ export const FillHugFixedControl = React.memo<FillHugFixedControlProps>((props) 
         stepSize={1}
         minimum={0}
         maximum={Infinity}
-        labelInner={'px'}
-        defaultUnitToHide={'px'}
+        labelInner={'H'}
+        defaultUnitToHide={null}
         focusOnMount={false}
       />
       <PopupList
-        value={optionalMap(selectOption, currentValue) ?? undefined}
+        value={optionalMap(selectOption, widthCurrentValue) ?? undefined}
         options={options}
-        onSubmitValue={NO_OP}
+        onSubmitValue={onSubmitHeight}
         controlStyles={controlStylesRef.current}
         containerMode='showBorderOnHover'
       />
       <PopupList
-        value={optionalMap(selectOption, currentValue) ?? undefined}
+        value={optionalMap(selectOption, heightCurrentValue) ?? undefined}
         options={options}
-        onSubmitValue={NO_OP}
+        onSubmitValue={onSubmitWidth}
         controlStyles={controlStylesRef.current}
         containerMode='showBorderOnHover'
       />
     </div>
   )
 })
+
+function strategyForMode(
+  fixedValue: CSSNumber,
+  prop: 'width' | 'height',
+  mode: FixedHugFill,
+): Array<InspectorStrategy> {
+  switch (mode) {
+    case 'fill':
+      return setPropFillStrategies(prop)
+    case 'hug':
+      return setPropHugStrategies(prop)
+    case 'fixed':
+      return setPropFixedStrategies(prop, fixedValue)
+    default:
+      assertNever(mode)
+  }
+}
--- editor/src/components/inspector/inspector-common.tsx
+++ editor/src/components/inspector/inspector-common.tsx
@@ -1,4 +1,5 @@
 import { MetadataUtils } from '../../core/model/element-metadata-utils'
+import { isStoryboardChild } from '../../core/shared/element-path'
 import { ElementInstanceMetadataMap } from '../../core/shared/element-template'
 import { ElementPath } from '../../core/shared/project-file-types'
 import { EditorState, EditorStorePatched } from '../editor/store/editor-state'
@@ -101,3 +102,11 @@ export function detectFlexDirection(
 
 export const isFlexColumn = (flexDirection: FlexDirection): boolean =>
   flexDirection.startsWith('column')
+
+export const hugContentsApplicable = (
+  metadata: ElementInstanceMetadataMap,
+  elementPath: ElementPath,
+): boolean => MetadataUtils.getChildrenPaths(metadata, elementPath).length > 0
+
+export const fillContainerApplicable = (elementPath: ElementPath): boolean =>
+  !isStoryboardChild(elementPath)
