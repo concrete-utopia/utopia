3f13f55354ac77ba7252fd019efe18302eb7a018
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2022-12-15 12:21:32 +0000

    feat(editor) Only show re-order slider on hover

--- editor/src/components/canvas/controls/reorder-slider-control.tsx
+++ editor/src/components/canvas/controls/reorder-slider-control.tsx
@@ -46,6 +46,10 @@ export const ReorderSliderControl = controlForStrategyMemoized(
         store.editor.canvas.interactionSession.activeControl.type === 'REORDER_SLIDER',
       'ReorderSliderControl isDragging',
     )
+    const isSelectedElementHovered = useEditorState((store) => {
+      const { hoveredViews, selectedViews } = store.editor
+      return hoveredViews.includes(selectedViews[0])
+    }, 'ReorderSliderControl isSelectedElementHovered')
 
     const { siblings, latestIndex, startingIndex, startingFrame } = useEditorState(
       (store) => {
@@ -110,7 +114,7 @@ export const ReorderSliderControl = controlForStrategyMemoized(
       } as CanvasPoint
     }, [startingFrame, controlAreaTopLeft, latestIndex, scale])
 
-    if (siblings.length > 1) {
+    if (siblings.length > 1 && (isDragging || isSelectedElementHovered)) {
       return (
         <CanvasOffsetWrapper>
           {when(
