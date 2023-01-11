7a92e4b0f9dad8a5bc9df6ab971d81fc053b6e24
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-01-11 10:31:08 +0100

    text edit mode is always active text editing

--- editor/src/components/editor/editor-modes.ts
+++ editor/src/components/editor/editor-modes.ts
@@ -157,10 +157,6 @@ export function isTextEditMode(value: Mode): value is TextEditMode {
   return value.type === 'textEdit'
 }
 
-export function isTextEditModeWithActiveTextEditing(value: Mode): value is TextEditMode {
-  return value.type === 'textEdit' && value.editedText != null
-}
-
 export function convertModeToSavedMode(mode: Mode): PersistedMode {
   switch (mode.type) {
     case 'live':
