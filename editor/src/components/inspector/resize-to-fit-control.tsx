7aa95563d3493e736fdeaf1b1859da9a6b7bf59e
Berci Kormendy <korber314@gmail.com>
2023-01-23 13:51:42 +0100

    use `commandsForFirstApplicableStrategy` instead of batched actions

--- editor/src/components/inspector/resize-to-fit-control.tsx
+++ editor/src/components/inspector/resize-to-fit-control.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { FlexRow, Icn, Tooltip } from '../../uuiui'
 import { EditorAction, EditorDispatch } from '../editor/action-types'
+import { applyCommandsAction } from '../editor/actions/action-creators'
 import { useDispatch } from '../editor/store/dispatch-context'
-import { Substores, useEditorState } from '../editor/store/store-hook'
+import { Substores, useEditorState, useRefEditorState } from '../editor/store/store-hook'
 import { setPropHugStrategies } from './inspector-strategies/inspector-strategies'
-import { executeFirstApplicableStrategy } from './inspector-strategies/inspector-strategy'
+import {
+  commandsForFirstApplicableStrategy,
+  executeFirstApplicableStrategy,
+} from './inspector-strategies/inspector-strategy'
 
 export const ResizeToFitControlTestId = 'ResizeToFitControlTestId'
 
@@ -12,39 +16,27 @@ interface ResizeToFitControlProps {}
 
 export const ResizeToFitControl = React.memo<ResizeToFitControlProps>(() => {
   const dispatch = useDispatch()
-  const selectedViews = useEditorState(
-    Substores.selectedViews,
-    (store) => store.editor.selectedViews,
-    'ResizeToFitControl selectedViews',
-  )
+  const selectedViewsRef = useRefEditorState((store) => store.editor.selectedViews)
 
-  const metadata = useEditorState(
-    Substores.metadata,
-    (store) => store.editor.jsxMetadata,
-    'ResizeToFitControl metadata',
-  )
+  const metadataRef = useRefEditorState((store) => store.editor.jsxMetadata)
 
   const onMouseDown = React.useCallback(() => {
-    let batchedActions: Array<EditorAction> = []
-    const batchedDispatch: EditorDispatch = (actions) => {
-      batchedActions.push(...actions)
+    const commands = [
+      ...(commandsForFirstApplicableStrategy(
+        metadataRef.current,
+        selectedViewsRef.current,
+        setPropHugStrategies('horizontal'),
+      ) ?? []),
+      ...(commandsForFirstApplicableStrategy(
+        metadataRef.current,
+        selectedViewsRef.current,
+        setPropHugStrategies('vertical'),
+      ) ?? []),
+    ]
+    if (commands.length > 0) {
+      dispatch([applyCommandsAction(commands)])
     }
-    executeFirstApplicableStrategy(
-      batchedDispatch,
-      metadata,
-      selectedViews,
-      setPropHugStrategies('horizontal'),
-    )
-    executeFirstApplicableStrategy(
-      batchedDispatch,
-      metadata,
-      selectedViews,
-      setPropHugStrategies('vertical'),
-    )
-    if (batchedActions.length > 0) {
-      dispatch(batchedActions)
-    }
-  }, [dispatch, metadata, selectedViews])
+  }, [dispatch, metadataRef, selectedViewsRef])
 
   return (
     <Tooltip title={'Resize to Fit'}>
