db5e684aae3f26b833a12dd8c1bc606b06d4963c
Bertalan Körmendy <korber314@gmail.com>
2023-01-27 12:00:14 +0100

    `x` to remove absolute positioning (#3157)
    * `x` to remove absolute positioning
    
    * use `nukeAllAbsolutePositioningPropsCommands`
    
    * tests
    
    * remove leftovers

--- editor/src/components/editor/shortcut-definitions.ts
+++ editor/src/components/editor/shortcut-definitions.ts
@@ -1,5 +1,4 @@
 import { Key, Keyboard } from '../../utils/keyboard'
-import { objectMap } from '../../core/shared/object-utils'
 import {
   ComplexMap,
   emptyComplexMap,
@@ -7,6 +6,7 @@ import {
   getKeysFromComplexMap,
   getValueFromComplexMap,
 } from '../../utils/map'
+import { CSSProperties } from 'twind'
 
 const key = Keyboard.key
 
@@ -82,6 +82,7 @@ export const COPY_STYLE_PROPERTIES = 'copy-style-properties'
 
 export const OPEN_EYEDROPPPER = 'open-eyedropper'
 export const CONVERT_TO_FLEX_CONTAINER = 'convert-to-flex-container'
+export const REMOVE_ABSOLUTE_POSITIONING = 'remove-absolute-positioning'
 
 export type ShortcutDetails = { [key: string]: Shortcut }
 
@@ -236,6 +237,7 @@ const shortcutDetailsWithDefaults: ShortcutDetails = {
     'Convert selected elements to flex containers',
     key('a', ['shift']),
   ),
+  [REMOVE_ABSOLUTE_POSITIONING]: shortcut(`Strip absolute sizing props props`, key('x', [])),
   [COPY_STYLE_PROPERTIES]: shortcut('Copy style properties', key('c', ['alt', 'cmd'])),
   [PASTE_STYLE_PROPERTIES]: shortcut('Paste style properties', key('v', ['alt', 'cmd'])),
 }
