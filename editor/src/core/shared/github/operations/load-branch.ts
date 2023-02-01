c90bea192d0c8724947933d582744d1557f30a22
Federico Ruggi <federico.ruggi@shopify.com>
2023-02-01 17:15:18 +0100

    handle and log github errors

--- editor/src/core/shared/github/operations/load-branch.ts
+++ editor/src/core/shared/github/operations/load-branch.ts
@@ -9,6 +9,7 @@ import {
 } from '../../../../components/editor/actions/action-creators'
 import {
   GithubData,
+  GithubOperation,
   GithubRepo,
   packageJsonFileFromProjectContents,
 } from '../../../../components/editor/store/editor-state'
@@ -20,6 +21,8 @@ import {
   connectRepo,
   getBranchContentFromServer,
   GetBranchContentResponse,
+  githubAPIError,
+  githubAPIErrorFromResponse,
   runGithubOperation,
 } from '../helpers'
 
@@ -38,83 +41,66 @@ export async function updateProjectWithBranchContent(
       githubRepo: githubRepo,
     },
     dispatch,
-    async () => {
+    async (operation: GithubOperation) => {
       const response = await getBranchContentFromServer(githubRepo, branchName, null, null)
-      if (response.ok) {
-        const responseBody: GetBranchContentResponse = await response.json()
-        switch (responseBody.type) {
-          case 'FAILURE':
-            dispatch(
-              [showToast(notice(`Error saving to Github: ${responseBody.failureReason}`, 'ERROR'))],
-              'everyone',
+      if (!response.ok) {
+        throw await githubAPIErrorFromResponse(operation, response)
+      }
+
+      const responseBody: GetBranchContentResponse = await response.json()
+      switch (responseBody.type) {
+        case 'FAILURE':
+          throw githubAPIError(operation, responseBody.failureReason)
+        case 'SUCCESS':
+          if (responseBody.branch == null) {
+            throw githubAPIError(operation, `Could not find branch ${branchName}`)
+          }
+          const newGithubData: Partial<GithubData> = {
+            upstreamChanges: null,
+          }
+          if (resetBranches) {
+            newGithubData.branches = null
+          }
+
+          const packageJson = packageJsonFileFromProjectContents(responseBody.branch.content)
+          if (packageJson != null && isTextFile(packageJson)) {
+            await refreshDependencies(
+              dispatch,
+              packageJson.fileContents.code,
+              currentDeps,
+              builtInDependencies,
+              {},
             )
-            break
-          case 'SUCCESS':
-            if (responseBody.branch == null) {
-              dispatch(
-                [showToast(notice(`Github: Could not find branch ${branchName}.`, 'ERROR'))],
-                'everyone',
-              )
-            } else {
-              const newGithubData: Partial<GithubData> = {
-                upstreamChanges: null,
-              }
-              if (resetBranches) {
-                newGithubData.branches = null
-              }
+          }
 
-              const packageJson = packageJsonFileFromProjectContents(responseBody.branch.content)
-              if (packageJson != null && isTextFile(packageJson)) {
-                await refreshDependencies(
-                  dispatch,
-                  packageJson.fileContents.code,
-                  currentDeps,
-                  builtInDependencies,
-                  {},
-                )
-              }
-
-              dispatch(
-                [
-                  ...connectRepo(
-                    resetBranches,
-                    githubRepo,
-                    responseBody.branch.originCommit,
-                    branchName,
-                    true,
-                  ),
-                  updateGithubChecksums(
-                    getProjectContentsChecksums(responseBody.branch.content, {}),
-                  ),
-                  updateProjectContents(responseBody.branch.content),
-                  updateBranchContents(responseBody.branch.content),
-                  showToast(
-                    notice(
-                      `Github: Updated the project with the content from ${branchName}`,
-                      'SUCCESS',
-                    ),
-                  ),
-                ],
-                'everyone',
-              )
-            }
-            break
-          default:
-            const _exhaustiveCheck: never = responseBody
-            throw new Error(`Github: Unhandled response body ${JSON.stringify(responseBody)}`)
-        }
-      } else {
-        dispatch(
-          [
-            showToast(
-              notice(
-                `Github: Unexpected status returned from endpoint: ${response.status}`,
-                'ERROR',
+          dispatch(
+            [
+              ...connectRepo(
+                resetBranches,
+                githubRepo,
+                responseBody.branch.originCommit,
+                branchName,
+                true,
               ),
-            ),
-          ],
-          'everyone',
-        )
+              updateGithubChecksums(getProjectContentsChecksums(responseBody.branch.content, {})),
+              updateProjectContents(responseBody.branch.content),
+              updateBranchContents(responseBody.branch.content),
+              showToast(
+                notice(
+                  `Github: Updated the project with the content from ${branchName}`,
+                  'SUCCESS',
+                ),
+              ),
+            ],
+            'everyone',
+          )
+          break
+        default:
+          const _exhaustiveCheck: never = responseBody
+          throw githubAPIError(
+            operation,
+            `Github: Unhandled response body ${JSON.stringify(responseBody)}`,
+          )
       }
       return []
     },
--- editor/src/core/shared/github/operations/load-repositories.ts
+++ editor/src/core/shared/github/operations/load-repositories.ts
@@ -1,16 +1,23 @@
 import urljoin from 'url-join'
 import { UTOPIA_BACKEND } from '../../../../common/env-vars'
 import { HEADERS, MODE } from '../../../../common/server'
-import { notice } from '../../../../components/common/notice'
 import { EditorAction, EditorDispatch } from '../../../../components/editor/action-types'
 import {
   setGithubState,
-  showToast,
   updateGithubData,
   updateGithubSettings,
 } from '../../../../components/editor/actions/action-creators'
-import { emptyGithubSettings } from '../../../../components/editor/store/editor-state'
-import { GithubFailure, RepositoryEntry, runGithubOperation } from '../helpers'
+import {
+  emptyGithubSettings,
+  GithubOperation,
+} from '../../../../components/editor/store/editor-state'
+import {
+  githubAPIError,
+  githubAPIErrorFromResponse,
+  GithubFailure,
+  RepositoryEntry,
+  runGithubOperation,
+} from '../helpers'
 
 export interface GetUsersPublicRepositoriesSuccess {
   type: 'SUCCESS'
@@ -22,34 +29,35 @@ export type GetUsersPublicRepositoriesResponse = GetUsersPublicRepositoriesSucce
 export async function getUsersPublicGithubRepositories(
   dispatch: EditorDispatch,
 ): Promise<Array<EditorAction>> {
-  return runGithubOperation({ name: 'loadRepositories' }, dispatch, async () => {
-    const url = urljoin(UTOPIA_BACKEND, 'github', 'user', 'repositories')
+  return runGithubOperation(
+    { name: 'loadRepositories' },
+    dispatch,
+    async (operation: GithubOperation) => {
+      const url = urljoin(UTOPIA_BACKEND, 'github', 'user', 'repositories')
 
-    const response = await fetch(url, {
-      method: 'GET',
-      credentials: 'include',
-      headers: HEADERS,
-      mode: MODE,
-    })
-    if (response.ok) {
+      const response = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+        headers: HEADERS,
+        mode: MODE,
+      })
+      if (!response.ok) {
+        throw await githubAPIErrorFromResponse(operation, response)
+      }
+
       const responseBody: GetUsersPublicRepositoriesResponse = await response.json()
       switch (responseBody.type) {
         case 'FAILURE':
-          const actions: EditorAction[] = [
-            showToast(
-              notice(
-                `Github: Error getting a user's repositories: ${responseBody.failureReason}`,
-                'ERROR',
-              ),
-            ),
-          ]
           if (responseBody.failureReason.includes('Authentication')) {
-            actions.push(
-              updateGithubSettings(emptyGithubSettings()),
-              setGithubState({ authenticated: false }),
+            dispatch(
+              [
+                updateGithubSettings(emptyGithubSettings()),
+                setGithubState({ authenticated: false }),
+              ],
+              'everyone',
             )
           }
-          return actions
+          throw githubAPIError(operation, responseBody.failureReason)
         case 'SUCCESS':
           return [
             updateGithubData({
@@ -58,14 +66,8 @@ export async function getUsersPublicGithubRepositories(
           ]
         default:
           const _exhaustiveCheck: never = responseBody
-          throw new Error(`Github: Unhandled response body ${JSON.stringify(responseBody)}`)
+          throw githubAPIError(operation, `Unhandled response body ${JSON.stringify(responseBody)}`)
       }
-    } else {
-      return [
-        showToast(
-          notice(`Github: Unexpected status returned from endpoint: ${response.status}`, 'ERROR'),
-        ),
-      ]
-    }
-  })
+    },
+  )
 }
