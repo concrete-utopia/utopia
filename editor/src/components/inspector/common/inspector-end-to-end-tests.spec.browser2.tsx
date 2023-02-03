82a3696db2c940f423dc4836e46e8300f9ef13ef
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-02-03 15:49:45 +0100

    Make 4/2/1 control prefer shorthands where applicable (#3240)
    * use shorthand if empty
    
    * use lengthpercent for radius, px for padding
    
    * use length for padding
    
    * test shorthand fix
    
    * allow percent in padding controls
    
    * expect undo steps
    
    * only assert single undo steps
    
    ---------
    
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>

--- editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser2.tsx
+++ editor/src/components/inspector/common/inspector-end-to-end-tests.spec.browser2.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable jest/expect-expect */
-import React from 'react'
 import { fireEvent, RenderResult, screen } from '@testing-library/react'
 import {
   BakedInStoryboardUID,
@@ -39,6 +38,7 @@ import { DefaultPackageJson, StoryboardFilePath } from '../../editor/store/edito
 import { createCodeFile } from '../../custom-code/code-file.test-utils'
 import { matchInlineSnapshotBrowser } from '../../../../test/karma-snapshots'
 import { EditorAction } from '../../editor/action-types'
+import { expectSingleUndoStep } from '../../../utils/utils.test-utils'
 
 async function getControl(
   controlTestId: string,
@@ -2052,6 +2052,149 @@ describe('inspector tests with real metadata', () => {
       paddingLeftControl.attributes.getNamedItemNS(null, 'data-controlstatus')?.value,
       `"simple"`,
     )
+  })
+  describe('padding controls shorthand', () => {
+    function makeCodeSnippetWithKeyValue(props: { [key: string]: any }): string {
+      const propsStr = Object.keys(props)
+        .map((k) => `${k}: ${JSON.stringify(props[k])},`)
+        .join('\n')
+      return `
+        <div
+          data-uid='aaa'
+        >
+          <div
+            style={{ ${propsStr} }}
+            data-uid='bbb'
+          >test</div>
+        </div>
+    `
+    }
+
+    const tests = [
+      {
+        name: 'without props',
+        startSnippet: makeCodeSnippetWithKeyValue({}),
+        control: async (renderResult: EditorRenderResult) => {
+          await expectSingleUndoStep(renderResult, async () => {
+            await setControlValue('padding-V', '20', renderResult.renderedDOM)
+          })
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({ padding: '20px 0px' }),
+      },
+      {
+        name: 'with shorthand',
+        startSnippet: makeCodeSnippetWithKeyValue({ padding: 10 }),
+        control: async (renderResult: EditorRenderResult) => {
+          await expectSingleUndoStep(renderResult, async () => {
+            await setControlValue('padding-one', '20', renderResult.renderedDOM)
+          })
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({ padding: 20 }),
+      },
+      {
+        name: 'with single value (2-values)',
+        startSnippet: makeCodeSnippetWithKeyValue({ paddingLeft: 10 }),
+        control: async (renderResult: EditorRenderResult) => {
+          await setControlValue('padding-V', '20', renderResult.renderedDOM)
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({
+          paddingLeft: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+        }),
+      },
+      {
+        name: 'with single value (1-value)',
+        startSnippet: makeCodeSnippetWithKeyValue({ paddingLeft: 10 }),
+        before: async (renderResult: EditorRenderResult) => {
+          await act(async () => {
+            fireEvent.click(screen.getByTestId('padding-cycle-mode'))
+            await renderResult.getDispatchFollowUpActionsFinished()
+            fireEvent.click(screen.getByTestId('padding-cycle-mode'))
+            await renderResult.getDispatchFollowUpActionsFinished()
+          })
+        },
+        control: async (renderResult: EditorRenderResult) => {
+          await setControlValue('padding-one', '20', renderResult.renderedDOM)
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({
+          paddingLeft: 20,
+          paddingTop: 20,
+          paddingBottom: 20,
+          paddingRight: 20,
+        }),
+      },
+      {
+        name: 'with multiple values (1-value)',
+        startSnippet: makeCodeSnippetWithKeyValue({ paddingLeft: 10, paddingRight: 20 }),
+        before: async (renderResult: EditorRenderResult) => {
+          await act(async () => {
+            fireEvent.click(screen.getByTestId('padding-cycle-mode'))
+            await renderResult.getDispatchFollowUpActionsFinished()
+            fireEvent.click(screen.getByTestId('padding-cycle-mode'))
+            await renderResult.getDispatchFollowUpActionsFinished()
+          })
+        },
+        control: async (renderResult: EditorRenderResult) => {
+          await setControlValue('padding-one', '20', renderResult.renderedDOM)
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({
+          paddingLeft: 20,
+          paddingRight: 20,
+          paddingTop: 20,
+          paddingBottom: 20,
+        }),
+      },
+      {
+        name: 'with shorthand (2-value)',
+        startSnippet: makeCodeSnippetWithKeyValue({ padding: 10 }),
+        before: async (renderResult: EditorRenderResult) => {
+          await act(async () => {
+            fireEvent.click(screen.getByTestId('padding-cycle-mode'))
+            await renderResult.getDispatchFollowUpActionsFinished()
+          })
+        },
+        control: async (renderResult: EditorRenderResult) => {
+          await expectSingleUndoStep(renderResult, async () => {
+            await setControlValue('padding-H', '20', renderResult.renderedDOM)
+          })
+        },
+        endSnippet: makeCodeSnippetWithKeyValue({ padding: '10px 20px' }),
+      },
+    ]
+
+    tests.forEach((tt) => {
+      it(`padding controls shorthand: ${tt.name}`, async () => {
+        const renderResult = await renderTestEditorWithCode(
+          makeTestProjectCodeWithSnippet(tt.startSnippet),
+          'await-first-dom-report',
+        )
+
+        const targetPath = EP.appendNewElementPath(TestScenePath, ['aaa', 'bbb'])
+
+        await act(async () => {
+          await renderResult.dispatch([selectComponents([targetPath], false)], false)
+        })
+
+        if (tt.before != null) {
+          await tt.before(renderResult)
+          await renderResult.getDispatchFollowUpActionsFinished()
+        }
+        await tt.control(renderResult)
+
+        await act(async () => {
+          const dispatchDone = renderResult.getDispatchFollowUpActionsFinished()
+          await renderResult.dispatch([selectComponents([targetPath], false)], true)
+          await dispatchDone
+        })
+
+        await renderResult.getDispatchFollowUpActionsFinished()
+
+        expect(getPrintedUiJsCode(renderResult.getEditorState())).toEqual(
+          makeTestProjectCodeWithSnippet(tt.endSnippet),
+        )
+      })
+    })
   })
 })
 
