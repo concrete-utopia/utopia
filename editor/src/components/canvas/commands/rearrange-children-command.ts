53e6f95f5104902dde63077eecb9bc41089ccddc
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-02-06 11:17:29 +0100

    rearrangeChildren command works

--- editor/src/components/canvas/commands/rearrange-children-command.ts
+++ editor/src/components/canvas/commands/rearrange-children-command.ts
@@ -1,33 +1,34 @@
 import { IndexPosition } from '../../../utils/utils'
 import { getUtopiaJSXComponentsFromSuccess } from '../../../core/model/project-file-utils'
 import * as EP from '../../../core/shared/element-path'
-import { ElementPath } from '../../../core/shared/project-file-types'
+import { ElementPath, StaticElementPath } from '../../../core/shared/project-file-types'
 import { EditorState, withUnderlyingTargetFromEditorState } from '../../editor/store/editor-state'
 import { reorderComponent } from '../canvas-utils'
 import { BaseCommand, CommandFunction, getPatchForComponentChange, WhenToRun } from './commands'
+import { rearrangeJsxChildren } from '../../../core/model/element-template-utils'
 
-export interface ReorderChildren extends BaseCommand {
-  type: 'REORDER_CHILDREN'
+export interface RearrangeChildren extends BaseCommand {
+  type: 'REARRANGE_CHILDREN'
   target: ElementPath
-  rearrangedChildrenOrder: Array<ElementPath>
+  rearrangedChildrenOrder: Array<StaticElementPath>
 }
 
-export function reorderChildren(
+export function rearrangeChildren(
   whenToRun: WhenToRun,
   target: ElementPath,
-  rearrangedChildrenOrder: Array<ElementPath>,
-): ReorderChildren {
+  rearrangedChildrenOrder: Array<StaticElementPath>,
+): RearrangeChildren {
   return {
-    type: 'REORDER_CHILDREN',
+    type: 'REARRANGE_CHILDREN',
     whenToRun: whenToRun,
     target: target,
     rearrangedChildrenOrder: rearrangedChildrenOrder,
   }
 }
 
-export const runReorderChildren: CommandFunction<ReorderChildren> = (
+export const runRearrangeChildren: CommandFunction<RearrangeChildren> = (
   editorState: EditorState,
-  command: ReorderChildren,
+  command: RearrangeChildren,
 ) => {
   const patch = withUnderlyingTargetFromEditorState(
     command.target,
@@ -35,7 +36,17 @@ export const runReorderChildren: CommandFunction<ReorderChildren> = (
     {},
     (success, underlyingElement, underlyingTarget, underlyingFilePath) => {
       const components = getUtopiaJSXComponentsFromSuccess(success)
-      return {}
+      const withRearrange = rearrangeJsxChildren(
+        EP.dynamicPathToStaticPath(command.target),
+        command.rearrangedChildrenOrder,
+        components,
+      )
+      return getPatchForComponentChange(
+        success.topLevelElements,
+        withRearrange,
+        success.imports,
+        underlyingFilePath,
+      )
     },
   )
   return {
