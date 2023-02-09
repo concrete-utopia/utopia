c11318d334bfe145b2a9439d1dc32e45ab45e9dc
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-02-09 16:00:23 +0100

    absolute move children of group-like divs!

--- editor/src/components/canvas/canvas-strategies/strategies/absolute-move-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/absolute-move-strategy.tsx
@@ -1,5 +1,8 @@
 import { MetadataUtils } from '../../../../core/model/element-metadata-utils'
-import { ElementInstanceMetadataMap } from '../../../../core/shared/element-template'
+import {
+  ElementInstanceMetadataMap,
+  getJSXAttribute,
+} from '../../../../core/shared/element-template'
 import { ImmediateParentBounds } from '../../controls/parent-bounds'
 import { ImmediateParentOutlines } from '../../controls/parent-outlines'
 import { honoursPropsPosition } from './absolute-utils'
@@ -10,6 +13,7 @@ import {
   getTargetPathsFromInteractionTarget,
   InteractionCanvasState,
   MoveStrategy,
+  targetPaths,
 } from '../canvas-strategy-types'
 import { InteractionSession } from '../interaction-state'
 import {
@@ -23,8 +27,83 @@ import {
   dragTargetsElementPaths,
   dragTargetsElementPathsLive,
 } from '../../controls/select-mode/drag-outline-control'
+import { toString } from '../../../../core/shared/element-path'
+import { ElementPath } from '../../../../core/shared/project-file-types'
 
 export function absoluteMoveStrategy(
+  canvasState: InteractionCanvasState,
+  interactionSession: InteractionSession | null,
+): MoveStrategy | null {
+  return (
+    [groupLikeMoveStrategy, realAbsoluteMoveStrategy]
+      .map((s) => s(canvasState, interactionSession))
+      .find((s) => s != null) ?? null
+  )
+}
+
+function groupLikeMoveStrategy(
+  canvasState: InteractionCanvasState,
+  interactionSession: InteractionSession | null,
+): MoveStrategy | null {
+  const selectedElements = getTargetPathsFromInteractionTarget(canvasState.interactionTarget)
+
+  let actualTargetsIfApplicable: Array<ElementPath> = []
+
+  const isApplicable =
+    selectedElements.length > 0 &&
+    getDragTargets(selectedElements).every((path) => {
+      const elementMetadata = MetadataUtils.findElementByElementPath(
+        canvasState.startingMetadata,
+        path,
+      )
+
+      const elementProps = canvasState?.startingAllElementProps[toString(path)]
+
+      const hasNoWidthOrHeightProps =
+        elementProps?.['style']?.['width'] == null && elementProps?.['style']?.['height'] == null
+
+      const parentIsNotFlex = elementMetadata?.specialSizeMeasurements.parentLayoutSystem !== 'flex'
+
+      const isApplicableElement =
+        elementMetadata?.specialSizeMeasurements.position === 'static' &&
+        elementMetadata?.specialSizeMeasurements.layoutSystemForChildren === 'flow' &&
+        hasNoWidthOrHeightProps &&
+        parentIsNotFlex
+
+      if (isApplicableElement) {
+        const childPaths = MetadataUtils.getChildrenPaths(canvasState.startingMetadata, path)
+        actualTargetsIfApplicable.push(...childPaths)
+      }
+
+      return isApplicableElement
+    })
+
+  if (!isApplicable) {
+    return null
+  }
+
+  // behave like actualTargetsIfApplicable were the original multiselection, then delegate to realAbsoluteMoveStrategy
+  const updatedCanvasState: InteractionCanvasState = {
+    ...canvasState,
+    interactionTarget: targetPaths(actualTargetsIfApplicable),
+  }
+
+  const realStrat = realAbsoluteMoveStrategy(updatedCanvasState, interactionSession)
+  if (realStrat == null) {
+    return null
+  }
+
+  return {
+    ...realStrat,
+    strategy: {
+      ...realStrat.strategy,
+      id: 'GROUP_LIKE_MOVE',
+      name: 'Move (children)',
+    },
+  }
+}
+
+function realAbsoluteMoveStrategy(
   canvasState: InteractionCanvasState,
   interactionSession: InteractionSession | null,
 ): MoveStrategy | null {
