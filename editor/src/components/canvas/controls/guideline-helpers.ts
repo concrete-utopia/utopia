0c699e222782ebc3883be6b0e4e29258adf942b2
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-02-10 11:41:42 +0100

    Revert "Filter out fragments from snapping"
    This reverts commit c56663a07f06e5c5463d8d44edf0d4380ca53825.

--- editor/src/components/canvas/controls/guideline-helpers.ts
+++ editor/src/components/canvas/controls/guideline-helpers.ts
@@ -40,15 +40,10 @@ export function collectParentAndSiblingGuidelines(
         // for now we only snap to parents and sibligns and not us or our descendants
         const isSibling = EP.isSiblingOf(maybeTarget, target)
         const isParent = EP.pathsEqual(parent, maybeTarget)
-        const maybeTargetElement = MetadataUtils.findElementByElementPath(
-          componentMetadata,
-          maybeTarget,
-        )
-        const isFragment = MetadataUtils.isFragmentFromMetadata(maybeTargetElement)
         const notSelectedOrDescendantOfSelected = targets.every(
           (view) => !EP.isDescendantOfOrEqualTo(maybeTarget, view),
         )
-        if ((isSibling || isParent) && !isFragment && notSelectedOrDescendantOfSelected) {
+        if ((isSibling || isParent) && notSelectedOrDescendantOfSelected) {
           const frame = MetadataUtils.getFrameInCanvasCoords(maybeTarget, componentMetadata)
           if (frame != null && isFiniteRectangle(frame)) {
             result.push(...Guidelines.guidelinesWithRelevantPointsForFrame(frame, 'include'))
@@ -82,13 +77,8 @@ export function collectSelfAndChildrenGuidelines(
 
       Utils.fastForEach(allPaths, (maybeTarget) => {
         if (EP.isChildOf(maybeTarget, target) && EP.toUid(maybeTarget) !== insertingElementId) {
-          const maybeTargetElement = MetadataUtils.findElementByElementPath(
-            componentMetadata,
-            maybeTarget,
-          )
-          const isFragment = MetadataUtils.isFragmentFromMetadata(maybeTargetElement)
           const frame = MetadataUtils.getFrameInCanvasCoords(maybeTarget, componentMetadata)
-          if (frame != null && isFiniteRectangle(frame) && !isFragment) {
+          if (frame != null && isFiniteRectangle(frame)) {
             result.push(...Guidelines.guidelinesWithRelevantPointsForFrame(frame, 'include'))
           }
         }
