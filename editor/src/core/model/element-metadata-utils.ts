810722e1d6a928ebe9f9b693157245726d446898
Sean Parsons <git@futurenotfound.com>
2023-02-13 12:21:56 +0000

    chore(pr) Added a comment explaining some logic that backfills frames.

--- editor/src/core/model/element-metadata-utils.ts
+++ editor/src/core/model/element-metadata-utils.ts
@@ -1757,6 +1757,9 @@ function fillSpyOnlyMetadataWithFramesFromChildren(
   }
 
   const elementsWithoutDomMetadata = Object.keys(fromSpy).filter((p) => fromDOM[p] == null)
+  // Sort and then reverse these, so that lower level elements are handled ahead of their parents
+  // and ancestors. This means that if there are a grandparent and parent which both lack global frames
+  // then the parent is fixed ahead of the grandparent, which will be based on the parent.
   elementsWithoutDomMetadata.sort()
   elementsWithoutDomMetadata.reverse()
 
