2b9fa8f03ed4e86052709fe0a12bdb5e9d86c731
Berci Kormendy <korber314@gmail.com>
2023-02-22 09:32:25 +0100

    wip - all buttons in one

--- editor/src/components/inspector/resize-to-fit-control.tsx
+++ editor/src/components/inspector/resize-to-fit-control.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import { createSelector } from 'reselect'
 import { FlexRow, Icn, Tooltip } from '../../uuiui'
 import { applyCommandsAction } from '../editor/actions/action-creators'
 import { useDispatch } from '../editor/store/dispatch-context'
 import { Substores, useEditorState, useRefEditorState } from '../editor/store/store-hook'
 import { metadataSelector, selectedViewsSelector } from './inpector-selectors'
-import { notFixedSizeOnBothAxes, toggleResizeToFitSetToFixed } from './inspector-common'
+import {
+  notFixedSizeOnBothAxes,
+  resizeToFillCommands,
+  resizeToFitCommands,
+  sizeToVisualDimensions,
+  toggleResizeToFillSetToFixed,
+  toggleResizeToFitSetToFixed,
+} from './inspector-common'
 
 export const ResizeToFitControlTestId = 'ResizeToFitControlTestId'
 
@@ -29,25 +36,45 @@ export const ResizeToFitControl = React.memo<ResizeToFitControlProps>(() => {
     'ResizeToFitControl notFixedSizeOnEitherAxisState',
   )
 
-  const onMouseDown = React.useCallback(() => {
-    const commands = toggleResizeToFitSetToFixed(metadataRef.current, selectedViewsRef.current)
+  const onResizeToFit = React.useCallback(() => {
+    const commands = resizeToFitCommands(metadataRef.current, selectedViewsRef.current)
     if (commands.length > 0) {
       dispatch([applyCommandsAction(commands)])
     }
   }, [dispatch, metadataRef, selectedViewsRef])
 
-  const toolTipText = notFixedSizeOnEitherAxisState ? 'Set to Fixed Size' : 'Resize to Fit'
+  const onResizeToFill = React.useCallback(() => {
+    const commands = resizeToFillCommands(metadataRef.current, selectedViewsRef.current)
+    if (commands.length > 0) {
+      dispatch([applyCommandsAction(commands)])
+    }
+  }, [dispatch, metadataRef, selectedViewsRef])
+
+  const onSetToFixedSize = React.useCallback(() => {
+    const commands = selectedViewsRef.current.flatMap((e) =>
+      sizeToVisualDimensions(metadataRef.current, e),
+    )
+    if (commands.length > 0) {
+      dispatch([applyCommandsAction(commands)])
+    }
+  }, [dispatch, metadataRef, selectedViewsRef])
+
+  const disabledStyles: CSSProperties = notFixedSizeOnEitherAxisState
+    ? {
+        cursor: 'pointer',
+        opacity: 0.5,
+        pointerEvents: 'none',
+      }
+    : { cursor: 'pointer' }
 
   return (
-    <Tooltip title={toolTipText}>
-      <FlexRow
-        data-testid={ResizeToFitControlTestId}
-        onMouseDown={onMouseDown}
-        style={{ cursor: 'pointer' }}
-      >
-        {notFixedSizeOnEitherAxisState ? (
-          <Icn type='fixed' color='main' category='layout/commands' width={16} height={16} />
-        ) : (
+    <FlexRow style={{ gap: 12 }}>
+      <Tooltip title={'Resize to Fit'}>
+        <div
+          data-testid={ResizeToFitControlTestId}
+          onMouseDown={onResizeToFit}
+          style={disabledStyles}
+        >
           <Icn
             type='fitToChildren'
             color='main'
@@ -55,8 +82,18 @@ export const ResizeToFitControl = React.memo<ResizeToFitControlProps>(() => {
             width={18}
             height={18}
           />
-        )}
-      </FlexRow>
-    </Tooltip>
+        </div>
+      </Tooltip>
+      <Tooltip title={'Resize to Fill'}>
+        <div onMouseDown={onResizeToFill} style={disabledStyles}>
+          <Icn type='growToParent' color='main' category='layout/commands' width={18} height={18} />
+        </div>
+      </Tooltip>
+      <Tooltip title={'Fixed size'}>
+        <div onMouseDown={onSetToFixedSize} style={disabledStyles}>
+          <Icn type='fixed' color='main' category='layout/commands' width={16} height={16} />
+        </div>
+      </Tooltip>
+    </FlexRow>
   )
 })
