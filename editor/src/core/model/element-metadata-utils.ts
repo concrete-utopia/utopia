99696979c00944c1b865dbcd2808ab0dbb774afc
Bertalan Körmendy <korber314@gmail.com>
2023-02-28 13:41:37 +0100

    Detect layout systems based on children (#3334)
    * unroll children when detecting layout system
    
    * `findLayoutSystemForChildren`
    
    * `findFlexDirectionForChildren`
    
    * factor out parent checks
    
    * PopupList style fixes (#3347)
    
    * fix PopupList noBorder container mode, changed value in 2 places
    
    * fixed rougue top margin in menu lists
    
    * changed dropdown for flex wrap to not change on hover
    
    * removed mistake
    
    * omit children length check in `singleAxisAutoLayoutContainerDirections`
    
    * use findLayoutSystemForChildren in `reparent-metastrategy`
    
    * use fallback when child layout systems are not the same
    
    * replaceNonDomElementPathsWithTheirChildrenRecursive
    
    * `isNonDomElement`
    
    * fix
    
    * use `isJSXElementLike`
    
    * Revert "use `isJSXElementLike`"
    
    This reverts commit 0292b8e6ee108fa1472fe8688e70986b4bb111d3.
    
    * `isNonDOMJSXElement`
    
    * remove non dom element check
    
    ---------
    
    Co-authored-by: Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
    Co-authored-by: McKayla Lankau <lankaukk@yahoo.com>

--- editor/src/core/model/element-metadata-utils.ts
+++ editor/src/core/model/element-metadata-utils.ts
@@ -6,7 +6,6 @@ import Utils from '../../utils/utils'
 import { getLayoutProperty } from '../layout/getLayoutProperty'
 import {
   mapDropNulls,
-  pluck,
   stripNulls,
   flatMapArray,
   uniqBy,
@@ -28,7 +27,6 @@ import {
   isRight,
   right,
   maybeEitherToMaybe,
-  left,
 } from '../shared/either'
 import {
   ElementInstanceMetadata,
@@ -53,6 +51,7 @@ import {
   isJSXConditionalExpression,
   emptyComputedStyle,
   emptyAttributeMetadatada,
+  DetectedLayoutSystem,
   JSXConditionalExpression,
 } from '../shared/element-template'
 import {
@@ -74,7 +73,6 @@ import {
   MaybeInfinityLocalRectangle,
   Size,
   zeroCanvasRect,
-  zeroLocalRect,
   zeroRectIfNullOrInfinity,
 } from '../shared/math-utils'
 import { optionalMap } from '../shared/optional-utils'
@@ -1787,6 +1785,55 @@ export const MetadataUtils = {
       isRight(element.element) &&
       isJSXConditionalExpression(element.element.value)
     )
+  },
+  findLayoutSystemForChildren(
+    metadata: ElementInstanceMetadataMap,
+    parentPath: ElementPath,
+  ): DetectedLayoutSystem {
+    const childrenPaths = MetadataUtils.getChildrenPathsUnordered(metadata, parentPath)
+    const children = mapDropNulls(
+      (path) => MetadataUtils.findElementByElementPath(metadata, path),
+      childrenPaths,
+    )
+
+    const fallbackLayout =
+      MetadataUtils.findElementByElementPath(metadata, parentPath)?.specialSizeMeasurements
+        .layoutSystemForChildren ?? 'none'
+
+    const parentLayouts = children.map((c) => c.specialSizeMeasurements.parentLayoutSystem)
+
+    if (parentLayouts.length === 0) {
+      return fallbackLayout
+    }
+    const allEqual = parentLayouts.slice(1).every((x) => x === parentLayouts[0])
+    if (!allEqual) {
+      return fallbackLayout
+    }
+    return parentLayouts[0]
+  },
+  findFlexDirectionForChildren(
+    metadata: ElementInstanceMetadataMap,
+    parentPath: ElementPath,
+  ): FlexDirection | null {
+    const childrenPaths = MetadataUtils.getChildrenPathsUnordered(metadata, parentPath)
+
+    const fallbackFlexDirection =
+      MetadataUtils.findElementByElementPath(metadata, parentPath)?.specialSizeMeasurements
+        .flexDirection ?? null
+
+    const children = mapDropNulls(
+      (path) => MetadataUtils.findElementByElementPath(metadata, path),
+      childrenPaths,
+    )
+    const flexDirections = children.map((c) => c.specialSizeMeasurements.parentFlexDirection)
+    if (flexDirections.length === 0) {
+      return fallbackFlexDirection
+    }
+    const allEqual = flexDirections.slice(1).every((x) => x === flexDirections[0])
+    if (!allEqual) {
+      return fallbackFlexDirection
+    }
+    return flexDirections[0]
   },
 }
 
