9f1c854895f7d30c57ec5942dfa629022479f14c
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2023-03-01 13:26:16 +0000

    Clearer handling when no default strategy available (#3364)
    * fix(Canvas) Clearer handling when no default strategy available
    
    * chore(tests) Adding test for strategy indicator when DO_NOTHING is active

--- editor/src/components/canvas/canvas-strategies/strategies/drag-to-move-metastrategy.spec.browser2.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/drag-to-move-metastrategy.spec.browser2.tsx
@@ -126,6 +126,40 @@ describe('Drag To Move Metastrategy', () => {
 })
 
 describe('Drag To Move Strategy Indicator', () => {
+  it('when the DO_NOTHING strategy is active, nothing is active in the Strategy Indicator', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(TestProject1),
+      'await-first-dom-report',
+    )
+
+    const targetElement = renderResult.renderedDOM.getByTestId('child-1')
+    const targetElementBounds = targetElement.getBoundingClientRect()
+    const canvasControlsLayer = renderResult.renderedDOM.getByTestId(CanvasControlsContainerID)
+
+    const startPoint = windowPoint({ x: targetElementBounds.x + 5, y: targetElementBounds.y + 5 })
+    const dragDelta = windowPoint({ x: 10, y: 10 })
+
+    const midDragCallback = async () => {
+      expect(renderResult.getEditorState().strategyState.currentStrategy).toEqual('DO_NOTHING')
+
+      const { showIndicator, dragType, reparent, ancestor } =
+        renderResult.getEditorState().editor.canvas.controls.dragToMoveIndicatorFlags
+
+      expect(showIndicator).toEqual(true)
+      expect(dragType).toEqual('none')
+      expect(reparent).toEqual('none')
+      expect(ancestor).toEqual(false)
+
+      const indicator = renderResult.renderedDOM.getByTestId('drag-strategy-indicator')
+      expect(indicator).toBeDefined()
+    }
+
+    await mouseClickAtPoint(canvasControlsLayer, startPoint, { modifiers: cmdModifier })
+    await mouseDragFromPointWithDelta(canvasControlsLayer, startPoint, dragDelta, {
+      modifiers: emptyModifiers,
+      midDragCallback: midDragCallback,
+    })
+  })
   it('when reparenting an element the Strategy Indicator is visible', async () => {
     const renderResult = await renderTestEditorWithCode(
       makeTestProjectCodeWithSnippet(TestProject1),
