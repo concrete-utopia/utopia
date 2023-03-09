dac9966b8937baf3b5d282970bc6fc0d35b473f5
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2023-03-09 17:07:45 +0100

    fix(flex-resize) use latest metadata to show guideline x

--- editor/src/components/canvas/canvas-strategies/strategies/flex-resize-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/flex-resize-strategy.tsx
@@ -243,7 +243,7 @@ export function flexResizeStrategy(
               edgePosition,
               snapToHug.snapDirection,
               metadata,
-              canvasState.startingMetadata,
+              interactionSession.latestMetadata,
               resizedBounds,
             )
             if (childGuideline != null) {
