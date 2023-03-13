c958f9955dc8f0c7b6c7223c04568d494d97d001
Berci Kormendy <korber314@gmail.com>
2023-03-13 18:10:57 +0100

    collect snap lines for goruplike elements

--- editor/src/components/canvas/controls/guideline-helpers.ts
+++ editor/src/components/canvas/controls/guideline-helpers.ts
@@ -20,11 +20,14 @@ import { ElementInstanceMetadataMap } from '../../../core/shared/element-templat
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import { EdgePosition } from '../canvas-types'
 import { defaultIfNull } from '../../../core/shared/optional-utils'
+import { AllElementProps } from '../../editor/store/editor-state'
+import { treatElementAsContentAffecting } from '../canvas-strategies/strategies/group-like-helpers'
 
 export const SnappingThreshold = 5
 
 export function collectParentAndSiblingGuidelines(
   componentMetadata: ElementInstanceMetadataMap,
+  allElementProps: AllElementProps,
   targets: Array<ElementPath>,
 ): Array<GuidelineWithRelevantPoints> {
   const allPaths = MetadataUtils.getAllPaths(componentMetadata)
@@ -34,23 +37,30 @@ export function collectParentAndSiblingGuidelines(
       componentMetadata,
       target,
     )
-    if (!pinnedAndNotAbsolutePositioned) {
-      const parent = EP.parentPath(target)
-      Utils.fastForEach(allPaths, (maybeTarget) => {
-        // for now we only snap to parents and sibligns and not us or our descendants
-        const isSibling = EP.isSiblingOf(maybeTarget, target)
-        const isParent = EP.pathsEqual(parent, maybeTarget)
-        const notSelectedOrDescendantOfSelected = targets.every(
-          (view) => !EP.isDescendantOfOrEqualTo(maybeTarget, view),
-        )
-        if ((isSibling || isParent) && notSelectedOrDescendantOfSelected) {
-          const frame = MetadataUtils.getFrameInCanvasCoords(maybeTarget, componentMetadata)
-          if (frame != null && isFiniteRectangle(frame)) {
-            result.push(...Guidelines.guidelinesWithRelevantPointsForFrame(frame, 'include'))
-          }
-        }
-      })
+    const isElementGrouplike = treatElementAsContentAffecting(
+      componentMetadata,
+      allElementProps,
+      target,
+    )
+
+    if (!isElementGrouplike && pinnedAndNotAbsolutePositioned) {
+      return
     }
+    const parent = EP.parentPath(target)
+    Utils.fastForEach(allPaths, (maybeTarget) => {
+      // for now we only snap to parents and sibligns and not us or our descendants
+      const isSibling = EP.isSiblingOf(maybeTarget, target)
+      const isParent = EP.pathsEqual(parent, maybeTarget)
+      const notSelectedOrDescendantOfSelected = targets.every(
+        (view) => !EP.isDescendantOfOrEqualTo(maybeTarget, view),
+      )
+      if ((isSibling || isParent) && notSelectedOrDescendantOfSelected) {
+        const frame = MetadataUtils.getFrameInCanvasCoords(maybeTarget, componentMetadata)
+        if (frame != null && isFiniteRectangle(frame)) {
+          result.push(...Guidelines.guidelinesWithRelevantPointsForFrame(frame, 'include'))
+        }
+      }
+    })
   })
   return result
 }
