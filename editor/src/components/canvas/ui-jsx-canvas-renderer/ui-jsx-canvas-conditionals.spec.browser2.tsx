70f8fdc3e20e5a728928847d2582327a0b7aee9d
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-03-14 15:59:36 +0000

    feature(navigator) Drag in and out of conditional clauses. (#3426)
    - Added `ReparentTargetParent` to cover reparenting to an `ElementPath` or into a conditional clause.
    - Implemented some support functions for `ReparentTargetParent`,
      like `getElementPathFromReparentTargetParent` to help integrate it into existing code.
    - Altered `getReparentOutcome` to use a `targetParent` of `ReparentTargetElement`.
    - `ReparentElement` command now uses `ReparentTargetParent`.
    - `editorMoveMultiSelectedTemplates` uses `ReparentTargetParent` for the `targetParent`.
    - Created `ConditionalClause` type which can be used to "target" a specific clause in
      a non-navigator specific way.
    - Renamed `displayAtElementPath` to `displayAtEntry` and `moveToElementPath` to `moveToEntry`.
    - Renamed `getDragSelections` to `getCurrentlySelectedEntries` as that more
      accurately describes what it now does.
    - Renamed `canDrop` function to `notDescendant`, so that a new function called `canDrop`
      can use the name.
    - `canDrag` in `NavigatorItemContainer` allows for synthetic navigator entries to be dragged.
    - `canBeReparentedIntoSelector` renamed to `elementSupportsChildrenSelector` and
      a new value of `canReparentInto` uses the result of that and a check for conditional clauses
      into account.
    - Incorporated fix from PR #3412 for `findJSXElementChildAtPath`, because it was necessary
      for this PR to work correctly.
    - `insertJSXElementChild` now caters for inserting into conditional clauses.
    - Adjust `navigatorDepth` to handle the conditional entry not holding the path of the branch down.

--- editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-conditionals.spec.browser2.tsx
+++ editor/src/components/canvas/ui-jsx-canvas-renderer/ui-jsx-canvas-conditionals.spec.browser2.tsx
@@ -87,13 +87,13 @@ describe('a project with conditionals', () => {
       'regular-storyboard/scene/app',
       'regular-storyboard/scene/app:app-root',
       'regular-storyboard/scene/app:app-root/conditional1',
-      'conditional-clause-storyboard/scene/app:app-root/conditional1/conditional2-then',
+      'conditional-clause-storyboard/scene/app:app-root/conditional1-then',
       'regular-storyboard/scene/app:app-root/conditional1/conditional2',
-      'conditional-clause-storyboard/scene/app:app-root/conditional1/conditional2/div-inside-conditionals-then',
+      'conditional-clause-storyboard/scene/app:app-root/conditional1/conditional2-then',
       'regular-storyboard/scene/app:app-root/conditional1/conditional2/div-inside-conditionals',
-      'conditional-clause-storyboard/scene/app:app-root/conditional1/conditional2/else-case-else',
+      'conditional-clause-storyboard/scene/app:app-root/conditional1/conditional2-else',
       'synthetic-storyboard/scene/app:app-root/conditional1/conditional2/else-case-attribute',
-      'conditional-clause-storyboard/scene/app:app-root/conditional1/else-case-else',
+      'conditional-clause-storyboard/scene/app:app-root/conditional1-else',
       'synthetic-storyboard/scene/app:app-root/conditional1/else-case-attribute',
     ])
   })
