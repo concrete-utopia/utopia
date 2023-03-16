2fb7d266126d757687272c38a8f1655c701b005d
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2023-03-16 12:28:21 +0000

    fix(Canvas) Slightly tweaked all reparenting weights

--- editor/src/components/canvas/canvas-strategies/strategies/reparent-metastrategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/reparent-metastrategy.tsx
@@ -42,6 +42,10 @@ interface ReparentFactoryAndDetails {
   factory: CanvasStrategyFactory
 }
 
+const DefaultReparentWeight = 4
+const FallbackReparentWeight = DefaultReparentWeight - 1
+const FlowReparentWeight = FallbackReparentWeight - 1
+
 export function getApplicableReparentFactories(
   canvasState: InteractionCanvasState,
   pointOnCanvas: CanvasPoint,
@@ -61,7 +65,7 @@ export function getApplicableReparentFactories(
   const factories: Array<ReparentFactoryAndDetails> = reparentStrategies.map((result) => {
     switch (result.strategy) {
       case 'REPARENT_AS_ABSOLUTE': {
-        const fitness = result.isFallback ? 2 : 3
+        const fitness = result.isFallback ? FallbackReparentWeight : DefaultReparentWeight
         if (allDraggedElementsAbsolute) {
           return {
             targetParent: result.target.newParent,
@@ -92,7 +96,12 @@ export function getApplicableReparentFactories(
         const targetParentDisplayType = parentLayoutSystem === 'flex' ? 'flex' : 'flow'
 
         // We likely never want flow insertion or re-parenting to be the default
-        const fitness = targetParentDisplayType === 'flow' ? 1 : result.isFallback ? 2 : 3
+        const fitness =
+          targetParentDisplayType === 'flow'
+            ? FlowReparentWeight
+            : result.isFallback
+            ? FallbackReparentWeight
+            : DefaultReparentWeight
 
         return {
           targetParent: result.target.newParent,
