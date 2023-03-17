8b0116301db2b44558ae9453e159ff0bedcbd911
Bertalan Körmendy <korber314@gmail.com>
2023-03-17 10:34:22 +0100

    Snap lines for resizing groups (#3425)
    * wip
    
    * get resize working
    
    * collect snap lines for goruplike elements
    
    * move
    
    * show correct parent bounds
    
    * `isResizingGrouplikeElement`
    
    * rectangle rounding
    
    * naming
    
    * round new frames
    
    * Revert "round new frames"
    
    This reverts commit 6c9798812b69f9db0d644495f42ec4edefea0930.
    
    * even more rectangle rounding
    
    * Revert "feature(navigator) Drag in and out of conditional clauses. (#3426)"
    
    This reverts commit 70f8fdc3e20e5a728928847d2582327a0b7aee9d.
    
    * wip - tests
    
    * tests
    
    * Revert "Revert "feature(navigator) Drag in and out of conditional clauses. (#3426)""
    
    This reverts commit 7452db7aa99f6495334526d8b88cb5966e343856.
    
    * tests for all content affecting elements
    
    * resolve todo
    
    * cleanup
    
    * cleanup2
    
    * remove unnecessary rounding
    
    * add tests for conditionals

--- editor/src/components/canvas/canvas-strategies/strategies/absolute-move-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/absolute-move-strategy.tsx
@@ -1,11 +1,11 @@
 import { MetadataUtils } from '../../../../core/model/element-metadata-utils'
-import { toString } from '../../../../core/shared/element-path'
 import { ImmediateParentBounds } from '../../controls/parent-bounds'
 import { ImmediateParentOutlines } from '../../controls/parent-outlines'
 import { ZeroSizedElementControls } from '../../controls/zero-sized-element-controls'
 import {
   controlWithProps,
   emptyStrategyApplicationResult,
+  getTargetPathsFromInteractionTarget,
   InteractionCanvasState,
   MoveStrategy,
 } from '../canvas-strategy-types'
@@ -22,11 +22,14 @@ export function absoluteMoveStrategy(
   canvasState: InteractionCanvasState,
   interactionSession: InteractionSession | null,
 ): MoveStrategy | null {
-  const targets = retargetStrategyToChildrenOfContentAffectingElements(canvasState)
+  const targets = flattenSelection(
+    getTargetPathsFromInteractionTarget(canvasState.interactionTarget),
+  )
+  const retargetedTargets = retargetStrategyToChildrenOfContentAffectingElements(canvasState)
 
   const isApplicable =
-    targets.length > 0 &&
-    flattenSelection(targets).every((element) => {
+    retargetedTargets.length > 0 &&
+    flattenSelection(retargetedTargets).every((element) => {
       const elementMetadata = MetadataUtils.findElementByElementPath(
         canvasState.startingMetadata,
         element,
@@ -76,9 +79,10 @@ export function absoluteMoveStrategy(
         ) {
           return applyMoveCommon(
             targets,
+            retargetedTargets,
             canvasState,
             interactionSession,
-            getAdjustMoveCommands(targets, canvasState, interactionSession),
+            getAdjustMoveCommands(retargetedTargets, canvasState, interactionSession),
           )
         }
         // Fallback for when the checks above are not satisfied.
