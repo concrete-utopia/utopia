dd114ef20cacda3c9141483c73732ea3152728ff
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2023-03-23 15:09:00 +0100

    fix(tests) update render count tests

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -64,7 +64,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`450`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`449`)
   })
 
   it('Clicking on opacity slider with a less simple project', async () => {
@@ -124,7 +124,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`461`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`460`)
   })
 
   it('Changing the selected view with a simple project', async () => {
@@ -178,7 +178,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`582`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`581`)
   })
 
   it('Changing the selected view with a less simple project', async () => {
@@ -242,6 +242,6 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`654`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`653`)
   })
 })
