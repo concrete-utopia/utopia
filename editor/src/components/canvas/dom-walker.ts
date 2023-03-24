2d3bf48e212632b704d871e3c176a3d3941a0d1e
Berci Kormendy <korber314@gmail.com>
2023-03-24 18:39:46 +0100

    `getClosestOffsetParent`

--- editor/src/components/canvas/dom-walker.ts
+++ editor/src/components/canvas/dom-walker.ts
@@ -914,7 +914,7 @@ function getSpecialMeasurements(
     left: isRight(borderLeftWidth) ? borderLeftWidth.value.value : 0,
   }
 
-  const offsetParent = element.offsetParent as HTMLElement | null
+  const offsetParent = getClosestOffsetParent(element) as HTMLElement | null
   const elementOrContainingParent =
     providesBoundsForAbsoluteChildren || offsetParent == null ? element : offsetParent
 
@@ -1364,4 +1364,16 @@ function walkElements(
   } else {
     return { childPaths: [], rootMetadata: {}, cachedPaths: [] }
   }
+}
+
+function getClosestOffsetParent(element: HTMLElement): Element | null {
+  let currentElement: HTMLElement | null = element
+
+  while (currentElement != null) {
+    if (currentElement.offsetParent != null) {
+      return currentElement.offsetParent
+    }
+    currentElement = currentElement.parentElement
+  }
+  return null
 }
