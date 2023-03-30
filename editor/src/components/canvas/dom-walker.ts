d9ab99e7915f1b682165415bb2203552b7058909
Berci Kormendy <korber314@gmail.com>
2023-03-30 11:26:57 +0200

    not needed after all

--- editor/src/components/canvas/dom-walker.ts
+++ editor/src/components/canvas/dom-walker.ts
@@ -1370,10 +1370,6 @@ function getClosestOffsetParent(element: HTMLElement): Element | null {
   let currentElement: HTMLElement | null = element
 
   while (currentElement != null) {
-    const currentElementStyle = window.getComputedStyle(currentElement)
-    if (isElementAContainingBlockForAbsolute(currentElementStyle)) {
-      return currentElement
-    }
     if (currentElement.offsetParent != null) {
       return currentElement.offsetParent
     }
