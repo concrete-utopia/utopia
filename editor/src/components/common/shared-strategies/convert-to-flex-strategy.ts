d54b9abc7440faaac257650ed6b092e7785f0266
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-04-06 15:01:20 +0200

    fix color

--- editor/src/components/common/shared-strategies/convert-to-flex-strategy.ts
+++ editor/src/components/common/shared-strategies/convert-to-flex-strategy.ts
@@ -43,7 +43,7 @@ export function convertLayoutToFlexCommands(
       return [
         showToastCommand(
           'Cannot be converted to Flex yet',
-          'WARNING',
+          'NOTICE',
           'cannot-convert-children-to-flex',
         ),
       ]
