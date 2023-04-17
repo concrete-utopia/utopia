7291722ebeb3bcd6159f0691c59c435c751f2bcf
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2023-04-17 17:38:46 +0200

    fix(unwrap) test for unwrapping a conditional

--- editor/src/components/editor/conditionals.spec.browser2.tsx
+++ editor/src/components/editor/conditionals.spec.browser2.tsx
@@ -491,6 +491,34 @@ describe('conditionals', () => {
          `),
       )
     })
+    it('can unwrap a conditional with only text', async () => {
+      const startSnippet = `
+        <div data-uid='aaa'>
+        {
+          // @utopia/uid=conditional
+          true ? 'hello': 'bello'
+        }
+        </div>
+      `
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(startSnippet),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.appendNewElementPath(TestScenePath, ['aaa', 'conditional'])
+
+      await act(async () => {
+        await renderResult.dispatch([unwrapElement(targetPath)], true)
+      })
+
+      expect(getPrintedUiJsCode(renderResult.getEditorState())).toEqual(
+        makeTestProjectCodeWithSnippet(`
+            <div data-uid='aaa'>
+              {'hello'}
+            </div>
+         `),
+      )
+    })
   })
   describe('paste', () => {
     it('can paste a single element into a conditional', async () => {
