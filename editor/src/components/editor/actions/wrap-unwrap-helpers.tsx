7bf8c8217b8ab34b8d184e15b5bb668d8d9d86b4
Eniko Bakos-Demeter <enidemi@users.noreply.github.com>
2023-04-21 11:53:01 +0200

    fix(wrap) update helper to use commands if possible

--- editor/src/components/editor/actions/wrap-unwrap-helpers.tsx
+++ editor/src/components/editor/actions/wrap-unwrap-helpers.tsx
@@ -1,9 +1,7 @@
 import { findMaybeConditionalExpression, getClauseOptic } from '../../../core/model/conditionals'
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import {
-  InsertChildAndDetails,
   generateUidWithExistingComponents,
-  insertChildAndDetails,
   transformJSXComponentAtPath,
 } from '../../../core/model/element-template-utils'
 import {
@@ -17,7 +15,6 @@ import {
   JSXElement,
   JSXElementChild,
   JSXFragment,
-  UtopiaJSXComponent,
   emptyComments,
   isJSExpressionValue,
   isJSXConditionalExpression,
@@ -28,19 +25,11 @@ import {
 import { modify } from '../../../core/shared/optics/optic-utilities'
 import { forceNotNull, optionalMap } from '../../../core/shared/optional-utils'
 import { ElementPath, Imports } from '../../../core/shared/project-file-types'
-import { absolute } from '../../../utils/utils'
-import {
-  normalisePathSuccessOrThrowError,
-  normalisePathToUnderlyingTarget,
-} from '../../custom-code/code-file'
+import { IndexPosition, absolute } from '../../../utils/utils'
 import { EditorDispatch } from '../action-types'
 import {
   EditorState,
-  SimpleParseSuccess,
-  getOpenUIJSFileKey,
   insertElementAtPath,
-  modifyParseSuccessAtPath,
-  modifyParseSuccessWithSimple,
   modifyUnderlyingTargetElement,
 } from '../store/editor-state'
 import {
@@ -52,7 +41,8 @@ import {
 } from '../store/insertion-path'
 import { deleteView } from './action-creators'
 import { UPDATE_FNS } from './actions'
-import { mergeImports } from '../../../core/workers/common/project-file-utils'
+import { foldAndApplyCommandsSimple } from '../../canvas/commands/commands'
+import { addElement } from '../../canvas/commands/add-element-command'
 
 export function unwrapConditionalClause(
   editor: EditorState,
@@ -199,197 +189,221 @@ export function wrapElementInsertions(
   importsToAdd: Imports,
   anyTargetIsARootElement: boolean,
   targetThatIsRootElementOfCommonParent: ElementPath | undefined,
-): { updatedEditor: EditorState; newPath: ElementPath | null; detailsOfUpdate: string | null } {
-  let viewPath: ElementPath | null = null
-  let detailsOfUpdate: string | null = null
+): { updatedEditor: EditorState; newPath: ElementPath | null } {
+  // TODO this entire targetThatIsRootElementOfCommonParent could be simplified by introducing a "rootElementInsertionPath" to InsertionPath
+  const staticTarget =
+    optionalMap(childInsertionPath, targetThatIsRootElementOfCommonParent) ?? parentPath
 
+  const newPath = anyTargetIsARootElement
+    ? EP.appendNewElementPath(getElementPathFromInsertionPath(parentPath), elementToInsert.uid)
+    : EP.appendToPath(getElementPathFromInsertionPath(parentPath), elementToInsert.uid)
+
+  const indexPosition = findIndexPositionInParent(targets, parentPath, editor.jsxMetadata)
+
+  switch (elementToInsert.type) {
+    case 'JSX_FRAGMENT': {
+      function pathsToBeWrappedInFragment(): ElementPath[] {
+        const elements: ElementPath[] = targets.filter((path) => {
+          return !targets
+            .filter((otherPath) => !EP.pathsEqual(otherPath, path))
+            .some((otherPath) => EP.isDescendantOf(path, otherPath))
+        })
+        const parents = new Set<ElementPath>()
+        elements.forEach((e) => parents.add(EP.parentPath(e)))
+        if (parents.size !== 1) {
+          return []
+        }
+        return elements
+      }
+
+      const children = pathsToBeWrappedInFragment()
+      if (children.length === 0) {
+        // nothing to do
+        return { updatedEditor: editor, newPath: null }
+      }
+
+      switch (staticTarget.type) {
+        case 'CHILD_INSERTION':
+          return {
+            updatedEditor: foldAndApplyCommandsSimple(editor, [
+              addElement('always', staticTarget, elementToInsert, indexPosition),
+            ]),
+            newPath: newPath,
+          }
+        case 'CONDITIONAL_CLAUSE_INSERTION':
+          const withTargetAdded = insertElementIntoJSXConditional(
+            editor,
+            staticTarget,
+            elementToInsert,
+          )
+          return { updatedEditor: withTargetAdded, newPath: newPath }
+        default:
+          const _exhaustiveCheck: never = staticTarget
+          return { updatedEditor: editor, newPath: null }
+      }
+    }
+    case 'JSX_ELEMENT': {
+      switch (staticTarget.type) {
+        case 'CHILD_INSERTION':
+          return {
+            updatedEditor: foldAndApplyCommandsSimple(editor, [
+              addElement('always', staticTarget, elementToInsert, indexPosition),
+            ]),
+            newPath: newPath,
+          }
+        case 'CONDITIONAL_CLAUSE_INSERTION':
+          const withTargetAdded = insertElementIntoJSXConditional(
+            editor,
+            staticTarget,
+            elementToInsert,
+          )
+          return { updatedEditor: withTargetAdded, newPath: newPath }
+        default:
+          const _exhaustiveCheck: never = staticTarget
+          return { updatedEditor: editor, newPath: null }
+      }
+    }
+    case 'JSX_CONDITIONAL_EXPRESSION': {
+      switch (staticTarget.type) {
+        case 'CHILD_INSERTION':
+          return {
+            updatedEditor: foldAndApplyCommandsSimple(editor, [
+              addElement('always', staticTarget, elementToInsert, indexPosition),
+            ]),
+            newPath: newPath,
+          }
+        case 'CONDITIONAL_CLAUSE_INSERTION':
+          const withTargetAdded = insertConditionalIntoConditionalClause(
+            editor,
+            staticTarget,
+            elementToInsert,
+          )
+          return { updatedEditor: withTargetAdded, newPath: newPath }
+        default:
+          const _exhaustiveCheck: never = staticTarget
+          return { updatedEditor: editor, newPath: null }
+      }
+    }
+    default:
+      const _exhaustiveCheck: never = elementToInsert
+      return { updatedEditor: editor, newPath: null }
+  }
+}
+
+function findIndexPositionInParent(
+  targets: Array<ElementPath>,
+  parentPath: InsertionPath,
+  metadata: ElementInstanceMetadataMap,
+) {
   let indexInParent: number | null = null
   if (parentPath != null && isChildInsertionPath(parentPath)) {
     indexInParent = optionalMap(
       (firstPathMatchingCommonParent) =>
-        MetadataUtils.getIndexInParent(editor.jsxMetadata, firstPathMatchingCommonParent),
+        MetadataUtils.getIndexInParent(metadata, firstPathMatchingCommonParent),
       targets.find((target) => EP.pathsEqual(EP.parentPath(target), parentPath.intendedParentPath)),
     )
   }
-
-  const underlyingTarget = normalisePathToUnderlyingTarget(
-    editor.projectContents,
-    editor.nodeModules.files,
-    forceNotNull('No storyboard file found', getOpenUIJSFileKey(editor)),
-    targetThatIsRootElementOfCommonParent ?? getElementPathFromInsertionPath(parentPath),
+  return (
+    optionalMap(
+      (index) =>
+        ({
+          type: 'before',
+          index: index,
+        } as IndexPosition),
+      indexInParent,
+    ) ?? undefined
   )
-
-  const targetSuccess = normalisePathSuccessOrThrowError(underlyingTarget)
-  const withWrapperAdded = modifyParseSuccessAtPath(
-    targetSuccess.filePath,
-    editor,
-    (parseSuccess) => {
-      const utopiaJSXComponents = getUtopiaJSXComponentsFromSuccess(parseSuccess)
-      let withTargetAdded: InsertChildAndDetails = insertChildAndDetails(utopiaJSXComponents)
-
-      function withInsertedJSXElement() {
-        return insertElementAtPath(
-          editor.projectContents,
-          editor.canvas.openFile?.filename ?? null,
-          parentPath,
-          elementToInsert,
-          utopiaJSXComponents,
-          optionalMap(
-            (index) => ({
-              type: 'before',
-              index: index,
-            }),
-            indexInParent,
-          ),
-        )
-      }
-
-      // TODO this entire targetThatIsRootElementOfCommonParent could be simplified by introducing a "rootElementInsertionPath" to InsertionPath
-      const staticTarget =
-        optionalMap(childInsertionPath, targetThatIsRootElementOfCommonParent) ?? parentPath
-
-      switch (elementToInsert.type) {
-        case 'JSX_FRAGMENT': {
-          function pathsToBeWrappedInFragment(): ElementPath[] {
-            const elements: ElementPath[] = targets.filter((path) => {
-              return !targets
-                .filter((otherPath) => !EP.pathsEqual(otherPath, path))
-                .some((otherPath) => EP.isDescendantOf(path, otherPath))
-            })
-            const parents = new Set<ElementPath>()
-            elements.forEach((e) => parents.add(EP.parentPath(e)))
-            if (parents.size !== 1) {
-              return []
-            }
-            return elements
-          }
-
-          const children = pathsToBeWrappedInFragment()
-          if (children.length === 0) {
-            // nothing to do
-            return parseSuccess
-          }
-
-          switch (staticTarget.type) {
-            case 'CHILD_INSERTION':
-              withTargetAdded = withInsertedJSXElement()
-              break
-            case 'CONDITIONAL_CLAUSE_INSERTION':
-              withTargetAdded = withElementInsertedIntoJSXConditional(
-                utopiaJSXComponents,
-                staticTarget,
-                elementToInsert,
-              )
-              break
-            default:
-              const _exhaustiveCheck: never = staticTarget
-              return parseSuccess
-          }
-
-          break
-        }
-        case 'JSX_ELEMENT': {
-          switch (staticTarget.type) {
-            case 'CHILD_INSERTION':
-              withTargetAdded = withInsertedJSXElement()
-              break
-            case 'CONDITIONAL_CLAUSE_INSERTION':
-              withTargetAdded = withElementInsertedIntoJSXConditional(
-                utopiaJSXComponents,
-                staticTarget,
-                elementToInsert,
-              )
-              break
-            default:
-              const _exhaustiveCheck: never = staticTarget
-              return parseSuccess
-          }
-          break
-        }
-        case 'JSX_CONDITIONAL_EXPRESSION': {
-          switch (staticTarget.type) {
-            case 'CHILD_INSERTION':
-              withTargetAdded = withInsertedJSXElement()
-              break
-            case 'CONDITIONAL_CLAUSE_INSERTION':
-              withTargetAdded = insertChildAndDetails(
-                transformJSXComponentAtPath(
-                  utopiaJSXComponents,
-                  getElementPathFromInsertionPath(staticTarget),
-                  (oldRoot) => {
-                    if (isJSXConditionalExpression(oldRoot)) {
-                      const clauseOptic = getClauseOptic(staticTarget.clause)
-                      return modify(
-                        clauseOptic,
-                        (clauseElement) => {
-                          return { ...elementToInsert, whenTrue: clauseElement }
-                        },
-                        oldRoot,
-                      )
-                    } else {
-                      return { ...oldRoot }
-                    }
-                  },
-                ),
-              )
-              break
-            default:
-              const _exhaustiveCheck: never = staticTarget
-              return parseSuccess
-          }
-          break
-        }
-        default:
-          const _exhaustiveCheck: never = elementToInsert
-          return parseSuccess
-      }
-
-      viewPath = anyTargetIsARootElement
-        ? EP.appendNewElementPath(getElementPathFromInsertionPath(parentPath), elementToInsert.uid)
-        : EP.appendToPath(getElementPathFromInsertionPath(parentPath), elementToInsert.uid)
-
-      detailsOfUpdate = withTargetAdded.insertionDetails
-      return modifyParseSuccessWithSimple((success: SimpleParseSuccess) => {
-        return {
-          ...success,
-          utopiaComponents: withTargetAdded.components,
-          imports: mergeImports(targetSuccess.filePath, success.imports, importsToAdd),
-        }
-      }, parseSuccess)
-    },
-  )
-  return { updatedEditor: withWrapperAdded, newPath: viewPath, detailsOfUpdate: detailsOfUpdate }
 }
 
-function withElementInsertedIntoJSXConditional(
-  utopiaJSXComponents: Array<UtopiaJSXComponent>,
+function insertElementIntoJSXConditional(
+  editor: EditorState,
   staticTarget: ConditionalClauseInsertionPath,
-  element: JSXElement | JSXFragment,
+  elementToInsert: JSXElement | JSXFragment,
 ) {
-  return insertChildAndDetails(
-    transformJSXComponentAtPath(
-      utopiaJSXComponents,
-      getElementPathFromInsertionPath(staticTarget),
-      (oldRoot) => {
-        if (isJSXConditionalExpression(oldRoot)) {
-          const clauseOptic = getClauseOptic(staticTarget.clause)
-          return modify(
-            clauseOptic,
-            (clauseElement) => {
-              return {
-                ...element,
-                children: [...element.children, clauseElement],
-              }
-            },
-            oldRoot,
-          )
-        } else {
-          return {
-            ...element,
-            children: [...element.children, oldRoot],
+  return modifyUnderlyingTargetElement(
+    staticTarget.intendedParentPath,
+    forceNotNull('No storyboard file found', editor.canvas.openFile?.filename),
+    editor,
+    (element) => element,
+    (success) => {
+      const components = getUtopiaJSXComponentsFromSuccess(success)
+      const updatedComponents = transformJSXComponentAtPath(
+        components,
+        getElementPathFromInsertionPath(staticTarget),
+        (oldRoot) => {
+          if (isJSXConditionalExpression(oldRoot)) {
+            const clauseOptic = getClauseOptic(staticTarget.clause)
+            return modify(
+              clauseOptic,
+              (clauseElement) => {
+                return {
+                  ...elementToInsert,
+                  children: [...elementToInsert.children, clauseElement],
+                }
+              },
+              oldRoot,
+            )
+          } else {
+            return {
+              ...elementToInsert,
+              children: [...elementToInsert.children, oldRoot],
+            }
           }
-        }
-      },
-    ),
+        },
+      )
+
+      const updatedTopLevelElements = applyUtopiaJSXComponentsChanges(
+        success.topLevelElements,
+        updatedComponents,
+      )
+
+      return {
+        ...success,
+        topLevelElements: updatedTopLevelElements,
+      }
+    },
+  )
+}
+function insertConditionalIntoConditionalClause(
+  editor: EditorState,
+  staticTarget: ConditionalClauseInsertionPath,
+  elementToInsert: JSXConditionalExpression,
+) {
+  return modifyUnderlyingTargetElement(
+    staticTarget.intendedParentPath,
+    forceNotNull('No storyboard file found', editor.canvas.openFile?.filename),
+    editor,
+    (element) => element,
+    (success) => {
+      const components = getUtopiaJSXComponentsFromSuccess(success)
+      const updatedComponents = transformJSXComponentAtPath(
+        components,
+        getElementPathFromInsertionPath(staticTarget),
+        (oldRoot) => {
+          if (isJSXConditionalExpression(oldRoot)) {
+            const clauseOptic = getClauseOptic(staticTarget.clause)
+            return modify(
+              clauseOptic,
+              (clauseElement) => {
+                return { ...elementToInsert, whenTrue: clauseElement }
+              },
+              oldRoot,
+            )
+          } else {
+            return { ...oldRoot }
+          }
+        },
+      )
+
+      const updatedTopLevelElements = applyUtopiaJSXComponentsChanges(
+        success.topLevelElements,
+        updatedComponents,
+      )
+
+      return {
+        ...success,
+        topLevelElements: updatedTopLevelElements,
+      }
+    },
   )
 }
