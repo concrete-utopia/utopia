56454e73ac1a4f306b903891e244e8d7c9539a20
Sean Parsons <git@futurenotfound.com>
2023-04-24 14:23:18 +0100

    chore(pr) Fixed performance regression test spec.

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -64,7 +64,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`620`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`454`)
   })
 
   it('Clicking on opacity slider with a less simple project', async () => {
@@ -124,7 +124,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`631`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`488`)
   })
 
   it('Changing the selected view with a simple project', async () => {
