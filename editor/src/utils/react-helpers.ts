e5f61467554362d100cfe365f372f87f043e421e
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-04-25 11:57:14 +0200

    MUCH smaller snapshots

--- editor/src/utils/react-helpers.ts
+++ editor/src/utils/react-helpers.ts
@@ -3,16 +3,16 @@ import { getDisplayName } from './canvas-react-utils'
 type FiberNodeThingy = { type: any; return?: FiberNodeThingy }
 type ElementThingy = { type: any; _owner?: FiberNodeThingy }
 
-function getNamedPathInner(element: FiberNodeThingy | undefined): string {
-  if (element == null) {
+function getNamedPathInner(element: FiberNodeThingy | undefined, depth: number): string {
+  if (depth === 0 || element == null) {
     return ''
   }
-  return `${getNamedPathInner(element.return)}/${getDisplayName(element.type)}`
+  return `${getNamedPathInner(element.return, depth - 1)}/${getDisplayName(element.type)}`
 }
 
 export function getNamedPath(element: ElementThingy | undefined): string {
   if (element == null) {
     return ''
   }
-  return `${getNamedPathInner(element._owner)}/${getDisplayName(element.type)}`
+  return `${getNamedPathInner(element._owner, 5)}/${getDisplayName(element.type)}`
 }
