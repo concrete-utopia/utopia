c826a63f4077765032c02e9d88bd17fdf92349c2
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-04-25 12:16:21 +0200

    tweaking the snapshot style more

--- editor/src/utils/react-helpers.ts
+++ editor/src/utils/react-helpers.ts
@@ -1,7 +1,7 @@
 import { getDisplayName } from './canvas-react-utils'
 
 type FiberNodeThingy = { type: any; return?: FiberNodeThingy }
-type ElementThingy = { type: any; _owner?: FiberNodeThingy }
+type ElementThingy = { type: any; _owner?: FiberNodeThingy; return?: FiberNodeThingy }
 
 function getNamedPathInner(element: FiberNodeThingy | undefined, depth: number): string {
   if (depth === 0 || element == null) {
@@ -10,9 +10,13 @@ function getNamedPathInner(element: FiberNodeThingy | undefined, depth: number):
   return `${getNamedPathInner(element.return, depth - 1)}/${getDisplayName(element.type)}`
 }
 
-export function getNamedPath(element: ElementThingy | undefined): string {
-  if (element == null) {
+export function getNamedPath(element: ElementThingy | undefined, depth: number = 3): string {
+  if (depth === 0 || element == null) {
     return ''
   }
-  return `${getNamedPathInner(element._owner, 5)}/${getDisplayName(element.type)}`
+  if (element._owner == null) {
+    return `${getNamedPath(element.return, depth - 1)}/${getDisplayName(element.type)}`
+  } else {
+    return `${getNamedPath(element._owner, depth)}/${getDisplayName(element.type)}`
+  }
 }
