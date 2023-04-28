9e433b73a25711d98de3642ba0f77d89fed32efd
Malte <malte.nuhn@gmail.com>
2023-04-28 15:37:18 +0100

    Update canvas-toolbar.tsx

--- editor/src/components/editor/canvas-toolbar.tsx
+++ editor/src/components/editor/canvas-toolbar.tsx
@@ -183,35 +183,10 @@ export const CanvasToolbar = React.memo(() => {
       onClick={stopPropagation}
     >
       <FlexColumn style={{ padding: 4 }}>
-        <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Scale</header>
-        <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
-          <Tooltip title='Zoom to 100%' placement='bottom'>
-            <SquareButton highlight style={{ textAlign: 'center', width: 32 }} onClick={zoom100pct}>
-              {zoomLevel * 100}%
-            </SquareButton>
-          </Tooltip>
-        </FlexRow>
-
-        <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
-          <Tooltip title='Zoom in' placement='bottom'>
-            <InsertModeButton
-              iconType='magnifyingglass-plus'
-              iconCategory='semantic'
-              onClick={zoomIn}
-            />
-          </Tooltip>
-          <Tooltip title='Zoom out' placement='bottom'>
-            <InsertModeButton
-              iconType='magnifyingglass-minus'
-              iconCategory='semantic'
-              onClick={zoomOut}
-            />
-          </Tooltip>
-        </FlexRow>
-        <Divider />
+        <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4, marginBottom: 4 }}></FlexRow>
         {/* ------------------------------------ */}
         <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Tools</header>
-        <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
+        <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4, marginBottom: 6 }}>
           <Tooltip title='Select' placement='bottom'>
             <InsertModeButton
               iconType='pointer'
@@ -226,11 +201,34 @@ export const CanvasToolbar = React.memo(() => {
               onClick={insertTextCallback}
             />
           </Tooltip>
+          <Tooltip title='Zoom in' placement='bottom'>
+            <InsertModeButton
+              iconType='magnifyingglass-plus'
+              iconCategory='semantic'
+              onClick={zoomIn}
+            />
+          </Tooltip>
+          <Tooltip title='Zoom out' placement='bottom'>
+            <InsertModeButton
+              iconType='magnifyingglass-minus'
+              iconCategory='semantic'
+              onClick={zoomOut}
+            />
+          </Tooltip>
+          <Tooltip title='Zoom to 100%' placement='bottom'>
+            <SquareButton
+              highlight
+              style={{ textAlign: 'center', width: '100%' }}
+              onClick={zoom100pct}
+            >
+              {zoomLevel * 100}%
+            </SquareButton>
+          </Tooltip>
         </FlexRow>
       </FlexColumn>
-      <Divider />
+
       {/* ------------------------------------ */}
-      <FlexColumn style={{ padding: 4 }}>
+      <FlexColumn style={{ padding: 4, marginBottom: 6 }}>
         <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Insert</header>
         <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
           <Tooltip title='Insert div' placement='bottom'>
@@ -271,9 +269,8 @@ export const CanvasToolbar = React.memo(() => {
           </Tooltip>
         </FlexRow>
       </FlexColumn>
-      <Divider />
       {/* ------------------------------------ */}
-      <FlexColumn style={{ padding: 4 }}>
+      <FlexColumn style={{ padding: 4, marginBottom: 6 }}>
         <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Convert</header>
         <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
           <Tooltip title='Converts an element or component into another' placement='bottom'>
@@ -286,9 +283,9 @@ export const CanvasToolbar = React.memo(() => {
           </Tooltip>
         </FlexRow>
       </FlexColumn>
-      <Divider />
+
       {/* ------------------------------------ */}
-      <FlexColumn style={{ padding: 4 }}>
+      <FlexColumn style={{ padding: 4, marginBottom: 6 }}>
         <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Organise</header>
         <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
           <Tooltip title='Wrap selection in div' placement='bottom'>
@@ -304,9 +301,8 @@ export const CanvasToolbar = React.memo(() => {
           </Tooltip>
         </FlexRow>
       </FlexColumn>
-      <Divider />
       {/* ------------------------------------ */}
-      <FlexColumn style={{ padding: 4 }}>
+      <FlexColumn style={{ padding: 4, marginBottom: 6 }}>
         <header style={{ paddingLeft: 4, fontSize: 10, fontWeight: 500 }}>Editor</header>
         <FlexRow style={{ flexWrap: 'wrap', gap: 4, padding: 4 }}>
           <Tooltip title='Toggle Live Mode' placement='bottom'>
@@ -398,18 +394,5 @@ const Tooltip = (props: TooltipProps) => {
       {/* TODO why do we need to wrap the children in a span? */}
       <span>{props.children}</span>
     </TooltipWithoutSpanFixme>
-  )
-}
-
-const Divider = () => {
-  return (
-    <div
-      style={{
-        height: 1,
-        width: '100%',
-        marginTop: 8,
-        backgroundColor: colorTheme.fg9.value,
-      }}
-    />
   )
 }
