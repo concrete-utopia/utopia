428e05540e493cc81b299591c486024c2b9d4a69
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-05-16 12:52:10 +0200

    test wrap_in_container command (#3684)
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>

---
+++ editor/src/components/canvas/commands/wrap-in-container-command.spec.tsx
@@ -0,0 +1,126 @@
+import { createBuiltInDependenciesList } from '../../../core/es-modules/package-manager/built-in-dependencies-list'
+import * as EP from '../../../core/shared/element-path'
+import {
+  JSXConditionalExpression,
+  JSXElement,
+  JSXFragment,
+  JSXTextBlock,
+} from '../../../core/shared/element-template'
+import { ElementPath } from '../../../core/shared/project-file-types'
+import { complexDefaultProjectPreParsed } from '../../../sample-projects/sample-project-utils.test-utils'
+import { EditorState, withUnderlyingTargetFromEditorState } from '../../editor/store/editor-state'
+import { renderTestEditorWithModel } from '../ui-jsx.test-utils'
+import { updateEditorStateWithPatches } from './commands'
+import { runWrapInContainerCommand, wrapInContainerCommand } from './wrap-in-container-command'
+
+describe('wrapInContainerCommand', () => {
+  function getElement(path: ElementPath, state: EditorState) {
+    return withUnderlyingTargetFromEditorState(path, state, null, (_, element) => element)
+  }
+
+  function getPath(uid: string) {
+    return EP.elementPath([
+      ['storyboard-entity', 'scene-1-entity', 'app-entity'],
+      ['app-outer-div', uid],
+    ])
+  }
+
+  it('wraps with a fragment', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      complexDefaultProjectPreParsed(),
+      'dont-await-first-dom-report',
+      createBuiltInDependenciesList(null),
+    )
+
+    const { editor } = renderResult.getEditorState()
+
+    const targetPath = getPath('card-instance')
+
+    const originalElement = getElement(targetPath, editor)
+    expect(originalElement).not.toBeNull()
+
+    const cmd = wrapInContainerCommand('always', targetPath, 'the-wrapper', 'fragment')
+    const result = runWrapInContainerCommand(editor, cmd)
+    const patchedEditor = updateEditorStateWithPatches(editor, result.editorStatePatches)
+
+    const wrapperPath = EP.appendToPath(EP.parentPath(targetPath), 'the-wrapper')
+
+    const oldElement = getElement(targetPath, patchedEditor)
+    expect(oldElement).toBeNull()
+
+    const wrapper = getElement(wrapperPath, patchedEditor)
+    expect(wrapper).not.toBeNull()
+    expect(wrapper?.type).toEqual('JSX_FRAGMENT')
+
+    const fragment = wrapper as JSXFragment
+    expect(fragment.children).toHaveLength(1)
+
+    const child = fragment.children[0]
+    expect(child).toEqual(originalElement)
+  })
+
+  it('wraps with a conditional', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      complexDefaultProjectPreParsed(),
+      'dont-await-first-dom-report',
+      createBuiltInDependenciesList(null),
+    )
+
+    const { editor } = renderResult.getEditorState()
+
+    const targetPath = getPath('card-instance')
+
+    const originalElement = getElement(targetPath, editor)
+    expect(originalElement).not.toBeNull()
+
+    const cmd = wrapInContainerCommand('always', targetPath, 'the-wrapper', 'conditional')
+    const result = runWrapInContainerCommand(editor, cmd)
+    const patchedEditor = updateEditorStateWithPatches(editor, result.editorStatePatches)
+
+    const wrapperPath = EP.appendToPath(EP.parentPath(targetPath), 'the-wrapper')
+
+    const oldElement = getElement(targetPath, patchedEditor)
+    expect(oldElement).toBeNull()
+
+    const wrapper = getElement(wrapperPath, patchedEditor)
+    expect(wrapper).not.toBeNull()
+    expect(wrapper?.type).toEqual('JSX_CONDITIONAL_EXPRESSION')
+
+    const conditional = wrapper as JSXConditionalExpression
+    // the true branch is the wrapped element
+    expect(conditional.whenTrue).toEqual(originalElement)
+    // the false branch is the default dummy text
+    expect(conditional.whenFalse.type).toEqual('JSX_ELEMENT')
+    const whenFalse = conditional.whenFalse as JSXElement
+    expect(whenFalse.children).toHaveLength(1)
+    expect(whenFalse.children[0].type).toEqual('JSX_TEXT_BLOCK')
+    expect((whenFalse.children[0] as JSXTextBlock).text).toEqual('False branch')
+  })
+
+  it('does nothing with a wrong path', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      complexDefaultProjectPreParsed(),
+      'dont-await-first-dom-report',
+      createBuiltInDependenciesList(null),
+    )
+
+    const { editor } = renderResult.getEditorState()
+
+    const targetPath = getPath('THIS-DOES-NOT-EXIST')
+
+    const originalElement = getElement(targetPath, editor)
+    expect(originalElement).toBeNull()
+
+    const cmd = wrapInContainerCommand('always', targetPath, 'the-wrapper', 'fragment')
+    const result = runWrapInContainerCommand(editor, cmd)
+    const patchedEditor = updateEditorStateWithPatches(editor, result.editorStatePatches)
+
+    const wrapperPath = EP.appendToPath(EP.parentPath(targetPath), 'the-wrapper')
+
+    const oldElement = getElement(targetPath, patchedEditor)
+    expect(oldElement).toBeNull()
+
+    const wrapper = getElement(wrapperPath, patchedEditor)
+    expect(wrapper).toBeNull()
+  })
+})
