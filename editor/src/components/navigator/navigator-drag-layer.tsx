d92f7517e21a20e926c2c45e09bd51aac2bb6376
Berci Kormendy <korber314@gmail.com>
2023-05-16 16:24:10 +0200

    add margin

--- editor/src/components/navigator/navigator-drag-layer.tsx
+++ editor/src/components/navigator/navigator-drag-layer.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useDragLayer } from 'react-dnd'
-import { regularNavigatorEntry } from '../editor/store/editor-state'
+import {
+  RegularNavigatorEntry,
+  navigatorEntryToKey,
+  regularNavigatorEntry,
+} from '../editor/store/editor-state'
 import { NavigatorItemDragAndDropWrapperProps } from './navigator-item/navigator-item-dnd-container'
 import { WindowPoint, windowPoint, zeroPoint } from '../../core/shared/math-utils'
 import { ItemLabel } from './navigator-item/item-label'
@@ -8,6 +12,18 @@ import { NO_OP } from '../../core/shared/utils'
 import { FlexRow, Icn } from '../../uuiui'
 import { useLayoutOrElementIcon } from './layout-element-icons'
 import { emptyElementPath } from '../../core/shared/element-path'
+import { Substores, useEditorState } from '../editor/store/store-hook'
+import { navigatorDepth } from './navigator-utils'
+import { getElementPadding } from './navigator-item/navigator-item'
+import { metadataSelector } from '../inspector/inpector-selectors'
+import createCachedSelector from 're-reselect'
+import { MetadataSubstate } from '../editor/store/store-hook-substore-types'
+
+const depthSelector = createCachedSelector(
+  metadataSelector,
+  (_: MetadataSubstate, navigatorEntry: RegularNavigatorEntry) => navigatorEntry,
+  (metadata, navigatorEntry) => navigatorDepth(navigatorEntry, metadata) + 1,
+)((_, navigatorEntry) => navigatorEntryToKey(navigatorEntry))
 
 export const NavigatorDragLayer = React.memo(() => {
   const { item, initialOffset, difference } = useDragLayer((monitor) => ({
@@ -16,12 +32,24 @@ export const NavigatorDragLayer = React.memo(() => {
     difference: (monitor.getDifferenceFromInitialOffset() as WindowPoint) ?? zeroPoint,
   }))
 
+  const navigatorEntry = React.useMemo(
+    () => regularNavigatorEntry(item?.elementPath ?? emptyElementPath),
+    [item?.elementPath],
+  )
+
+  const entryNavigatorDepth = useEditorState(
+    Substores.metadata,
+    (store) => depthSelector(store, navigatorEntry),
+    'NavigatorDragLayer metadata',
+  )
+
   const offset = windowPoint({
     x: initialOffset.x + difference.x,
     y: initialOffset.y + difference.y,
   })
-  const navigatorEntry = regularNavigatorEntry(item?.elementPath ?? emptyElementPath)
   const icon = useLayoutOrElementIcon(navigatorEntry)?.iconProps ?? {}
+  const label = item?.label ?? ''
+  const selected = item?.selected ?? false
 
   return (
     <div
@@ -34,27 +62,26 @@ export const NavigatorDragLayer = React.memo(() => {
         height: '100%',
       }}
     >
-      {item == null ? null : (
-        <FlexRow
-          style={{
-            width: '300px',
-            transform: `translate(${offset.x}px, ${offset.y}px)`,
-            fontWeight: 600,
-          }}
-        >
-          <Icn {...icon} color={'main'} />
-          <ItemLabel
-            key={`label-${item.label}`}
-            testId={`navigator-item-label-${item.label}`}
-            name={item.label}
-            isDynamic={false}
-            target={navigatorEntry}
-            selected={item.selected}
-            dispatch={NO_OP}
-            inputVisible={false}
-          />
-        </FlexRow>
-      )}
+      <FlexRow
+        style={{
+          paddingLeft: getElementPadding(entryNavigatorDepth),
+          width: '300px',
+          transform: `translate(${offset.x}px, ${offset.y}px)`,
+          fontWeight: 600,
+        }}
+      >
+        <Icn {...icon} color={'main'} />
+        <ItemLabel
+          key={`label-${label}`}
+          testId={`navigator-item-label-${label}`}
+          name={label}
+          isDynamic={false}
+          target={navigatorEntry}
+          selected={selected}
+          dispatch={NO_OP}
+          inputVisible={false}
+        />
+      </FlexRow>
     </div>
   )
 })
