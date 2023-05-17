d7ea4bdff88a66dcb2d46234563008960d81b3fd
Sean Parsons <git@futurenotfound.com>
2023-05-17 13:13:31 +0100

    chore(pr) Apply styling to server code.

--- server/src/Utopia/Web/Utils/Limits.hs
+++ server/src/Utopia/Web/Utils/Limits.hs
@@ -2,10 +2,11 @@
 
 module Utopia.Web.Utils.Limits where
 
-import Control.Monad.Trans.Control
-import Control.Concurrent.QSem.Lifted
-import Control.Exception.Lifted
-import Protolude hiding (bracket_, waitQSem, signalQSem)
+import           Control.Concurrent.QSem.Lifted
+import           Control.Exception.Lifted
+import           Control.Monad.Trans.Control
+import           Protolude                      hiding (bracket_, signalQSem,
+                                                 waitQSem)
 
 limitWithSemaphore :: (MonadBaseControl IO m) => QSem -> m a -> m a
 limitWithSemaphore semaphore action = bracket_ (waitQSem semaphore) (signalQSem semaphore) action
