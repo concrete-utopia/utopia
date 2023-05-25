976e843e9ff6f4f30b6a5a2781eb69f68c30c579
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-05-25 13:39:54 +0200

    introducing the concept of a not-quite-frame: shown as Frame, slightly greyed out

--- editor/src/components/canvas/canvas-strategies/strategies/contracts/contract-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/contracts/contract-helpers.ts
@@ -1,12 +1,15 @@
 import { ContentAffectingType } from '../group-like-helpers'
 
-export type EditorContract = 'fragment' | 'frame'
+export type EditorContract = 'fragment' | 'frame' | 'not-quite-frame'
 
 export function getEditorContractForContentAffectingType(
   type: ContentAffectingType | null,
 ): EditorContract {
-  if (type === 'fragment') {
+  if (type === 'fragment' || type === 'conditional') {
     return 'fragment'
+  }
+  if (type === 'sizeless-div') {
+    return 'not-quite-frame'
   }
   return 'frame'
 }
