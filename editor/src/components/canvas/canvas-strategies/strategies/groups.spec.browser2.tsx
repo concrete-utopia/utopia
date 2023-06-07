4a6848daed99c5018dc74fa08cc0addabbb8529e
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-06-07 15:59:10 +0200

    :(

--- editor/src/components/canvas/canvas-strategies/strategies/groups.spec.browser2.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/groups.spec.browser2.tsx
@@ -544,74 +544,6 @@ describe('Groups behaviors', () => {
       })
     })
 
-    // Group Resize support is coming in the next PR
-    xdescribe('Resizing The Group', () => {
-      it('if the group has no width/height prop, resize it in a fragment-like manner', async () => {
-        const editor = await renderProjectWithGroup(`
-        <Group data-testid='group' style={{position: 'absolute', left: 50, top: 50}}>
-          <div 
-            style={{
-              backgroundColor: 'red',
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: 100,
-              height: 100,
-            }}
-          />
-          <div 
-            style={{
-              backgroundColor: 'red',
-              position: 'absolute',
-              top: 150,
-              left: 150,
-              width: 50,
-              height: 50,
-            }}
-          />
-          <div 
-            style={{
-              backgroundColor: 'red',
-              position: 'absolute',
-              right: 0,
-              top: 0,
-              width: 25,
-              height: 25,
-            }}
-          />
-        </Group>
-      `)
-        const groupDiv = editor.renderedDOM.getByTestId('group')
-
-        expect(groupDiv.style.width).toBe('200px')
-        expect(groupDiv.style.height).toBe('200px')
-
-        await selectComponentsForTest(editor, [fromString(`${GroupPath}`)])
-
-        await resizeElement(
-          editor,
-          windowPoint({ x: 100, y: 100 }),
-          EdgePositionBottomRight,
-          emptyModifiers,
-        )
-
-        expect(groupDiv.style.width).toBe('300px')
-        expect(groupDiv.style.height).toBe('300px')
-      })
-
-      it(
-        'if the group does have width/height prop, resize it, and update the children in a fragment-like manner',
-      )
-      it('nested Fragments inside the group exhibit proper fragment-like resize behavior')
-      it('nested Groups inside the group exhibit proper group-like resize behavior')
-      it('an accidental static child disables all Group-like features')
-    })
-
-    describe('Moving The Group', () => {
-      it('if it has left/top/right/bottom pins, it moves like a regular Frame')
-      it('if it has no left/top/right/bottom pins, moving adds left/top')
-    })
-
     describe('Moving one Child', () => {
       it('moving a child inside the group move the child and offsets the group and all group children so they stay within the bounds', async () => {
         const editor = await renderProjectWithGroup(`
@@ -1369,34 +1301,6 @@ describe('Groups behaviors', () => {
           height: 25,
         })
       })
-    })
-
-    describe('Resizing one Child', () => {
-      it(
-        'resizing a child inside the group move the child and offsets the group and all group children so they stay within the bounds',
-      )
-
-      it('resizing a child to expand bottom-right')
-
-      it('resizing a child to expand top-left')
-
-      it('group with width/height prop gets updated in the editor')
-
-      it('children with pins to the right and bottom work properly')
-
-      it('children offset behavior works with nested Fragments')
-
-      it('an accidental static child disables all Group-like features')
-    })
-
-    describe('Inserting into the Grouop', () => {
-      it('draw-to-insert into the group is not possible')
-      it('canvas drag to reparent into the group is not possible')
-    })
-
-    describe('Navigator shows Warning for non group-like Group components', () => {
-      it('warns if group has a static child')
-      it('warns if group has a child with percentage pin')
     })
   })
 })
