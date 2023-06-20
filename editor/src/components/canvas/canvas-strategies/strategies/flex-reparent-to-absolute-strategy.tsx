385bf8be09f8e3aad5e1fc1cd40d7a583c13ed20
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-06-20 16:12:59 +0200

    Fix navigator ghosts during reparenting (#3834)
    * Fix navigator ghosts during reparenting
    
    * update snapshots

--- editor/src/components/canvas/canvas-strategies/strategies/flex-reparent-to-absolute-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/flex-reparent-to-absolute-strategy.tsx
@@ -105,6 +105,7 @@ export function baseFlexReparentToAbsoluteStrategy(
                     const absoluteReparentStrategyToUse = baseAbsoluteReparentStrategy(
                       reparentTarget,
                       0,
+                      customStrategyState,
                     )
                     const reparentCommands =
                       absoluteReparentStrategyToUse(
