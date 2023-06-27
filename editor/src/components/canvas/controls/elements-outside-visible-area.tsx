124fb8243a03ae40becc58d6dfbda43a498b19fb
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-06-27 14:15:38 +0200

    Elements outside visible area indicators (#3842)
    * display indicators for elements outside visible area
    
    * trim down variables
    
    * accommodate for canvas toolbar
    
    * skew
    
    * cluster arrows
    
    * update snaps
    
    * add comment
    
    * improve perf
    
    * center arrow
    
    * scaling work
    
    * fix laggy transform
    
    * fix scaling and perf
    
    * fix clustering
    
    * use dynamicblue
    
    * move trimming to hook
    
    * use helper for axis
    
    * indicator bg and shadow
    
    * refactor type
    
    * drop status
    
    * more readable code
    
    * test
    
    * add types
    
    * move to separate file
    
    * move hook inside ElementsOutsideVisibleAreaIndicators
    
    * remove unused directives
    
    * use clamp
    
    * loop for indicators instead of map+reduce
    
    * use windowPoint helper
    
    ---------
    
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>

---
+++ editor/src/components/canvas/controls/elements-outside-visible-area.tsx
@@ -0,0 +1,126 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+import { jsx } from '@emotion/react'
+import React from 'react'
+import { ElementPath } from '../../../core/shared/project-file-types'
+import { useColorTheme } from '../../../uuiui'
+import { useDispatch } from '../../editor/store/dispatch-context'
+import {
+  ElementOutisdeVisibleAreaIndicatorSize,
+  getIndicatorClusterLabel,
+  useElementsOutsideVisibleArea,
+} from './elements-outside-visible-area-hooks'
+import { scrollToElement } from '../../editor/actions/action-creators'
+import { when } from '../../../utils/react-conditionals'
+
+export const ElementsOutsideVisibleAreaIndicators = React.memo(
+  ({
+    canvasRef,
+    localSelectedViews,
+    localHighlightedViews,
+  }: {
+    canvasRef: React.MutableRefObject<HTMLDivElement | null>
+    localSelectedViews: ElementPath[]
+    localHighlightedViews: ElementPath[]
+  }) => {
+    const dispatch = useDispatch()
+    const colorTheme = useColorTheme()
+
+    const scrollTo = React.useCallback(
+      (path: ElementPath) => () => {
+        dispatch([scrollToElement(path, false)])
+      },
+      [dispatch],
+    )
+
+    const indicators = useElementsOutsideVisibleArea(
+      canvasRef,
+      localHighlightedViews,
+      localSelectedViews,
+    )
+
+    return (
+      <>
+        {indicators.map((indicator) => {
+          const color = colorTheme.dynamicBlue.value
+          const clusterRightMargin = indicator.cluster < 10 ? 7 : indicator.cluster === 10 ? 14 : 19
+          const isCluster = indicator.cluster > 1
+          const testId = indicator.id + (isCluster ? `-cluster-${indicator.cluster}` : '')
+
+          return (
+            <div
+              key={indicator.id}
+              data-testid={testId}
+              title='Scroll to element'
+              style={{
+                position: 'absolute',
+                left: indicator.position.x,
+                top: indicator.position.y,
+                transform: `rotate(${indicator.angle}rad)`,
+                color: color,
+                fontWeight: 'bolder',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                backgroundColor: 'transparent',
+                width: ElementOutisdeVisibleAreaIndicatorSize,
+                height: ElementOutisdeVisibleAreaIndicatorSize,
+              }}
+              onClick={scrollTo(indicator.path)}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  position: 'relative',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  cursor: 'pointer',
+                }}
+              >
+                <div
+                  style={{
+                    fontSize: 15,
+                    width: ElementOutisdeVisibleAreaIndicatorSize - 5,
+                    height: ElementOutisdeVisibleAreaIndicatorSize - 5,
+                    display: 'flex',
+                    marginRight: 2,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    paddingBottom: 1,
+                    borderRadius: '100%',
+                    boxShadow: `${colorTheme.canvasControlsSizeBoxShadowColor20.value} 0px 0px 3px`,
+                  }}
+                  css={{
+                    backgroundColor: colorTheme.bg0.value,
+                    '&:hover': {
+                      color: 'white',
+                      backgroundColor: color,
+                    },
+                  }}
+                >
+                  ←
+                </div>
+                {when(
+                  isCluster,
+                  <div
+                    style={{
+                      position: 'absolute',
+                      right: -clusterRightMargin,
+                      fontSize: 9,
+                      transform: `rotate(${Math.PI * 2 - indicator.angle}rad)`, // rotate the label back so it always "faces" the reading direction
+                    }}
+                  >
+                    {getIndicatorClusterLabel(indicator)}
+                  </div>,
+                )}
+              </div>
+            </div>
+          )
+        })}
+      </>
+    )
+  },
+)
+
+ElementsOutsideVisibleAreaIndicators.displayName = 'ElementsOutsideVisibleAreaIndicators'
---
+++ editor/src/components/canvas/controls/elements-outside-visible.spec.browser2.tsx
@@ -0,0 +1,340 @@
+import { act, screen } from '@testing-library/react'
+import * as EP from '../../../core/shared/element-path'
+import { selectComponents } from '../../editor/actions/meta-actions'
+import { keyDown, mouseDragFromPointToPoint } from '../event-helpers.test-utils'
+import {
+  EditorRenderResult,
+  makeTestProjectCodeWithSnippet,
+  renderTestEditorWithCode,
+} from '../ui-jsx.test-utils'
+import { CanvasControlsContainerID } from './new-canvas-controls'
+import { getIndicatorId } from './elements-outside-visible-area-hooks'
+import { ElementPath } from '../../../core/shared/project-file-types'
+
+async function panCanvas(
+  x: number,
+  y: number,
+  canvasControlsLayer: HTMLElement,
+  controlsBounds: DOMRect,
+) {
+  return act(async () => {
+    keyDown('Space')
+    await mouseDragFromPointToPoint(
+      canvasControlsLayer,
+      {
+        x: controlsBounds.x + controlsBounds.width / 2,
+        y: controlsBounds.y + controlsBounds.height / 2,
+      },
+      {
+        x: controlsBounds.x + controlsBounds.width / 2 + x,
+        y: controlsBounds.y + controlsBounds.height / 2 + y,
+      },
+    )
+  })
+}
+
+async function selectAndPan(
+  renderResult: EditorRenderResult,
+  targetPaths: ElementPath[],
+  panX: number,
+  panY: number,
+) {
+  await renderResult.dispatch(selectComponents(targetPaths, true), true)
+
+  const canvasControlsLayer = renderResult.renderedDOM.getByTestId(CanvasControlsContainerID)
+  const controlsBounds = canvasControlsLayer.getBoundingClientRect()
+
+  await panCanvas(panX, panY, canvasControlsLayer, controlsBounds)
+}
+
+const farAway = 99999999 // px
+
+describe('elements outside visible area', () => {
+  describe('single element', () => {
+    it('shows the indicator on the left side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['left']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], -farAway, 0)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['left']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('182px')
+      expect(indicator?.style.left).toBe('363px')
+    })
+    it('shows the indicator on the right side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['right']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], farAway, 0)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['right']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('182px')
+      expect(indicator?.style.left).toBe('1920px')
+    })
+    it('shows the indicator on the top side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['top']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], 0, -farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['top']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('0px')
+      expect(indicator?.style.left).toMatch('493px')
+    })
+    it('shows the indicator on the bottom side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['bottom']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], 0, farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['bottom']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('936px')
+      expect(indicator?.style.left).toMatch('493px')
+    })
+    it('shows the indicator on the top-left side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['top', 'left']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], -farAway, -farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['top', 'left']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('0px')
+      expect(indicator?.style.left).toMatch('363px')
+    })
+    it('shows the indicator on the top-right side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['top', 'right']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], farAway, -farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['top', 'right']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('0px')
+      expect(indicator?.style.left).toMatch('1920px')
+    })
+    it('shows the indicator on the bottom-left side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['bottom', 'left']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], -farAway, farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['bottom', 'left']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('936px')
+      expect(indicator?.style.left).toMatch('282px')
+    })
+    it('shows the indicator on the bottom-right side', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='foo' style={{
+          width: 50,
+          height: 50,
+          position: "absolute",
+          left: 100,
+          top: 100,
+          background: "#f09"
+        }} />
+      `),
+        'await-first-dom-report',
+      )
+
+      const targetPath = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:foo')
+      expect(screen.queryByTestId(getIndicatorId(targetPath, ['bottom', 'right']))).toBeNull()
+
+      await selectAndPan(renderResult, [targetPath], farAway, farAway)
+
+      const indicator = screen.queryByTestId(getIndicatorId(targetPath, ['bottom', 'right']))
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('936px')
+      expect(indicator?.style.left).toMatch('1920px')
+    })
+  })
+  describe('multiple elements', () => {
+    it('shows indicators for two elements outside of the visible area', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='container'>
+          <div data-uid='foo' style={{
+            width: 50,
+            height: 50,
+            position: "absolute",
+            left: 300,
+            top: 100,
+            background: "#f09"
+          }} />
+          <div data-uid='bar' style={{
+            width: 50,
+            height: 50,
+            position: "absolute",
+            left: 0,
+            top: 300,
+            background: "#09f"
+          }} />
+        </div>
+      `),
+        'await-first-dom-report',
+      )
+
+      const foo = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:container/foo')
+      const bar = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:container/bar')
+      expect(screen.queryByTestId(getIndicatorId(foo, ['top']))).toBeNull()
+      expect(screen.queryByTestId(getIndicatorId(bar, ['left']))).toBeNull()
+
+      await selectAndPan(renderResult, [foo, bar], -250, -300)
+
+      const indicatorFoo = screen.queryByTestId(getIndicatorId(foo, ['top']))
+      expect(indicatorFoo).not.toBeNull()
+      expect(indicatorFoo?.style.top).toBe('0px')
+      expect(indicatorFoo?.style.left).toMatch('443px')
+
+      const indicatorBar = screen.queryByTestId(getIndicatorId(bar, ['left']))
+      expect(indicatorBar).not.toBeNull()
+      expect(indicatorBar?.style.top).toBe('82px')
+      expect(indicatorBar?.style.left).toMatch('363px')
+    })
+    it('shows a single indicator for clustered elements', async () => {
+      const renderResult = await renderTestEditorWithCode(
+        makeTestProjectCodeWithSnippet(`
+        <div data-uid='container'>
+          <div data-uid='foo' style={{
+            width: 50,
+            height: 50,
+            position: "absolute",
+            left: 300,
+            top: 100,
+            background: "#f09"
+          }} />
+          <div data-uid='bar' style={{
+            width: 50,
+            height: 50,
+            position: "absolute",
+            left: 0,
+            top: 300,
+            background: "#09f"
+          }} />
+        </div>
+      `),
+        'await-first-dom-report',
+      )
+
+      const foo = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:container/foo')
+      const bar = EP.fromString('utopia-storyboard-uid/scene-aaa/app-entity:container/bar')
+      expect(screen.queryByTestId(getIndicatorId(foo, ['top']))).toBeNull()
+      expect(screen.queryByTestId(getIndicatorId(bar, ['left']))).toBeNull()
+      expect(screen.queryByTestId(getIndicatorId(bar, ['top', 'left']) + '-cluster-2')).toBeNull()
+
+      await selectAndPan(renderResult, [foo, bar], -farAway, -farAway)
+
+      expect(screen.queryByTestId(getIndicatorId(foo, ['top']))).toBeNull()
+      expect(screen.queryByTestId(getIndicatorId(bar, ['left']))).toBeNull()
+
+      const indicator = screen.queryByTestId(getIndicatorId(foo, ['top', 'left']) + '-cluster-2')
+      expect(indicator).not.toBeNull()
+      expect(indicator?.style.top).toBe('0px')
+      expect(indicator?.style.left).toMatch('363px')
+    })
+  })
+})
