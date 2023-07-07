1841133746d3ae2b3ce0d58ee5f9e2e642325165
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-07-07 15:06:18 +0200

    QueueGroupTrueUp command

---
+++ editor/src/components/canvas/commands/queue-group-true-up-command.ts
@@ -0,0 +1,31 @@
+import * as EP from '../../../core/shared/element-path'
+import type { ElementPath } from '../../../core/shared/project-file-types'
+import type { EditorState } from '../../editor/store/editor-state'
+import type { BaseCommand, CommandFunction } from './commands'
+
+export interface QueueGroupTrueUp extends BaseCommand {
+  type: 'QUEUE_GROUP_TRUE_UP'
+  element: ElementPath
+}
+
+export function queueGroupTrueUp(element: ElementPath): QueueGroupTrueUp {
+  return {
+    type: 'QUEUE_GROUP_TRUE_UP',
+    whenToRun: 'on-complete',
+    element: element,
+  }
+}
+
+export const runQueueGroupTrueUp: CommandFunction<QueueGroupTrueUp> = (
+  editorState: EditorState,
+  command: QueueGroupTrueUp,
+) => {
+  return {
+    commandDescription: `Queue element for group true-up once the intearction finished: ${EP.toString(
+      command.element,
+    )}`,
+    editorStatePatches: [
+      { trueUpGroupsForElementAfterDomWalkerRuns: { $push: [command.element] } },
+    ],
+  }
+}
