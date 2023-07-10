106f9d676d254e51f285ec75ea51366d37b552de
McKayla Lankau <lankaukk@yahoo.com>
2023-07-10 12:05:50 -0400

    pls fix errors

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -187,7 +187,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`570`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`569`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -253,7 +253,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`642`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`641`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
