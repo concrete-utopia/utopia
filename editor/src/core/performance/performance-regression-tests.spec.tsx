e8095c3f774979ebc5ee4378a149174f6fccba77
Federico Ruggi <federico.ruggi@shopify.com>
2023-07-12 11:34:51 +0200

    update snaps

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`472`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`486`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`543`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`557`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
