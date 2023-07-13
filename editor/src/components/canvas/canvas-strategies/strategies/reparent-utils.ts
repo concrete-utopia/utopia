c7ea0d272ca77ad741860e9aed585d79dfa9ac6b
Sean Parsons <git@futurenotfound.com>
2023-07-13 15:31:43 +0100

    chore(pr) Fix broken refactor.

--- editor/src/components/canvas/canvas-strategies/strategies/reparent-utils.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/reparent-utils.ts
@@ -48,8 +48,8 @@ import { getUtopiaID } from '../../../../core/shared/uid-utils'
 import type { IndexPosition } from '../../../../utils/utils'
 import { fastForEach } from '../../../../core/shared/utils'
 import { addElements } from '../../commands/add-elements-command'
-import { addPossibleTrueUp } from '../../commands/queue-group-true-up-command'
 import type { ElementPathTrees } from '../../../../core/shared/element-path-tree'
+import { getRequiredGroupTrueUps } from '../../commands/queue-group-true-up-command'
 
 interface GetReparentOutcomeResult {
   commands: Array<CanvasCommand>
@@ -144,7 +144,7 @@ export function getReparentOutcome(
       )
       commands.push(addImportsToFile(whenToRun, newTargetFilePath, importsToAdd))
       commands.push(reparentElement(whenToRun, toReparent.target, newParent, indexPosition))
-      commands.push(...addPossibleTrueUp(metadata, pathTrees, toReparent.target))
+      commands.push(...getRequiredGroupTrueUps(metadata, pathTrees, toReparent.target))
       newPath = EP.appendToPath(newParentElementPath, EP.toUid(toReparent.target))
       break
     case 'ELEMENT_TO_REPARENT':
