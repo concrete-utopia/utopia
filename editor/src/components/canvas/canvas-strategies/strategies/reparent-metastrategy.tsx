eb164a31a93751871e36ab4e5188168b227a04e3
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2023-07-17 16:40:06 +0100

    fix(editor) Make the entire existingParents path static

--- editor/src/components/canvas/canvas-strategies/strategies/reparent-metastrategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/reparent-metastrategy.tsx
@@ -255,7 +255,7 @@ export const reparentMetaStrategy: MetaCanvasStrategy = (
     return []
   }
 
-  const existingParents = reparentSubjects.map((p) => EP.makeLastPartOfPathStatic(EP.parentPath(p)))
+  const existingParents = reparentSubjects.map((p) => EP.dynamicPathToStaticPath(EP.parentPath(p)))
 
   const startingTargetsToFilter = getStartingTargetParentsToFilterOut(
     canvasState,
