58b81cb4c15ac0ab0703c1184784a90d976de91c
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-07-19 16:48:03 +0200

    Revert "Added delete test"
    This reverts commit adc5e40a632af537a99c9a8aa4b92b24ffa8f0af.

--- editor/src/components/editor/actions/actions.spec.browser2.tsx
+++ editor/src/components/editor/actions/actions.spec.browser2.tsx
@@ -89,251 +89,223 @@ describe('actions', () => {
       {
         name: 'delete selected element',
         input: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
-        />
-        <View
-          style={{ background: '#f90', width: 50, height: 50 }}
-          data-uid='ccc'
-          data-testid='ccc'
-        />
-      </View>
-      `,
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <View
+        style={{ background: '#f90', width: 50, height: 50 }}
+        data-uid='ccc'
+        data-testid='ccc'
+      />
+    </View>
+    `,
         targets: [makeTargetPath('aaa/bbb')],
         wantCode: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#f90', width: 50, height: 50 }}
-          data-uid='ccc'
-          data-testid='ccc'
-        />
-      </View>
-      `,
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#f90', width: 50, height: 50 }}
+        data-uid='ccc'
+        data-testid='ccc'
+      />
+    </View>
+    `,
         wantSelection: [makeTargetPath('aaa')],
       },
       {
         name: 'delete multiple elements',
         input: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
-        />
-        <View
-          style={{ background: '#f90', width: 50, height: 50 }}
-          data-uid='ccc'
-          data-testid='ccc'
-        />
-        <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
-        />
-      </View>
-      `,
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <View
+        style={{ background: '#f90', width: 50, height: 50 }}
+        data-uid='ccc'
+        data-testid='ccc'
+      />
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+    </View>
+    `,
         targets: [makeTargetPath('aaa/bbb'), makeTargetPath('aaa/ddd')],
         wantCode: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#f90', width: 50, height: 50 }}
-          data-uid='ccc'
-          data-testid='ccc'
-        />
-      </View>
-      `,
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#f90', width: 50, height: 50 }}
+        data-uid='ccc'
+        data-testid='ccc'
+      />
+    </View>
+    `,
         wantSelection: [makeTargetPath('aaa')],
       },
       {
         name: 'delete empty fragments (single fragment)',
         input: `
-      <View data-uid='aaa'>
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <React.Fragment data-uid='000'>
         <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
+          style={{ background: '#f90', width: 50, height: 50 }}
+          data-uid='ccc'
+          data-testid='ccc'
         />
-        <React.Fragment data-uid='000'>
-          <View
-            style={{ background: '#f90', width: 50, height: 50 }}
-            data-uid='ccc'
-            data-testid='ccc'
-          />
-        </React.Fragment>
-        <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
-        />
-      </View>
-      `,
+      </React.Fragment>
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+    </View>
+    `,
         targets: [makeTargetPath('aaa/000/ccc')],
         wantCode: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
-        />
-        <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
-        />
-      </View>
-      `,
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+    </View>
+    `,
         wantSelection: [makeTargetPath('aaa')],
       },
       {
         name: "don't delete fragments if not empty",
         input: `
-      <View data-uid='aaa'>
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <React.Fragment data-uid='000'>
         <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
+          style={{ background: '#f90', width: 50, height: 50 }}
+          data-uid='ccc'
+          data-testid='ccc'
         />
-        <React.Fragment data-uid='000'>
-          <View
-            style={{ background: '#f90', width: 50, height: 50 }}
-            data-uid='ccc'
-            data-testid='ccc'
-          />
-          <View
-            style={{ background: '#90f', width: 50, height: 50 }}
-            data-uid='eee'
-            data-testid='eee'
-          />
-        </React.Fragment>
         <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
+          style={{ background: '#90f', width: 50, height: 50 }}
+          data-uid='eee'
+          data-testid='eee'
         />
-      </View>
-      `,
+      </React.Fragment>
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+    </View>
+    `,
         targets: [makeTargetPath('aaa/000/eee')],
         wantCode: `
-      <View data-uid='aaa'>
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <React.Fragment>
         <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
+          style={{ background: '#f90', width: 50, height: 50 }}
+          data-uid='ccc'
+          data-testid='ccc'
         />
-        <React.Fragment>
-          <View
-            style={{ background: '#f90', width: 50, height: 50 }}
-            data-uid='ccc'
-            data-testid='ccc'
-          />
-        </React.Fragment>
-        <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
-        />
-      </View>
-      `,
+      </React.Fragment>
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+    </View>
+    `,
         wantSelection: [makeTargetPath('aaa/000')],
       },
       {
         name: 'delete empty fragments (multiple targets)',
         input: `
-      <View data-uid='aaa'>
+    <View data-uid='aaa'>
+      <View
+        style={{ background: '#09f', width: 50, height: 50 }}
+        data-uid='bbb'
+        data-testid='bbb'
+      />
+      <React.Fragment data-uid='000'>
         <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
+          style={{ background: '#f90', width: 50, height: 50 }}
+          data-uid='ccc'
+          data-testid='ccc'
         />
-        <React.Fragment data-uid='000'>
-          <View
-            style={{ background: '#f90', width: 50, height: 50 }}
-            data-uid='ccc'
-            data-testid='ccc'
-          />
-          <View
-            style={{ background: '#90f', width: 50, height: 50 }}
-            data-uid='eee'
-            data-testid='eee'
-          />
-        </React.Fragment>
         <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
+          style={{ background: '#90f', width: 50, height: 50 }}
+          data-uid='eee'
+          data-testid='eee'
         />
-        <React.Fragment data-uid='001'>
-          <View
-            style={{ background: '#0f9', width: 50, height: 50 }}
-            data-uid='fff'
-            data-testid='fff'
-          />
-          <View
-            style={{ background: '#9f0', width: 50, height: 50 }}
-            data-uid='ggg'
-            data-testid='ggg'
-          />
-        </React.Fragment>
-      </View>
-      `,
+      </React.Fragment>
+      <View
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
+      />
+      <React.Fragment data-uid='001'>
+        <View
+          style={{ background: '#0f9', width: 50, height: 50 }}
+          data-uid='fff'
+          data-testid='fff'
+        />
+        <View
+          style={{ background: '#9f0', width: 50, height: 50 }}
+          data-uid='ggg'
+          data-testid='ggg'
+        />
+      </React.Fragment>
+    </View>
+    `,
         targets: [
           makeTargetPath('aaa/000/eee'),
           makeTargetPath('aaa/001/fff'),
           makeTargetPath('aaa/001/ggg'),
         ],
         wantCode: `
-      <View data-uid='aaa'>
-        <View
-          style={{ background: '#09f', width: 50, height: 50 }}
-          data-uid='bbb'
-          data-testid='bbb'
-        />
-        <React.Fragment>
-          <View
-            style={{ background: '#f90', width: 50, height: 50 }}
-            data-uid='ccc'
-            data-testid='ccc'
-          />
-        </React.Fragment>
-        <View
-          style={{ background: '#f09', width: 50, height: 50 }}
-          data-uid='ddd'
-          data-testid='ddd'
-        />
-      </View>
-      `,
-        wantSelection: [makeTargetPath('aaa/000'), makeTargetPath('aaa')],
-      },
-      {
-        name: 'delete expression',
-        input: `
     <View data-uid='aaa'>
-      {[0,1,2,3].map(() => (<View
+      <View
         style={{ background: '#09f', width: 50, height: 50 }}
         data-uid='bbb'
         data-testid='bbb'
-      />))}
+      />
+      <React.Fragment>
+        <View
+          style={{ background: '#f90', width: 50, height: 50 }}
+          data-uid='ccc'
+          data-testid='ccc'
+        />
+      </React.Fragment>
       <View
-        style={{ background: '#f90', width: 50, height: 50 }}
-        data-uid='ccc'
-        data-testid='ccc'
+        style={{ background: '#f09', width: 50, height: 50 }}
+        data-uid='ddd'
+        data-testid='ddd'
       />
     </View>
     `,
-        targets: [makeTargetPath('aaa/331')],
-        wantCode: `
-    <View data-uid='aaa'>
-      <View
-        style={{ background: '#f90', width: 50, height: 50 }}
-        data-uid='ccc'
-        data-testid='ccc'
-      />
-    </View>
-    `,
-        wantSelection: [makeTargetPath('aaa')],
+        wantSelection: [makeTargetPath('aaa/000'), makeTargetPath('aaa')],
       },
     ]
     tests.forEach((tt, idx) => {
