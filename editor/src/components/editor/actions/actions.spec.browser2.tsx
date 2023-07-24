40db7b78b18326052a1a2dba64fa82fdb71815bf
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2023-07-24 14:38:12 +0100

    fix(tests) Fixed rounding errors in tests caused by merge

--- editor/src/components/editor/actions/actions.spec.browser2.tsx
+++ editor/src/components/editor/actions/actions.spec.browser2.tsx
@@ -2328,7 +2328,7 @@ export var storyboard = (props) => {
         style={{
           backgroundColor: '#da82c9',
           position: 'absolute',
-          left: 737.5,
+          left: 738,
           top: 316,
           width: 244,
           height: 208,
@@ -2339,7 +2339,7 @@ export var storyboard = (props) => {
         style={{
           backgroundColor: '#da82c9',
           position: 'absolute',
-          left: 991.5,
+          left: 992,
           top: 316,
           width: 244,
           height: 208,
@@ -2350,7 +2350,7 @@ export var storyboard = (props) => {
         style={{
           backgroundColor: '#da82c9',
           position: 'absolute',
-          left: 1245.5,
+          left: 1246,
           top: 316,
           width: 244,
           height: 208,
@@ -2361,7 +2361,7 @@ export var storyboard = (props) => {
         style={{
           backgroundColor: '#da82c9',
           position: 'absolute',
-          left: 1499.5,
+          left: 1500,
           top: 316,
           width: 244,
           height: 208,
@@ -3236,7 +3236,7 @@ export var storyboard = (props) => {
                 <div data-uid='bbb' style={{position: 'absolute', width: 50, height: 40, top: 30, left: 20}}>Hello!</div>
               </div>`,
             targets: [makeTargetPath('root/bbb')],
-            result: `<div data-uid='aai' style={{position: 'absolute', width: 50, height: 40, top: 400, left: 834.5}}>Hello!</div>`,
+            result: `<div data-uid='aai' style={{position: 'absolute', width: 50, height: 40, top: 400, left: 835}}>Hello!</div>`,
           },
           {
             name: `paste a flex child into the storyboard`,
@@ -3248,7 +3248,7 @@ export var storyboard = (props) => {
                 </div>
               </div>`,
             targets: [makeTargetPath('root/bbb/ddd')],
-            result: `<div data-uid='aak' style={{ height: 20, top: 410, left: 674.5, position: 'absolute' }}>
+            result: `<div data-uid='aak' style={{ height: 20, top: 410, left: 675, position: 'absolute' }}>
                 <div data-uid='aae' style={{ width: 20, height: 20 }}/>
               </div>`,
           },
@@ -3259,8 +3259,8 @@ export var storyboard = (props) => {
               <div data-uid='bello' style={{ position: 'absolute', top: 30, left: 30, contain: 'layout', height: 20 }}>bello</div>
             </div>`,
             targets: [makeTargetPath('root/hello'), makeTargetPath('root/bello')],
-            result: `<div data-uid='hel' style={{ position: 'absolute', top: 405, left: 853.5, contain: 'layout', height: 20 }}>hello</div>
-            <div data-uid='bel' style={{ position: 'absolute', top: 415, left: 833.5, contain: 'layout', height: 20 }}>bello</div>`,
+            result: `<div data-uid='hel' style={{ position: 'absolute', top: 405, left: 854, contain: 'layout', height: 20 }}>hello</div>
+            <div data-uid='bel' style={{ position: 'absolute', top: 415, left: 834, contain: 'layout', height: 20 }}>bello</div>`,
           },
         ]
 
@@ -3842,8 +3842,8 @@ export var storyboard = (
         position: 'absolute',
         width: 44,
         height: 33,
-        top: 403.5,
-        left: 837.5,
+        top: 404,
+        left: 838,
         backgroundColor: '#cee5ff',
       }}
       onClick={undefined}
@@ -3971,11 +3971,7 @@ export var storyboard = (
     <div
       data-label='grandParent'
       data-uid='roo'
-      style={{
-        top: 420,
-        left: 759.5,
-        position: 'absolute',
-      }}
+      style={{ top: 420, left: 760, position: 'absolute' }}
     >
       <div
         data-label='parent'
@@ -4556,11 +4552,7 @@ export var storyboard = (
     </Scene>
     <div
       data-uid='roo'
-      style={{
-        top: 420,
-        left: 759.5,
-        position: 'absolute',
-      }}
+      style={{ top: 420, left: 760, position: 'absolute' }}
     >
       <div data-uid='par'>
         <div
