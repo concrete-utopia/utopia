7833e8291b8e1ee3bcbbd28b6d40c1c61850e05e
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-07-26 15:15:14 +0100

    feature(tests) Add an option for full logging of browser tests. (#3988)
    - Added `FullConsoleMessages` logging reporter.
    - Added configuration to the Karma config handling to switch on `FullConsoleMessages`
      if `full-output` is passed to the runner.
    - Added `test-browser-full-output` npm script.

---
+++ editor/test/karma-custom-reporter/full-console-messages.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+var Levels = ['error', 'warn', 'info', 'debug', 'log']
+var FullConsoleMessages = function (baseReporterDecorator, formatError, config) {
+  baseReporterDecorator(this)
+  var self = this
+  /**
+   * @type Array<{type: string, log: string }>
+   */
+  var consoleMessagesForSpec = []
+  /**
+   * @type Array<{ {type: string, log: string, specName: string}}>
+   */
+  var allConsoleMessages = []
+
+  function printCleanConsoleMessages(consoleMessages) {
+    var messagesFiltered = []
+    consoleMessages.forEach((message) => {
+      self.write(`${message.type.toUpperCase()} ${message.log}\n`)
+      self.write(`this console message is from: \n  ${message.specName}\n`)
+      self.write(`\n`)
+    })
+  }
+
+  self.onBrowserLog = function (browser, log, type) {
+    consoleMessagesForSpec.push({
+      type: type,
+      log: log,
+    })
+  }
+
+  self.onRunComplete = function (browsers, results) {
+    printCleanConsoleMessages(allConsoleMessages)
+  }
+
+  self.onSpecComplete = function (browsers, results) {
+    const withSpecName = consoleMessagesForSpec.map((messageForSpec) => ({
+      ...messageForSpec,
+      specName: `${results.suite} ${results.description}`,
+    }))
+    allConsoleMessages.push(...withSpecName)
+    consoleMessagesForSpec = []
+  }
+}
+
+FullConsoleMessages.$inject = ['baseReporterDecorator', 'formatError', 'config']
+
+module.exports = {
+  'reporter:full-console-messages': ['type', FullConsoleMessages],
+}
