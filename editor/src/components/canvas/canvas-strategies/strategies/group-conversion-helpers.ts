4ca97c45bcd2b1ef110e9f04c7c0d960ab06b943
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-08-01 16:56:52 +0200

    Prevent wrapping/cutting empty groups (#4016)
    * prevent cutting empty groups on the sb
    
    * prevent wrapping an empty group into a group
    
    * helper
    
    * don't wrap via insert menu
    
    * tests
    
    ---------
    
    Co-authored-by: Federico Ruggi <federico.ruggi@shopify.com>

--- editor/src/components/canvas/canvas-strategies/strategies/group-conversion-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/group-conversion-helpers.ts
@@ -92,6 +92,7 @@ import { updateSelectedViews } from '../../commands/update-selected-views-comman
 import { getLayoutProperty } from '../../../../core/layout/getLayoutProperty'
 import { styleStringInArray } from '../../../../utils/common-constants'
 import type { StyleLayoutProp } from '../../../../core/layout/layout-helpers-new'
+import { isEmptyGroup, treatElementAsGroupLike } from './group-helpers'
 
 const GroupImport: Imports = {
   'utopia-api': {
@@ -684,6 +685,11 @@ export function createWrapInGroupActions(
     return showToast(
       notice('Only simple JSX Elements can be wrapped into Groups for now 🙇', 'ERROR'),
     )
+  }
+
+  const anyTargetIsAnEmptyGroup = orderedActionTargets.some((e) => isEmptyGroup(metadata, e))
+  if (anyTargetIsAnEmptyGroup) {
+    return showToast(notice('Empty Groups cannot be wrapped into Groups', 'ERROR'))
   }
 
   // TODO if any target doesn't honour the size or offset prop, refuse wrapping and show toast!
