cbc3862e49361e05a450164cb7e029510f3e00a3
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-08-03 17:52:23 +0200

    Added tests

---
+++ editor/src/components/editor/maps.spec.browser2.tsx
@@ -0,0 +1,97 @@
+import { renderTestEditorWithCode } from '../canvas/ui-jsx.test-utils'
+import * as EP from '../../core/shared/element-path'
+import { MetadataUtils } from '../../core/model/element-metadata-utils'
+
+function getProjectTestCode(arrayLength: number, countOverride: number | string | null) {
+  return `import * as React from 'react'
+  import { Storyboard } from 'utopia-api'
+  export var storyboard = () => {
+    const arr = new Array(${arrayLength}).fill(1)
+    return (
+      <Storyboard data-uid='sb'>
+        <div data-uid='group'>
+          {
+            // @utopia/uid=map
+            ${countOverride != null ? `// @utopia/map-count=${countOverride}` : ''}
+            arr.map((i) => (
+              <div>
+                <div>inner div</div>
+              </div>
+            ))
+          }
+        </div>
+      </Storyboard>
+    )
+  }
+`
+}
+
+describe('map count override', () => {
+  const testCases = [
+    {
+      length: 4,
+      countOverride: null,
+      expectedCount: 4,
+    },
+    {
+      length: 4,
+      countOverride: 4,
+      expectedCount: 4,
+    },
+    {
+      length: 4,
+      countOverride: 3,
+      expectedCount: 3,
+    },
+    {
+      length: 4,
+      countOverride: 2,
+      expectedCount: 2,
+    },
+    {
+      length: 4,
+      countOverride: 1,
+      expectedCount: 1,
+    },
+    {
+      length: 4,
+      countOverride: 0,
+      expectedCount: 0,
+    },
+    {
+      length: 4,
+      countOverride: 'foo',
+      expectedCount: 4,
+    },
+    {
+      length: 4,
+      countOverride: 5,
+      expectedCount: 4,
+    },
+    {
+      length: 4,
+      countOverride: NaN,
+      expectedCount: 4,
+    },
+  ]
+
+  testCases.forEach(({ length, countOverride, expectedCount }) =>
+    it(`overriding original count ${length} with ${countOverride} results in count ${expectedCount}`, async () => {
+      const renderResult = await renderTestEditorWithCode(
+        getProjectTestCode(length, countOverride),
+        'await-first-dom-report',
+      )
+
+      const mapPath = EP.fromString('sb/group/map')
+      const editorState = renderResult.getEditorState().editor
+
+      const mapChildren = MetadataUtils.getChildrenOrdered(
+        editorState.jsxMetadata,
+        editorState.elementPathTree,
+        mapPath,
+      )
+
+      expect(mapChildren).toHaveLength(expectedCount)
+    }),
+  )
+})
--- editor/src/components/navigator/navigator-map.spec.browser2.tsx
+++ editor/src/components/navigator/navigator-map.spec.browser2.tsx
@@ -8,7 +8,7 @@ import { BakedInStoryboardUID, BakedInStoryboardVariableName } from '../../core/
 import * as EP from '../../core/shared/element-path'
 import { getMapCounterTestId } from './navigator-item/map-counter'
 
-function getProjectCode<T>(arr: Array<T>): string {
+function getProjectCode<T>(arr: Array<T>, countOverride?: number): string {
   return formatTestProjectCode(`import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
 
@@ -35,20 +35,23 @@ export var ${BakedInStoryboardVariableName} = (
         data-uid='containing-div'
         data-testid='containing-div'
       >
-        {${JSON.stringify(arr)}.map(() => (
-          <div
-            style={{
-              height: 150,
-              width: 150,
-              position: 'absolute',
-              left: 154,
-              top: 134,
-              backgroundColor: 'lightblue',
-            }}
-            data-uid='map-child-div'
-            data-testid='map-child-div'
-          />
-        ))}
+        {
+          // @utopia/uid=map
+          ${countOverride != null ? `// @utopia/map-count=${countOverride}` : ''}
+          ${JSON.stringify(arr)}.map(() => (
+            <div
+              style={{
+                height: 150,
+                width: 150,
+                position: 'absolute',
+                left: 154,
+                top: 134,
+                backgroundColor: 'lightblue',
+              }}
+              data-uid='map-child-div'
+              data-testid='map-child-div'
+            />)
+          )}
         <div
           style={{
             height: 150,
@@ -68,27 +71,68 @@ export var ${BakedInStoryboardVariableName} = (
 `)
 }
 
-const testArrays = [[], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3]]
-
 describe('maps in the navigator', () => {
-  testArrays.forEach((arr) => {
-    it(`shows counter for map items with list length ${arr.length}`, async () => {
-      const renderResult = await renderTestEditorWithCode(
-        getProjectCode(arr),
-        'await-first-dom-report',
-      )
+  describe('with default count value', () => {
+    const testArrays = [[], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3]]
+    testArrays.forEach((arr) => {
+      it(`shows counter for map items with list length ${arr.length}`, async () => {
+        const renderResult = await renderTestEditorWithCode(
+          getProjectCode(arr),
+          'await-first-dom-report',
+        )
 
-      const mapParent = EP.fromString('utopia-storyboard-uid/scene-aaa/containing-div/')
-      const mapElement = MetadataUtils.getChildrenOrdered(
-        renderResult.getEditorState().editor.jsxMetadata,
-        renderResult.getEditorState().editor.elementPathTree,
-        mapParent,
-      )[0]
+        const mapPath = EP.fromString('utopia-storyboard-uid/scene-aaa/containing-div/map')
+        const counterTestId = getMapCounterTestId(mapPath)
+        const counter = await renderResult.renderedDOM.findByTestId(counterTestId)
 
-      const counterTestId = getMapCounterTestId(mapElement.elementPath)
-      const counter = await renderResult.renderedDOM.findByTestId(counterTestId)
+        expect(counter.textContent).toEqual(arr.length.toString())
+      })
+    })
 
-      expect(counter.textContent).toEqual(arr.length.toString())
+    describe('with overridden count value', () => {
+      const testData = [
+        {
+          arr: [0, 1, 2, 3],
+          overrideCount: 0,
+        },
+        {
+          arr: [0, 1, 2, 3],
+          overrideCount: 1,
+        },
+        {
+          arr: [0, 1, 2, 3],
+          overrideCount: 2,
+        },
+        {
+          arr: [0, 1, 2, 3],
+          overrideCount: 3,
+        },
+        {
+          arr: [0, 1, 2, 3],
+          overrideCount: 4,
+        },
+      ]
+
+      testData.forEach(({ arr, overrideCount }) => {
+        it(`shows counter for map items with list length ${arr.length}`, async () => {
+          const renderResult = await renderTestEditorWithCode(
+            getProjectCode(arr, overrideCount),
+            'await-first-dom-report',
+          )
+
+          const mapParent = EP.fromString('utopia-storyboard-uid/scene-aaa/containing-div/')
+          const mapElement = MetadataUtils.getChildrenOrdered(
+            renderResult.getEditorState().editor.jsxMetadata,
+            renderResult.getEditorState().editor.elementPathTree,
+            mapParent,
+          )[0]
+
+          const counterTestId = getMapCounterTestId(mapElement.elementPath)
+          const counter = await renderResult.renderedDOM.findByTestId(counterTestId)
+
+          expect(counter.textContent).toEqual(overrideCount.toString())
+        })
+      })
     })
   })
 })
