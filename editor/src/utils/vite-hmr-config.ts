a0686bed22e37bc1fa66df8729b0a409da80b57f
Berci Kormendy <korber314@gmail.com>
2023-08-07 10:11:37 +0200

    update webpack stuff

--- editor/src/utils/vite-hmr-config.ts
+++ editor/src/utils/vite-hmr-config.ts
@@ -4,7 +4,7 @@
 // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
 if (import.meta.hot) {
   // this is written without the != null part to make sure Webpack (and Vite) recognizes and tree shakes it
-  import.meta.hot?.decline() // this _should_ be working but does not for some reason
+  import.meta.hot?.invalidate() // this _should_ be working but does not for some reason
   import.meta.hot?.on('vite:beforeUpdate', (event) => {
     if (event.updates.some((u) => u.path === '/editor.tsx')) {
       import.meta.hot?.invalidate()
