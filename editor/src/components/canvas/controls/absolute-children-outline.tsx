89cf59cdc3a8f867331104a26600e612d10336fc
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2023-08-23 11:49:12 +0100

    feature(groups) Do not include the absolute children outline for groups. (#4110)
    - Added `testId` to `OutlineControlProps`.
    - `OutlineControl` now includes a `data-testid` value so it can be easily identified.
    - `AbsoluteChildrenOutline` filters out selected views which are group like.
    - Changed some types from `Array<ElementPath>` to `ReadonlyArray<ElementPath>` to make
      them more compatible with `arrayAccumulate`.

--- editor/src/components/canvas/controls/absolute-children-outline.tsx
+++ editor/src/components/canvas/controls/absolute-children-outline.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { arrayAccumulate } from '../../../core/shared/array-utils'
+import type { ElementPath } from '../../../core/shared/project-file-types'
 import { MetadataUtils } from '../../../core/model/element-metadata-utils'
 import { Substores, useEditorState } from '../../editor/store/store-hook'
+import { treatElementAsGroupLike } from '../canvas-strategies/strategies/group-helpers'
 import { CanvasOffsetWrapper } from './canvas-offset-wrapper'
 import { OutlineControl } from './select-mode/simple-outline-control'
 
@@ -8,15 +11,29 @@ export const AbsoluteChildrenOutline = React.memo(() => {
   const absoluteChildren = useEditorState(
     Substores.metadata,
     (store) => {
-      return store.editor.selectedViews.flatMap((view) => {
-        return MetadataUtils.getChildrenPathsOrdered(
-          store.editor.jsxMetadata,
-          store.editor.elementPathTree,
-          view,
-        ).filter((child) => {
-          const metadata = MetadataUtils.findElementByElementPath(store.editor.jsxMetadata, child)
-          return MetadataUtils.isPositionAbsolute(metadata)
-        })
+      return arrayAccumulate<ElementPath>((childrenArray) => {
+        // Work through the selected views...
+        for (const selectedView of store.editor.selectedViews) {
+          // ...As long as they are not groups.
+          if (!treatElementAsGroupLike(store.editor.jsxMetadata, selectedView)) {
+            // Taking the children of those selected views...
+            const selectedViewChildren = MetadataUtils.getChildrenPathsOrdered(
+              store.editor.jsxMetadata,
+              store.editor.elementPathTree,
+              selectedView,
+            )
+            for (const selectedViewChild of selectedViewChildren) {
+              const metadata = MetadataUtils.findElementByElementPath(
+                store.editor.jsxMetadata,
+                selectedViewChild,
+              )
+              // ...Only including those children if they are positioned absolutely.
+              if (MetadataUtils.isPositionAbsolute(metadata)) {
+                childrenArray.push(selectedViewChild)
+              }
+            }
+          }
+        }
       })
     },
     'AbsoluteChildrenOutline absoluteChildrenBoundingBox',
@@ -28,6 +45,7 @@ export const AbsoluteChildrenOutline = React.memo(() => {
   return (
     <CanvasOffsetWrapper>
       <OutlineControl
+        testId={`absolute-children-outline`}
         targets={absoluteChildren}
         outlineStyle={'dotted'}
         color='multiselect-bounds'
