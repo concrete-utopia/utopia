33f721ef0b2a430b756fbee1532228b2400da633
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-08-29 16:11:53 +0200

    Crea route manifest from project contents

--- editor/src/components/canvas/remix/remix-utils.tsx
+++ editor/src/components/canvas/remix/remix-utils.tsx
@@ -1,4 +1,13 @@
-import type { UNSAFE_FutureConfig as FutureConfig } from '@remix-run/react'
+import type {
+  UNSAFE_FutureConfig as FutureConfig,
+  UNSAFE_EntryRoute as EntryRoute,
+  UNSAFE_RouteManifest as RouteManifest,
+} from '@remix-run/react'
+import type { ProjectContentTreeRoot } from '../../assets'
+import { flatRoutes } from '../../../third-party/remix/flat-routes'
+import type { ConfigRoute } from '../../../third-party/remix/routes'
+
+const ROOT_DIR = '/src'
 
 export const DefaultFutureConfig: FutureConfig = {
   v2_dev: true,
@@ -9,4 +18,44 @@ export const DefaultFutureConfig: FutureConfig = {
   v2_meta: false,
   v2_normalizeFormMethod: false,
   v2_routeConvention: true,
+}
+
+export interface EntryRouteWithFilePath extends EntryRoute {
+  filePath: string
+}
+
+export function createRouteManifestFromProjectContents(
+  projectContents: ProjectContentTreeRoot,
+): RouteManifest<EntryRouteWithFilePath> | null {
+  const routesFromRemix = flatRoutes(ROOT_DIR, projectContents)
+
+  if (routesFromRemix == null) {
+    return null
+  }
+  return patchRemixRoutes(routesFromRemix)
+}
+
+function patchRemixRoutes(routesFromRemix: RouteManifest<ConfigRoute> | null) {
+  const routesFromRemixWithRoot: RouteManifest<ConfigRoute> = {
+    ...routesFromRemix,
+    root: { path: '', id: 'root', file: 'root.js', parentId: '' },
+  }
+
+  let resultRoutes: RouteManifest<EntryRouteWithFilePath> = {}
+  Object.values(routesFromRemixWithRoot).reduce((acc, route) => {
+    // Maybe we should fill hasAction and hasLoader properly, but it is not used for anything
+    acc[route.id] = {
+      ...route,
+      parentId: route.parentId ?? 'root',
+      module: '',
+      hasAction: false,
+      hasLoader: false,
+      hasCatchBoundary: false,
+      hasErrorBoundary: false,
+      filePath: `${ROOT_DIR}/${route.file}`,
+    }
+    return acc
+  }, {} as RouteManifest<EntryRouteWithFilePath>)
+
+  return resultRoutes
 }
