1485acdc27d54ba29fb6f91162582d8b38291bb0
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-08-30 13:54:17 +0200

    Generating route manifest for Remix (#4126)
    * Added dependencies
    
    * New file api function
    
    * Added remix files
    
    * Crea route manifest from project contents
    
    * Test draft
    
    * Fix tests
    
    * Remove accidental pnpm install
    
    * pnpm install path-browserify
    
    * Minimatch should not be a dev dependency
    
    * Revert accidental minimatch version bump
    
    * Added remix unit tests
    
    * Inject FileOps instead of relying on utopia project contents
    
    * Remove typo
    
    * Remove ^ from dependency versions
    
    * Switch github links to permalinks

--- editor/src/components/canvas/remix/remix-utils.tsx
+++ editor/src/components/canvas/remix/remix-utils.tsx
@@ -1,4 +1,15 @@
-import type { UNSAFE_FutureConfig as FutureConfig } from '@remix-run/react'
+import type {
+  UNSAFE_FutureConfig as FutureConfig,
+  UNSAFE_EntryRoute as EntryRoute,
+  UNSAFE_RouteManifest as RouteManifest,
+} from '@remix-run/react'
+import type { ProjectContentTreeRoot } from '../../assets'
+import { getContentsTreeFromPath } from '../../assets'
+import type { FileOps } from '../../../third-party/remix/flat-routes'
+import { flatRoutes } from '../../../third-party/remix/flat-routes'
+import type { ConfigRoute } from '../../../third-party/remix/routes'
+
+const ROOT_DIR = '/src'
 
 export const DefaultFutureConfig: FutureConfig = {
   v2_dev: true,
@@ -9,4 +20,67 @@ export const DefaultFutureConfig: FutureConfig = {
   v2_meta: false,
   v2_normalizeFormMethod: false,
   v2_routeConvention: true,
+}
+
+export interface EntryRouteWithFilePath extends EntryRoute {
+  filePath: string
+}
+
+// This is necessary to create a simple node.fs-like implementation for Utopia projectContents, which
+// can be used by the Remix functions to parse the routes
+export function projectContentsToFileOps(projectContents: ProjectContentTreeRoot): FileOps {
+  return {
+    existsSync: (file: string): boolean => getContentsTreeFromPath(projectContents, file) != null,
+    readdirSync: (dir: string): Array<string> => {
+      const projectDir = getContentsTreeFromPath(projectContents, dir)
+      let entries =
+        projectDir != null && projectDir.type === 'PROJECT_CONTENT_DIRECTORY'
+          ? Object.values(projectDir.children).map((tree) => `/${tree.fullPath}`)
+          : []
+      return entries
+    },
+    isDirectory: (file: string): boolean => {
+      const projectFile = getContentsTreeFromPath(projectContents, file)
+      return projectFile != null && projectFile.type === 'PROJECT_CONTENT_DIRECTORY'
+    },
+    isFile: (file: string): boolean => {
+      const projectFile = getContentsTreeFromPath(projectContents, file)
+      return projectFile != null && projectFile.type === 'PROJECT_CONTENT_FILE'
+    },
+  }
+}
+
+export function createRouteManifestFromProjectContents(
+  projectContents: ProjectContentTreeRoot,
+): RouteManifest<EntryRouteWithFilePath> | null {
+  const routesFromRemix = flatRoutes(ROOT_DIR, projectContentsToFileOps(projectContents))
+
+  if (routesFromRemix == null) {
+    return null
+  }
+  return patchRemixRoutes(routesFromRemix)
+}
+
+function patchRemixRoutes(routesFromRemix: RouteManifest<ConfigRoute> | null) {
+  const routesFromRemixWithRoot: RouteManifest<ConfigRoute> = {
+    ...routesFromRemix,
+    root: { path: '', id: 'root', file: 'root.js', parentId: '' },
+  }
+
+  const resultRoutes = Object.values(routesFromRemixWithRoot).reduce((acc, route) => {
+    // Maybe we should fill hasAction and hasLoader properly, but it is not used for anything
+    acc[route.id] = {
+      ...route,
+      parentId: route.parentId ?? 'root',
+      module: '',
+      hasAction: false,
+      hasLoader: false,
+      hasCatchBoundary: false,
+      hasErrorBoundary: false,
+      filePath: `${ROOT_DIR}/${route.file}`,
+    }
+    return acc
+  }, {} as RouteManifest<EntryRouteWithFilePath>)
+
+  return resultRoutes
 }
