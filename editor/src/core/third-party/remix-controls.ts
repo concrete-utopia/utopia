195dbaabb3db1b8ef59df334ed140c767db13042
Berci Kormendy <korber314@gmail.com>
2023-09-04 12:36:54 +0200

    3rd party controls for Link

---
+++ editor/src/core/third-party/remix-controls.ts
@@ -0,0 +1,38 @@
+import type { ComponentDescriptorsForFile } from '../../components/custom-code/code-file'
+import {
+  emptyComments,
+  jsExpressionValue,
+  jsxAttributesEntry,
+  jsxElementWithoutUID,
+  jsxTextBlock,
+} from '../shared/element-template'
+
+export const RemixRunReactComponents: ComponentDescriptorsForFile = {
+  Link: {
+    properties: {
+      to: { control: 'string-input' },
+    },
+    variants: [
+      {
+        insertMenuLabel: 'Link',
+        importsToAdd: {
+          '@remix-run/react': {
+            importedWithName: null,
+            importedFromWithin: [
+              {
+                name: 'Link',
+                alias: 'Link',
+              },
+            ],
+            importedAs: null,
+          },
+        },
+        elementToInsert: jsxElementWithoutUID(
+          'Link',
+          [jsxAttributesEntry('to', jsExpressionValue('/', emptyComments), emptyComments)],
+          [jsxTextBlock('Link')],
+        ),
+      },
+    ],
+  },
+}
