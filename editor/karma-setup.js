070e2c83d2a83c1f483d4f6e5ef03b6348586af2
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-09-08 16:24:25 +0200

    does it fix Karma circular dependencies?

--- editor/karma-setup.js
+++ editor/karma-setup.js
@@ -1,3 +1,10 @@
+/**
+ * importing the shared entry point imports file, which should in theory mean that
+ * the real running Editor and the test editors resolve dependencies in the same order,
+ * which should in theory prevent diverging circular dependencies
+ */
+import './src/templates/editor-entry-point-imports'
+
 // // The matchers API
 import expect from 'expect'
 import * as BrowserFS from 'browserfs'
