85c1b56f0d03b549c43cba4bfc6e43ccd5daae62
Berci Kormendy <korber314@gmail.com>
2023-09-08 14:20:01 +0200

    test for remix content disappearing

--- editor/src/components/canvas/remix/remix-rendering.spec.browser2.tsx
+++ editor/src/components/canvas/remix/remix-rendering.spec.browser2.tsx
@@ -1,7 +1,14 @@
+import type { WindowPoint } from '../../../core/shared/math-utils'
+import { windowPoint } from '../../../core/shared/math-utils'
 import { createModifiedProject } from '../../../sample-projects/sample-project-utils.test-utils'
+import type { Modifiers } from '../../../utils/modifiers'
+import { emptyModifiers, cmdModifier } from '../../../utils/modifiers'
 import { setFeatureForBrowserTestsUseInDescribeBlockOnly } from '../../../utils/utils.test-utils'
 import { StoryboardFilePath } from '../../editor/store/editor-state'
+import { CanvasControlsContainerID } from '../controls/new-canvas-controls'
+import { mouseDownAtPoint, mouseDragFromPointWithDelta } from '../event-helpers.test-utils'
 import { REMIX_SCENE_TESTID } from '../ui-jsx-canvas-renderer/remix-scene-component'
+import type { EditorRenderResult } from '../ui-jsx.test-utils'
 import { renderTestEditorWithModel } from '../ui-jsx.test-utils'
 
 const storyboardFileContent = `
@@ -24,9 +31,12 @@ export var storyboard = (
   </Storyboard>
 );
 `
+
+/* eslint jest/expect-expect: ["error", { "assertFunctionNames": ["expect", "expectRemixSceneToBeRendered"] }] */
+
 describe('Remix content', () => {
   setFeatureForBrowserTestsUseInDescribeBlockOnly('Remix support', true)
-  it('Renders the remix container with placeholder text', async () => {
+  it('Renders the remix container with actual content', async () => {
     const project = createModifiedProject({
       [StoryboardFilePath]: `import * as React from 'react'
       import { RemixScene, Storyboard } from 'utopia-api'
@@ -65,15 +75,78 @@ describe('Remix content', () => {
       }
       `,
     })
+
     const renderResult = await renderTestEditorWithModel(project, 'await-first-dom-report')
-    const [remixScene] = await renderResult.renderedDOM.findAllByTestId(REMIX_SCENE_TESTID)
-    expect(remixScene).toBeDefined()
+    await expectRemixSceneToBeRendered(renderResult)
+  })
 
-    const [rootText] = await renderResult.renderedDOM.findAllByText('This is root!')
-    expect(rootText).toBeDefined()
+  it('Remix content is rendered while a canvas interaction is in progress', async () => {
+    const DraggedElementId = 'dragme'
+    const project = createModifiedProject({
+      [StoryboardFilePath]: `import * as React from 'react'
+      import { RemixScene, Storyboard } from 'utopia-api'
+      
+      export var storyboard = (
+        <Storyboard>
+          <RemixScene
+            style={{
+              width: 700,
+              height: 759,
+              position: 'absolute',
+              left: 212,
+              top: 128,
+            }}
+            data-label='Playground'
+          />
+          <div
+            style={{
+              backgroundColor: '#aaaaaa33',
+              position: 'absolute',
+              left: 213,
+              top: 47,
+              width: 130,
+              height: 61,
+            }}
+            data-uid='46a'
+            data-testid='${DraggedElementId}'
+          />
+        </Storyboard>
+      )
+      `,
+      ['/src/root.js']: `import React from 'react'
+      import { Outlet } from '@remix-run/react'
+      
+      export default function Root() {
+        return (
+          <div>
+            This is root!
+            <Outlet />
+          </div>
+        )
+      }
+      `,
+      ['/src/routes/_index.js']: `import React from 'react'
 
-    const [indexText] = await renderResult.renderedDOM.findAllByText('Hello Remix!')
-    expect(indexText).toBeDefined()
+      export default function Index() {
+        return <h1>Hello Remix!</h1>
+      }
+      `,
+    })
+
+    const renderResult = await renderTestEditorWithModel(project, 'await-first-dom-report')
+
+    const targetElement = renderResult.renderedDOM.getByTestId(DraggedElementId)
+    const targetElementBounds = targetElement.getBoundingClientRect()
+    const canvasControlsLayer = renderResult.renderedDOM.getByTestId(CanvasControlsContainerID)
+
+    const startPoint = windowPoint({ x: targetElementBounds.x + 5, y: targetElementBounds.y + 5 })
+    const dragDelta = windowPoint({ x: 40, y: -25 })
+
+    await expectRemixSceneToBeRendered(renderResult)
+
+    await dragElement(canvasControlsLayer, startPoint, dragDelta, emptyModifiers, () =>
+      expectRemixSceneToBeRendered(renderResult),
+    )
   })
 })
 
@@ -89,3 +162,28 @@ describe('Remix content with feature switch off', () => {
     ).rejects.toThrow()
   })
 })
+
+async function dragElement(
+  canvasControlsLayer: HTMLElement,
+  startPoint: WindowPoint,
+  dragDelta: WindowPoint,
+  modifiers: Modifiers,
+  midDragCallback: () => Promise<void>,
+): Promise<void> {
+  await mouseDownAtPoint(canvasControlsLayer, startPoint, { modifiers: cmdModifier })
+  await mouseDragFromPointWithDelta(canvasControlsLayer, startPoint, dragDelta, {
+    modifiers,
+    midDragCallback,
+  })
+}
+
+async function expectRemixSceneToBeRendered(editor: EditorRenderResult) {
+  const [remixScene] = await editor.renderedDOM.findAllByTestId(REMIX_SCENE_TESTID)
+  expect(remixScene).toBeDefined()
+
+  const [rootText] = await editor.renderedDOM.findAllByText('This is root!')
+  expect(rootText).toBeDefined()
+
+  const [indexText] = await editor.renderedDOM.findAllByText('Hello Remix!')
+  expect(indexText).toBeDefined()
+}
