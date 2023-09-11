a1e3a9ea75e8d4549c505c4fd18e51204ce3db13
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2023-09-11 16:34:42 +0100

    fix(tests) Update tests

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`742`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`982`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`787`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1097`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
