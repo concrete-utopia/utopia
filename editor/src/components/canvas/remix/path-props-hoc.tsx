6c2bb92d0161f295d3c0ea44f4e16ea14c4cf826
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-09-15 13:36:12 +0200

    Support multiple Remix Outlets (#4191)
    * thrid party controls
    
    * SafeLink and SafeOutlet
    
    * built-in component for outlet
    
    * wip
    
    * wip - get path parts
    
    * Inject all the relative paths to PathPropHOCs
    
    * it works
    
    * correct types
    
    * Simplify
    
    * Added some tests
    
    ---------
    
    Co-authored-by: Berci Kormendy <korber314@gmail.com>

--- editor/src/components/canvas/remix/path-props-hoc.tsx
+++ editor/src/components/canvas/remix/path-props-hoc.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { UTOPIA_PATH_KEY } from '../../../core/model/utopia-constants'
+import { OutletPathContext } from './remix-utils'
+import * as EP from '../../../core/shared/element-path'
 
-export function PathPropHOC<P>(Wrapped: React.ComponentType<P>, path: string) {
+type PropsWithPath<OriginalProps> = OriginalProps & { [UTOPIA_PATH_KEY]: string }
+
+type WrapperProps<P> = {
+  Wrapped: React.ComponentType<PropsWithPath<P>>
+  originalProps: P
+}
+
+function Wrapper<P>({ Wrapped, originalProps }: WrapperProps<P>): JSX.Element {
+  const outletContext = React.useContext(OutletPathContext)
+  if (outletContext == null) {
+    throw new Error('Route module should be rendered in an OutletPathContext')
+  }
+
+  const propsWithPath = {
+    [UTOPIA_PATH_KEY]: EP.toString(outletContext),
+    ...originalProps,
+  }
+  return <Wrapped {...propsWithPath} />
+}
+
+export function PathPropHOC<P>(Wrapped: React.ComponentType<PropsWithPath<P>>) {
   return (props: P): JSX.Element => {
-    const propsWithPath = {
-      [UTOPIA_PATH_KEY]: path,
-      ...props,
-    }
-    return <Wrapped {...propsWithPath} />
+    return <Wrapper Wrapped={Wrapped} originalProps={props} />
   }
 }
