0ebd9d8eb9a101a7d72b5919b00380a3f26aea58
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-09-28 15:25:11 +0200

    Do not show strategy controls when all selected uids are the same

--- editor/src/components/canvas/controls/select-mode/non-resizable-control.tsx
+++ editor/src/components/canvas/controls/select-mode/non-resizable-control.tsx
@@ -8,8 +8,6 @@ import { controlForStrategyMemoized } from '../../canvas-strategies/canvas-strat
 import type { EdgePosition } from '../../canvas-types'
 import { useBoundingBox } from '../bounding-box-hooks'
 import { CanvasOffsetWrapper } from '../canvas-offset-wrapper'
-import { areAllUidsTheSame } from './simple-outline-control'
-import * as EP from '../../../../core/shared/element-path'
 
 const selectedElementsSelector = (store: { editor: { selectedViews: ElementPath[] } }) =>
   store.editor.selectedViews
@@ -20,23 +18,7 @@ export const NonResizableControl = controlForStrategyMemoized(() => {
     selectedElementsSelector,
     'NonResizableControl selectedElements',
   )
-  if (areAllUidsTheSame(selectedElements)) {
-    return (
-      <>
-        {selectedElements.map((e) => (
-          <NonResizableControlInner key={EP.toString(e)} selectedElements={[e]} />
-        ))}
-      </>
-    )
-  }
-  return <NonResizableControlInner selectedElements={selectedElements} />
-})
 
-const NonResizableControlInner = ({
-  selectedElements,
-}: {
-  selectedElements: Array<ElementPath>
-}) => {
   const controlRef = useBoundingBox(selectedElements, (ref, boundingBox) => {
     ref.current.style.display = 'block'
     ref.current.style.left = boundingBox.x + 'px'
@@ -78,7 +60,7 @@ const NonResizableControlInner = ({
       </div>
     </CanvasOffsetWrapper>
   )
-}
+})
 
 interface NonResizablePointProps {
   position: EdgePosition
