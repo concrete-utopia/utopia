4444025b5e70d2a6a322f084b5d87672f7731f83
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-09-29 11:49:27 +0200

    Chore/grid panels horizonal resizing (#4258)
    * fix the navigator default width to 268 so things aren't cropped
    
    * super basic column resize, first commit
    
    * disable resize for menu columns
    
    * make the resize column invisible
    
    * store panelState in an Atom, introduce getColumnWidth
    
    * columns are more structured not
    
    * the columns store the widths!
    
    * add mouse catcher to resizer
    
    * make the inspector and navigator respect resize
    
    * handle right hand side resize correctly
    
    * Update grid-panels-state.tsx
    
    * Update grid-panels-state.tsx
    
    * pulling GridColumnResizeHandle to a separate component
    
    * Update grid-panels-drag-targets.tsx
    
    * Update grid-panels-drag-targets.tsx
    
    * Update grid-panels-container.tsx
    
    * clamped menu resize!
    
    * oh I really really hate this
    
    * oops

--- editor/src/components/canvas/grid-panels-drag-targets.tsx
+++ editor/src/components/canvas/grid-panels-drag-targets.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { when } from '../../utils/react-conditionals'
 import { colorTheme } from '../../uuiui'
+import { usePropControlledRef_DANGEROUS } from '../inspector/common/inspector-utils'
+import { CSSCursor } from './canvas-types'
 import { useGridPanelDragInfo, useGridPanelDropArea } from './grid-panels-dnd'
 import type { LayoutUpdate, StoredPanel } from './grid-panels-state'
-import { ExtraHorizontalDropTargetPadding, GridPanelHorizontalGapHalf } from './grid-panels-state'
+import {
+  ExtraHorizontalDropTargetPadding,
+  GridPanelHorizontalGapHalf,
+  ResizeColumnWidth,
+  wrapAroundColIndex,
+} from './grid-panels-state'
 
 export const ColumnDragTargets = React.memo(
   (props: {
@@ -10,8 +18,13 @@ export const ColumnDragTargets = React.memo(
     canDrop: (itemToMove: StoredPanel, newPosition: LayoutUpdate) => void
     onDrop: (itemToMove: StoredPanel, newPosition: LayoutUpdate) => void
   }) => {
-    const { columnIndex, onDrop, canDrop } = props
+    const { columnIndex: columnIndexProp, onDrop, canDrop } = props
+
+    const columnIndex = wrapAroundColIndex(columnIndexProp)
+
     const { isDragActive, draggedPanel } = useGridPanelDragInfo()
+
+    const leftSideOfGrid = columnIndex > -1
 
     const dropBeforeColumn: LayoutUpdate = React.useMemo(
       () => ({
@@ -57,7 +70,7 @@ export const ColumnDragTargets = React.memo(
             pointerEvents: 'initial',
             gridRowStart: 1,
             gridRowEnd: -1,
-            gridColumn: `col ${columnIndex > -1 ? columnIndex + 1 : columnIndex} / span 1`,
+            gridColumn: `col ${leftSideOfGrid ? columnIndex + 1 : columnIndex} / span 1`,
             display: isDragActive && canDropBefore ? 'block' : 'none',
             width: 2 * ExtraHorizontalDropTargetPadding + 2 * GridPanelHorizontalGapHalf,
             height: '100%',
@@ -82,7 +95,7 @@ export const ColumnDragTargets = React.memo(
             pointerEvents: 'initial',
             gridRowStart: 1,
             gridRowEnd: -1,
-            gridColumn: `col ${columnIndex > -1 ? columnIndex + 1 : columnIndex} / span 1`,
+            gridColumn: `col ${leftSideOfGrid ? columnIndex + 1 : columnIndex} / span 1`,
             display: isDragActive && canDropAfter ? 'block' : 'none',
             width: 2 * ExtraHorizontalDropTargetPadding + 2 * GridPanelHorizontalGapHalf,
             height: '100%',
@@ -100,6 +113,89 @@ export const ColumnDragTargets = React.memo(
             }}
           />
         </div>
+      </>
+    )
+  },
+)
+
+export const GridColumnResizeHandle = React.memo(
+  (props: {
+    columnIndex: number
+    columnWidth: number
+    setColumnWidth: (columnIndex: number, newWidth: number) => void
+  }) => {
+    const { columnIndex: columnIndexProp, columnWidth, setColumnWidth } = props
+
+    const columnIndex = wrapAroundColIndex(columnIndexProp)
+    const columnWidthRef = usePropControlledRef_DANGEROUS(columnWidth)
+    const [isResizing, setIsResizing] = React.useState(false)
+
+    const leftSideOfGrid = columnIndex > -1
+
+    const handleResizeMouseDown = React.useCallback(
+      (mouseDownEvent: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const startingColumnWidth = columnWidthRef.current
+        setIsResizing(true)
+
+        const onMouseMove = (mouseMoveEvent: MouseEvent) => {
+          const mouseDelta = mouseMoveEvent.clientX - mouseDownEvent.clientX
+          const negateDelta = leftSideOfGrid ? 1 : -1
+          setColumnWidth(columnIndex, startingColumnWidth + mouseDelta * negateDelta)
+        }
+        const onMouseUp = () => {
+          setIsResizing(false)
+          window.removeEventListener('mousemove', onMouseMove, { capture: true })
+          window.removeEventListener('mouseup', onMouseUp, { capture: true })
+        }
+        window.addEventListener('mousemove', onMouseMove, { capture: true })
+        window.addEventListener('mouseup', onMouseUp, { capture: true })
+      },
+      [leftSideOfGrid, columnIndex, columnWidthRef, setColumnWidth],
+    )
+
+    return (
+      <>
+        <div
+          onMouseDown={handleResizeMouseDown}
+          style={{
+            zIndex: 1, // I feel really bad about this, but _something_ is over this draggable div if there are multiple panels in a column
+            position: 'absolute',
+            pointerEvents: 'initial',
+            gridRowStart: 1,
+            gridRowEnd: -1,
+            gridColumn: `col ${leftSideOfGrid ? columnIndex + 1 : columnIndex} / span 1`,
+            width: ResizeColumnWidth,
+            height: '100%',
+            ...(leftSideOfGrid
+              ? { right: -ResizeColumnWidth / 2 + GridPanelHorizontalGapHalf } // for the left hand side resize columns
+              : { left: -ResizeColumnWidth / 2 + GridPanelHorizontalGapHalf }), // right hand side resize columns
+            cursor: CSSCursor.ResizeEW,
+            userSelect: isResizing ? 'none' : 'auto',
+          }}
+        />
+        {
+          // this is a mouse catcher div only visible during a resize, to prevent VSCode from capturing our mouse, and to prevent the flickering of the horizontal resize cursor
+          when(
+            isResizing,
+            <div
+              style={{
+                pointerEvents: 'initial',
+                height: '100%',
+                width: '100%',
+                backgroundColor: 'rgba(0,0,0,0)',
+                opacity: 0,
+                position: 'fixed',
+                zIndex: 9999,
+                top: '0',
+                left: '0',
+                bottom: '0',
+                right: '0',
+                cursor: CSSCursor.ResizeEW,
+                userSelect: 'none',
+              }}
+            />,
+          )
+        }
       </>
     )
   },
