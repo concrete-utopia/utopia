44a1171644f1ed9391b072fd85316552e3b5d62f
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-10-03 13:23:38 +0200

    Don't reparent out of groups (unless outside of a scene) (#4298)
    * don't reparent out of groups
    
    * newline please
    
    * add test for app scene
    
    * add comment for future selves

--- editor/src/components/canvas/canvas-strategies/strategies/absolute-utils.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/absolute-utils.ts
@@ -1,6 +1,11 @@
 import { MetadataUtils } from '../../../../core/model/element-metadata-utils'
+import { uniqBy } from '../../../../core/shared/array-utils'
+import * as EP from '../../../../core/shared/element-path'
+import type { ElementInstanceMetadataMap } from '../../../../core/shared/element-template'
 import type { ElementPath } from '../../../../core/shared/project-file-types'
+import type { InsertionPath } from '../../../editor/store/insertion-path'
 import type { InteractionCanvasState } from '../canvas-strategy-types'
+import { treatElementAsGroupLike } from './group-helpers'
 
 export function supportsStyle(canvasState: InteractionCanvasState, element: ElementPath): boolean {
   return MetadataUtils.targetUsesProperty(
@@ -28,4 +33,41 @@ export function honoursPropsPosition(
     canvasState.projectContents,
     MetadataUtils.findElementByElementPath(canvasState.startingMetadata, element),
   )
+}
+
+export function shouldKeepMovingDraggedGroupChildren(
+  jsxMetadata: ElementInstanceMetadataMap,
+  paths: ElementPath[],
+  reparentTarget: InsertionPath,
+): boolean {
+  // all selected elements must be under the same non-root hierarchy
+  const commonParent = EP.getCommonParent(paths, true)
+  if (commonParent == null || EP.isStoryboardPath(commonParent)) {
+    return false
+  }
+
+  // all elements must be group children
+  if (!paths.every((path) => treatElementAsGroupLike(jsxMetadata, EP.parentPath(path)))) {
+    return false
+  }
+
+  // all elements must not be crossing scenes
+  const sceneAncestors = uniqBy(
+    paths.flatMap((path) => EP.getAncestors(path)),
+    EP.pathsEqual,
+  ).filter((path) => MetadataUtils.isProbablyScene(jsxMetadata, path))
+  if (sceneAncestors.length > 0) {
+    const commonSceneParent = EP.getCommonParent(
+      [...sceneAncestors, reparentTarget.intendedParentPath],
+      true,
+    )
+    if (
+      commonSceneParent == null ||
+      !MetadataUtils.isProbablyScene(jsxMetadata, commonSceneParent)
+    ) {
+      return false
+    }
+  }
+
+  return true
 }
