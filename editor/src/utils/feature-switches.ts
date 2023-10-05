b5a263f9609d908dcae752ad1000a3b5cd0b2ece
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-10-05 13:01:34 +0200

    enable Grid Panels FS by default

--- editor/src/utils/feature-switches.ts
+++ editor/src/utils/feature-switches.ts
@@ -42,7 +42,7 @@ let FeatureSwitches: { [feature in FeatureName]: boolean } = {
   'Performance Test Triggers': !(PRODUCTION_CONFIG as boolean),
   'Canvas Strategies Debug Panel': false,
   'Project Thumbnail Generation': false,
-  'Draggable Floating Panels': false,
+  'Draggable Floating Panels': !IS_TEST_ENVIRONMENT,
   'Remix support': false,
   'Debug - Print UIDs': false,
 }
