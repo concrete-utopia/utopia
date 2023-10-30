ea12d5af3971dfb7f9e96e3316c28cbc478652d6
Berci Kormendy <korber314@gmail.com>
2023-10-30 14:19:11 +0100

    re-update snapshots

--- editor/src/core/property-controls/property-controls-local.spec.tsx
+++ editor/src/core/property-controls/property-controls-local.spec.tsx
@@ -10,53 +10,73 @@ describe('registered property controls', () => {
     const testCode = Prettier.format(
       `
         import * as React from 'react'
-        import { Scene, Storyboard, View, registerModule } from 'utopia-api'
-
+        import {
+          Scene,
+          Storyboard,
+          View,
+          registerModule,
+        } from 'utopia-api'
+        
         export var App = (props) => {
-          return (
-            <div>hello</div>
-          )
+          return <div>hello</div>
         }
-
-        registerModule(
-          '/src/card',
-          {
-            Card: {
-              properties: {
-                label: {
-                  control: 'string-input',
-                },
-                background: {
-                  control: 'color',
-                },
-                visible: {
-                  control: 'checkbox',
-                  defaultValue: true,
-                },
-              },
-              variants: [
-                {
-                  code: '<Card />',
-                  label: 'Card',
-                },
-                {
-                  code: '<Card person={DefaultPerson} />',
-                  label: 'ID Card',
-                  additionalImports: "import { DefaultPerson } from '/src/defaults';",
-                },
-              ],
-            },
-          }
+        
+        export var Card = ({ person }) => (
+          <h1>Hello, {person ?? 'John Doe'}</h1>
         )
-
+        
+        registerModule('/src/storyboard.js', {
+          Card: {
+            supportsChildren: false,
+            properties: {
+              label: {
+                control: 'string-input',
+              },
+              background: {
+                control: 'color',
+              },
+              visible: {
+                control: 'checkbox',
+                defaultValue: true,
+              },
+            },
+            variants: [
+              {
+                code: '<Card />',
+                label: 'Card',
+              },
+              {
+                code: '<Card person={DefaultPerson} />',
+                label: 'ID Card',
+                additionalImports:
+                  "import { DefaultPerson } from '/src/defaults';",
+              },
+            ],
+          },
+        })
+        
         export var storyboard = (props) => {
           return (
-            <Storyboard data-uid='${BakedInStoryboardUID}'>
+            <Storyboard>
               <Scene
-                style={{ position: 'absolute', left: 0, top: 0, width: 400, height: 400 }}
-                data-uid='${TestScene0UID}'
+                style={{
+                  position: 'absolute',
+                  left: 0,
+                  top: 0,
+                  width: 400,
+                  height: 400,
+                }}
               >
-                <App data-uid='${TestAppUID}' />
+                <App />
+                <Card
+                  style={{
+                    position: 'absolute',
+                    left: 100,
+                    top: 148,
+                    width: 107,
+                    height: 52,
+                  }}
+                />
               </Scene>
             </Storyboard>
           )
@@ -69,6 +89,67 @@ describe('registered property controls', () => {
     const editorState = renderResult.getEditorState().editor
 
     // TODO: figure this out before merge
-    expect(editorState.propertyControlsInfo['/src/card']).toMatchInlineSnapshot(`undefined`)
+    expect(editorState.propertyControlsInfo['/src/storyboard.js']).toMatchInlineSnapshot(`
+      Object {
+        "Card": Object {
+          "properties": Object {
+            "background": Object {
+              "control": "color",
+            },
+            "label": Object {
+              "control": "string-input",
+            },
+            "visible": Object {
+              "control": "checkbox",
+            },
+          },
+          "supportsChildren": false,
+          "variants": Array [
+            Object {
+              "elementToInsert": [Function],
+              "importsToAdd": Object {
+                "/src/storyboard.js": Object {
+                  "importedAs": null,
+                  "importedFromWithin": Array [
+                    Object {
+                      "alias": "Card",
+                      "name": "Card",
+                    },
+                  ],
+                  "importedWithName": null,
+                },
+              },
+              "insertMenuLabel": "Card",
+            },
+            Object {
+              "elementToInsert": [Function],
+              "importsToAdd": Object {
+                "/src/defaults": Object {
+                  "importedAs": null,
+                  "importedFromWithin": Array [
+                    Object {
+                      "alias": "DefaultPerson",
+                      "name": "DefaultPerson",
+                    },
+                  ],
+                  "importedWithName": null,
+                },
+                "/src/storyboard.js": Object {
+                  "importedAs": null,
+                  "importedFromWithin": Array [
+                    Object {
+                      "alias": "Card",
+                      "name": "Card",
+                    },
+                  ],
+                  "importedWithName": null,
+                },
+              },
+              "insertMenuLabel": "ID Card",
+            },
+          ],
+        },
+      }
+    `)
   })
 })
