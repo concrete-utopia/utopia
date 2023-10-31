79e549a060bdb3687c2cd2b699e3198a52801ac8
Berci Kormendy <korber314@gmail.com>
2023-10-31 15:50:13 +0100

    only include appDirectory in test config

--- editor/src/components/canvas/remix/remix-utils.spec.browser2.tsx
+++ editor/src/components/canvas/remix/remix-utils.spec.browser2.tsx
@@ -35,15 +35,6 @@ const remixConfigJsFromRemixDocs = `
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   appDirectory: "src",
-  assetsBuildDirectory: "public/build",
-  ignoredRouteFiles: ["**/.*"],
-  publicPath: "/build/",
-  routes(defineRoutes) {
-    return defineRoutes((route) => {
-      route("/somewhere/cool/*", "catchall.tsx");
-    });
-  },
-  serverBuildPath: "build/index.js",
 };
 `
 
