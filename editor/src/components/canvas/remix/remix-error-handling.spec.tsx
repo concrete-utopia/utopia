3f2285363385d9a7c7f142135f52465e4e92070f
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-11-03 13:48:20 +0100

    move the two Remix Error Handling test files into one

--- editor/src/components/canvas/remix/remix-error-handling.spec.tsx
+++ editor/src/components/canvas/remix/remix-error-handling.spec.tsx
@@ -1,14 +1,21 @@
-import { setFeatureForUnitTestsUseInDescribeBlockOnly } from '../../../utils/utils.test-utils'
+import { makeTestProjectCodeWithSnippet, renderTestEditorWithCode } from '../ui-jsx.test-utils'
 import { runTestReturningErrorBoundaries } from './remix-error-handling.test-utils'
 
-// This test and its sibling have to be kept in separate files,
-// as otherwise it causes issues with Jest for some reason
-
 describe('Remix error handling', () => {
+  it('Supports a user-defined error boundary', async () => {
+    const renderResult = await renderTestEditorWithCode(
+      makeTestProjectCodeWithSnippet(`<div>Helo</div>`),
+      'await-first-dom-report',
+    )
+
+    expect(renderResult.renderedDOM.getByText('Helo')).toBeDefined()
+  })
+
   it('Bubbles errors to the canvas if no error boundary exists', async () => {
     const { customBoundary, canvasOverlay } = await runTestReturningErrorBoundaries(
       'without-custom-boundary',
     )
+
     expect(customBoundary).toBeNull()
     expect(canvasOverlay).not.toBeNull()
   })
