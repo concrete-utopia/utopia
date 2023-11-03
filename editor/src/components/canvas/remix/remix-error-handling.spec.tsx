e44f4a83c0bdb8e0de11797dca23bd64a6a0493a
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-11-03 17:33:32 +0100

    Fix flaky Jest test (#4470)
    * patch useHighlightCallbacks so it doesn't run in Jest
    
    * move the two Remix Error Handling test files into one
    
    * oooooooooops

--- editor/src/components/canvas/remix/remix-error-handling.spec.tsx
+++ editor/src/components/canvas/remix/remix-error-handling.spec.tsx
@@ -1,8 +1,5 @@
-import { setFeatureForUnitTestsUseInDescribeBlockOnly } from '../../../utils/utils.test-utils'
+import { makeTestProjectCodeWithSnippet, renderTestEditorWithCode } from '../ui-jsx.test-utils'
 import { runTestReturningErrorBoundaries } from './remix-error-handling.test-utils'
-
-// This test and its sibling have to be kept in separate files,
-// as otherwise it causes issues with Jest for some reason
 
 describe('Remix error handling', () => {
   it('Supports a user-defined error boundary', async () => {
@@ -11,5 +8,14 @@ describe('Remix error handling', () => {
     )
     expect(customBoundary).not.toBeNull()
     expect(canvasOverlay).toBeNull()
+  })
+
+  it('Bubbles errors to the canvas if no error boundary exists', async () => {
+    const { customBoundary, canvasOverlay } = await runTestReturningErrorBoundaries(
+      'without-custom-boundary',
+    )
+
+    expect(customBoundary).toBeNull()
+    expect(canvasOverlay).not.toBeNull()
   })
 })
