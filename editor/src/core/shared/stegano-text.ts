902758ac17c22bb4ad08897265a726ae7dd49819
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-11-09 19:51:58 +0100

    stegano-text helpers

---
+++ editor/src/core/shared/stegano-text.ts
@@ -0,0 +1,30 @@
+import { vercelStegaCombine, vercelStegaDecodeAll } from '@vercel/stega'
+
+export interface SteganoTextData {
+  originalString: string
+  filePath: string
+  startPosition: number
+  endPosition: number
+}
+
+export function steganoTextData(
+  originalString: string,
+  filePath: string,
+  startPosition: number,
+  endPosition: number,
+): SteganoTextData {
+  return {
+    originalString: originalString,
+    filePath: filePath,
+    startPosition: startPosition,
+    endPosition: endPosition,
+  }
+}
+
+export function encodeSteganoData(originalString: string, data: SteganoTextData): string {
+  return vercelStegaCombine(originalString, data)
+}
+
+export function decodeSteganoData(encodedString: string): Array<SteganoTextData> {
+  return vercelStegaDecodeAll(encodedString)
+}
