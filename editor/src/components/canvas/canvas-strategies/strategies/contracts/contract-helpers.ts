95378f09c36bf3a8b672cf48262801446c486014
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-11-15 10:34:02 +0100

    much more agressive Wrapper detection

--- editor/src/components/canvas/canvas-strategies/strategies/contracts/contract-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/contracts/contract-helpers.ts
@@ -1,7 +1,10 @@
+import { MetadataUtils } from '../../../../../core/model/element-metadata-utils'
+import { toString } from '../../../../../core/shared/element-path'
 import type { ElementPathTrees } from '../../../../../core/shared/element-path-tree'
 import type { ElementInstanceMetadataMap } from '../../../../../core/shared/element-template'
 import type { ElementPath } from '../../../../../core/shared/project-file-types'
 import { assertNever } from '../../../../../core/shared/utils'
+import invariant from '../../../../../third-party/remix/invariant'
 import type { AllElementProps } from '../../../../editor/store/editor-state'
 import { getElementFragmentLikeType } from '../fragment-like-helpers'
 import { treatElementAsGroupLike } from '../group-helpers'
@@ -27,9 +30,41 @@ export function getEditorContractForElement(
     return 'wrapper-div'
   }
 
+  // let's check if the element is collapsed in any direction, and return wrapper-div for those cases too
+  const collapsedWrapper = isCollapsedWrapper(metadata, pathTrees, path)
+
+  if (collapsedWrapper) {
+    return 'wrapper-div'
+  }
+
   if (fragmentLikeType == null) {
     return 'frame'
   }
 
   assertNever(fragmentLikeType)
+}
+
+function isCollapsedWrapper(
+  metadata: ElementInstanceMetadataMap,
+  pathTrees: ElementPathTrees,
+  path: ElementPath,
+): boolean {
+  const children = MetadataUtils.getChildrenOrdered(metadata, pathTrees, path)
+  const atLeastOnAbsoluteChild = children.some(MetadataUtils.isPositionAbsolute)
+
+  const element = MetadataUtils.findElementByElementPath(metadata, path)
+
+  if (element == null) {
+    return false
+  }
+
+  invariant(element != null, `Found null metadata at ${toString(path)}`)
+
+  const widthCollapsed = element.specialSizeMeasurements.computedHugProperty.width === 'collapsed'
+  const heightCollapsed = element.specialSizeMeasurements.computedHugProperty.height === 'collapsed'
+
+  // the element will be considered a Collapsed Wrapper if:
+  // 1. it has at least one absolute child
+  // 2. its Width or Height HugProperty is set to Collapsed
+  return atLeastOnAbsoluteChild && (widthCollapsed || heightCollapsed)
 }
