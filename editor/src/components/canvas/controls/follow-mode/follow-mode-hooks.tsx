2c1813f2c35af51994950ebd433900cc41e4aed5
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-11-20 10:50:31 +0100

    noop const

--- editor/src/components/canvas/controls/follow-mode/follow-mode-hooks.tsx
+++ editor/src/components/canvas/controls/follow-mode/follow-mode-hooks.tsx
@@ -1,11 +1,12 @@
 import type { MouseCallbacks } from '../select-mode/select-mode-hooks'
 import { NO_OP } from '../../../../core/shared/utils'
-import { useKeepShallowReferenceEquality } from '../../../../utils/react-performance'
+
+const noop: MouseCallbacks = {
+  onMouseMove: NO_OP,
+  onMouseDown: NO_OP,
+  onMouseUp: NO_OP,
+}
 
 export function useFollowModeSelectAndHover(): MouseCallbacks {
-  return useKeepShallowReferenceEquality({
-    onMouseMove: NO_OP,
-    onMouseDown: NO_OP,
-    onMouseUp: NO_OP,
-  })
+  return noop
 }
