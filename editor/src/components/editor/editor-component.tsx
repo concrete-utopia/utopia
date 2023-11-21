2669f5280dae9e05ce1e5c5f99d0622cad315cb8
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-11-21 18:28:18 +0100

    Fix resolveUsers on new project

--- editor/src/components/editor/editor-component.tsx
+++ editor/src/components/editor/editor-component.tsx
@@ -318,6 +318,7 @@ export const EditorComponentInner = React.memo((props: EditorProps) => {
     }
     if (projectId != null) {
       pushProjectURLToBrowserHistory(projectId, projectName)
+      ;(window as any).utopiaProjectID = projectId
     }
   }, [projectName, projectId])
 
