b875630c6436011e110c07b0cefd85592558dd8b
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-11-22 11:30:43 +0100

    follower badge

--- editor/src/components/user-bar.tsx
+++ editor/src/components/user-bar.tsx
@@ -12,7 +12,7 @@ import {
 } from '../core/shared/multiplayer'
 import { Avatar, Tooltip, useColorTheme } from '../uuiui'
 import { Substores, useEditorState } from './editor/store/store-hook'
-import { unless, when } from '../utils/react-conditionals'
+import { when } from '../utils/react-conditionals'
 import { MultiplayerWrapper } from '../utils/multiplayer-wrapper'
 import { useDispatch } from './editor/store/dispatch-context'
 import { showToast, switchEditorMode } from './editor/actions/action-creators'
@@ -166,6 +166,7 @@ const MultiplayerUserBar = React.memo(() => {
                 coloredTooltip={true}
                 onClick={toggleFollowing(other.id)}
                 active={isFollowMode(mode) && mode.playerId === other.id}
+                follower={other.following === myUser.id}
               />
             )
           })}
@@ -196,7 +197,7 @@ const MultiplayerUserBar = React.memo(() => {
 })
 MultiplayerUserBar.displayName = 'MultiplayerUserBar'
 
-export const MultiplayerAvatar = React.memo(
+const MultiplayerAvatar = React.memo(
   (props: {
     name: string
     tooltip: string
@@ -207,6 +208,7 @@ export const MultiplayerAvatar = React.memo(
     onClick?: () => void
     active?: boolean
     size?: number
+    follower?: boolean
   }) => {
     const picture = React.useMemo(() => {
       return isDefaultAuth0AvatarURL(props.picture ?? null) ? null : props.picture
@@ -215,7 +217,7 @@ export const MultiplayerAvatar = React.memo(
     const colorTheme = useColorTheme()
     return (
       <Tooltip
-        title={props.tooltip}
+        title={`${props.tooltip}${props.follower === true ? ' (following you)' : ''}`}
         placement='bottom'
         backgroundColor={props.coloredTooltip ? props.color.background : undefined}
         textColor={props.coloredTooltip ? props.color.foreground : undefined}
@@ -236,6 +238,7 @@ export const MultiplayerAvatar = React.memo(
             cursor: 'pointer',
             boxShadow:
               props.active === true ? `0px 0px 15px ${colorTheme.primary.value}` : undefined,
+            position: 'relative',
           }}
           onClick={props.onClick}
         >
@@ -244,6 +247,24 @@ export const MultiplayerAvatar = React.memo(
             size={props.border === true ? 22 : undefined}
             initials={props.name}
           />
+          {when(
+            props.follower === true,
+            <div
+              style={{
+                position: 'absolute',
+                top: -6,
+                right: -6,
+                borderRadius: '100%',
+                backgroundColor: colorTheme.primary.value,
+                color: colorTheme.white.value,
+                width: 10,
+                height: 10,
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            />,
+          )}
         </div>
       </Tooltip>
     )
