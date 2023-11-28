a7b414de75bdd9b4233158a1a1de4366b35a709b
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-11-28 15:16:22 +0100

    More snapshot updates

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`764`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1583`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
