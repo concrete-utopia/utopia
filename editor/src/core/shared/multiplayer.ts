a279f4141b3db979730980aa784a415749efb1b8
lankaukk <lankaukk@yahoo.com>
2023-11-28 16:34:26 -0500

    new colors, no theme

--- editor/src/core/shared/multiplayer.ts
+++ editor/src/core/shared/multiplayer.ts
@@ -8,40 +8,26 @@ export type MultiplayerColor = {
   foreground: string
 }
 
-export const multiplayerColors = {
-  light: [
-    { background: '#0092C0', foreground: 'white' },
-    { background: '#128400', foreground: 'white' },
-    { background: '#2A00D1', foreground: 'white' },
-    { background: '#5C00D1', foreground: 'white' },
-    { background: '#87C700', foreground: 'black' },
-    { background: '#8C6D00', foreground: 'white' },
-    { background: '#9C0000', foreground: 'white' },
-    { background: '#B16CB7', foreground: 'white' },
-    { background: '#B60479', foreground: 'white' },
-    { background: '#E7B400', foreground: 'black' },
-  ],
-  dark: [
-    { background: '#007DA4', foreground: 'white' },
-    { background: '#19B500', foreground: 'black' },
-    { background: '#3EFFF3', foreground: 'black' },
-    { background: '#6842FF', foreground: 'white' },
-    { background: '#C4FF46', foreground: 'black' },
-    { background: '#CE00D3', foreground: 'white' },
-    { background: '#DD0000', foreground: 'white' },
-    { background: '#FF00A8', foreground: 'white' },
-    { background: '#FFBA08', foreground: 'black' },
-    { background: '#FFFF00', foreground: 'black' },
-  ],
-}
+export const multiplayerColors = [
+  { background: '#E72861', foreground: 'white' },
+  { background: '#DF65A7', foreground: 'black' },
+  { background: '#EB6837', foreground: 'white' },
+  { background: '#D1B143', foreground: 'black' },
+  { background: '#7EC609', foreground: 'black' },
+  { background: '#0CB04E', foreground: 'white' },
+  { background: '#25B5B5', foreground: 'black' },
+  { background: '#547ADA', foreground: 'white' },
+  { background: '#A05FD3', foreground: 'white' },
+  { background: '#7382A7', foreground: 'white' },
+]
 
 function randomMultiplayerColor(): number {
-  return Math.floor(Math.random() * multiplayerColors.light.length)
+  return Math.floor(Math.random() * multiplayerColors.length)
 }
 
 export function possiblyUniqueColor(existing: (number | null)[]): number {
   return possiblyUniqueInArray(
-    multiplayerColors.light.map((_, index) => index),
+    multiplayerColors.map((_, index) => index),
     existing,
     randomMultiplayerColor(),
   )
@@ -56,8 +42,7 @@ export function multiplayerColorFromIndex(colorIndex: number | null): Multiplaye
     return fallbackColor
   }
 
-  const colors =
-    getPreferredColorScheme() === 'dark' ? multiplayerColors.dark : multiplayerColors.light
+  const colors = multiplayerColors
   return safeIndex(colors, colorIndex) ?? fallbackColor
 }
 
