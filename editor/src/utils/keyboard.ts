2465e8af9a05e99c4a42655384868c52ffce8a6b
Balint Gabor <127662+gbalint@users.noreply.github.com>
2023-12-08 17:02:51 +0100

    Some prettier changes

--- editor/src/utils/keyboard.ts
+++ editor/src/utils/keyboard.ts
@@ -30,7 +30,7 @@ export type Letter =
   | 'z'
 
 const Digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const
-export type Digit = typeof Digits[number]
+export type Digit = (typeof Digits)[number]
 
 export const isDigit = (c: string): c is Digit => (Digits as readonly string[]).includes(c)
 
