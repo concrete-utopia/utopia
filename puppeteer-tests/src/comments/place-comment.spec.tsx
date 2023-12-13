a1d3a0a0cdafa87a321427d2070e42749d51d16f
Berci Kormendy <korber314@gmail.com>
2023-12-13 17:41:06 +0100

    wip - flesh out puppeteer test

--- puppeteer-tests/src/comments/place-comment.spec.tsx
+++ puppeteer-tests/src/comments/place-comment.spec.tsx
@@ -1,8 +1,16 @@
 import { setupBrowser } from '../utils'
+import type { Browser } from 'puppeteer'
+
+async function getFirstTab(browser: Browser) {
+  const [firstTab] = await browser.pages()
+  return firstTab
+}
 
 describe('Comments test', () => {
   it('can place a comment', async () => {
     const { page, browser } = await setupBrowser('http://localhost:8000/p/', 120000)
+    const tab = await getFirstTab(browser)
+    tab.click('#sign-in-button')
     expect(1).toEqual(1)
   })
 })
