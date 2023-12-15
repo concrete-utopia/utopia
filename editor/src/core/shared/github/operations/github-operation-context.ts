dc31d8009d2646b5ee5eebeb6211935ad15ab29f
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2023-12-15 18:02:40 +0100

    Revert "removing unnecessary capability from operationContext of getProcessedParsedProjectFromGithubRepo"
    This reverts commit cc5ecf81b97c7f9601d991a348d6acdcfbe2ba8d.

--- editor/src/core/shared/github/operations/github-operation-context.ts
+++ editor/src/core/shared/github/operations/github-operation-context.ts
@@ -1,10 +1,8 @@
 import type { ProjectContentTreeRoot } from '../../../../components/assets'
 import type { UtopiaTsWorkers } from '../../../workers/common/worker-types'
 
-export type GithubFetch = (url: string, options: RequestInit) => Promise<Response>
-
 export interface GithubOperationContext {
-  fetch: GithubFetch
+  fetch: (url: string, options: RequestInit) => Promise<Response>
   updateProjectContentsWithParseResults: (
     workers: UtopiaTsWorkers,
     projectContents: ProjectContentTreeRoot,
--- editor/src/core/shared/github/operations/load-branch.ts
+++ editor/src/core/shared/github/operations/load-branch.ts
@@ -37,11 +37,11 @@ import {
   saveGithubAsset,
 } from '../helpers'
 import { updateProjectContentsWithParseResults } from '../../parser-projectcontents-utils'
-import type { GithubFetch, GithubOperationContext } from './github-operation-context'
+import type { GithubOperationContext } from './github-operation-context'
 import { createStoryboardFileIfNecessary } from '../../../../components/editor/actions/actions'
 
 export const saveAssetsToProject =
-  (operationContext: { fetch: GithubFetch }) =>
+  (operationContext: GithubOperationContext) =>
   async (
     githubRepo: GithubRepo,
     projectID: string,
@@ -207,7 +207,7 @@ export async function getProcessedParsedProjectFromGithubRepo(
   branchName: string,
   githubRepo: GithubRepo,
   onUpdate: (update: UpdateGithubOperations | AddToast) => void,
-  operationContext: { fetch: GithubFetch },
+  operationContext: GithubOperationContext,
   resetBranches: boolean,
   workers: UtopiaTsWorkers,
   projectID: string,
@@ -256,7 +256,7 @@ async function loadBranchFromGithub(
   branchName: string,
   githubRepo: GithubRepo,
   onUpdate: (update: UpdateGithubOperations | AddToast) => void,
-  operationContext: { fetch: GithubFetch },
+  operationContext: GithubOperationContext,
 ) {
   return runGithubOperation2(
     {
