de666a818c9dbf0ac4a59d855efb0a40eccd4cd3
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-01-10 11:01:08 +0100

    update tests with store hook

--- editor/src/uuiui/inputs/string-input.spec.browser2.tsx
+++ editor/src/uuiui/inputs/string-input.spec.browser2.tsx
@@ -3,6 +3,10 @@ import type { RenderResult } from '@testing-library/react'
 import { render } from '@testing-library/react'
 import React from 'react'
 import { StringInput } from './string-input'
+import {
+  TestInspectorContextProvider,
+  getStoreHook,
+} from '../../components/inspector/common/inspector.test-utils'
 
 describe('StringInput', () => {
   function checkPlaceholder(renderResult: RenderResult, expectedPlaceholder: string | null): void {
@@ -14,36 +18,62 @@ describe('StringInput', () => {
     }
   }
   it('ensures that no placeholder property shows in the input field by default', () => {
-    const result = render(<StringInput testId='placeholdertest' controlStatus='simple' />)
+    const storeHookForTest = getStoreHook()
+    const result = render(
+      <TestInspectorContextProvider
+        selectedViews={storeHookForTest.getState().editor.selectedViews}
+        editorStoreData={storeHookForTest}
+      >
+        <StringInput testId='placeholdertest' controlStatus='simple' />
+      </TestInspectorContextProvider>,
+    )
     checkPlaceholder(result, null)
   })
   it('ensures that the placeholder property shows in the input field', () => {
+    const storeHookForTest = getStoreHook()
     const result = render(
-      <StringInput
-        testId='placeholdertest'
-        placeholder='this is a placeholder'
-        controlStatus='simple'
-      />,
+      <TestInspectorContextProvider
+        selectedViews={storeHookForTest.getState().editor.selectedViews}
+        editorStoreData={storeHookForTest}
+      >
+        <StringInput
+          testId='placeholdertest'
+          placeholder='this is a placeholder'
+          controlStatus='simple'
+        />
+      </TestInspectorContextProvider>,
     )
     checkPlaceholder(result, 'this is a placeholder')
   })
   it('ensures that the unknown control styles property shows in the input field', () => {
+    const storeHookForTest = getStoreHook()
     const result = render(
-      <StringInput
-        testId='placeholdertest'
-        placeholder='this is a placeholder'
-        controlStatus='multiselect-simple-unknown-css'
-      />,
+      <TestInspectorContextProvider
+        selectedViews={storeHookForTest.getState().editor.selectedViews}
+        editorStoreData={storeHookForTest}
+      >
+        <StringInput
+          testId='placeholdertest'
+          placeholder='this is a placeholder'
+          controlStatus='multiselect-simple-unknown-css'
+        />
+      </TestInspectorContextProvider>,
     )
     checkPlaceholder(result, 'Unknown')
   })
   it('ensures that the mixed control styles property shows in the input field', () => {
+    const storeHookForTest = getStoreHook()
     const result = render(
-      <StringInput
-        testId='placeholdertest'
-        placeholder='this is a placeholder'
-        controlStatus='multiselect-mixed-simple-or-unset'
-      />,
+      <TestInspectorContextProvider
+        selectedViews={storeHookForTest.getState().editor.selectedViews}
+        editorStoreData={storeHookForTest}
+      >
+        <StringInput
+          testId='placeholdertest'
+          placeholder='this is a placeholder'
+          controlStatus='multiselect-mixed-simple-or-unset'
+        />
+      </TestInspectorContextProvider>,
     )
     checkPlaceholder(result, 'Mixed')
   })
