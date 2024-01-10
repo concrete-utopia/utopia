87b201cef8183651c7e3edb121bdbcd7ace4c1f1
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-01-10 09:33:30 +0100

    update snaps

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`704`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`710`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`749`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`755`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -183,7 +183,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`535`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`538`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`607`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`610`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
