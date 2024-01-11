209bf8f1113b9dd8508d772e2cc00467f5ffd472
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-01-11 17:10:30 +0100

    Update github-clone-overlay.tsx

--- editor/src/components/github/github-clone-overlay.tsx
+++ editor/src/components/github/github-clone-overlay.tsx
@@ -136,7 +136,6 @@ const GitClonePseudoElement = React.memo((props: { githubRepo: GithubRepoWithBra
 })
 
 const awaitLoadActionDispatchedByPersistenceMachine = (): Promise<{ projectId: string }> => {
-  // TODO move this WAY out of Editor.tsx
   invariant(
     PubSub.countSubscriptions(LoadActionsDispatched) === 0,
     'At this point, awaitLoadActionDispatchedByPersistenceMachine should have zero listeners',
