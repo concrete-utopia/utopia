4237cce855028c2a56967652ee8b6178cb469682
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-01-16 11:56:06 +0100

    make the RightPane tab menu scroll horizontally

--- editor/src/components/canvas/design-panel-root.tsx
+++ editor/src/components/canvas/design-panel-root.tsx
@@ -183,7 +183,13 @@ export const RightPane = React.memo<ResizableRightPaneProps>((props) => {
     >
       <TitleBarUserProfile panelData={props.panelData} />
       <FlexRow
-        style={{ marginBottom: 10, gap: 2, alignSelf: 'stretch', flexShrink: 0 }}
+        style={{
+          marginBottom: 10,
+          gap: 2,
+          alignSelf: 'stretch',
+          flexShrink: 0,
+          overflowX: 'scroll',
+        }}
         css={undefined}
       >
         <MenuTab
