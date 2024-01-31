d66404e1136a8fc396a27b22becbfc8da0f5cd62
Berci Kormendy <korber314@gmail.com>
2024-01-31 10:53:24 +0100

    wip - test

---
+++ editor/src/components/inspector/sections/component-section/component-section.spec.browser2.tsx
@@ -0,0 +1,8 @@
+describe('Set element prop via the data picker', () => {
+  it('can pick from the property data picker', () => {
+    // set up project with vars in scope
+    // open the prop picker
+    // click the second option
+    // check the string on the screen
+  })
+})
