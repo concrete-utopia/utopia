b8770717ee4a4d03e65aa1546ea152dd41b9e7f1
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-02 12:23:15 +0100

    readability

--- utopia-remix/app/util/methods.server.ts
+++ utopia-remix/app/util/methods.server.ts
@@ -2,5 +2,5 @@ const methods = ["GET", "POST", "PUT", "DELETE", "OPTIONS"] as const;
 export type Method = (typeof methods)[number];
 
 export function isMethod(s: string): s is Method {
-  return methods.some((m) => m === s);
+  return methods.some((method) => method === s);
 }
