41bd6b07f770ee454b259d0f18bc28e4ede2e57d
Liad Yosef <liad.yosef@gmail.com>
2024-02-05 17:19:49 +0200

    only change dns order for dev environments

--- utopia-remix/app/env.server.ts
+++ utopia-remix/app/env.server.ts
@@ -1,4 +1,5 @@
 export const ServerEnvironment = {
+  environment: process.env.NODE_ENV ?? "development",
   // The URL of the actual backend server in the form <scheme>://<host>:<port>
   BackendURL: process.env.BACKEND_URL ?? "",
   // the CORS allowed origin for incoming requests
