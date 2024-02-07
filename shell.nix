87ef4d127ea6b7705270812f9bc2c03b2d68def9
Federico Ruggi <federico.ruggi@shopify.com>
2024-02-07 17:05:46 +0100

    update plumbing

--- shell.nix
+++ shell.nix
@@ -526,6 +526,8 @@ let
       #!/usr/bin/env bash
       set -e
       cd $(${pkgs.git}/bin/git rev-parse --show-toplevel)/utopia-remix
+      ${pnpm}/bin/pnpm install
+      ${pnpm}/bin/pnpm exec prisma generate
       PORT=8002 pnpm run dev
     '')
   ];
