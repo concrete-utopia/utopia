201bb3e2216f618bd358b6c4a2eeb71aa403ad2c
Berci Kormendy <korber314@gmail.com>
2024-02-07 16:26:07 +0100

    run 3

--- editor/src/components/canvas/remix/utopia-remix-root-component.tsx
+++ editor/src/components/canvas/remix/utopia-remix-root-component.tsx
@@ -214,7 +214,7 @@ export interface UtopiaRemixRootComponentProps {
 export const UtopiaRemixRootComponent = (props: UtopiaRemixRootComponentProps) => {
   const remixDerivedDataRef = useRefEditorState((store) => store.derived.remixData)
 
-  // run 2
+  // run 3
   const prevRoutes = React.useRef<(RouteObject | DataRouteObject)[] | null>(null)
   const routesI = useGetRoutes(props.getLoadContext)
   const routes = React.useMemo(() => {
