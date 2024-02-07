58136c1b13808e43c941d825690cb8352b5a6b52
Federico Ruggi <federico.ruggi@shopify.com>
2024-02-07 18:29:55 +0100

    actually wait for pg

--- utopia-remix/run-integration-tests.sh
+++ utopia-remix/run-integration-tests.sh
@@ -5,7 +5,7 @@ set -e
 docker compose down -v
 docker compose up -d
 
-sleep 5s
+until docker exec utopia-remix-db-test pg_isready ; do sleep 1 ; done
 
 export DATABASE_URL='postgres://postgres:password@localhost:54322/postgres?sslmode=disable'
 
