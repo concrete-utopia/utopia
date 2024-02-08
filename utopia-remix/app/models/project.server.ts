eb8f00eff0d0b086e9bed23b6abb6803bd72d650
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-08 18:05:14 +0100

    remove pagination, return all projects

--- utopia-remix/app/models/project.server.ts
+++ utopia-remix/app/models/project.server.ts
@@ -1,15 +1,20 @@
 import { Project } from 'prisma-client'
 import { prisma } from '../db.server'
 
-export async function listProjects(params: {
-  ownerId: string
-  offset?: number
-  limit?: number
-}): Promise<Project[]> {
+export type ProjectWithoutContent = Omit<Project, 'content'>
+
+export async function listProjects(params: { ownerId: string }): Promise<ProjectWithoutContent[]> {
   return prisma.project.findMany({
+    select: {
+      id: true,
+      proj_id: true,
+      owner_id: true,
+      title: true,
+      created_at: true,
+      modified_at: true,
+      deleted: true,
+    },
     where: { owner_id: params.ownerId },
     orderBy: { modified_at: 'desc' },
-    take: params.limit,
-    skip: params.offset,
   })
 }
