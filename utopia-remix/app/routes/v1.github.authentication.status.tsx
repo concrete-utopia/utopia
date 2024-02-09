8af265db1718ea752a31394195dbd4ac2ccd6e31
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-09 13:29:31 +0100

    args interface

--- utopia-remix/app/routes/v1.github.authentication.status.tsx
+++ utopia-remix/app/routes/v1.github.authentication.status.tsx
@@ -3,7 +3,7 @@ import { handle, handleOptions } from '../util/api.server'
 import { proxy } from '../util/proxy.server'
 
 export async function loader(args: LoaderFunctionArgs) {
-  return handle(args.request, {
+  return handle(args, {
     OPTIONS: handleOptions,
     GET: proxy,
   })
--- utopia-remix/app/routes/v1.github.save.$projectId.tsx
+++ utopia-remix/app/routes/v1.github.save.$projectId.tsx
@@ -3,13 +3,13 @@ import { proxy } from '../util/proxy.server'
 import { handle, handleOptions } from '../util/api.server'
 
 export async function loader(args: LoaderFunctionArgs) {
-  return handle(args.request, {
+  return handle(args, {
     OPTIONS: handleOptions,
   })
 }
 
 export async function action(args: ActionFunctionArgs) {
-  return handle(args.request, {
+  return handle(args, {
     POST: proxy,
   })
 }
