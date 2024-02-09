2b3a600601a2165f5395f83859a55a22f63ca9a2
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-09 16:57:31 +0100

    check for unpinned deps

---
+++ check-unpinned-deps.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const { join } = require('path')
+
+/**
+ * Scan dependencies in the package.json files in these folders.
+ */
+const folders = ['.', './editor', './utopia-remix', './puppeteer-tests']
+
+const unpinnedDeps = folders.flatMap((projectPath) => {
+  const path = join(projectPath, 'package.json')
+  const packageJson = JSON.parse(fs.readFileSync(path), 'utf8')
+  const allDeps = {
+    ...packageJson.dependencies,
+    // ...packageJson.devDependencies,
+  }
+  return Object.entries(allDeps)
+    .filter(([, version]) => version.match(/^[\^~]/))
+    .map(([name, version]) => ({ path, name, version }))
+})
+
+if (unpinnedDeps.length > 0) {
+  console.log(`🚨 Found ${unpinnedDeps.length} unpinned dependencies.`)
+  console.log(unpinnedDeps)
+  process.exit(1)
+}
