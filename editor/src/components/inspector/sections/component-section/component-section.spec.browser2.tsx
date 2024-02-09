a41985dcd37291b7bfa4da39bebc491fc40e0aea
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-02-09 16:10:04 +0100

    Added test for registerComponent

--- editor/src/components/inspector/sections/component-section/component-section.spec.browser2.tsx
+++ editor/src/components/inspector/sections/component-section/component-section.spec.browser2.tsx
@@ -1,7 +1,7 @@
 import { within } from '@testing-library/react'
 import * as EP from '../../../../core/shared/element-path'
 import { selectComponentsForTest, wait } from '../../../../utils/utils.test-utils'
-import { mouseClickAtPoint } from '../../../canvas/event-helpers.test-utils'
+import { mouseClickAtPoint, pressKey } from '../../../canvas/event-helpers.test-utils'
 import { renderTestEditorWithCode } from '../../../canvas/ui-jsx.test-utils'
 import {
   DataPickerPopupButtonTestId,
@@ -32,6 +32,27 @@ describe('Set element prop via the data picker', () => {
     await mouseClickAtPoint(secondOption, { x: 2, y: 2 })
     expect(within(theScene).queryByText('Alternate title')).not.toBeNull()
     expect(within(theInspector).queryByText('Alternate title')).not.toBeNull()
+  })
+})
+
+describe('Controls from registerComponent', () => {
+  it('registering internal component', async () => {
+    const editor = await renderTestEditorWithCode(
+      registerInternalComponentProject,
+      'await-first-dom-report',
+    )
+    await selectComponentsForTest(editor, [EP.fromString('sb/scene/pg')])
+
+    const dataPickerOpenerButton = editor.renderedDOM.getByTestId(
+      `title-string-input-property-control`,
+    )
+    dataPickerOpenerButton.focus()
+    // await mouseClickAtPoint(dataPickerOpenerButton, { x: 2, y: 2 })
+    document.execCommand('insertText', false, 'New title')
+    await pressKey('Enter', { targetElement: dataPickerOpenerButton })
+
+    const theScene = editor.renderedDOM.getByTestId('scene')
+    expect(within(theScene).queryByText('New title')).not.toBeNull()
   })
 })
 
@@ -88,3 +109,73 @@ export var storyboard = (
     </Scene>
   </Storyboard>
 )`
+
+const registerInternalComponentProject = `import * as React from 'react'
+import {
+  Storyboard,
+  Scene,
+  registerComponent,
+} from 'utopia-api'
+
+function Title({ text }) {
+  const content = 'Content'
+
+  return <h2 data-uid='0cd'>{text}</h2>
+}
+
+var Playground = ({ style, title }) => {
+  return (
+    <div style={style} data-uid='root'>
+      <Title text={title} data-uid='title' />
+    </div>
+  )
+}
+
+export var storyboard = (
+  <Storyboard data-uid='sb'>
+    <Scene
+      style={{
+        width: 521,
+        height: 266,
+        position: 'absolute',
+        left: 554,
+        top: 247,
+        backgroundColor: 'white',
+      }}
+      data-uid='scene'
+      data-testid='scene'
+      commentId='120'
+    >
+      <Playground
+        style={{
+          width: 454,
+          height: 177,
+          position: 'absolute',
+          left: 34,
+          top: 44,
+          backgroundColor: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        title='Hello Utopia'
+        data-uid='pg'
+      />
+    </Scene>
+  </Storyboard>
+)
+
+registerComponent(Playground, {
+  supportsChildren: false,
+  properties: {
+    title: {
+      control: 'string-input',
+    },
+  },
+  variants: [
+    {
+      code: '<Playground />',
+    },
+  ],
+})
+`
