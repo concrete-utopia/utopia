adcc1b4bb1c2d7be61d2011bfeb06c3591b4f575
Sean Parsons <git@futurenotfound.com>
2024-02-16 18:50:54 +0000

    feature(editor) Added additional test.

--- editor/src/components/canvas/scoped-variables.spec.tsx
+++ editor/src/components/canvas/scoped-variables.spec.tsx
@@ -116,7 +116,206 @@ function projectWithInlineComponent(): PersistentModel {
   return persistentModel
 }
 
+function projectWithInlineComponentDestructuredContents(): ProjectContents {
+  function createCodeFile(path: string, contents: string): TextFile {
+    return textFile(textFileContents(contents, unparsed, RevisionsState.CodeAhead), null, null, 0)
+  }
+
+  return {
+    '/package.json': textFile(
+      textFileContents(
+        JSON.stringify(DefaultPackageJson, null, 2),
+        unparsed,
+        RevisionsState.CodeAhead,
+      ),
+      null,
+      null,
+      0,
+    ),
+    '/src': directory(),
+    '/assets': directory(),
+    '/public': directory(),
+    [StoryboardFilePath]: createCodeFile(
+      StoryboardFilePath,
+      `import * as React from 'react'
+import Utopia, {
+  Scene,
+  Storyboard,
+  registerModule,
+} from 'utopia-api'
+import { App } from '/src/app.js'
+
+export var storyboard = (
+  <Storyboard data-uid='storyboard-entity'>
+    <Scene
+      data-label='Imported App'
+      data-uid='scene-1-entity'
+      style={{ position: 'absolute', left: 0, top: 0, width: 375, height: 812 }}
+    >
+      <App data-uid='app-entity' />
+    </Scene>
+  </Storyboard>
+)`,
+    ),
+    '/src/app.js': createCodeFile(
+      '/src/app.js',
+      `import * as React from 'react'
+
+
+export const FakeAwait = (props) => {
+  const computed =
+    typeof props.children === 'function'
+      ? props.children({first: 1, second: {thing: 'a string'}})
+      : 'Oh no'
+  return <div data-uid='fakeawait'>{computed}</div>
+}
+
+export var App = (props) => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        background: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <FakeAwait data-uid={'fakeawaitelement'}>
+        {({first, second: {thing}}) => {
+          return (
+            <div
+              data-uid={'something-div'}
+              style={{
+                width: '100%',
+                height: '100%',
+                background: 'white',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              {first}
+            </div>
+          )
+        }}
+      </FakeAwait>
+    </div>
+  )
+}`,
+    ),
+  }
+}
+
+function projectWithInlineComponentDestructured(): PersistentModel {
+  const projectContents: ProjectContents = projectWithInlineComponentDestructuredContents()
+  const persistentModel = persistentModelForProjectContents(contentsToTree(projectContents))
+  return persistentModel
+}
+
 describe('scoped variables', () => {
+  it('includes scoped variables for an inline component destructuring the parameter', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      projectWithInlineComponentDestructured(),
+      'dont-await-first-dom-report',
+    )
+    expect(renderResult.getEditorState().editor.variablesInScope).toMatchInlineSnapshot(`
+      Object {
+        "storyboard-entity": Object {},
+        "storyboard-entity/scene-1-entity": Object {},
+        "storyboard-entity/scene-1-entity/app-entity": Object {},
+        "storyboard-entity/scene-1-entity/app-entity:cb0": Object {
+          "props": Object {
+            "insertionCeiling": Object {
+              "parts": Array [
+                Array [
+                  "storyboard-entity",
+                  "scene-1-entity",
+                  "app-entity",
+                ],
+              ],
+              "type": "elementpath",
+            },
+            "spiedValue": Object {
+              "data-uid": "app-entity",
+            },
+          },
+        },
+        "storyboard-entity/scene-1-entity/app-entity:cb0/fakeawaitelement": Object {
+          "props": Object {
+            "insertionCeiling": Object {
+              "parts": Array [
+                Array [
+                  "storyboard-entity",
+                  "scene-1-entity",
+                  "app-entity",
+                ],
+              ],
+              "type": "elementpath",
+            },
+            "spiedValue": Object {
+              "data-uid": "app-entity",
+            },
+          },
+        },
+        "storyboard-entity/scene-1-entity/app-entity:cb0/fakeawaitelement/5b3/something-div~~~1": Object {
+          "first": Object {
+            "insertionCeiling": Object {
+              "parts": Array [
+                Array [
+                  "storyboard-entity",
+                  "scene-1-entity",
+                  "app-entity",
+                ],
+                Array [
+                  "cb0",
+                  "fakeawaitelement",
+                  "5b3",
+                  "something-div~~~1",
+                ],
+              ],
+              "type": "elementpath",
+            },
+            "spiedValue": 1,
+          },
+          "props": Object {
+            "insertionCeiling": Object {
+              "parts": Array [
+                Array [
+                  "storyboard-entity",
+                  "scene-1-entity",
+                  "app-entity",
+                ],
+              ],
+              "type": "elementpath",
+            },
+            "spiedValue": Object {
+              "data-uid": "app-entity",
+            },
+          },
+          "thing": Object {
+            "insertionCeiling": Object {
+              "parts": Array [
+                Array [
+                  "storyboard-entity",
+                  "scene-1-entity",
+                  "app-entity",
+                ],
+                Array [
+                  "cb0",
+                  "fakeawaitelement",
+                  "5b3",
+                  "something-div~~~1",
+                ],
+              ],
+              "type": "elementpath",
+            },
+            "spiedValue": "a string",
+          },
+        },
+      }
+    `)
+  })
+
   it('includes scoped variables for an inline component', async () => {
     const renderResult = await renderTestEditorWithModel(
       projectWithInlineComponent(),
