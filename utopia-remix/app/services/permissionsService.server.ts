0a21098180bc0a944ddf2857e5a8b7ed8a3dc1ec
Liad Yosef <liad.yosef@gmail.com>
2024-02-27 00:16:40 +0200

    remove enums

--- utopia-remix/app/services/permissionsService.server.ts
+++ utopia-remix/app/services/permissionsService.server.ts
@@ -1,15 +1,18 @@
 import * as fgaService from './fgaService.server'
 
-export enum UserProjectPermission {
-  CAN_VIEW_PROJECT,
-  CAN_FORK_PROJECT,
-  CAN_PLAY_PROJECT,
-  CAN_EDIT_PROJECT,
-  CAN_COMMENT_PROJECT,
-  CAN_SHOW_PRESENCE,
-  CAN_REQUEST_ACCESS,
-  CAN_SEE_LIVE_CHANGES,
-}
+export const UserProjectPermission = {
+  CAN_VIEW_PROJECT: 0,
+  CAN_FORK_PROJECT: 1,
+  CAN_PLAY_PROJECT: 2,
+  CAN_EDIT_PROJECT: 3,
+  CAN_COMMENT_PROJECT: 4,
+  CAN_SHOW_PRESENCE: 5,
+  CAN_REQUEST_ACCESS: 6,
+  CAN_SEE_LIVE_CHANGES: 7,
+} as const
+
+export type UserProjectPermission =
+  (typeof UserProjectPermission)[keyof typeof UserProjectPermission]
 
 export async function hasUserProjectPermission(
   projectId: string,
