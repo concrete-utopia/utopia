55dd28f18b3d93adb2e268aa23f6f8720ffc9b54
Liad Yosef <liad.yosef@gmail.com>
2024-02-29 19:49:52 +0200

    cr

--- utopia-remix/app/models/projectAccess.server.ts
+++ utopia-remix/app/models/projectAccess.server.ts
@@ -6,19 +6,21 @@ export async function setProjectAccess(params: {
   projectId: string
   accessLevel: AccessLevel
 }): Promise<void> {
-  await prisma.projectAccess.upsert({
-    where: {
-      project_id: params.projectId,
-    },
-    update: {
-      access_level: params.accessLevel,
-      modified_at: new Date(),
-    },
-    create: {
-      project_id: params.projectId,
-      access_level: params.accessLevel,
-      modified_at: new Date(),
-    },
+  await prisma.$transaction(async (tx) => {
+    await tx.projectAccess.upsert({
+      where: {
+        project_id: params.projectId,
+      },
+      update: {
+        access_level: params.accessLevel,
+        modified_at: new Date(),
+      },
+      create: {
+        project_id: params.projectId,
+        access_level: params.accessLevel,
+        modified_at: new Date(),
+      },
+    })
+    await permissionsService.setProjectAccess(params.projectId, params.accessLevel)
   })
-  await permissionsService.setProjectAccess(params.projectId, params.accessLevel)
 }
