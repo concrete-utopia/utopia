122ae0ae5e1bc8f0c865a9c50725c740ef408398
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-29 11:06:42 +0100

    comment

--- utopia-remix/app/hooks/useCleanupOperations.tsx
+++ utopia-remix/app/hooks/useCleanupOperations.tsx
@@ -4,36 +4,41 @@ import React from 'react'
 import { isLikeApiError } from '../util/errors'
 import { operationFetcherKeyPrefix } from './useFetcherWithOperation'
 
+/**
+ * This hook will react to fetchers changes, so that the ones tied to ongoing Operations,
+ * when transitioning from loading to idle (and hence being cleared) will have their corresponding
+ * Operations either removed or marked as errored, depending on the result of the fetcher data
+ * received back.
+ */
 export function useCleanupOperations() {
   const { operationsToCleanup, resetOperationsToCleanup } = useLoadingFetchers()
   const consumeOperations = useConsumeOperations()
 
+  // when the operations to cleanup change, consume the related operations and then reset
+  // them.
   React.useEffect(() => {
-    const operationsToCleanupList = Object.entries(operationsToCleanup).map(([key, value]) => {
-      return {
-        key: key,
-        value: value,
-      }
-    })
-
-    if (operationsToCleanupList.length > 0) {
-      consumeOperations(operationsToCleanupList)
-      resetOperationsToCleanup(operationsToCleanupList.map((e) => e.key))
+    const keysToCleanup = Object.keys(operationsToCleanup)
+    if (keysToCleanup.length > 0) {
+      consumeOperations(operationsToCleanup)
+      resetOperationsToCleanup(keysToCleanup) // explicitly send the current list of keys
     }
   }, [operationsToCleanup, consumeOperations, resetOperationsToCleanup])
 }
 
+/**
+ * Consume the given operations, deleting them if successful and marking them as errored otherwise.
+ */
 function useConsumeOperations() {
   const removeOperation = useProjectsStore((store) => store.removeOperation)
   const updateOperation = useProjectsStore((store) => store.updateOperation)
 
   return React.useCallback(
-    (operations: { key: string; value: { data: unknown } }[]) => {
-      for (const op of operations) {
-        if (isLikeApiError(op.value.data)) {
-          updateOperation(op.key, { errored: true })
+    (operations: OperationFetcherData) => {
+      for (const [key, value] of Object.entries(operations)) {
+        if (isLikeApiError(value.data)) {
+          updateOperation(key, { errored: true })
         } else {
-          removeOperation(op.key)
+          removeOperation(key)
         }
       }
     },
@@ -43,6 +48,9 @@ function useConsumeOperations() {
 
 type OperationFetcherData = { [key: string]: { data: unknown } }
 
+/**
+ * Listen to fetcher changes and return a list of operations to cleanup.
+ */
 function useLoadingFetchers() {
   const fetchers = useFetchers()
 
