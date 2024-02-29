8b14132fca25a53e0d5c8c87d07e9dc6d927e244
Sean Parsons <git@futurenotfound.com>
2024-02-29 11:19:19 +0000

    fix(remix) Fix issue with Prisma binaries being incompatible. - Added new nixpkgs version to `release.nix`. - Updated the Prisma environment variables for version 5.xx.

--- release.nix
+++ release.nix
@@ -12,7 +12,15 @@ let
     sha256 = "11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
   }) { inherit config; };
 
+  recentPkgs = import (builtins.fetchTarball {
+    name = "nixos-23.11";
+    url = https://github.com/NixOS/nixpkgs/archive/23.11.tar.gz;
+    # Hash obtained using `nix-prefetch-url --unpack <url>`
+    sha256 = "1ndiv385w1qyb3b18vw13991fzb9wg4cl21wglk89grsfsnra41k";
+  }) { inherit config; };
+
 in
   { 
     pkgs = pkgs;
+    recentPkgs = recentPkgs;
   }
