99c5a979ca6e3e60d7310bdfbd4dc6035ed5ee62
Liad Yosef <liad.yosef@gmail.com>
2024-02-29 19:22:25 +0200

    env vars

--- utopia-remix/app/env.server.ts
+++ utopia-remix/app/env.server.ts
@@ -19,9 +19,9 @@ export const ServerEnvironment = {
   AUTH0_CLIENT_ID: optionalEnv('AUTH0_CLIENT_ID', '<AUTH0_CLIENT_ID>'),
   AUTH0_REDIRECT_URI: optionalEnv('AUTH0_REDIRECT_URI', '<AUTH0_REDIRECT_URI>'),
   // FGA Credentials
-  FGA_STORE_ID: mustEnv('FGA_STORE_ID'),
-  FGA_CLIENT_ID: mustEnv('FGA_CLIENT_ID'),
-  FGA_SECRET: mustEnv('FGA_SECRET'),
+  FGA_STORE_ID: mustEnv('FGA_STORE_ID', { test: '<FGA_STORE_ID>' }),
+  FGA_CLIENT_ID: mustEnv('FGA_CLIENT_ID', { test: '<FGA_CLIENT_ID>' }),
+  FGA_SECRET: mustEnv('FGA_SECRET', { test: '<FGA_SECRET>' }),
 }
 
 export type BrowserEnvironment = {
@@ -32,9 +32,13 @@ export const BrowserEnvironment: BrowserEnvironment = {
   EDITOR_URL: mustEnv('REACT_APP_EDITOR_URL'),
 }
 
-function mustEnv(key: string): string {
+function mustEnv(key: string, envFallback: Partial<Record<AppEnv, string>> = {}): string {
   const value = process.env[key]
   if (value == null) {
+    const fallback = envFallback[ServerEnvironment.environment]
+    if (fallback != null) {
+      return fallback
+    }
     throw new Error(`missing required environment variable ${key}`)
   }
   return value
