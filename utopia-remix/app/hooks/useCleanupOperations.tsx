b3ff948efa67121b8cbe3c67620b2f712cc9cc6c
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-02-29 11:54:22 +0100

    simplify a bit

--- utopia-remix/app/hooks/useCleanupOperations.tsx
+++ utopia-remix/app/hooks/useCleanupOperations.tsx
@@ -11,68 +11,32 @@ import { operationFetcherKeyPrefix } from './useFetcherWithOperation'
  * received back.
  */
 export function useCleanupOperations() {
-  const { operationsToCleanup, resetOperationsToCleanup } = useLoadingFetchers()
-  const consumeOperations = useConsumeOperations()
-
-  // when the operations to cleanup change, consume the related operations and then reset
-  // them.
-  React.useEffect(() => {
-    const keysToCleanup = Object.keys(operationsToCleanup)
-    if (keysToCleanup.length > 0) {
-      consumeOperations(operationsToCleanup)
-      resetOperationsToCleanup(keysToCleanup) // explicitly send the current list of keys
-    }
-  }, [operationsToCleanup, consumeOperations, resetOperationsToCleanup])
-}
-
-/**
- * Consume the given operations, deleting them if successful and marking them as errored otherwise.
- */
-function useConsumeOperations() {
-  const removeOperation = useProjectsStore((store) => store.removeOperation)
-  const updateOperation = useProjectsStore((store) => store.updateOperation)
-
-  return React.useCallback(
-    (operations: OperationFetcherData) => {
-      for (const [key, value] of Object.entries(operations)) {
-        if (isLikeApiError(value.data)) {
-          updateOperation(key, { errored: true })
-        } else {
-          removeOperation(key)
-        }
-      }
-    },
-    [updateOperation, removeOperation],
-  )
-}
-
-type OperationFetcherData = { [key: string]: { data: unknown } }
-
-/**
- * Listen to fetcher changes and return a list of operations to cleanup.
- */
-function useLoadingFetchers() {
   const fetchers = useFetchers()
+  const consumeOperations = useConsumeOperations()
 
   // accumulated fetchers that were seen in the loading state
   const [loadingFetchers, setLoadingFetchers] = React.useState<OperationFetcherData>({})
   // list of fetchers that need to be cleaned up
-  const [operationsToCleanup, setOperationsToCleanup] = React.useState<OperationFetcherData>({})
+  const [operationsToCleanup, setOperationsToCleanup] = React.useState<
+    { key: string; value: { data: unknown } }[]
+  >([])
 
-  // list of keys to be cleaned up
-  const keysToCleanup = React.useMemo(() => {
-    return Object.keys(loadingFetchers).filter((key) => {
-      return !fetchers.some((f) => f.key === key)
-    })
+  // list of fetchers that were loading but are not available anymore (because gone to idle)
+  const disappearedFetchers = React.useMemo(() => {
+    return Object.entries(loadingFetchers)
+      .filter(([key]) => {
+        return !fetchers.some((f) => f.key === key)
+      })
+      .map(([key, value]) => ({ key, value }))
   }, [fetchers, loadingFetchers])
 
   // reset operations
   const resetOperationsToCleanup = React.useCallback(
-    (targets: string[]) => {
+    (targets: { key: string; value: { data: unknown } }[]) => {
       setLoadingFetchers((loading) => {
-        setOperationsToCleanup({})
+        setOperationsToCleanup([])
         return Object.entries(loading).reduce((acc, [key, value]) => {
-          if (!targets.includes(key)) {
+          if (!targets.some((target) => target.key === key)) {
             acc[key] = value
           }
           return acc
@@ -82,7 +46,7 @@ function useLoadingFetchers() {
     [loadingFetchers],
   )
 
-  // fetchers currently in the loading state
+  // the fetchers currently in the loading state
   const currentLoadingFetchers = React.useMemo(() => {
     return fetchers
       .filter((fetcher) => {
@@ -110,14 +74,44 @@ function useLoadingFetchers() {
 
   // when the keys to cleanup change, update the operations to cleanup
   React.useEffect(() => {
-    if (keysToCleanup.length > 0) {
-      const newOperationsToCleanup: OperationFetcherData = {}
-      for (const key of keysToCleanup) {
-        newOperationsToCleanup[key] = loadingFetchers[key]
+    if (disappearedFetchers.length > 0) {
+      const newOperationsToCleanup: { key: string; value: { data: unknown } }[] = []
+      for (const { key, value } of disappearedFetchers) {
+        newOperationsToCleanup.push({ key, value })
       }
       setOperationsToCleanup(newOperationsToCleanup)
     }
-  }, [keysToCleanup, loadingFetchers])
+  }, [disappearedFetchers, loadingFetchers])
 
-  return { operationsToCleanup, resetOperationsToCleanup }
+  // when the operations to cleanup change, consume the related operations and then reset
+  // them.
+  React.useEffect(() => {
+    if (operationsToCleanup.length > 0) {
+      consumeOperations(operationsToCleanup)
+      resetOperationsToCleanup(operationsToCleanup)
+    }
+  }, [operationsToCleanup, consumeOperations, resetOperationsToCleanup])
 }
+
+/**
+ * Consume the given operations, deleting them if successful and marking them as errored otherwise.
+ */
+function useConsumeOperations() {
+  const removeOperation = useProjectsStore((store) => store.removeOperation)
+  const updateOperation = useProjectsStore((store) => store.updateOperation)
+
+  return React.useCallback(
+    (operations: { key: string; value: { data: unknown } }[]) => {
+      for (const { key, value } of operations) {
+        if (isLikeApiError(value.data)) {
+          updateOperation(key, { errored: true })
+        } else {
+          removeOperation(key)
+        }
+      }
+    },
+    [updateOperation, removeOperation],
+  )
+}
+
+type OperationFetcherData = { [key: string]: { data: unknown } }
