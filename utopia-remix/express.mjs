a3a6c4b39d85b570e6f0d09e40eefe35079f3f00
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-04 15:37:54 +0100

    i'll still call it "cool proxy"

--- utopia-remix/express.mjs
+++ utopia-remix/express.mjs
@@ -1,32 +1,51 @@
 import { createRequestHandler } from '@remix-run/express'
-import express from 'express'
 import cors from 'cors'
-import { createProxyMiddleware } from 'http-proxy-middleware'
+import express from 'express'
+import * as http from 'node:http'
 
+// Remix bundle
 import * as build from './build/index.js'
 
-const proxyMiddleware = createProxyMiddleware({
-  target: 'http://localhost:8001',
-  changeOrigin: true,
-  ws: true,
+function proxy(originalRequest, originalResponse) {
+  const proxyRequest = http.request(
+    {
+      // target the right server
+      protocol:
+        process.env.APP_ENV === 'local' || process.env.APP_ENV === 'test' ? 'http:' : 'https:',
+      host: process.env.EXPRESS_PROXY_TARGET_HOST,
+      port: process.env.EXPRESS_PROXY_TARGET_PORT,
+
+      // proxy everything
+      path: originalRequest.originalUrl,
+      method: originalRequest.method,
+      headers: originalRequest.headers,
+    },
+    (proxyResponse) => {
+      originalResponse.writeHead(proxyResponse.statusCode, proxyResponse.headers)
+      proxyResponse.pipe(originalResponse)
+    },
+  )
+
+  originalRequest.pipe(proxyRequest)
+}
+
+const corsMiddleware = cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
 })
 
 const app = express()
-app.use(
-  cors({
-    origin: 'http://localhost:8000',
-    credentials: true,
-  }),
-)
 
-app.use('/v1', proxyMiddleware)
-app.use('/p', proxyMiddleware)
-app.use('/editor', proxyMiddleware)
-app.use('/hashed-assets.json', proxyMiddleware)
-app.use('/vscode', proxyMiddleware)
+app.use('/editor', proxy)
+app.use('/hashed-assets.json', proxy)
+app.use('/p', proxy)
+app.use('/v1', proxy)
+app.use('/vscode', proxy)
 
+app.use(corsMiddleware)
+app.use(express.static('public'))
 app.all('*', createRequestHandler({ build }))
 
-app.use(express.static('public'))
-
-app.listen(8002)
+app.listen(process.env.PORT, () => {
+  console.log(`Express listening on :${process.env.PORT}`)
+})
