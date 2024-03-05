c3bfcddbbae6f009bdb8eaa166991a4dfbd2acef
Liad Yosef <liad.yosef@gmail.com>
2024-03-05 18:43:28 +0200

    change validators

--- utopia-remix/app/routes/v1.collaboration.tsx
+++ utopia-remix/app/routes/v1.collaboration.tsx
@@ -1,8 +1,7 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
 import { handle, handleOptions } from '../util/api.server'
 import { proxy } from '../util/proxy.server'
-import { validateProjectAccess } from '../handlers/validators'
-import { UserProjectPermission } from '../types'
+import { ALLOW } from '../handlers/validators'
 
 export async function loader(args: LoaderFunctionArgs) {
   return handle(args, {
@@ -14,7 +13,7 @@ export async function action(args: ActionFunctionArgs) {
   return handle(args, {
     PUT: {
       handler: proxy,
-      validator: validateProjectAccess(UserProjectPermission.CAN_MANAGE_PROJECT),
+      validator: ALLOW,
     },
   })
 }
--- utopia-remix/app/routes/v1.projectid.tsx
+++ utopia-remix/app/routes/v1.projectid.tsx
@@ -1,8 +1,7 @@
 import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
 import { proxy } from '../util/proxy.server'
 import { handle, handleOptions } from '../util/api.server'
-import { validateProjectAccess } from '../handlers/validators'
-import { UserProjectPermission } from '../types'
+import { ALLOW } from '../handlers/validators'
 
 export async function loader(args: LoaderFunctionArgs) {
   return handle(args, {
@@ -14,7 +13,7 @@ export async function action(args: ActionFunctionArgs) {
   return handle(args, {
     POST: {
       handler: proxy,
-      validator: validateProjectAccess(UserProjectPermission.CAN_MANAGE_PROJECT),
+      validator: ALLOW,
     },
   })
 }
