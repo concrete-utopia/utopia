b2404bd77f927b212f63e7b6fb8d0fb3390a8a91
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-03-07 17:29:12 +0100

    Update karma-hydrogen.conf.js

--- editor/karma-hydrogen.conf.js
+++ editor/karma-hydrogen.conf.js
@@ -6,5 +6,10 @@ module.exports = function (config) {
   config.set({
     // list of files / patterns to load in the browser
     files: [...config.files, './src/**/*.spec.hydrogen.browser2.+(ts|tsx)'],
+    client: {
+      mocha: {
+        timeout: 100000,
+      },
+    },
   })
 }
