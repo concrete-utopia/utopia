caf056adf1b42daca58ebdbaeaaf410046acef09
lankaukk <lankaukk@yahoo.com>
2024-03-08 13:47:16 -0500

    fix

--- website-next/pages/projects.tsx
+++ website-next/pages/projects.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react'
 import { ProjectsPage } from '../components/projects/projects-page-component'
-import '@radix-ui/themes/styles.css'
-import { Theme } from '@radix-ui/themes'
 
 export default function Projects() {
-  return (
-    <Theme>
-      <ProjectsPage />
-    </Theme>
-  )
+  return <ProjectsPage />
 }
