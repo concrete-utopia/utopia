96980ffeb18eb105adabfb79c83bbdf6116fef66
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-11 15:19:11 +0100

    rename

--- utopia-remix/app/util/auth0.server.ts
+++ utopia-remix/app/util/auth0.server.ts
@@ -5,9 +5,9 @@ export function auth0LoginURL(): string {
   const behaviour = 'auto-close'
 
   const useAuth0 =
-    ServerEnvironment.Auth0Endpoint !== '' &&
-    ServerEnvironment.Auth0ClientId !== '' &&
-    ServerEnvironment.Auth0RedirectUri !== ''
+    ServerEnvironment.AUTH0_ENDPOINT !== '' &&
+    ServerEnvironment.AUTH0_CLIENT_ID !== '' &&
+    ServerEnvironment.AUTH0_REDIRECT_URI !== ''
   if (!useAuth0) {
     console.warn(
       'Auth0 is disabled, if you need it be sure to set the AUTH0_ENDPOINT, AUTH0_CLIENT_ID, AUTH0_REDIRECT_URI environment variables',
@@ -18,12 +18,12 @@ export function auth0LoginURL(): string {
     return url.href
   }
 
-  const url = new URL(`https://${ServerEnvironment.Auth0Endpoint}/authorize`)
+  const url = new URL(`https://${ServerEnvironment.AUTH0_ENDPOINT}/authorize`)
   url.searchParams.set('scope', 'openid profile email')
   url.searchParams.set('response_type', 'code')
-  url.searchParams.set('client_id', ServerEnvironment.Auth0ClientId)
+  url.searchParams.set('client_id', ServerEnvironment.AUTH0_CLIENT_ID)
 
-  const redirectURL = new URL(ServerEnvironment.Auth0RedirectUri)
+  const redirectURL = new URL(ServerEnvironment.AUTH0_REDIRECT_URI)
   redirectURL.searchParams.set('onto', behaviour)
 
   url.searchParams.set('redirect_uri', redirectURL.href)
