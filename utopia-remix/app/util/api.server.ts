f6d564db2e677f5d9b737148c58f8e2e3df7e706
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-12 17:52:00 +0100

    Express server (#4987)
    * add motion
    
    * fetcher with operation
    
    * operation
    
    * use the new fetchers
    
    * split storage
    
    * active operation divs
    
    * cleanup operations
    
    * types
    
    * op with key
    
    * use v3_fetcherPersist
    
    * no need to check null
    
    * separate operation component
    
    * wip
    
    * correct state flow
    
    * remove debug
    
    * remove wait
    
    * adjust cleanup
    
    * add spacing
    
    * split hooks
    
    * more efficient hooks
    
    * comment
    
    * more comment
    
    * easier to read
    
    * simplify a bit
    
    * a much simpler hook
    
    * split
    
    * document the code and split it
    
    * 6 digits
    
    * fix color
    
    * remove unused
    
    * move down
    
    * fix project passing
    
    * adjust assertion
    
    * getOperationDescription
    
    * unless
    
    * wip
    
    * add env vars
    
    * clean up deps
    
    * i'll still call it "cool proxy"
    
    * remove unneeded dep
    
    * use originalrequest's proto
    
    * express template + integrate with remix and hot reload
    
    * pfft
    
    * comment and section
    
    * ascii banner
    
    * remove line ref
    
    * fallback for auth0
    
    * add routes
    
    * update ports and proxied routes
    
    * update route
    
    * fix(all) locally run express on 8000 and 8001, and haskell on 8002
    
    * comment out
    
    * remove
    
    * fix equality check
    
    * fix links
    
    * rename
    
    * fix finish flow
    
    * finish mirroring routes
    
    * add monitoring route
    
    * adjust default headers
    
    * revert pointless change
    
    * clean up
    
    * Update utopia-remix/readme.md
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
    
    * Update utopia-remix/readme.md
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
    
    * Update utopia-remix/readme.md
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
    
    * update validators
    
    * use let
    
    * pin dev deps
    
    ---------
    
    Co-authored-by: RheeseyB <1044774+Rheeseyb@users.noreply.github.com>

--- utopia-remix/app/util/api.server.ts
+++ utopia-remix/app/util/api.server.ts
@@ -4,12 +4,12 @@ import * as cookie from 'cookie'
 import { UserDetails } from 'prisma-client'
 import { PrismaClientKnownRequestError } from 'prisma-client/runtime/library.js'
 import invariant from 'tiny-invariant'
-import { ServerEnvironment } from '../env.server'
+import { ALLOW } from '../handlers/validators'
 import { getUserFromSession } from '../models/session.server'
 import { ApiError } from './errors'
 import { Method } from './methods.server'
 import { Status } from './statusCodes'
-import { ALLOW } from '../handlers/validators'
+import { ServerEnvironment } from '../env.server'
 
 interface ErrorResponse {
   error: string
@@ -19,13 +19,15 @@ type EmptyResponse = Record<string, never>
 
 export type ApiResponse<T> = TypedResponse<T | ErrorResponse | EmptyResponse>
 
-const defaultResponseHeaders = new Headers({
-  'Access-Control-Allow-Origin': ServerEnvironment.CORSOrigin,
-  'Access-Control-Allow-Credentials': 'true',
-  'Access-Control-Allow-Headers': 'content-type, origin, cookie',
-  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-  'Cache-control': 'no-cache',
-})
+function defaultResponseHeaders(): Headers {
+  return new Headers({
+    'Access-Control-Allow-Origin': ServerEnvironment.CORSOrigin,
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Headers': 'content-type, origin, cookie',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+    'Cache-control': 'no-cache',
+  })
+}
 
 export function alwaysAllow(
   handler: (request: Request, params: Params<string>) => Promise<unknown>,
@@ -37,7 +39,7 @@ export function alwaysAllow(
 }
 
 async function optionsHandler(): Promise<TypedResponse<EmptyResponse>> {
-  return json({}, { headers: defaultResponseHeaders })
+  return json({}, { headers: defaultResponseHeaders() })
 }
 export const handleOptions = alwaysAllow(optionsHandler)
 
@@ -79,18 +81,19 @@ async function handleMethod<T>(
     if (validator != null) {
       await validator(request, params)
     }
+
     const resp = await fn(request, params)
     if (resp instanceof Response) {
-      const mergedHeaders = new Headers(defaultResponseHeaders)
+      let headers = new Headers()
       resp.headers.forEach((value, key) => {
-        mergedHeaders.set(key, value)
+        headers.set(key, value)
       })
       return new Response(resp.body, {
         status: resp.status,
-        headers: mergedHeaders,
+        headers: headers,
       })
     }
-    return json(resp, { headers: defaultResponseHeaders })
+    return json(resp, { headers: { ...defaultResponseHeaders() } })
   } catch (err) {
     const { message, status, name } = getErrorData(err)
 
@@ -98,7 +101,7 @@ async function handleMethod<T>(
 
     return json(
       { error: name, status: status, message: message },
-      { headers: defaultResponseHeaders, status: status },
+      { headers: defaultResponseHeaders(), status: status },
     )
   }
 }
@@ -145,6 +148,7 @@ export async function proxiedResponse(response: Response): Promise<unknown> {
 }
 
 export const SESSION_COOKIE_NAME = 'JSESSIONID'
+
 function getSessionId(request: Request): string | null {
   const cookieHeader = request.headers.get('cookie') ?? ''
   const cookies = cookie.parse(cookieHeader)
