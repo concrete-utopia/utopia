a965f9986257ebe3f79e952644749889395b5b60
lankaukk <lankaukk@yahoo.com>
2024-03-12 16:40:29 -0400

    meta title

--- utopia-remix/app/root.tsx
+++ utopia-remix/app/root.tsx
@@ -54,6 +54,8 @@ export default function App() {
       <head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <title>Utopia</title>
+        <meta property='og:title' content='Utopia' />;
         <Meta />
         <Links />
       </head>
