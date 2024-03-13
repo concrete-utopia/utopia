a9edbfd3866eb97c4b42a23b64ea33be1373c4e3
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-13 19:04:50 +0100

    comment

--- utopia-remix/app/models/projectAccessRequest.server.ts
+++ utopia-remix/app/models/projectAccessRequest.server.ts
@@ -8,9 +8,16 @@ function makeRequestToken(): string {
   return uuid.v4()
 }
 
+/**
+ * Create a new access request for a given project and the given user.
+ * The user must not be an owner of the project.
+ * If successful, a new request in the pending state will be created.
+ * If there already is a request for the projectId+userId pair, nothing happens.
+ */
 export async function createAccessRequest(params: { projectId: string; userId: string }) {
   const token = makeRequestToken()
   await prisma.$transaction(async (tx) => {
+    // let's check that the project exists, is not soft-deleted, and is not owned by the user
     const project = await tx.project.findFirst({
       where: {
         proj_id: params.projectId,
@@ -43,6 +50,9 @@ export async function createAccessRequest(params: { projectId: string; userId: s
   })
 }
 
+/**
+ * Update the status for the given request (via its token) for the given project owned by ownerId.
+ */
 export async function updateAccessRequestStatus(params: {
   projectId: string
   ownerId: string
@@ -50,17 +60,19 @@ export async function updateAccessRequestStatus(params: {
   status: AccessRequestStatus
 }) {
   await prisma.$transaction(async (tx) => {
-    const project = await tx.project.findFirst({
+    // check that the project exists
+    const projectCount = await tx.project.count({
       where: { proj_id: params.projectId, owner_id: params.ownerId },
-      include: { ProjectAccessRequest: { select: { id: true, token: true } } },
     })
-    ensure(project != null, 'project not found', Status.NOT_FOUND)
-
-    const request = project.ProjectAccessRequest.find((r) => r.token === params.token)
-    ensure(request != null, 'request not found', Status.NOT_FOUND)
+    ensure(projectCount === 1, 'project not found', Status.NOT_FOUND)
 
     await tx.projectAccessRequest.update({
-      where: { id: request.id },
+      where: {
+        project_id_token: {
+          project_id: params.projectId,
+          token: params.token,
+        },
+      },
       data: {
         status: params.status,
         updated_at: new Date(),
