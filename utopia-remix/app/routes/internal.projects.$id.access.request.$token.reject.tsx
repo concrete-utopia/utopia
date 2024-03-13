fc2961d51757a8876a9f830eac2713fa2cb950a8
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-13 18:52:09 +0100

    routes

---
+++ utopia-remix/app/routes/internal.projects.$id.access.request.$token.reject.tsx
@@ -0,0 +1,37 @@
+import type { ActionFunctionArgs } from '@remix-run/node'
+import type { Params } from '@remix-run/react'
+import { ensure, handle, requireUser } from '../util/api.server'
+import { Status } from '../util/statusCodes'
+import { validateProjectAccess } from '../handlers/validators'
+import { updateAccessRequestStatus } from '../models/projectAccessRequest.server'
+import { AccessRequestStatus, UserProjectPermission } from '../types'
+
+export async function action(args: ActionFunctionArgs) {
+  return handle(args, {
+    POST: {
+      handler: handleApproveAccessRequest,
+      validator: validateProjectAccess(UserProjectPermission.CAN_MANAGE_PROJECT, {
+        getProjectId: (params) => params.id,
+      }),
+    },
+  })
+}
+
+export async function handleApproveAccessRequest(req: Request, params: Params<string>) {
+  const user = await requireUser(req)
+
+  const projectId = params.id
+  ensure(projectId != null, 'project id is null', Status.BAD_REQUEST)
+
+  const token = params.token
+  ensure(token != null && typeof token === 'string', 'token is invalid', Status.BAD_REQUEST)
+
+  await updateAccessRequestStatus({
+    projectId: projectId,
+    ownerId: user.user_id,
+    token: token,
+    status: AccessRequestStatus.REJECTED,
+  })
+
+  return {}
+}
