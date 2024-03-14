65b547298bfd491e14c67adda3a4863bbf2013f8
lankaukk <lankaukk@yahoo.com>
2024-03-14 14:39:17 -0400

    project card size

--- utopia-remix/app/styles/projects.css.ts
+++ utopia-remix/app/styles/projects.css.ts
@@ -7,7 +7,7 @@ export const projectCards = recipe({
       overflow: 'hidden scroll',
       scrollbarColor: '#a1a1a130 transparent',
       display: 'grid',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+      gridTemplateColumns: 'repeat(auto-fill, minmax(270px, 1fr))',
       justifyContent: 'space-between',
       alignContent: 'flex-start',
       gridGap: '40px',
