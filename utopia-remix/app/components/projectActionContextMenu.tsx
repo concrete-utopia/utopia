68caab7dbb6641e9b36ea14b9dd6f3050968819a
lankaukk <lankaukk@yahoo.com>
2024-03-15 10:20:22 -0400

    address comments

--- utopia-remix/app/components/projectActionContextMenu.tsx
+++ utopia-remix/app/components/projectActionContextMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useProjectsStore } from '../store'
 import type { ProjectAccessRequestWithUserDetails } from '../types'
 import {
