b2394401092480a07c35bb05753c8d8c3388ec07
Liad Yosef <liad.yosef@gmail.com>
2024-03-18 10:38:38 +0200

    always give all permissions to the owner

--- utopia-remix/app/routes/v1.project.$id.permissions.tsx
+++ utopia-remix/app/routes/v1.project.$id.permissions.tsx
@@ -4,6 +4,7 @@ import type { Params } from '@remix-run/react'
 import { getAllPermissions } from '../services/permissionsService.server'
 import { ALLOW } from '../handlers/validators'
 import { Status } from '../util/statusCodes'
+import { getProjectOwnerById } from '../models/project.server'
 
 export async function loader(args: LoaderFunctionArgs) {
   return handle(args, {
@@ -17,6 +18,7 @@ async function getUserProjectPermissions(req: Request, params: Params<string>) {
   const userId = user?.user_id ?? null
   const { id } = params
   ensure(id != null, 'projectId is null', Status.BAD_REQUEST)
-  const permissionsResult = await getAllPermissions(id, userId)
-  return permissionsResult
+  const ownerId = await getProjectOwnerById({ id: id })
+  ensure(ownerId != null, `Project ${id} not found or has no owner`, Status.NOT_FOUND)
+  return getAllPermissions(id, userId, ownerId)
 }
