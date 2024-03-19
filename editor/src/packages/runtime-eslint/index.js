8bfe5b275404c1d87994689c464b1ba86a5bed84
Liad Yosef <liad.yosef@gmail.com>
2024-03-19 16:16:00 +0200

    types

--- editor/src/packages/runtime-eslint/index.js
+++ editor/src/packages/runtime-eslint/index.js
@@ -4,7 +4,7 @@ var __importDefault =
   function (mod) {
     return mod && mod.__esModule ? mod : { default: mod }
   }
-exports.__esModule = true
+Object.defineProperty(exports, '__esModule', { value: true })
 exports.BabelEslint =
   exports.Linter =
   exports.eslintImportRuleNoWebpackSyntax =
@@ -15,20 +15,35 @@ exports.BabelEslint =
   exports.reactPluginRules =
     void 0
 var eslint_plugin_react_1 = require('eslint-plugin-react')
-exports.reactPluginRules = eslint_plugin_react_1.rules
+Object.defineProperty(exports, 'reactPluginRules', {
+  enumerable: true,
+  get: function () {
+    return eslint_plugin_react_1.rules
+  },
+})
 var eslint_plugin_react_hooks_1 = require('eslint-plugin-react-hooks')
-exports.reactHooksPluginRules = eslint_plugin_react_hooks_1.rules
+Object.defineProperty(exports, 'reactHooksPluginRules', {
+  enumerable: true,
+  get: function () {
+    return eslint_plugin_react_hooks_1.rules
+  },
+})
 var eslint_plugin_jsx_a11y_1 = require('eslint-plugin-jsx-a11y')
-exports.jsxA11yPluginRules = eslint_plugin_jsx_a11y_1.rules
+Object.defineProperty(exports, 'jsxA11yPluginRules', {
+  enumerable: true,
+  get: function () {
+    return eslint_plugin_jsx_a11y_1.rules
+  },
+})
 var first_1 = __importDefault(require('eslint-plugin-import/lib/rules/first'))
-exports.eslintImportRuleFirst = first_1['default']
+exports.eslintImportRuleFirst = first_1.default
 var no_amd_1 = __importDefault(require('eslint-plugin-import/lib/rules/no-amd'))
-exports.eslintImportRuleNoAmd = no_amd_1['default']
+exports.eslintImportRuleNoAmd = no_amd_1.default
 var no_webpack_loader_syntax_1 = __importDefault(
   require('eslint-plugin-import/lib/rules/no-webpack-loader-syntax'),
 )
-exports.eslintImportRuleNoWebpackSyntax = no_webpack_loader_syntax_1['default']
+exports.eslintImportRuleNoWebpackSyntax = no_webpack_loader_syntax_1.default
 var eslint4b_1 = __importDefault(require('eslint4b'))
-exports.Linter = eslint4b_1['default']
+exports.Linter = eslint4b_1.default
 var babel_eslint_1 = __importDefault(require('babel-eslint'))
-exports.BabelEslint = babel_eslint_1['default']
+exports.BabelEslint = babel_eslint_1.default
