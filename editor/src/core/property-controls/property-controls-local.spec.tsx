abfb47cd1d144c343c23847aa943425d065dbaaa
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-03-19 13:19:35 +0100

    Adapt unit test to new registerComponent

--- editor/src/core/property-controls/property-controls-local.spec.tsx
+++ editor/src/core/property-controls/property-controls-local.spec.tsx
@@ -1,71 +1,71 @@
-import { PrettierConfig } from 'utopia-vscode-common'
-import { renderTestEditorWithCode, TestAppUID } from '../../components/canvas/ui-jsx.test-utils'
+import { renderTestEditorWithModel, TestAppUID } from '../../components/canvas/ui-jsx.test-utils'
 import { BakedInStoryboardUID } from '../model/scene-utils'
 import { TestScene0UID } from '../model/test-ui-js-file.test-utils'
-import * as Prettier from 'prettier/standalone'
 import { wait } from '../model/performance-scripts'
+import { createModifiedProject } from '../../sample-projects/sample-project-utils.test-utils'
+import { StoryboardFilePath } from '../../components/editor/store/editor-state'
+
+const project = createModifiedProject({
+  [StoryboardFilePath]: `import * as React from 'react'
+  import { Scene, Storyboard, View } from 'utopia-api'
+
+  export var App = (props) => {
+    return (
+      <div>hello</div>
+    )
+  }
+
+  export var storyboard = (props) => {
+    return (
+      <Storyboard data-uid='${BakedInStoryboardUID}'>
+        <Scene
+          style={{ position: 'absolute', left: 0, top: 0, width: 400, height: 400 }}
+          data-uid='${TestScene0UID}'
+        >
+          <App data-uid='${TestAppUID}' />
+        </Scene>
+      </Storyboard>
+    )
+  }`,
+  ['utopia/components.utopia.js']: `const Components = {
+    '/src/card': {
+      Card: {
+        supportsChildren: false,
+        properties: {
+          label: {
+            control: 'string-input',
+          },
+          background: {
+            control: 'color',
+          },
+          visible: {
+            control: 'checkbox',
+            defaultValue: true,
+          },
+        },
+        variants: [
+          {
+            code: '<Card />',
+            label: 'Card',
+          },
+          {
+            code: '<Card person={DefaultPerson} />',
+            label: 'ID Card',
+            additionalImports:
+              "import { DefaultPerson } from '/src/defaults';",
+          },
+        ],
+      },
+    },
+  }
+  
+  export default Components
+`,
+})
 
 describe('registered property controls', () => {
   it('registered controls with registerModule are in editor state', async () => {
-    const testCode = Prettier.format(
-      `
-        import * as React from 'react'
-        import { Scene, Storyboard, View, registerModule } from 'utopia-api'
-
-        export var App = (props) => {
-          return (
-            <div>hello</div>
-          )
-        }
-
-        registerModule(
-          '/src/card',
-          {
-            Card: {
-              supportsChildren: false,
-              properties: {
-                label: {
-                  control: 'string-input',
-                },
-                background: {
-                  control: 'color',
-                },
-                visible: {
-                  control: 'checkbox',
-                  defaultValue: true,
-                },
-              },
-              variants: [
-                {
-                  code: '<Card />',
-                  label: 'Card',
-                },
-                {
-                  code: '<Card person={DefaultPerson} />',
-                  label: 'ID Card',
-                  additionalImports: "import { DefaultPerson } from '/src/defaults';",
-                },
-              ],
-            },
-          }
-        )
-
-        export var storyboard = (props) => {
-          return (
-            <Storyboard data-uid='${BakedInStoryboardUID}'>
-              <Scene
-                style={{ position: 'absolute', left: 0, top: 0, width: 400, height: 400 }}
-                data-uid='${TestScene0UID}'
-              >
-                <App data-uid='${TestAppUID}' />
-              </Scene>
-            </Storyboard>
-          )
-        }`,
-      PrettierConfig,
-    )
-
-    const renderResult = await renderTestEditorWithCode(testCode, 'dont-await-first-dom-report')
+    const renderResult = await renderTestEditorWithModel(project, 'await-first-dom-report')
     await wait(10) // this is quite ugly but we want to wait for a timeout(0) in ui-jsx-canvas before calling validateControlsToCheck
     const editorState = renderResult.getEditorState().editor
 
