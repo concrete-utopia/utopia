c99efe4fdbe4b011f88d70d25fccb872aa73191d
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2024-03-19 11:05:19 +0000

    fix(env-vars) Fixed 2 URLs in env-vars.ts

--- website-next/components/common/env-vars.ts
+++ website-next/components/common/env-vars.ts
@@ -59,7 +59,7 @@ const SECONDARY_BASE_URL: string = PRODUCTION_CONFIG
   : BRANCHES_CONFIG
   ? 'https://momentumworks.co/'
   : BARE_HOST === 'localhost:8000'
-  ? 'http://localhost:8002'
+  ? 'http://localhost:8001'
   : BASE_URL
 
 export const PROBABLY_ELECTRON: boolean =
@@ -85,7 +85,7 @@ export const VSCODE_EDITOR_IFRAME_BASE_URL: string = PRODUCTION_CONFIG
   : STAGING_CONFIG
   ? 'https://utopia.pizza/'
   : BRANCHES_CONFIG
-  ? 'https://momentumworks.co/'
+  ? 'https://utopia.fish/'
   : BARE_HOST === 'localhost:8000' || BARE_HOST === 'localhost:8001'
   ? 'http://localhost:8000'
   : BASE_URL
