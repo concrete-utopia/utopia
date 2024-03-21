f1ee51471864e0e0204c3efc6ad3d2dc446e1025
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-21 17:11:07 +0100

    restore

--- utopia-remix/app/routes/internal.projects.$id.access.requests.tsx
+++ utopia-remix/app/routes/internal.projects.$id.access.requests.tsx
@@ -3,6 +3,7 @@ import { json } from '@remix-run/node'
 import type { Params } from '@remix-run/react'
 import { validateProjectAccess } from '../handlers/validators'
 import { listProjectAccessRequests } from '../models/projectAccessRequest.server'
+import { UserProjectPermission } from '../types'
 import { ensure, handle, requireUser } from '../util/api.server'
 import { Status } from '../util/statusCodes'
 
