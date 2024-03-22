93710592f56d9e4a7c10e90cc23dc8718de25401
Berci Kormendy <korber314@gmail.com>
2024-03-22 17:45:17 +0100

    wip - tests

--- editor/src/components/navigator/navigator.spec.browser2.tsx
+++ editor/src/components/navigator/navigator.spec.browser2.tsx
@@ -919,6 +919,109 @@ export default Components
 `,
   })
 
+const projectWithThirdPartyRenderProp = (renderPropSource: string) =>
+  createModifiedProject({
+    [StoryboardFilePath]: `import * as React from 'react'
+import * as Utopia from 'utopia-api'
+import {
+  Storyboard,
+  Scene,
+} from 'utopia-api'
+function Card({ header, children }) {
+  return (
+    <div data-uid='root'>
+      <h2>{header}</h2>
+      {children}
+    </div>
+  )
+}
+var Playground = ({ style }) => {
+  return (
+    <div style={style} data-uid='dbc'>
+      <Card ${renderPropSource} data-uid='78c'>
+        <p>Card contents</p>
+      </Card>
+    </div>
+  )
+}
+export var storyboard = (
+  <Storyboard data-uid='sb'>
+    <Scene
+      style={{
+        width: 521,
+        height: 266,
+        position: 'absolute',
+        left: 554,
+        top: 247,
+        backgroundColor: 'white',
+      }}
+      data-uid='scene'
+      data-testid='scene'
+      commentId='120'
+    >
+      <Playground
+        style={{
+          width: 454,
+          height: 177,
+          position: 'absolute',
+          left: 34,
+          top: 44,
+          backgroundColor: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        className='playground'
+        css={{ color: 'red' }}
+        data-uid='pg'
+      />
+    </Scene>
+  </Storyboard>
+)
+`,
+    ['/utopia/components.utopia.js']: `
+    // import { Title } from "../src/title"
+    
+    const Components = {
+      '/utopia/storyboard': {
+        Card: {
+          properties: {
+            header: {
+              control: 'jsx',
+              preferredChildComponents: [
+                {
+                  name: 'Heading',
+                  additionalImports: '../src/heading',
+                  variants: [
+                    {
+                      label: 'Heading',
+                      code: '<Heading>Title</Heading>',
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          supportsChildren: true,
+          variants: [],
+        },
+      },
+    }
+    
+    export default Components    
+`,
+    ['/src/heading.js']: `import React from 'react'
+export function Heading({ children }) {
+  return <h1 data-uid='708'>{children}</h1>
+}
+`,
+    ['/src/title.js']: `import React from 'react'
+export function Title({ children }) {
+return <h2 data-uid='708'>{children}</h2>
+}
+`,
+  })
+
 function getProjectCodeForMultipleSelection(): string {
   return `import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
@@ -5412,6 +5515,40 @@ describe('Navigator row order', () => {
   it('is correct for a project with elements with render prop set to `null`', async () => {
     const renderResult = await renderTestEditorWithModel(
       projectWithRenderProp('header={null}'),
+      'await-first-dom-report',
+    )
+
+    await renderResult.getDispatchFollowUpActionsFinished()
+
+    expect(renderResult.getEditorState().derived.navigatorTargets.map(navigatorEntryToKey)).toEqual(
+      [
+        'regular-sb/scene',
+        'regular-sb/scene/pg',
+        'regular-sb/scene/pg:dbc',
+        'regular-sb/scene/pg:dbc/78c',
+        'render-prop-sb/scene/pg:dbc/78c/prop-label-header-header',
+        'slot_sb/scene/pg:dbc/78c/prop-label-header', // <- the slot is shown
+        'render-prop-sb/scene/pg:dbc/78c/prop-label-children-children',
+        'regular-sb/scene/pg:dbc/78c/88b',
+      ],
+    )
+    expect(
+      renderResult.getEditorState().derived.visibleNavigatorTargets.map(navigatorEntryToKey),
+    ).toEqual([
+      'regular-sb/scene',
+      'regular-sb/scene/pg',
+      'regular-sb/scene/pg:dbc',
+      'regular-sb/scene/pg:dbc/78c',
+      'render-prop-sb/scene/pg:dbc/78c/prop-label-header-header',
+      'slot_sb/scene/pg:dbc/78c/prop-label-header', // <- the slot is shown
+      'render-prop-sb/scene/pg:dbc/78c/prop-label-children-children',
+      'regular-sb/scene/pg:dbc/78c/88b',
+    ])
+  })
+
+  it('is correct for a project where an import is present in the component registration', async () => {
+    const renderResult = await renderTestEditorWithModel(
+      projectWithThirdPartyRenderProp(''),
       'await-first-dom-report',
     )
 
