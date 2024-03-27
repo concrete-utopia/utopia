cdb2878d28dc85a7a61689dcc99c97a5d11700d2
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-27 13:13:55 +0100

    update imports

--- utopia-remix/app/routes/settings.tsx
+++ utopia-remix/app/routes/settings.tsx
@@ -1,10 +1,10 @@
 import type { LoaderFunctionArgs } from '@remix-run/node'
-import React from 'react'
-import { requireUser } from '../util/api.server'
 import { Link } from '@remix-run/react'
+import React from 'react'
 import urlJoin from 'url-join'
+import { useAppStore } from '../stores/appStore'
+import { requireUser } from '../util/api.server'
 import { auth0LoginURL } from '../util/auth0.server'
-import { useProjectsStore } from '../store'
 
 export async function loader(args: LoaderFunctionArgs) {
   await requireUser(args.request, { redirect: auth0LoginURL() })
@@ -12,7 +12,7 @@ export async function loader(args: LoaderFunctionArgs) {
 }
 
 const SettingsPage = React.memo(() => {
-  const env = useProjectsStore((store) => store.env)
+  const env = useAppStore((store) => store.env)
   return (
     <div>
       <Link to={urlJoin(env?.EDITOR_URL ?? '', '/logout')}>Log out</Link>
