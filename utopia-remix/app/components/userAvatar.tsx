32b448bc4e62ca54ed82ac56b9d9ab903a2d7b6a
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-03-28 09:36:47 +0100

    (chore) CDN link helpers (#5131)
    * cdn link helper
    
    * remove question marks

--- utopia-remix/app/components/userAvatar.tsx
+++ utopia-remix/app/components/userAvatar.tsx
@@ -2,10 +2,10 @@ import classNames from 'classnames'
 import type { CSSProperties } from 'react'
 import React from 'react'
 import { useIsDarkMode } from '../hooks/useIsDarkMode'
-import { useAppStore } from '../stores/appStore'
 import { sprinkles } from '../styles/sprinkles.css'
 import { when } from '../util/react-conditionals'
 import { multiplayerInitialsFromName } from '../util/strings'
+import { useCDNLink } from '../util/cdnLink'
 
 export const UserAvatar = React.memo(
   ({
@@ -23,11 +23,13 @@ export const UserAvatar = React.memo(
     style?: CSSProperties
     className?: string
   }) => {
-    const env = useAppStore((store) => store.env)
+    const cdnLink = useCDNLink()
+
     const isDarkMode = useIsDarkMode()
     const starIcon = React.useMemo(() => {
       return isDarkMode ? 'star-white-14x14@2x.png' : 'star-black-14x14@2x.png'
     }, [isDarkMode])
+
     return (
       <div
         className={classNames(sprinkles({ backgroundColor: 'secondary' }), className)}
@@ -50,7 +52,7 @@ export const UserAvatar = React.memo(
         {when(
           starBadge === true,
           <img
-            src={`${env?.UTOPIA_CDN_URL}/${starIcon}`}
+            src={cdnLink(starIcon)}
             style={{
               width: 13,
               height: 13,
