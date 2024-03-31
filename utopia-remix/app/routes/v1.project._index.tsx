a8f131096f3307f4a217e2bab11aea2316b46ac1
Liad Yosef <liad.yosef@gmail.com>
2024-04-01 01:08:31 +0300

    remove GET

--- utopia-remix/app/routes/v1.project._index.tsx
+++ utopia-remix/app/routes/v1.project._index.tsx
@@ -1,8 +1,7 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
-import { proxy } from '../util/proxy.server'
 import { handle, handleOptions } from '../util/api.server'
-import { AccessLevel, UserProjectPermission } from '../types'
-import { ALLOW, validateProjectAccess } from '../handlers/validators'
+import { AccessLevel } from '../types'
+import { ALLOW } from '../handlers/validators'
 import type { Params } from '@remix-run/react'
 import { createProjectAccess } from '../models/projectAccess.server'
 import { Status } from '../util/statusCodes'
@@ -13,13 +12,6 @@ import { action as createProjectInternal } from './v1.project.$id'
 export async function loader(args: LoaderFunctionArgs) {
   return handle(args, {
     OPTIONS: handleOptions,
-    GET: {
-      handler: proxy,
-      validator: validateProjectAccess(UserProjectPermission.CAN_VIEW_PROJECT, {
-        canRequestAccess: true,
-        getProjectId: (params) => params.id,
-      }),
-    },
   })
 }
 
