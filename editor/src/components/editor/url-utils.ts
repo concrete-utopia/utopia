1e2afd07b67c7018a74b86eb8b10f619ced21685
Liad Yosef <liad.yosef@gmail.com>
2024-04-04 16:55:29 +0300

    change boolean to be enum

--- editor/src/components/editor/url-utils.ts
+++ editor/src/components/editor/url-utils.ts
@@ -1,7 +1,10 @@
-export function readParamFromUrl(param: string, removeFromUrl: 'remove-from-url' | 'keep-url-intact' = 'keep-url-intact'): string | null {
+export function readParamFromUrl(
+  param: string,
+  removeFromUrl: 'remove-from-url' | 'keep-url-intact' = 'keep-url-intact',
+): string | null {
   const url = new URL(window.location.href)
   const result = url.searchParams.get(param)
-  if (removeFromUrl) {
+  if (removeFromUrl === 'remove-from-url') {
     url.searchParams.delete(param)
     window.history.replaceState({}, '', url.toString())
   }
