2ac48e83b4d412f620107028f621bfc61fa85641
Liad Yosef <liady@users.noreply.github.com>
2024-04-04 16:54:26 +0300

    Update editor/src/components/editor/url-utils.ts
    Co-authored-by: Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>

--- editor/src/components/editor/url-utils.ts
+++ editor/src/components/editor/url-utils.ts
@@ -1,4 +1,4 @@
-export function readParamFromUrl(param: string, removeFromUrl: boolean = false): string | null {
+export function readParamFromUrl(param: string, removeFromUrl: 'remove-from-url' | 'keep-url-intact' = 'keep-url-intact'): string | null {
   const url = new URL(window.location.href)
   const result = url.searchParams.get(param)
   if (removeFromUrl) {
