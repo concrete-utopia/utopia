12a2d9c5d9b5ae6565148a205ff64449941b54f7
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-04-09 23:01:17 +0200

    matching active path

--- editor/src/components/navigator/left-pane/pages-pane.tsx
+++ editor/src/components/navigator/left-pane/pages-pane.tsx
@@ -1,11 +1,47 @@
+import { useAtom } from 'jotai'
 import React from 'react'
+import { matchRoutes, type DataRouteObject } from 'react-router'
+import * as EP from '../../../core/shared/element-path'
 import { FlexColumn, FlexRow, UtopiaTheme, colorTheme } from '../../../uuiui'
+import { RemixIndexPathLabel } from '../../canvas/remix/remix-utils'
+import {
+  ActiveRemixSceneAtom,
+  RemixNavigationAtom,
+} from '../../canvas/remix/utopia-remix-root-component'
+import { Substores, useEditorState } from '../../editor/store/store-hook'
+
+const sortByFilename = (a: DataRouteObject, b: DataRouteObject) =>
+  a.path?.localeCompare(b.path ?? '') ?? 0
 
 export const PagesPane = React.memo((props) => {
+  const remixRoutes: Array<DataRouteObject> = useEditorState(
+    Substores.derived,
+    (store) =>
+      (store.derived.remixData?.routes ?? [])
+        .flatMap((route) => route.children ?? [])
+        .sort(sortByFilename),
+    'PagesPane remixRoutes',
+  )
+
+  const [navigationControls] = useAtom(RemixNavigationAtom)
+  const [activeRemixScene] = useAtom(ActiveRemixSceneAtom)
+
+  const pathname = navigationControls[EP.toString(activeRemixScene)]?.location?.pathname ?? ''
+
+  const matchResult = matchRoutes(remixRoutes, pathname)
+
   return (
     <FlexColumn style={{ height: '100%', overflowY: 'scroll' }}>
-      {['1', '2', '3'].map((pageRoute: string) => {
-        return <PageRouteEntry key={pageRoute} filepath={pageRoute} active={pageRoute === '1'} />
+      {remixRoutes.map((route: DataRouteObject, index) => {
+        const { path } = route
+        const pathToDisplay = path ?? RemixIndexPathLabel
+        return (
+          <PageRouteEntry
+            key={path}
+            filepath={pathToDisplay}
+            active={matchResult?.[0].route.path === path}
+          />
+        )
       })}
     </FlexColumn>
   )
@@ -19,6 +55,7 @@ const PageRouteEntry = React.memo<PageRouteEntryProps>((props) => {
   return (
     <FlexRow
       style={{
+        flexShrink: 0,
         color: colorTheme.neutralForeground.value,
         backgroundColor: props.active ? colorTheme.subtleBackground.value : 'transparent',
         marginLeft: 8,
@@ -44,7 +81,7 @@ const PageRouteEntry = React.memo<PageRouteEntryProps>((props) => {
           textOverflow: 'ellipsis',
         }}
       >
-        {props.filepath}
+        {props.filepath === '' ? '(index)' : props.filepath}
       </span>
     </FlexRow>
   )
