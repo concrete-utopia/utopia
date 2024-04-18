ca9ec604f4223ac87ffbec46ac8fdc91def225ad
lankaukk <lankaukk@yahoo.com>
2024-04-18 11:09:08 -0400

    update snapshots

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -183,7 +183,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`541`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`517`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`631`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`607`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
