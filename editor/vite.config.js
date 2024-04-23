9c52a92d161b69d8ee66de7bcb4bd139eea3e685
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-04-23 15:29:47 +0200

    vite fix

--- editor/vite.config.js
+++ editor/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
     publicDir: '../../resources/editor',
     server: {
       port: 8088,
-      host: '0.0.0.0',
+      host: '127.0.0.1',
       fs: {
         allow: [
           '../..',
@@ -34,7 +34,7 @@ export default defineConfig(({ mode }) => {
       },
       hmr: {
         protocol: 'ws',
-        host: '0.0.0.0',
+        host: '127.0.0.1',
         port: 8088,
       },
     },
