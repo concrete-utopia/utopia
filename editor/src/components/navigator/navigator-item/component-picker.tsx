1d1fee9dd9dc225ef096c8871374d21183b67e69
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-04-24 11:59:51 +0200

    Delete unused code

--- editor/src/components/navigator/navigator-item/component-picker.tsx
+++ editor/src/components/navigator/navigator-item/component-picker.tsx
@@ -2,24 +2,18 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react'
 import React from 'react'
-import { Icons, UtopiaTheme, useColorTheme } from '../../../uuiui'
+import { Icons } from '../../../uuiui'
 import { dark } from '../../../uuiui/styles/theme/dark'
-import { capitalize } from '../../../core/shared/string-utils'
-import { type PreferredChildComponentDescriptor } from '../../custom-code/internal-property-controls'
-import { jsxElementWithoutUID, type JSXElementChild } from '../../../core/shared/element-template'
+import type { JSXElementChild } from '../../../core/shared/element-template'
 import { type Imports } from '../../../core/shared/project-file-types'
 import { elementFromInsertMenuItem } from '../../editor/insert-callbacks'
 import { componentInfo, type ComponentInfo } from '../../custom-code/code-file'
-import { when } from '../../../utils/react-conditionals'
-import { defaultImportsForComponentModule } from '../../../core/property-controls/property-controls-local'
-import type { InsertMenuItem, InsertMenuItemGroup } from '../../canvas/ui/floating-insert-menu'
-import { assertNever } from '../../../core/shared/utils'
+import type { InsertMenuItemGroup } from '../../canvas/ui/floating-insert-menu'
 import { UIGridRow } from '../../../components/inspector/widgets/ui-grid-row'
 import { FlexRow } from 'utopia-api'
 
 export interface ComponentPickerProps {
   insertionTargetName: string
-  preferredComponents: Array<PreferredChildComponentDescriptor>
   allComponents: Array<InsertMenuItemGroup>
   onItemClick: (elementToInsert: ElementToInsert) => React.MouseEventHandler
   onClickCloseButton?: React.MouseEventHandler
@@ -43,16 +37,7 @@ export function componentPickerOptionTestId(componentName: string, variant?: str
 }
 
 export const ComponentPicker = React.memo((props: ComponentPickerProps) => {
-  const colorTheme = useColorTheme()
-  const [selectedTab, setSelectedTab] = React.useState<'preferred' | 'all'>('preferred')
   const [filter, setFilter] = React.useState<string>('')
-
-  const preferredComponentsToShow =
-    filter.trim() === ''
-      ? props.preferredComponents
-      : props.preferredComponents.filter((v) =>
-          v.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase().trim()),
-        )
 
   const allComponentsToShow: InsertMenuItemGroup[] = []
 
@@ -81,12 +66,7 @@ export const ComponentPicker = React.memo((props: ComponentPickerProps) => {
       }}
       data-testId={componentPickerTestIdForProp(props.insertionTargetName)}
     >
-      <ComponentPickerTopSection
-        targetProp={props.insertionTargetName}
-        onFilterChange={setFilter}
-        onSelectTab={setSelectedTab}
-        onClickCloseButton={props.onClickCloseButton}
-      />
+      <ComponentPickerTopSection onFilterChange={setFilter} />
       <ComponentPickerComponentSection
         components={componentsToShow}
         onItemClick={props.onItemClick}
@@ -96,27 +76,11 @@ export const ComponentPicker = React.memo((props: ComponentPickerProps) => {
 })
 
 interface ComponentPickerTopSectionProps {
-  targetProp: string
   onFilterChange: (filter: string) => void
-  onSelectTab: (tab: 'preferred' | 'all') => void
-  onClickCloseButton?: React.MouseEventHandler
 }
 
 const ComponentPickerTopSection = React.memo((props: ComponentPickerTopSectionProps) => {
-  const { targetProp, onFilterChange, onSelectTab, onClickCloseButton } = props
-  const [selectedTab, setSelectedTabState] = React.useState<'preferred' | 'all'>('preferred')
-  const setSelectedTab = React.useCallback(
-    (tab: 'preferred' | 'all') => {
-      setSelectedTabState(tab)
-      onSelectTab(tab)
-    },
-    [onSelectTab],
-  )
-  const switchToPreferredTab = React.useCallback(
-    () => setSelectedTab('preferred'),
-    [setSelectedTab],
-  )
-  const switchToAllTab = React.useCallback(() => setSelectedTab('all'), [setSelectedTab])
+  const { onFilterChange } = props
 
   return (
     <div
@@ -131,73 +95,11 @@ const ComponentPickerTopSection = React.memo((props: ComponentPickerTopSectionPr
   )
 })
 
-interface PickerTabButtonProps {
-  title: string
-  isSelected: boolean
-  onClick: React.MouseEventHandler
-}
-
-const PickerTabButton = React.memo((props: PickerTabButtonProps) => {
-  const colorTheme = useColorTheme()
-  const { title, isSelected, onClick } = props
-  return (
-    <div
-      style={{
-        fontWeight: 600,
-        color: isSelected ? colorTheme.black.value : colorTheme.subduedForeground.value,
-        cursor: 'pointer',
-      }}
-      onClick={onClick}
-    >
-      {title}
-    </div>
-  )
-})
-
-interface PickerPropLabelProps {
-  targetProp: string
-}
-
-const PickerPropLabel = React.memo((props: PickerPropLabelProps) => {
-  const { targetProp } = props
-  const colorTheme = useColorTheme()
-
-  return (
-    <div
-      style={{
-        border: '1px solid rgb(0, 0, 0, 1)',
-        borderRadius: 3,
-        height: 21,
-        contain: 'layout',
-      }}
-    >
-      <div
-        style={{
-          border: '1px solid rgb(0, 0, 0, 1)',
-          height: 21,
-          borderRadius: 3,
-          padding: 3,
-          margin: -1, // Honestly I give up
-          position: 'relative',
-          left: 3,
-          top: -2,
-          lineHeight: 'normal',
-          backgroundColor: colorTheme.white.value,
-        }}
-        data-testId={`${componentPickerTestIdForProp(targetProp)}-prop-field`}
-      >
-        {capitalize(targetProp)}
-      </div>
-    </div>
-  )
-})
-
 interface FilterBarProps {
   onFilterChange: (filter: string) => void
 }
 
 const FilterBar = React.memo((props: FilterBarProps) => {
-  const colorTheme = useColorTheme()
   const { onFilterChange } = props
 
   const [filter, setFilterState] = React.useState<string>('')
@@ -278,41 +180,6 @@ const ComponentPickerComponentSection = React.memo(
         })}
       </div>
     )
-
-    //   return (
-    //     <div
-    //       style={{
-    //         padding: 16,
-    //         display: 'flex',
-    //         flexDirection: 'column',
-    //         width: '100%',
-    //         height: 'max-content',
-    //         overflowY: 'auto',
-    //         gap: 10,
-    //       }}
-    //     >
-    //       {components.type === 'component-descriptor'
-    //         ? components.value.map((comp) => {
-    //             return (
-    //               <ComponentPickerOption
-    //                 key={`${comp.name}-label`}
-    //                 component={{ type: 'component-descriptor', value: comp }}
-    //                 onItemClick={onItemClick}
-    //               />
-    //             )
-    //           })
-    //         : components.value.map((comp) => {
-    //             return (
-    //               <ComponentPickerOption
-    //                 key={`${comp.label}-label`}
-    //                 component={{ type: 'insert-menu-item', value: comp }}
-    //                 onItemClick={onItemClick}
-    //               />
-    //             )
-    //           })}
-    //     </div>
-    //   )
-    // },
   },
 )
 
@@ -326,7 +193,6 @@ function variantsForComponent(component: InsertMenuItemGroup): ComponentInfo[] {
 }
 
 const ComponentPickerOption = React.memo((props: ComponentPickerOptionProps) => {
-  const colorTheme = useColorTheme()
   const { component, onItemClick } = props
 
   const variants = variantsForComponent(component)
@@ -372,50 +238,6 @@ const ComponentPickerOption = React.memo((props: ComponentPickerOptionProps) => 
           </UIGridRow>
         </FlexRow>
       ))}
-    </div>
-  )
-})
-
-interface ComponentPickerVariantProps {
-  componentName: string
-  variant: ComponentInfo
-  onItemClick: (elementToInsert: ElementToInsert) => React.MouseEventHandler
-}
-
-const ComponentPickerVariant = React.memo((props: ComponentPickerVariantProps) => {
-  const colorTheme = useColorTheme()
-  const { onItemClick, variant, componentName } = props
-
-  return (
-    <div
-      onClick={onItemClick({
-        elementToInsert: (uid) => elementFromInsertMenuItem(variant.elementToInsert(), uid),
-        additionalImports: variant.importsToAdd,
-      })}
-      css={{
-        backgroundColor: colorTheme.bg5.value,
-        paddingTop: 5,
-        paddingRight: 5,
-        paddingBottom: 5,
-        paddingLeft: 5,
-        borderTopLeftRadius: 3,
-        borderTopRightRadius: 3,
-        borderBottomRightRadius: 3,
-        borderBottomLeftRadius: 3,
-        // TODO BRANCH this needs to go since (empty) should just be the component name
-        // once we've stratified this
-        color:
-          variant.insertMenuLabel === '(empty)'
-            ? colorTheme.subduedForeground.value
-            : colorTheme.black.value,
-        '&:hover': {
-          backgroundColor: colorTheme.dynamicBlue10.value,
-        },
-        cursor: 'pointer',
-      }}
-      data-testId={componentPickerOptionTestId(componentName, variant.insertMenuLabel)}
-    >
-      {variant.insertMenuLabel}
     </div>
   )
 })
