c798efa0c05cfa8b2c2e15b2c2052a84807c3ac2
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-05-04 10:18:53 +0200

    Update performance-regression-tests.spec.tsx

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`620`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`615`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
