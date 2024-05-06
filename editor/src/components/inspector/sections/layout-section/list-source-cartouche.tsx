04252f4cc6a185bae098a87cce10fa55872ffd0c
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-05-06 09:39:47 +0200

    removing unused imports

--- editor/src/components/inspector/sections/layout-section/list-source-cartouche.tsx
+++ editor/src/components/inspector/sections/layout-section/list-source-cartouche.tsx
@@ -1,43 +1,15 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */ import { jsx } from '@emotion/react'
-import createCachedSelector from 're-reselect'
 import React from 'react'
-import { usePopper } from 'react-popper'
-import type { ArrayControlDescription } from 'utopia-api/core'
 import { MetadataUtils } from '../../../../core/model/element-metadata-utils'
-import { mapDropNulls } from '../../../../core/shared/array-utils'
-import { isLeft } from '../../../../core/shared/either'
-import * as EP from '../../../../core/shared/element-path'
-import type {
-  ElementInstanceMetadata,
-  ElementInstanceMetadataMap,
-  JSXMapExpression,
-} from '../../../../core/shared/element-template'
-import { isJSXMapExpression } from '../../../../core/shared/element-template'
 import type { ElementPath } from '../../../../core/shared/project-file-types'
 import { NO_OP } from '../../../../core/shared/utils'
-import type { JSXParsedValue } from '../../../../utils/value-parser-utils'
-import {
-  Button,
-  FlexRow,
-  Icn,
-  InspectorSectionIcons,
-  UtopiaTheme,
-  useColorTheme,
-} from '../../../../uuiui'
 import { Substores, useEditorState } from '../../../editor/store/store-hook'
-import type { MetadataSubstate } from '../../../editor/store/store-hook-substore-types'
-import { UIGridRow } from '../../widgets/ui-grid-row'
-import { DataPickerPopupButtonTestId } from '../component-section/component-section'
-import { DataPickerPopup, dataPickerForAnElement } from '../component-section/data-picker-popup'
 import {
   DataCartoucheInner,
   getTextContentOfElement,
 } from '../component-section/data-reference-cartouche'
-import { JSXPropertyControlForListSection } from '../component-section/property-control-controls'
-import { useVariablesInScopeForSelectedElement } from '../component-section/variables-in-scope-utils'
 import { mapExpressionValueToMapSelector, useDataPickerButtonListSource } from './list-section'
-import { NavigatorEntry } from '../../../editor/store/editor-state'
 
 interface MapListSourceCartoucheProps {
   target: ElementPath
