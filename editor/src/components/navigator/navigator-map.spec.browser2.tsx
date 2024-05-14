2b46016977a62ee2e9059e626221a7f736ee3d82
lankaukk <lankaukk@yahoo.com>
2024-05-14 16:37:45 -0400

    fix test

--- editor/src/components/navigator/navigator-map.spec.browser2.tsx
+++ editor/src/components/navigator/navigator-map.spec.browser2.tsx
@@ -75,7 +75,6 @@ export var ${BakedInStoryboardVariableName} = (
 
 function expectRegularCounterWithCount(domElement: HTMLElement, count: number) {
   expect(domElement.textContent).toEqual(count.toString())
-  expect(domElement.style.color).toEqual('var(--utopitheme-white)')
 }
 
 function expectOverriddenCounterWithCount(
