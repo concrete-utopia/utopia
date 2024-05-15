ec411646e6c23b0f76a4fcfc6f1126a33661e500
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-05-15 17:18:05 +0200

    hash with 128

--- editor/src/core/shared/hash.ts
+++ editor/src/core/shared/hash.ts
@@ -1,6 +1,6 @@
-import { murmur3 } from 'murmurhash-js'
+import * as murmur from 'murmurhash3js'
 
 export function hashObject(input: any): string {
   const data = JSON.stringify(input)
-  return murmur3(data).toString(16)
+  return murmur.x86.hash128(data)
 }
