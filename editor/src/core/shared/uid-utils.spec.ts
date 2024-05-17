a7d9ebc82fe11daabf95086737eafd2824877a4d
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-05-17 15:07:00 +0200

    fix

--- editor/src/core/shared/uid-utils.spec.ts
+++ editor/src/core/shared/uid-utils.spec.ts
@@ -1,19 +1,6 @@
 import type { HighlightBoundsForUids } from './project-file-types'
 import type { UIDMappings } from './uid-utils'
-import { generateConsistentUID, updateHighlightBounds } from './uid-utils'
-
-let someExistingIDs: Set<string> = new Set(['aaa', 'pqr'])
-
-describe('generateConsistentUID', () => {
-  it('if the starting value is unused return it', () => {
-    const actualResult = generateConsistentUID('xyz')
-    expect(actualResult).toBe('xyz')
-  })
-  it('if the starting value is used generate another', () => {
-    const actualResult = generateConsistentUID('pqr')
-    expect(actualResult).toBe('aab')
-  })
-})
+import { updateHighlightBounds } from './uid-utils'
 
 describe('updateHighlightBounds', () => {
   it('caters for mappings which could destroy each other', () => {
