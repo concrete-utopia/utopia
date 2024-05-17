d07fad4864803556521426becdce72373bd4e3c8
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-05-17 14:34:34 +0200

    Source map caching

--- editor/src/third-party/react-error-overlay/utils/parser.ts
+++ editor/src/third-party/react-error-overlay/utils/parser.ts
@@ -6,8 +6,12 @@
  */
 
 /* @flow */
+import { SOURCE_MAP_SEPARATOR, SourceMapCache } from '../../../core/shared/code-exec-utils'
+import { RawSourceMap } from '../../../core/workers/ts/ts-typings/RawSourceMap'
+import { generateUUID } from '../../../utils/utils'
+import { extractSourceMapUrl } from './getSourceMap'
 import StackFrame from './stack-frame'
-
+import { strToBase64, base64ToStr } from '@root/encoding/base64'
 const regexExtractLocation = /\(?(.+?)(?::(\d+))?(?::(\d+))?\)?$/
 
 function extractLocation(token: string): [string, number, number] {
@@ -26,15 +30,25 @@ function extractLocation(token: string): [string, number, number] {
 const regexValidFrame_Chrome = /^\s*(at|in)\s.+(:\d+)/
 const regexValidFrame_FireFox = /(^|@)\S+:\d+|.+line\s+\d+\s+>\s+(eval|Function).+/
 
-const maxParseableErrorLength = 10000
+function replaceBase64SourceMapWithCacheKey(
+  stackLine: string,
+  sourceMapCache: SourceMapCache,
+): string {
+  const splitStackLine = stackLine.split(SOURCE_MAP_SEPARATOR)
+  if (splitStackLine.length < 3) {
+    return stackLine
+  }
+  const uid = generateUUID()
+  sourceMapCache[uid] = JSON.parse(base64ToStr(splitStackLine[1]))
+  return `${splitStackLine[0]}${SOURCE_MAP_SEPARATOR}${uid}${splitStackLine[2]}`
+}
 
-export function parseStack(stack: string[]): StackFrame[] {
+export function parseStack(stack: string[], sourceMapCache: SourceMapCache): StackFrame[] {
   const frames = stack
-    .filter(
-      (e) =>
-        e.length < maxParseableErrorLength &&
-        (regexValidFrame_Chrome.test(e) || regexValidFrame_FireFox.test(e)),
-    )
+    // Replace the full source maps in the lines (encoded with base64) with a unique identifier and put the sourcemap into a cache.
+    // This is needed because the regular expression operations later are too slow for really long lines.
+    .map((e) => replaceBase64SourceMapWithCacheKey(e, sourceMapCache))
+    .filter((e) => regexValidFrame_Chrome.test(e) || regexValidFrame_FireFox.test(e))
     .map((e) => {
       if (regexValidFrame_FireFox.test(e)) {
         // Strip eval, we don't care about it
@@ -68,18 +82,21 @@ export function parseStack(stack: string[]): StackFrame[] {
  * Turns an <code>Error</code>, or similar object, into a set of <code>StackFrame</code>s.
  * @alias parse
  */
-function parseError(error: Error | string | string[] | { stack: string }): StackFrame[] {
+function parseError(
+  error: Error | string | string[] | { stack: string },
+  sourceMapCache: SourceMapCache,
+): StackFrame[] {
   if (error == null) {
     throw new Error('You cannot pass a null object.')
   }
   if (typeof error === 'string') {
-    return parseStack(error.split('\n'))
+    return parseStack(error.split('\n'), sourceMapCache)
   }
   if (Array.isArray(error)) {
-    return parseStack(error)
+    return parseStack(error, sourceMapCache)
   }
   if (typeof error.stack === 'string') {
-    return parseStack(error.stack.split('\n'))
+    return parseStack(error.stack.split('\n'), sourceMapCache)
   }
   throw new Error('The error you provided does not contain a stack trace.')
 }
