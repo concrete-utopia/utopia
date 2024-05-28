e95eae7c817ccb6ef467089a2e31c4bdeefec8b9
Berci Kormendy <korber314@gmail.com>
2024-05-28 11:47:50 +0200

    update snapshots

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`894`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`897`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`636`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`638`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
