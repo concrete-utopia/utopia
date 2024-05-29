5c9d7a50c0a6a11ec28c18ac149838ecf43ce6cc
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-05-29 16:44:41 +0200

    update snaps

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`900`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`906`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`639`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`643`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
