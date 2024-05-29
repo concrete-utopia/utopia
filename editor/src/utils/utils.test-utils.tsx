cf8961acd06fa71379ba97749b97042f213b20f0
Sean Parsons <git@futurenotfound.com>
2024-05-29 15:21:44 +0100

    fix(inspector) When loading component descriptors, take account of file mappings. - `importedFromWhere` now invokes `applyFilePathMappingsToFilePath`   for the path that it returns so that it applies the path mapping rules to the   results gained. - Added a call to `getFilePathMappings` in a few places to support a call to   `getComponentDescriptorForTarget`.

--- editor/src/utils/utils.test-utils.tsx
+++ editor/src/utils/utils.test-utils.tsx
@@ -107,6 +107,7 @@ export const testRenderContext: RenderContext = {
   highlightBounds: null,
   editedText: null,
   variablesInScope: {},
+  filePathMappings: [],
 }
 
 export function delay(time: number): Promise<void> {
