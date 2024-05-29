1cb14efe1473066b50bcc6f33dbba9912de5b1c0
Bertalan Körmendy <korber314@gmail.com>
2024-05-29 15:44:57 +0200

    Data Selector Popup (#5755)
    * wip - with static data
    
    * rows
    
    * wip - clicking on stuff
    
    * correct navigation
    
    * update variable on apply
    
    * use correct colors
    
    * do not traverse when clicking in the left hand column
    
    * disable back button when current value path is empty
    
    * only traverse on double click
    
    * focusedVariableChildren is no longer a state
    
    * separating selected vs navigatedTo path
    
    * showing the selected path in the breadcrumbs
    
    * ElementSelector uses zero-based indexes
    
    * delete selection on navigation
    
    * add role to data reference cartouche
    
    * use the cartouche ui
    
    * top bar for primitive variables
    
    * show selection on cartouche
    
    * grid column
    
    * temporarily disable the back button
    
    * border color + selected
    
    * assorted navigation fixes
    
    * home button too
    
    * be able to select primitiveVars and folderVars
    
    * tooltip wraps icon too
    
    * implementing CartoucheUI datatype prop
    
    * subdue the "folder" border color
    
    * hover
    
    * removing currentHoveredPath
    
    * apply button
    
    * using mckayla's new icons
    
    * introducing pickTargetType
    
    * Revert "introducing pickTargetType"
    
    This reverts commit 42e87035a8f6f1657a80de9561728a8c99120779.
    
    * only matching elements get a cartouche
    
    * wip - value preview styles
    
    * do not show monospaced font for information role cartouche
    
    * wip - top bar
    
    * Fix to X
    
    * tweaking the breadcrumbs to look like an input field
    
    * tweaking the "input field" css
    
    * the valuePreviewText observes hover
    
    * organize imports
    
    * Update data-selector-modal.tsx
    
    * Update data-selector-modal.tsx
    
    * Update data-selector-modal.tsx
    
    * Update data-selector-modal.tsx
    
    * basc test
    
    * update snapshots
    
    * cleanup
    
    * Update data-selector-modal.tsx
    
    * check children length in childvars
    
    ---------
    
    Co-authored-by: Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>

--- editor/src/components/inspector/sections/component-section/cartouche-ui.tsx
+++ editor/src/components/inspector/sections/component-section/cartouche-ui.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
-import { FlexRow, Icn, Icons, Tooltip, UtopiaStyles, useColorTheme } from '../../../../uuiui'
+import type { IcnColor } from '../../../../uuiui'
+import { FlexRow, Icn, Tooltip, UtopiaStyles, useColorTheme } from '../../../../uuiui'
 import { when } from '../../../../utils/react-conditionals'
+
+export interface HoverHandlers {
+  onMouseEnter: (e: React.MouseEvent) => void
+  onMouseLeave: (e: React.MouseEvent) => void
+}
 
 export type CartoucheUIProps = React.PropsWithChildren<{
   tooltip: string
   source: 'internal' | 'external' | 'literal'
+  role: 'selection' | 'information' | 'folder'
+  datatype: 'renderable' | 'boolean' | 'array' | 'object'
   inverted: boolean
   selected: boolean
   testId: string
+  preview?: boolean
   onDelete?: (e: React.MouseEvent) => void
   onClick?: (e: React.MouseEvent) => void
   onDoubleClick?: (e: React.MouseEvent) => void
+  onHover?: HoverHandlers
 }>
 
 export const CartoucheUI = React.forwardRef(
   (props: CartoucheUIProps, ref: React.Ref<HTMLDivElement>) => {
-    const { tooltip, onClick, onDoubleClick, onDelete, children, source, inverted, selected } =
-      props
+    const {
+      tooltip,
+      onClick,
+      onDoubleClick,
+      onDelete,
+      children,
+      source,
+      inverted,
+      selected,
+      role,
+      datatype,
+      onHover,
+      preview = false,
+    } = props
 
     const colorTheme = useColorTheme()
 
@@ -30,6 +52,8 @@ export const CartoucheUI = React.forwardRef(
 
     const borderColor = inverted
       ? colorTheme.white.value
+      : role === 'folder' && !selected
+      ? colorTheme.verySubduedForeground.value
       : source === 'external'
       ? colorTheme.green.value
       : colorTheme.selectionBlue.value
@@ -42,44 +66,61 @@ export const CartoucheUI = React.forwardRef(
 
     const foregroundColor = inverted
       ? colorTheme.white.value
-      : source === 'literal'
+      : source === 'literal' || role === 'information' || role === 'folder'
       ? colorTheme.neutralForeground.value
       : primaryForegoundColorToUse
 
     const backgroundColor =
-      source === 'literal' ? colorTheme.fg0Opacity10.value : primaryBackgroundColorToUse
+      role === 'information' || role === 'folder'
+        ? colorTheme.neutralBackground.value
+        : source === 'literal'
+        ? colorTheme.fg0Opacity10.value
+        : primaryBackgroundColorToUse
 
     const wrappedOnClick = useStopPropagation(onClick)
     const wrappedOnDoubleClick = useStopPropagation(onDoubleClick)
     const wrappedOnDelete = useStopPropagation(onDelete)
 
+    const shouldShowBorder = selected || role === 'folder'
+
     return (
       <div
         onClick={wrappedOnClick}
         onDoubleClick={wrappedOnDoubleClick}
+        onMouseEnter={onHover?.onMouseEnter}
+        onMouseLeave={onHover?.onMouseLeave}
         style={{
           minWidth: 0, // this ensures that the div can never expand the allocated grid space
         }}
         ref={ref}
       >
-        <FlexRow
-          style={{
-            cursor: 'pointer',
-            fontSize: 10,
-            fontWeight: 400,
-            color: foregroundColor,
-            backgroundColor: backgroundColor,
-            border: selected ? '1px solid ' + borderColor : '1px solid transparent',
-            padding: '0px 6px 0 4px',
-            borderRadius: 4,
-            height: 20,
-            display: 'flex',
-            flex: 1,
-            gap: 4,
-          }}
-        >
-          {source === 'literal' ? null : <Icons.NavigatorData color={cartoucheIconColor} />}
-          <Tooltip title={tooltip}>
+        <Tooltip title={tooltip}>
+          <FlexRow
+            style={{
+              cursor: 'pointer',
+              fontSize: 10,
+              fontWeight: 400,
+              color: foregroundColor,
+              backgroundColor: backgroundColor,
+              border: shouldShowBorder ? '1px solid ' + borderColor : '1px solid transparent',
+              padding: '0px 6px 0 4px',
+              borderRadius: 4,
+              height: 20,
+              display: 'flex',
+              flex: 1,
+              gap: 4,
+              opacity: preview ? 0.5 : 1,
+            }}
+          >
+            {source === 'literal' ? null : (
+              <Icn
+                category='navigator-element'
+                type={dataTypeToIconType(datatype)}
+                color={cartoucheIconColor}
+                width={12}
+                height={12}
+              />
+            )}
             <div
               style={{
                 flex: 1,
@@ -92,31 +133,49 @@ export const CartoucheUI = React.forwardRef(
                 /* Beginning of string */
                 direction: source === 'literal' ? 'ltr' : 'rtl', // TODO we need a better way to ellipsize the beginnign because rtl eats ' " marks
                 textAlign: 'left',
-                ...UtopiaStyles.fontStyles.monospaced,
+                ...(role !== 'information' ? UtopiaStyles.fontStyles.monospaced : {}),
               }}
             >
               {children}
               &lrm;
               {/* the &lrm; non-printing character is added to fix the punctuation marks disappearing because of direction: rtl */}
             </div>
-          </Tooltip>
-          {when(
-            onDelete != null,
-            <Icn
-              category='semantic'
-              type='cross'
-              color={cartoucheIconColor}
-              width={12}
-              height={12}
-              data-testid={`delete-${props.testId}`}
-              onClick={wrappedOnDelete}
-            />,
-          )}
-        </FlexRow>
+            {when(
+              datatype === 'object' && role === 'folder',
+              // a trailing ellipsis is added to indicate that the object can be traversed
+              <span>…</span>,
+            )}
+            {when(
+              onDelete != null,
+              <Icn
+                category='semantic'
+                type='cross'
+                color={cartoucheIconColor}
+                width={12}
+                height={12}
+                data-testid={`delete-${props.testId}`}
+                onClick={wrappedOnDelete}
+              />,
+            )}
+          </FlexRow>
+        </Tooltip>
       </div>
     )
   },
 )
+
+function dataTypeToIconType(dataType: CartoucheUIProps['datatype']): string {
+  switch (dataType) {
+    case 'renderable':
+      return 'data'
+    case 'boolean':
+      return '👻'
+    case 'array':
+      return 'array'
+    case 'object':
+      return 'object'
+  }
+}
 
 function useStopPropagation(callback: ((e: React.MouseEvent) => void) | undefined) {
   return React.useCallback(
