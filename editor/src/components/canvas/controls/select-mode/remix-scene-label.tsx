69f6fd90fcfaa6ec141dba50acc19f7d3ffc5b6d
lankaukk <lankaukk@yahoo.com>
2024-05-31 16:04:46 -0400

    remove unused import

--- editor/src/components/canvas/controls/select-mode/remix-scene-label.tsx
+++ editor/src/components/canvas/controls/select-mode/remix-scene-label.tsx
@@ -9,7 +9,7 @@ import {
 import type { ElementPath } from '../../../../core/shared/project-file-types'
 import { NO_OP } from '../../../../core/shared/utils'
 import { Modifier } from '../../../../utils/modifiers'
-import { FlexRow, Icn, Tooltip, useColorTheme } from '../../../../uuiui'
+import { FlexRow, Tooltip, useColorTheme } from '../../../../uuiui'
 import { clearHighlightedViews, selectComponents } from '../../../editor/actions/action-creators'
 import { useDispatch } from '../../../editor/store/dispatch-context'
 import { Substores, useEditorState, useRefEditorState } from '../../../editor/store/store-hook'
