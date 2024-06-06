93677438fe46dbfd9f1a9b4548146fe919adaad0
Liad Yosef <liady@users.noreply.github.com>
2024-06-06 12:47:04 +0300

    chore: re-export relevant types from utopia-shared (#5811)
    **Problem:**
    as we moved types from our editor to `utopia-shared`- we need to make
    sure they don't diverge.
    
    **Fix:**
    since a lot of places in the code currently import those files, we make
    sure that `element-template.ts` in the editor re-exports the source of
    truth types from `utopia-shared`
    
    **Manual Tests:**
    I hereby swear that:
    
    - [X] I opened a hydrogen project and it loaded
    - [X] I could navigate to various routes in Preview mode

--- utopia-shared/src/types/element-template.ts
+++ utopia-shared/src/types/element-template.ts
@@ -1,4 +1,5 @@
 import type { PropertyPath } from './project-file-types'
+import { RawSourceMap } from './RawSourceMap'
 
 export interface ParsedComments {
   leadingComments: Array<Comment>
@@ -66,12 +67,9 @@ export interface JSOpaqueArbitraryStatement {
   uid: string
 }
 
-export interface JSAssignment<
-  L extends JSIdentifier = JSIdentifier,
-  R extends JSExpression = JSExpression,
-> {
+export interface JSAssignment<R extends JSExpression = JSExpression> {
   type: 'JS_ASSIGNMENT'
-  leftHandSide: L
+  leftHandSide: BoundParam
   rightHandSide: R
 }
 
@@ -93,17 +91,6 @@ export interface JSExpressionOtherJavaScript extends WithComments, WithElementsW
   definedElsewhere: Array<string>
   sourceMap: RawSourceMap | null
   uid: string
-}
-
-export interface RawSourceMap {
-  version: number
-  sources: string[]
-  names: string[]
-  sourceRoot?: string
-  sourcesContent?: string[]
-  transpiledContentUtopia?: string
-  mappings: string
-  file: string
 }
 
 export interface JSXMapExpression extends WithComments {
@@ -296,13 +283,13 @@ export type JSXElementChildren = Array<JSXElementChild>
 export interface RegularParam {
   type: 'REGULAR_PARAM'
   paramName: string
-  defaultExpression: JSExpressionMapOrOtherJavascript | null
+  defaultExpression: JSExpression | null
 }
 
 export interface DestructuredParamPart {
   propertyName: string | undefined
   param: Param
-  defaultExpression: JSExpressionMapOrOtherJavascript | null
+  defaultExpression: JSExpression | null
 }
 
 export interface DestructuredObject {
@@ -404,17 +391,6 @@ export type ImportInfo = SameFileOrigin | ImportedOrigin
 
 export type ActiveAndDefaultConditionValues = { active: boolean; default: boolean }
 export type ConditionValue = ActiveAndDefaultConditionValues | 'not-a-conditional'
-
-export interface EarlyReturnVoid {
-  type: 'EARLY_RETURN_VOID'
-}
-
-export interface EarlyReturnResult {
-  type: 'EARLY_RETURN_RESULT'
-  result: unknown
-}
-
-export type EarlyReturn = EarlyReturnVoid | EarlyReturnResult
 
 export type DetectedLayoutSystem = 'flex' | 'grid' | 'flow' | 'none'
 export type TextDirection = 'ltr' | 'rtl'
