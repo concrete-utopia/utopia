782b54ec1690564aa6a1db006aa367c33d677603
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-06-10 14:53:11 +0200

    Run dom walker after resize/mutation observer changes in select mode (#5838)
    **Problem:**
    In the sample store we encountered out-of-date navigator several times,
    e.g. after initial load or after remix navigation.
    Sometimes the problem is automatically fixed after a while, sometimes
    you need to interact with the editor, and then the navigator is fixed.
    See: https://github.com/concrete-utopia/utopia/issues/5824
    
    **Fix:**
    When you navigate with Remix, or you wait for asynchronous load of data
    and for the changes after that, then there is no final dispatch when the
    content is ready (which would trigger a dom walker run).
    We experienced this problem earlier in live mode: when the state of the
    app changes in live mode, we need to guarantee to run the dom walker,
    even though no dispatch has happened. To fix this, we rely in resize and
    mutation observers, see,
    https://github.com/concrete-utopia/utopia/pull/4167
    
    Even though In the original PR the observers were not turned in select
    mode due to performance reasons, in my experience the performance impact
    is acceptable, because we don't listen to the observer during canvas
    interactions. So we only have extra dom walker runs after one-off
    interactions, e.g. after changing something in the inspector.
    
    I stopped using the resize observer polyfill, because ResizeObserver is
    widely supported on modern browsers, so that just looked as a potential
    source of problems (and the native solution should be better regarding
    performance too). I still needed to keep the polyfill for unit tests,
    because jsdom doesn't support ResizeObserver.
    
    I disabled the dom walker caching tests, because they were already
    invalid (they expected exactly the opposite what their title
    described....). Fully understanding why these tests are like this and
    fix them would greatly increase the scope of this PR, and it is an
    urgent fix for a project like the sample store.
    
    **Manual Tests:**
    I hereby swear that:
    
    - [X] I opened a hydrogen project and it loaded
    - [X] I could navigate to various routes in Preview mode
    
    Fixes https://github.com/concrete-utopia/utopia/issues/5824

--- editor/src/components/canvas/dom-walker.ts
+++ editor/src/components/canvas/dom-walker.ts
@@ -1,8 +1,6 @@
 import React from 'react'
 import { sides } from 'utopia-api/core'
 import * as ResizeObserverSyntheticDefault from 'resize-observer-polyfill'
-const ResizeObserver = ResizeObserverSyntheticDefault.default ?? ResizeObserverSyntheticDefault
-
 import * as EP from '../../core/shared/element-path'
 import type {
   DetectedLayoutSystem,
@@ -88,10 +86,12 @@ import { pick } from '../../core/shared/object-utils'
 import { getFlexAlignment, getFlexJustifyContent, MaxContent } from '../inspector/inspector-common'
 import type { EditorDispatch } from '../editor/action-types'
 import { runDOMWalker } from '../editor/actions/action-creators'
-import { isLiveMode } from '../editor/editor-modes'
+
+export const ResizeObserver =
+  window.ResizeObserver ?? ResizeObserverSyntheticDefault.default ?? ResizeObserverSyntheticDefault
 
 const MutationObserverConfig = { attributes: true, childList: true, subtree: true }
-const ObserversAvailable = (window as any).MutationObserver != null && ResizeObserver != null
+const ObserversAvailable = window.MutationObserver != null && ResizeObserver != null
 
 function elementLayoutSystem(computedStyle: CSSStyleDeclaration | null): DetectedLayoutSystem {
   if (computedStyle == null) {
@@ -170,7 +170,7 @@ function isScene(node: Node): node is HTMLElement {
   )
 }
 
-function findParentScene(target: HTMLElement): string | null {
+function findParentScene(target: Element): string | null {
   // First check if the node is a Scene element, which could be nested at any level
   const sceneID = getDOMAttribute(target, UTOPIA_SCENE_ID_KEY)
   if (sceneID != null) {
@@ -538,12 +538,19 @@ export function initDomWalkerObservers(
   editorStore: UtopiaStoreAPI,
   dispatch: EditorDispatch,
 ): { resizeObserver: ResizeObserver; mutationObserver: MutationObserver } {
+  // Warning: I modified this code so it runs in all modes, not just in live mode. We still don't trigger
+  // the DOM walker during canvas interactions, so the performance impact doesn't seem that bad. But it is
+  // necessary, because after remix navigation, and after dynamic changes coming from loaders sometimes the
+  // dom walker was not executed after all the changes.
+  //
+  // This was the original comment here when this only ran in live mode:
+  //
   // Warning: These observers only trigger the DOM walker whilst in live mode to ensure metadata is up to date
   // when interacting with the actual running application / components. There are likely edge cases where we
   // also want these to trigger the DOM walker whilst in select mode, but if we find such a case we need to
   // adequately assess the performance impact of doing so, and ideally find a way to only do so when the observed
   // change was not triggered by a user interaction
-  const resizeObserver = new ResizeObserver((entries: any) => {
+  const resizeObserver = new ResizeObserver((entries: ResizeObserverEntry[]) => {
     const canvasInteractionHappening = selectCanvasInteractionHappening(editorStore.getState())
     const selectedViews = editorStore.getState().editor.selectedViews
     if (canvasInteractionHappening) {
@@ -560,7 +567,7 @@ export function initDomWalkerObservers(
           shouldRunDOMWalker = true
         }
       }
-      if (shouldRunDOMWalker && isLiveMode(editorStore.getState().editor.mode)) {
+      if (shouldRunDOMWalker) {
         dispatch([runDOMWalker()])
       }
     }
@@ -592,7 +599,7 @@ export function initDomWalkerObservers(
           }
         }
       }
-      if (shouldRunDOMWalker && isLiveMode(editorStore.getState().editor.mode)) {
+      if (shouldRunDOMWalker) {
         dispatch([runDOMWalker()])
       }
     }
