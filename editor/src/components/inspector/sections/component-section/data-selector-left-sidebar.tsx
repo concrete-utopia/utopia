cb8ef5b027aa702316bb02589ab50ee5488bcf1a
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-06-12 11:07:34 +0200

    a fake search button!

--- editor/src/components/inspector/sections/component-section/data-selector-left-sidebar.tsx
+++ editor/src/components/inspector/sections/component-section/data-selector-left-sidebar.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import type { ElementPath } from 'utopia-shared/src/types'
-import * as EP from '../../../../core/shared/element-path'
-import { FlexColumn, colorTheme } from '../../../../uuiui'
+import { FlexColumn, FlexRow, LargerIcons, colorTheme } from '../../../../uuiui'
 import {
   insertionCeilingToString,
   insertionCeilingsEqual,
   type FileRootPath,
 } from '../../../canvas/ui-jsx-canvas'
+import { stopPropagation } from '../../common/inspector-utils'
 import { DataPickerRow } from './data-selector-columns'
 
 interface SelectableScope {
@@ -25,23 +25,43 @@ export const DataSelectorLeftSidebar = React.memo((props: DataSelectorLeftSideba
   const { scopes, setSelectedScope, activeScope } = props
   return (
     <FlexColumn
+      onClick={stopPropagation}
       style={{
         alignSelf: 'stretch',
         minWidth: 150,
-        paddingRight: 8,
+        padding: 8,
+        paddingTop: 16,
         borderRight: `1px solid ${colorTheme.subduedBorder.cssValue}`,
       }}
     >
-      {scopes.map((scope) => {
-        return (
-          <ScopeRow
-            key={insertionCeilingToString(scope.scope)}
-            scope={scope}
-            selected={insertionCeilingsEqual(scope.scope, activeScope)}
-            setSelectedScope={setSelectedScope}
-          />
-        )
-      })}
+      <FlexRow
+        style={{
+          height: 24,
+          marginBottom: 16,
+          padding: '6px 8px',
+          borderRadius: 16,
+          gap: 8,
+          border: `1px solid ${colorTheme.fg7.value}`,
+        }}
+      >
+        <LargerIcons.MagnifyingGlass style={{ zoom: 0.6 }} />
+      </FlexRow>
+      <span style={{ fontSize: 10, fontWeight: 'bold', color: colorTheme.fg4.cssValue }}>
+        Scopes
+      </span>
+      <FlexColumn>
+        {scopes.map((scope) => {
+          return (
+            <ScopeRow
+              key={insertionCeilingToString(scope.scope)}
+              scope={scope}
+              selected={insertionCeilingsEqual(scope.scope, activeScope)}
+              disabled={!scope.hasContent}
+              setSelectedScope={setSelectedScope}
+            />
+          )
+        })}
+      </FlexColumn>
     </FlexColumn>
   )
 })
@@ -50,22 +70,26 @@ const ScopeRow = React.memo(
   (props: {
     scope: SelectableScope
     selected: boolean
+    disabled: boolean
     setSelectedScope: (scope: ElementPath | FileRootPath) => void
   }) => {
-    const { scope, selected, setSelectedScope } = props
+    const { scope, selected, disabled, setSelectedScope } = props
 
     const onClick = React.useCallback(
       (e: React.MouseEvent<HTMLDivElement>) => {
         e.stopPropagation()
-        setSelectedScope(scope.scope)
+        if (!disabled) {
+          setSelectedScope(scope.scope)
+        }
       },
-      [setSelectedScope, scope.scope],
+      [setSelectedScope, disabled, scope.scope],
     )
 
     return (
       <DataPickerRow
         style={{
           backgroundColor: selected ? colorTheme.bg4.value : undefined,
+          color: disabled ? colorTheme.fg6.value : colorTheme.neutralForeground.value,
         }}
         onClick={onClick}
       >
