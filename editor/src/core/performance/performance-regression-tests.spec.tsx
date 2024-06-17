90c3120e53465c28b65fb9a736ae6d44456492e8
Sean Parsons <git@futurenotfound.com>
2024-06-17 15:29:58 +0100

    fix(pr) Corrected the performance snapshots.

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`773`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`797`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`993`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1017`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -183,7 +183,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`525`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`549`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`635`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`659`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
