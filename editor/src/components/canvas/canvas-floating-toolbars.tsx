a1b813845e9c93e1e66522aed865da9dbd99d8cd
benwolfram <ben.wolfram@shopify.com>
2024-06-19 11:13:59 -0500

    remove floating closed panel buttons

--- editor/src/components/canvas/canvas-floating-toolbars.tsx
+++ editor/src/components/canvas/canvas-floating-toolbars.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FlexColumn, FlexRow } from '../../uuiui'
 import { CanvasToolbar } from '../editor/canvas-toolbar'
-import { ClosedPanels } from '../editor/closed-panels'
 import { Substores, useEditorState } from '../editor/store/store-hook'
 import { ErrorOverlayComponent } from './canvas-error-overlay'
 import { SafeModeErrorOverlay } from './canvas-wrapper-component'
@@ -32,18 +31,16 @@ export const CanvasFloatingToolbars = React.memo((props: { style: React.CSSPrope
           position: 'absolute',
           top: 0,
           alignItems: 'flex-start',
-          justifyContent: 'space-between',
+          justifyContent: 'center',
           padding: 6,
           width: '100%',
           height: '100%',
         }}
       >
-        <ClosedPanels side='left' />
         <FlexRow style={{ width: 250, alignItems: 'flex-start', gap: 10 }}>
           <CanvasToolbar />
           <CanvasStrategyPicker />
         </FlexRow>
-        <ClosedPanels side='right' />
       </FlexRow>
       {/* The error overlays are deliberately the last here so they hide other canvas UI */}
       {safeMode ? <SafeModeErrorOverlay /> : <ErrorOverlayComponent />}
