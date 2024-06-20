b99bb7a4a4201daf569082cf6d1ea25318c13644
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-06-20 21:42:20 +0200

    Update snapshots

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1147`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1135`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`716`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`710`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
