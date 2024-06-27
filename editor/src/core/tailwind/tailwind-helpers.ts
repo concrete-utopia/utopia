ab27f6f51bc37fe8e7b1615e2c44e97658111aeb
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-06-27 13:10:17 -0400

    Add padding props to tailwind-helpers

--- editor/src/core/tailwind/tailwind-helpers.ts
+++ editor/src/core/tailwind/tailwind-helpers.ts
@@ -46,16 +46,127 @@ const tailwindDimensionMap: { [key: string]: string } = {
   'h-auto': 'auto',
   'h-full': '100%',
   'h-screen': '100vh',
+
+  // Padding classes
+  'p-0': '0px',
+  'p-1': '0.25rem', // 4px
+  'p-2': '0.5rem', // 8px
+  'p-3': '0.75rem', // 12px
+  'p-4': '1rem', // 16px
+  'p-5': '1.25rem', // 20px
+  'p-6': '1.5rem', // 24px
+  'p-8': '2rem', // 32px
+  'p-10': '2.5rem', // 40px
+  'p-12': '3rem', // 48px
+  'p-16': '4rem', // 64px
+  'p-20': '5rem', // 80px
+  'p-24': '6rem', // 96px
+  'p-32': '8rem', // 128px
+  'p-40': '10rem', // 160px
+  'p-48': '12rem', // 192px
+  'p-56': '14rem', // 224px
+  'p-64': '16rem', // 256px
+
+  // Padding left classes
+  'pl-0': '0px',
+  'pl-1': '0.25rem', // 4px
+  'pl-2': '0.5rem', // 8px
+  'pl-3': '0.75rem', // 12px
+  'pl-4': '1rem', // 16px
+  'pl-5': '1.25rem', // 20px
+  'pl-6': '1.5rem', // 24px
+  'pl-8': '2rem', // 32px
+  'pl-10': '2.5rem', // 40px
+  'pl-12': '3rem', // 48px
+  'pl-16': '4rem', // 64px
+  'pl-20': '5rem', // 80px
+  'pl-24': '6rem', // 96px
+  'pl-32': '8rem', // 128px
+  'pl-40': '10rem', // 160px
+  'pl-48': '12rem', // 192px
+  'pl-56': '14rem', // 224px
+  'pl-64': '16rem', // 256px
+
+  // Padding right classes
+  'pr-0': '0px',
+  'pr-1': '0.25rem', // 4px
+  'pr-2': '0.5rem', // 8px
+  'pr-3': '0.75rem', // 12px
+  'pr-4': '1rem', // 16px
+  'pr-5': '1.25rem', // 20px
+  'pr-6': '1.5rem', // 24px
+  'pr-8': '2rem', // 32px
+  'pr-10': '2.5rem', // 40px
+  'pr-12': '3rem', // 48px
+  'pr-16': '4rem', // 64px
+  'pr-20': '5rem', // 80px
+  'pr-24': '6rem', // 96px
+  'pr-32': '8rem', // 128px
+  'pr-40': '10rem', // 160px
+  'pr-48': '12rem', // 192px
+  'pr-56': '14rem', // 224px
+  'pr-64': '16rem', // 256px
+
+  // Padding bottom classes
+  'pb-0': '0px',
+  'pb-1': '0.25rem', // 4px
+  'pb-2': '0.5rem', // 8px
+  'pb-3': '0.75rem', // 12px
+  'pb-4': '1rem', // 16px
+  'pb-5': '1.25rem', // 20px
+  'pb-6': '1.5rem', // 24px
+  'pb-8': '2rem', // 32px
+  'pb-10': '2.5rem', // 40px
+  'pb-12': '3rem', // 48px
+  'pb-16': '4rem', // 64px
+  'pb-20': '5rem', // 80px
+  'pb-24': '6rem', // 96px
+  'pb-32': '8rem', // 128px
+  'pb-40': '10rem', // 160px
+  'pb-48': '12rem', // 192px
+  'pb-56': '14rem', // 224px
+  'pb-64': '16rem', // 256px
+
+  // Padding top classes
+  'pt-0': '0px',
+  'pt-1': '0.25rem', // 4px
+  'pt-2': '0.5rem', // 8px
+  'pt-3': '0.75rem', // 12px
+  'pt-4': '1rem', // 16px
+  'pt-5': '1.25rem', // 20px
+  'pt-6': '1.5rem', // 24px
+  'pt-8': '2rem', // 32px
+  'pt-10': '2.5rem', // 40px
+  'pt-12': '3rem', // 48px
+  'pt-16': '4rem', // 64px
+  'pt-20': '5rem', // 80px
+  'pt-24': '6rem', // 96px
+  'pt-32': '8rem', // 128px
+  'pt-40': '10rem', // 160px
+  'pt-48': '12rem', // 192px
+  'pt-56': '14rem', // 224px
+  'pt-64': '16rem', // 256px
+
   // Add more mappings as needed
 }
 
-export type TailwindProp = 'width' | 'height'
+export type TailwindProp =
+  | 'width'
+  | 'height'
+  | 'padding-left'
+  | 'padding-right'
+  | 'padding-bottom'
+  | 'padding-top'
 
 export function getTailwindPropFromPropertyPath(property: PropertyPath): TailwindProp | null {
   const lastProp = PP.lastPartToString(property)
   switch (lastProp) {
     case 'width':
     case 'height':
+    case 'padding-left':
+    case 'padding-right':
+    case 'padding-bottom':
+    case 'padding-top':
       return lastProp
     default:
       return null
