abaed545f182bb6e89adf87169776664020a9804
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-07-12 16:42:02 +0200

    bumping why-did-you-render

--- editor/src/templates/editor-entry-point-imports.tsx
+++ editor/src/templates/editor-entry-point-imports.tsx
@@ -1,8 +1,8 @@
-// import * as React from 'react'
-// const whyDidYouRender = require('@welldone-software/why-did-you-render')
-// whyDidYouRender(React, {
-//   trackAllPureComponents: true,
-// })
+import * as React from 'react'
+import whyDidYouRender from '@welldone-software/why-did-you-render'
+whyDidYouRender(React, {
+  trackAllPureComponents: true,
+})
 
 import '../vite-shims'
 
