8a18ff305106a99168e3c25a229783b62bc68ee5
lankaukk <lankaukk@yahoo.com>
2024-07-24 18:11:35 -0400

    dropdown arrows for popup lists

--- editor/src/components/inspector/sections/style-section/container-subsection/blendmode-row.tsx
+++ editor/src/components/inspector/sections/style-section/container-subsection/blendmode-row.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import * as PP from '../../../../../core/shared/property-path'
+import { PropertyLabel } from '../../../widgets/property-label'
 import { useInspectorStyleInfo } from '../../../common/property-path-hooks'
+import { UIGridRow } from '../../../widgets/ui-grid-row'
 import { PopupList } from '../../../../../uuiui'
 import type { SelectOption } from '../../../../../uuiui-deps'
 import { InspectorContextMenuItems, InspectorContextMenuWrapper } from '../../../../../uuiui-deps'
@@ -9,42 +11,17 @@ const blendModeOptions = [
   { value: 'normal', label: 'Normal' },
   { value: 'multiply', label: 'Multiply' },
   { value: 'screen', label: 'Screen' },
-  { value: 'overlay', label: 'Overlay' },
   { value: 'darken', label: 'Darken' },
-  { value: 'lighten', label: 'Lighten' },
-  { value: 'color-dodge', label: 'Color Dodge' },
-  { value: 'color-burn', label: 'Color Burn' },
-  { value: 'hard-light', label: 'Hard Light' },
-  { value: 'soft-light', label: 'Soft Light' },
-  { value: 'difference', label: 'Difference' },
-  { value: 'exclusion', label: 'Exclusion' },
-  { value: 'hue', label: 'Hue' },
-  { value: 'saturation', label: 'Saturation' },
-  { value: 'color', label: 'Color' },
-  { value: 'luminosity', label: 'Luminosity' },
 ]
 
 const blendModeProp = [PP.create('style', 'mixBlendMode')]
 
 export const BlendModeRow = React.memo(() => {
   const blendModeMetadata = useInspectorStyleInfo('mixBlendMode')
-
-  // Check if blendModeMetadata is correctly populated
-  if (!blendModeMetadata) {
-    console.error('blendModeMetadata is not available')
-    return null
-  }
-
   const [onSubmitBlendModeOption] = blendModeMetadata.useSubmitValueFactory(
     (selectedOption: SelectOption) => selectedOption.value,
   )
-
   const blendMode = blendModeMetadata.value
-
-  // Check if blendMode is correctly populated
-  if (!blendMode) {
-    console.error('blendMode is not available')
-  }
 
   const whichBlendModeOption = blendModeOptions.find((option) => option.value === blendMode)
 
@@ -53,7 +30,6 @@ export const BlendModeRow = React.memo(() => {
     ['mixBlendMode'],
     blendModeMetadata.onUnsetValues,
   )
-
   return (
     <InspectorContextMenuWrapper
       id='blendmode-row-context-menu'
@@ -66,7 +42,7 @@ export const BlendModeRow = React.memo(() => {
         options={blendModeOptions}
         onSubmitValue={onSubmitBlendModeOption}
         controlStyles={blendModeMetadata.controlStyles}
-        style={{ background: 'transparent', width: 133 }}
+        style={{ background: 'transparent' }}
       />
     </InspectorContextMenuWrapper>
   )
