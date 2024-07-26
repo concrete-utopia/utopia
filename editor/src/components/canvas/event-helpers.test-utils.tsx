ad367caeda5e803abff5bd6a8610fa7290d63647
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-07-26 18:24:17 +0200

    Mouse down needs separate modifier config

--- editor/src/components/canvas/event-helpers.test-utils.tsx
+++ editor/src/components/canvas/event-helpers.test-utils.tsx
@@ -197,6 +197,7 @@ export async function mouseDragFromPointToPoint(
   endPoint: Point,
   options: {
     modifiers?: Modifiers
+    mouseDownModifiers?: Modifiers
     eventOptions?: MouseEventInit
     staggerMoveEvents?: boolean
     midDragCallback?: () => Promise<void>
@@ -216,10 +217,13 @@ export async function mouseDragFromPointToPoint(
   if (options.moveBeforeMouseDown) {
     await mouseMoveToPoint(eventSourceElement, startPoint, options)
   }
+
+  const mouseDownOptions = { ...options, modifiers: options.mouseDownModifiers }
+
   if (options.realMouseDown) {
-    dispatchMouseDownEventAtPoint(startPoint, options)
+    dispatchMouseDownEventAtPoint(startPoint, mouseDownOptions)
   } else {
-    await mouseDownAtPoint(eventSourceElement, startPoint, options)
+    await mouseDownAtPoint(eventSourceElement, startPoint, mouseDownOptions)
   }
 
   if (staggerMoveEvents) {
