4faf980e110918f1700ba778437d1c69f02413ed
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-07-30 14:01:01 +0200

    fix grid lines

--- editor/src/components/canvas/controls/grid-controls.tsx
+++ editor/src/components/canvas/controls/grid-controls.tsx
@@ -651,8 +651,8 @@ export const GridControls = controlForStrategyMemoized(() => {
           const placeholders = Array.from(Array(grid.cells).keys())
           let style: CSSProperties = {
             position: 'absolute',
-            top: grid.frame.y,
-            left: grid.frame.x,
+            top: grid.frame.y - 1, // account for border!
+            left: grid.frame.x - 1, // account for border!
             width: grid.frame.width,
             height: grid.frame.height,
             display: 'grid',
@@ -707,7 +707,17 @@ export const GridControls = controlForStrategyMemoized(() => {
                     id={id}
                     data-testid={id}
                     style={{
-                      border: `1px solid ${borderColor}`,
+                      borderTop: `1px solid ${borderColor}`,
+                      borderLeft: `1px solid ${borderColor}`,
+                      borderBottom:
+                        countedRow >= grid.rows || (grid.rowGap != null && grid.rowGap > 0)
+                          ? `1px solid ${borderColor}`
+                          : undefined,
+                      borderRight:
+                        countedColumn >= grid.columns ||
+                        (grid.columnGap != null && grid.columnGap > 0)
+                          ? `1px solid ${borderColor}`
+                          : undefined,
                       position: 'relative',
                     }}
                     data-grid-row={countedRow}
