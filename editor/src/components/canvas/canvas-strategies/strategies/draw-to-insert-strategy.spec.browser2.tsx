c154942f86ce16ac03d13d6ef123045bcb916cba
Berci Kormendy <korber314@gmail.com>
2024-08-05 13:46:49 +0200

    use slightlyOffsetWindowPointBecauseVeryWeirdIssue, remove rounding

--- editor/src/components/canvas/canvas-strategies/strategies/draw-to-insert-strategy.spec.browser2.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/draw-to-insert-strategy.spec.browser2.tsx
@@ -213,12 +213,12 @@ export var storyboard = (
       const grid = editor.renderedDOM.getByTestId('grid')
       const gridBB = grid.getBoundingClientRect()
 
-      const target: WindowPoint = windowPoint(
-        slightlyOffsetWindowPointBecauseVeryWeirdIssue({
-          x: Math.ceil(gridBB.x + gridBB.width / 2),
-          y: Math.ceil(gridBB.y + gridBB.height / 2),
+      const target: WindowPoint = slightlyOffsetWindowPointBecauseVeryWeirdIssue(
+        windowPoint({
+          x: gridBB.x + gridBB.width / 2,
+          y: gridBB.y + gridBB.height / 2,
         }),
-      )
+      ) as WindowPoint
 
       const canvasControlsLayer = editor.renderedDOM.getByTestId(CanvasControlsContainerID)
 
