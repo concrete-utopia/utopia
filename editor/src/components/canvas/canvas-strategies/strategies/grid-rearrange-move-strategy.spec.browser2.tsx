bc7a1da7df0c1c31e3dc7b5f7c913e44963c4250
Berci Kormendy <korber314@gmail.com>
2024-08-08 16:45:07 +0200

    test for move

--- editor/src/components/canvas/canvas-strategies/strategies/grid-rearrange-move-strategy.spec.browser2.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/grid-rearrange-move-strategy.spec.browser2.tsx
@@ -1,16 +1,19 @@
 import * as EP from '../../../../core/shared/element-path'
 import { getRectCenter, localRectangle } from '../../../../core/shared/math-utils'
-import { selectComponentsForTest } from '../../../../utils/utils.test-utils'
+import { selectComponentsForTest, wait } from '../../../../utils/utils.test-utils'
 import CanvasActions from '../../canvas-actions'
 import { GridCellTestId } from '../../controls/grid-controls'
 import { mouseDragFromPointToPoint } from '../../event-helpers.test-utils'
+import type { EditorRenderResult } from '../../ui-jsx.test-utils'
 import { renderTestEditorWithCode } from '../../ui-jsx.test-utils'
 import { gridCellTargetId } from './grid-helpers'
 
 describe('grid rearrange move strategy', () => {
   it('can rearrange elements on a grid', async () => {
+    const editor = await renderTestEditorWithCode(ProjectCode, 'await-first-dom-report')
+
     const testId = 'aaa'
-    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest({
+    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest(editor, {
       scale: 1,
       pathString: `sb/scene/grid/${testId}`,
       testId: testId,
@@ -24,8 +27,10 @@ describe('grid rearrange move strategy', () => {
   })
 
   it('can rearrange element with no explicit grid props set', async () => {
+    const editor = await renderTestEditorWithCode(ProjectCode, 'await-first-dom-report')
+
     const testId = 'bbb'
-    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest({
+    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest(editor, {
       scale: 1,
       pathString: `sb/scene/grid/${testId}`,
       testId: testId,
@@ -39,8 +44,10 @@ describe('grid rearrange move strategy', () => {
   })
 
   it('can rearrange elements on a grid (zoom out)', async () => {
+    const editor = await renderTestEditorWithCode(ProjectCode, 'await-first-dom-report')
+
     const testId = 'aaa'
-    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest({
+    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest(editor, {
       scale: 0.5,
       pathString: `sb/scene/grid/${testId}`,
       testId: testId,
@@ -54,8 +61,10 @@ describe('grid rearrange move strategy', () => {
   })
 
   it('can rearrange elements on a grid (zoom in)', async () => {
+    const editor = await renderTestEditorWithCode(ProjectCode, 'await-first-dom-report')
+
     const testId = 'aaa'
-    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest({
+    const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = await runMoveTest(editor, {
       scale: 2,
       pathString: `sb/scene/grid/${testId}`,
       testId: testId,
@@ -67,11 +76,127 @@ describe('grid rearrange move strategy', () => {
       gridRowStart: '2',
     })
   })
+
+  describe('grids within grids', () => {
+    it('can move a grid child that is a grid itself', async () => {
+      const editor = await renderTestEditorWithCode(
+        `import * as React from 'react'
+import { Storyboard } from 'utopia-api'
+
+export var storyboard = (
+  <Storyboard data-uid='sb'>
+    <div
+      data-uid='grid'
+      data-testid='grid'
+      style={{
+        position: 'absolute',
+        left: -94,
+        top: 698,
+        display: 'grid',
+        gap: 10,
+        width: 600,
+        height: 'max-content',
+        gridTemplateColumns: '2.4fr 1fr 1fr',
+        gridTemplateRows: '99px 109px 90px',
+      }}
+    >
+      <div
+        data-uid='grid-inside-grid'
+        data-testid='grid-inside-grid'
+        style={{
+          backgroundColor: 'green',
+          contain: 'layout',
+          display: 'grid',
+          gap: 5,
+          gridTemplateColumns: '1fr 1fr 1fr',
+          gridTemplateRows: '1fr',
+          gridColumn: 1,
+          gridRow: 2,
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: '#aaaaaa33',
+            width: 66,
+            height: 67,
+            contain: 'layout',
+          }}
+          data-uid='ad3e425d-a224-44a9-a303-465f80e0'
+        />
+        <div
+          style={{
+            backgroundColor: '#aaaaaa33',
+            width: 66,
+            height: 67,
+            contain: 'layout',
+          }}
+          data-uid='d805fd1f-fdcd-422f-a785-f023409c'
+        />
+        <div
+          style={{
+            backgroundColor: '#aaaaaa33',
+            width: 66,
+            height: 67,
+            contain: 'layout',
+          }}
+          data-uid='b2124463-bd84-414f-960b-29689a92'
+        />
+      </div>
+    </div>
+  </Storyboard>
+)
+`,
+        'await-first-dom-report',
+      )
+
+      const testId = 'grid-inside-grid'
+      const elementPathToDrag = EP.fromString(`sb/grid/${testId}`)
+
+      await selectComponentsForTest(editor, [elementPathToDrag])
+
+      const sourceGridCell = editor.renderedDOM.getByTestId(GridCellTestId(elementPathToDrag))
+      const targetGridCell = editor.renderedDOM.getByTestId(
+        gridCellTargetId(EP.fromString('sb/grid'), 2, 3),
+      )
+
+      const sourceRect = sourceGridCell.getBoundingClientRect()
+      const targetRect = targetGridCell.getBoundingClientRect()
+
+      await mouseDragFromPointToPoint(
+        sourceGridCell,
+        {
+          x: sourceRect.x + 10,
+          y: sourceRect.y + 10,
+        },
+        getRectCenter(
+          localRectangle({
+            x: targetRect.x,
+            y: targetRect.y,
+            width: targetRect.width,
+            height: targetRect.height,
+          }),
+        ),
+      )
+
+      await wait(10000)
+
+      const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } =
+        editor.renderedDOM.getByTestId(testId).style
+
+      expect({ gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd }).toEqual({
+        gridColumnEnd: 'auto',
+        gridColumnStart: '3',
+        gridRowEnd: 'auto',
+        gridRowStart: '2',
+      })
+    })
+  })
 })
 
-async function runMoveTest(props: { scale: number; pathString: string; testId: string }) {
-  const editor = await renderTestEditorWithCode(ProjectCode, 'await-first-dom-report')
-
+async function runMoveTest(
+  editor: EditorRenderResult,
+  props: { scale: number; pathString: string; testId: string },
+) {
   const elementPathToDrag = EP.fromString(props.pathString)
 
   await selectComponentsForTest(editor, [elementPathToDrag])
