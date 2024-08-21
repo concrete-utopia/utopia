a84f0680f19a01793b0589a64e7c1f6371fc0a32
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-08-21 12:57:33 +0200

    adding navigator test to dom-sampler
    oops

--- editor/src/components/canvas/dom-sampler.spec.tsx
+++ editor/src/components/canvas/dom-sampler.spec.tsx
@@ -1,5 +1,9 @@
 import { BakedInStoryboardVariableName } from '../../core/model/scene-utils'
-import { runDOMWalker } from '../editor/actions/action-creators'
+import * as EP from '../../core/shared/element-path'
+import { runDOMWalker, setFocusedElement } from '../editor/actions/action-creators'
+import { navigatorEntryToKey } from '../editor/store/editor-state'
+import { getNavigatorTargetsFromEditorState } from '../navigator/navigator-utils'
+import { NavigatorTestProjectWithSyntheticElements } from '../navigator/navigator.test-utils'
 import { formatTestProjectCode, renderTestEditorWithCode } from './ui-jsx.test-utils'
 
 describe('Basic Dom Sampler tests', () => {
@@ -129,6 +133,47 @@ export var Playground = ({ style }) => {
         "sb/cond4",
       ]
     `)
+  })
+
+  it('The ElementPathTree and thus the navigator tree are correct for a test project with synthetic elements', async () => {
+    const editor = await renderTestEditorWithCode(
+      NavigatorTestProjectWithSyntheticElements,
+      'await-first-dom-report',
+    )
+
+    await editor.dispatch([setFocusedElement(EP.fromString('sb/sc/app:app-root/card'))], true)
+    await editor.getDispatchFollowUpActionsFinished()
+
+    expect(
+      getNavigatorTargetsFromEditorState(editor.getEditorState().editor).navigatorTargets.map(
+        navigatorEntryToKey,
+      ),
+    ).toEqual([
+      'regular-sb/1e7',
+      'regular-sb/sc',
+      'regular-sb/sc/app',
+      'regular-sb/sc/app:app-root',
+      'regular-sb/sc/app:app-root/card',
+      'regular-sb/sc/app:app-root/card:card-root',
+      'regular-sb/sc/app:app-root/card:card-root/30d',
+      'regular-sb/sc/app:app-root/card:card-root/card-span',
+      'regular-sb/sc/app:app-root/card/card-child',
+      'regular-sb/sc/app:app-root/children-code-block',
+      'regular-sb/sc/app:app-root/frag',
+      'regular-sb/sc/app:app-root/frag/frag-child',
+      'regular-sb/sc/app:app-root/frag/cond-1',
+      'conditional-clause-sb/sc/app:app-root/frag/cond-1-true-case',
+      'regular-sb/sc/app:app-root/frag/cond-1/cond-1-true',
+      'regular-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-1-true-child',
+      'regular-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-2',
+      'conditional-clause-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-2-true-case',
+      'regular-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-2/cond-2-child',
+      'conditional-clause-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-2-false-case',
+      'synthetic-sb/sc/app:app-root/frag/cond-1/cond-1-true/cond-2/d84-attribute',
+      'conditional-clause-sb/sc/app:app-root/frag/cond-1-false-case',
+      'synthetic-sb/sc/app:app-root/frag/cond-1/019-attribute',
+      'regular-sb/sc/app/app-child',
+    ])
   })
 })
 
