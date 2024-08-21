bf83092dce63ac57433f3bfe058ab875e4409d6c
Sean Parsons <git@futurenotfound.com>
2024-08-21 14:56:31 +0100

    fix(pr) Updated performance regression tests.

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -183,7 +183,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`529`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`539`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -249,7 +249,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`702`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`712`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 })
