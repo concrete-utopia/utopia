a479f063a5aa19fc18f6ea5a79684fa023f0fde9
Sean Parsons <git@futurenotfound.com>
2024-08-29 14:55:50 +0100

    fix(pr) Fixed up performance snapshots.

--- editor/src/core/performance/performance-regression-tests.spec.tsx
+++ editor/src/core/performance/performance-regression-tests.spec.tsx
@@ -65,7 +65,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`743`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`737`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
@@ -127,7 +127,7 @@ describe('React Render Count Tests -', () => {
 
     const renderCountAfter = renderResult.getNumberOfRenders()
     // if this breaks, GREAT NEWS but update the test please :)
-    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`1091`)
+    expect(renderCountAfter - renderCountBefore).toMatchInlineSnapshot(`989`)
     expect(renderResult.getRenderInfo()).toMatchSnapshot()
   })
 
