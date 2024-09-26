7180ccc93fe1ce67e1bf572092a84666101a9193
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-09-26 14:22:45 +0200

    clean up

--- editor/src/components/inspector/alignment-buttons.tsx
+++ editor/src/components/inspector/alignment-buttons.tsx
@@ -8,7 +8,7 @@ import * as EP from '../../core/shared/element-path'
 import { zeroRectangle, zeroRectIfNullOrInfinity } from '../../core/shared/math-utils'
 import * as PP from '../../core/shared/property-path'
 import { assertNever } from '../../core/shared/utils'
-import { Button, Icn, Icons, SquareButton } from '../../uuiui'
+import { Button, Icn, Icons } from '../../uuiui'
 import type { DropdownMenuItem } from '../../uuiui/radix-components'
 import {
   DropdownMenu,
@@ -22,105 +22,39 @@ import {
   unsetProperty,
 } from '../editor/actions/action-creators'
 import { useDispatch } from '../editor/store/dispatch-context'
-import { Substores, useEditorState } from '../editor/store/store-hook'
+import { Substores, useEditorState, useRefEditorState } from '../editor/store/store-hook'
 import { getControlStyles } from './common/control-styles'
 import { OptionChainControl } from './controls/option-chain-control'
 import { UIGridRow } from './widgets/ui-grid-row'
 
 type ActiveAlignments = { [key in Alignment]: boolean }
 
-export const AlignmentButtons = React.memo((props: { numberOfTargets: number }) => {
+export const AlignmentButtons = React.memo(() => {
   const dispatch = useDispatch()
 
-  const disableAlign = props.numberOfTargets === 0
+  const selectedViews = useRefEditorState((store) => store.editor.selectedViews)
 
-  const selectedViews = useEditorState(
-    Substores.metadata,
-    (store) => store.editor.selectedViews,
-    'AlignmentButtons selectedViews',
-  )
+  const disableAlign = React.useMemo(() => selectedViews.current.length === 0, [selectedViews])
+  const disableDistribute = React.useMemo(() => selectedViews.current.length < 3, [selectedViews])
 
-  const jsxMetadata = useEditorState(
-    Substores.metadata,
-    (store) => store.editor.jsxMetadata,
-    'AlignmentButtons jsxMetadata',
-  )
+  const activeAlignments = useActiveAlignments()
+  const hasActiveAlignments = React.useMemo(() => {
+    return (
+      activeAlignments.bottom ||
+      activeAlignments.left ||
+      activeAlignments.right ||
+      activeAlignments.vcenter ||
+      activeAlignments.hcenter ||
+      activeAlignments.top
+    )
+  }, [activeAlignments])
 
-  const activeAlignments: ActiveAlignments = React.useMemo(() => {
-    function isActive(alignment: 'left' | 'hcenter' | 'right' | 'top' | 'vcenter' | 'bottom') {
-      return (
-        selectedViews.length > 0 &&
-        selectedViews.every((view) => {
-          const isFlexOrGridChild = MetadataUtils.isFlexOrGridChild(jsxMetadata, view)
+  const getUnsetAlignmentsActions = useGetUnsetAlignmentsActions(activeAlignments)
 
-          const meta = MetadataUtils.findElementByElementPath(jsxMetadata, view)
-          if (meta == null) {
-            return false
-          }
-
-          const { align, justify } = MetadataUtils.getRelativeAlignJustify(jsxMetadata, view)
-
-          const parent = MetadataUtils.findElementByElementPath(jsxMetadata, EP.parentPath(view))
-          const parentFrame =
-            parent != null ? zeroRectIfNullOrInfinity(parent.globalFrame) : zeroRectangle
-
-          switch (alignment) {
-            case 'bottom':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[align] === 'flex-end' ||
-                    meta.specialSizeMeasurements[align] === 'end'
-                : parent != null &&
-                    meta.specialSizeMeasurements.offset.y ===
-                      parentFrame.height - meta.specialSizeMeasurements.clientHeight
-            case 'hcenter':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[justify] === 'center'
-                : parent != null &&
-                    meta.specialSizeMeasurements.offset.x ===
-                      Math.ceil(
-                        parentFrame.width / 2 - meta.specialSizeMeasurements.clientWidth / 2,
-                      )
-            case 'left':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[justify] === 'flex-start' ||
-                    meta.specialSizeMeasurements[justify] === 'start'
-                : meta.specialSizeMeasurements.offset.x === 0
-            case 'right':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[justify] === 'flex-end' ||
-                    meta.specialSizeMeasurements[justify] === 'end'
-                : parent != null &&
-                    meta.specialSizeMeasurements.offset.x ===
-                      parentFrame.width - meta.specialSizeMeasurements.clientWidth
-            case 'top':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[align] === 'flex-start' ||
-                    meta.specialSizeMeasurements[align] === 'start'
-                : meta.specialSizeMeasurements.offset.y === 0
-            case 'vcenter':
-              return isFlexOrGridChild
-                ? meta.specialSizeMeasurements[align] === 'center'
-                : parent != null &&
-                    meta.specialSizeMeasurements.offset.y ===
-                      Math.ceil(
-                        parentFrame.height / 2 - meta.specialSizeMeasurements.clientHeight / 2,
-                      )
-            default:
-              assertNever(alignment)
-              return false
-          }
-        })
-      )
-    }
-    return {
-      left: isActive('left'),
-      hcenter: isActive('hcenter'),
-      right: isActive('right'),
-      top: isActive('top'),
-      vcenter: isActive('vcenter'),
-      bottom: isActive('bottom'),
-    }
-  }, [jsxMetadata, selectedViews])
+  const unsetAllAlignments = React.useCallback(() => {
+    const actions = getUnsetAlignmentsActions().map((a) => a.action)
+    dispatch(actions)
+  }, [dispatch, getUnsetAlignmentsActions])
 
   const distributeSelected = React.useCallback(
     (distribution: Distribution) => {
@@ -128,8 +62,6 @@ export const AlignmentButtons = React.memo((props: { numberOfTargets: number }) 
     },
     [dispatch],
   )
-
-  const disableDistribute = props.numberOfTargets < 3
 
   const distributeHorizontal = React.useCallback(
     () => distributeSelected('horizontal'),
@@ -148,54 +80,6 @@ export const AlignmentButtons = React.memo((props: { numberOfTargets: number }) 
       </Button>
     )
   }, [])
-
-  const activeAlignmentsList = React.useMemo(() => {
-    return Object.entries(activeAlignments).reduce((acc, [key, set]) => {
-      if (set) {
-        return acc.concat(key as Alignment)
-      }
-      return acc
-    }, [] as Alignment[])
-  }, [activeAlignments])
-
-  const hasActiveAlignments = React.useMemo(() => {
-    return activeAlignmentsList.length > 0
-  }, [activeAlignmentsList])
-
-  const getUnsetAlignmentsActions = React.useCallback(() => {
-    return mapDropNulls((path) => {
-      let actions: { alignments: string[]; action: EditorAction }[] = []
-
-      const isFlexOrGridChild = MetadataUtils.isFlexOrGridChild(jsxMetadata, path)
-      const { align, justify } = MetadataUtils.getRelativeAlignJustify(jsxMetadata, path)
-
-      if (activeAlignments.left || activeAlignments.hcenter || activeAlignments.right) {
-        const alignments = activeAlignmentsList.filter(
-          (alignment) => alignment === 'left' || alignment === 'hcenter' || alignment === 'right',
-        )
-        actions.push({
-          action: unsetProperty(path, PP.create('style', isFlexOrGridChild ? justify : 'left')),
-          alignments: alignments,
-        })
-      }
-
-      if (activeAlignments.top || activeAlignments.vcenter || activeAlignments.bottom) {
-        const alignments = activeAlignmentsList.filter(
-          (alignment) => alignment === 'top' || alignment === 'vcenter' || alignment === 'bottom',
-        )
-        actions.push({
-          action: unsetProperty(path, PP.create('style', isFlexOrGridChild ? align : 'top')),
-          alignments: alignments,
-        })
-      }
-
-      return actions
-    }, selectedViews).flat()
-  }, [jsxMetadata, activeAlignments, activeAlignmentsList, selectedViews])
-
-  const unsetAllAlignments = React.useCallback(() => {
-    dispatch(getUnsetAlignmentsActions().map((a) => a.action))
-  }, [dispatch, getUnsetAlignmentsActions])
 
   const alignSelected = React.useCallback(
     (alignment: Alignment) => {
@@ -354,32 +238,142 @@ export const AlignmentButtons = React.memo((props: { numberOfTargets: number }) 
 })
 AlignmentButtons.displayName = 'AlignmentButtons'
 
-interface AlignDistributeButtonProps {
-  onClick: () => void
-  toolTip: string
-  iconType: string
-  disabled: boolean
-  active: boolean
+function useActiveAlignments(): ActiveAlignments {
+  const selectedViews = useRefEditorState((store) => store.editor.selectedViews)
+
+  const jsxMetadata = useEditorState(
+    Substores.metadata,
+    (store) => store.editor.jsxMetadata,
+    'useActiveAlignments jsxMetadata',
+  )
+
+  const isActive = React.useCallback(
+    (alignment: Alignment) => {
+      if (selectedViews.current.length === 0) {
+        return false
+      }
+      return selectedViews.current.every((view) => {
+        const isFlexOrGridChild = MetadataUtils.isFlexOrGridChild(jsxMetadata, view)
+        const { align, justify } = MetadataUtils.getRelativeAlignJustify(jsxMetadata, view)
+
+        const meta = MetadataUtils.findElementByElementPath(jsxMetadata, view)
+        if (meta == null) {
+          return false
+        }
+
+        const parent = MetadataUtils.findElementByElementPath(jsxMetadata, EP.parentPath(view))
+        const parentFrame =
+          parent != null ? zeroRectIfNullOrInfinity(parent.globalFrame) : zeroRectangle
+
+        switch (alignment) {
+          case 'top':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[align] === 'flex-start' ||
+                  meta.specialSizeMeasurements[align] === 'start'
+              : meta.specialSizeMeasurements.offset.y === 0
+          case 'bottom':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[align] === 'flex-end' ||
+                  meta.specialSizeMeasurements[align] === 'end'
+              : parent != null &&
+                  meta.specialSizeMeasurements.offset.y ===
+                    parentFrame.height - meta.specialSizeMeasurements.clientHeight
+          case 'vcenter':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[align] === 'center'
+              : parent != null &&
+                  meta.specialSizeMeasurements.offset.y ===
+                    Math.ceil(
+                      parentFrame.height / 2 - meta.specialSizeMeasurements.clientHeight / 2,
+                    )
+          case 'left':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[justify] === 'flex-start' ||
+                  meta.specialSizeMeasurements[justify] === 'start'
+              : meta.specialSizeMeasurements.offset.x === 0
+          case 'right':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[justify] === 'flex-end' ||
+                  meta.specialSizeMeasurements[justify] === 'end'
+              : parent != null &&
+                  meta.specialSizeMeasurements.offset.x ===
+                    parentFrame.width - meta.specialSizeMeasurements.clientWidth
+          case 'hcenter':
+            return isFlexOrGridChild
+              ? meta.specialSizeMeasurements[justify] === 'center'
+              : parent != null &&
+                  meta.specialSizeMeasurements.offset.x ===
+                    Math.ceil(parentFrame.width / 2 - meta.specialSizeMeasurements.clientWidth / 2)
+          default:
+            assertNever(alignment)
+            return false
+        }
+      })
+    },
+    [jsxMetadata, selectedViews],
+  )
+
+  return React.useMemo(() => {
+    return {
+      left: isActive('left'),
+      hcenter: isActive('hcenter'),
+      right: isActive('right'),
+      top: isActive('top'),
+      vcenter: isActive('vcenter'),
+      bottom: isActive('bottom'),
+    }
+  }, [isActive])
 }
 
-const AlignDistributeButton = React.memo<AlignDistributeButtonProps>(
-  (props: AlignDistributeButtonProps) => {
-    return (
-      <SquareButton
-        highlight
-        disabled={props.disabled}
-        onClick={props.onClick}
-        spotlight={props.active}
-      >
-        <Icn
-          tooltipText={props.toolTip}
-          category='inspector'
-          type={props.iconType}
-          width={16}
-          height={16}
-        />
-      </SquareButton>
-    )
-  },
-)
-AlignDistributeButton.displayName = 'AlignDistributeButton'
+type UnsetAlignment = {
+  alignments: Alignment[]
+  action: EditorAction
+}
+
+function useGetUnsetAlignmentsActions(activeAlignments: ActiveAlignments) {
+  const selectedViews = useRefEditorState((store) => store.editor.selectedViews)
+
+  const jsxMetadata = useEditorState(
+    Substores.metadata,
+    (store) => store.editor.jsxMetadata,
+    'useGetUnsetAlignmentsActions jsxMetadata',
+  )
+
+  return React.useCallback((): UnsetAlignment[] => {
+    const activeAlignmentsList = Object.entries(activeAlignments).reduce((acc, [key, set]) => {
+      if (set) {
+        return acc.concat(key as Alignment)
+      }
+      return acc
+    }, [] as Alignment[])
+
+    return mapDropNulls((path) => {
+      let actions: UnsetAlignment[] = []
+
+      const isFlexOrGridChild = MetadataUtils.isFlexOrGridChild(jsxMetadata, path)
+      const { align, justify } = MetadataUtils.getRelativeAlignJustify(jsxMetadata, path)
+
+      if (activeAlignments.left || activeAlignments.hcenter || activeAlignments.right) {
+        const alignments: Alignment[] = activeAlignmentsList.filter(
+          (alignment) => alignment === 'left' || alignment === 'hcenter' || alignment === 'right',
+        )
+        actions.push({
+          action: unsetProperty(path, PP.create('style', isFlexOrGridChild ? justify : 'left')),
+          alignments: alignments,
+        })
+      }
+
+      if (activeAlignments.top || activeAlignments.vcenter || activeAlignments.bottom) {
+        const alignments: Alignment[] = activeAlignmentsList.filter(
+          (alignment) => alignment === 'top' || alignment === 'vcenter' || alignment === 'bottom',
+        )
+        actions.push({
+          action: unsetProperty(path, PP.create('style', isFlexOrGridChild ? align : 'top')),
+          alignments: alignments,
+        })
+      }
+
+      return actions
+    }, selectedViews.current).flat()
+  }, [activeAlignments, selectedViews, jsxMetadata])
+}
