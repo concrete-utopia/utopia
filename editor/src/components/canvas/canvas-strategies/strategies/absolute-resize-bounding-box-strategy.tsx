5e39bd962a4be21869454e3678b53589d646730b
Berci Kormendy <korber314@gmail.com>
2024-10-02 13:00:57 +0200

    add child groups

--- editor/src/components/canvas/canvas-strategies/strategies/absolute-resize-bounding-box-strategy.tsx
+++ editor/src/components/canvas/canvas-strategies/strategies/absolute-resize-bounding-box-strategy.tsx
@@ -296,7 +296,7 @@ export function absoluteResizeBoundingBoxStrategy(
                 updateHighlightedViews('mid-interaction', []),
                 setCursorCommand(pickCursorFromEdgePosition(edgePosition)),
               ],
-              retargetedTargets,
+              [...retargetedTargets, ...childGroups],
             )
           }
         } else {
