0a538b79a05035c16559c523c84952272490ecfb
Balint Gabor <127662+gbalint@users.noreply.github.com>
2024-10-03 12:14:08 +0200

    Better grid resize element strategy (#6457)
    **Problem:**
    The original grid-resize-element strategy was written in a way, that it
    resized based on the data that which cell is currently under the mouse.
    This made it impossible to resize with the mouse e.g. going in between
    cells over the gap, or outside of the grid.
    
    **Fix:**
    Let's rely on the resize bounding box coming from
    `resizeBoundingBoxFromSide`, and we can resize to those cells which have
    an intersection with the resize bounding box.
    
    As an extra, I refactored the `getMetadataWithGridCellBounds` function,
    so that it returns the updated custom strategy state, and the strategies
    don't have to construct that (this strategy state update guarantees that
    if the grid cell bounds were only found in the latestmetadata, then we
    store them in the custom strategy state, so they are kept until the end
    of the interaction). For the note, some grid strategies add further
    changes to the strategy state, after the subsequent strategy refactors
    these will be removed.
    
    **Manual Tests:**
    I hereby swear that:
    
    - [x] I opened a hydrogen project and it loaded
    - [x] I could navigate to various routes in Play mode

--- editor/src/components/canvas/canvas-strategies/strategies/grid-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/grid-helpers.ts
@@ -774,12 +774,12 @@ export function getMetadataWithGridCellBounds(
   customStrategyState: CustomStrategyState,
 ): {
   metadata: ElementInstanceMetadata | null
-  foundIn: 'startingMetadata' | 'latestMetadata' | 'strategyState' | null
+  customStrategyState: CustomStrategyState | null
 } {
   if (path == null) {
     return {
       metadata: null,
-      foundIn: null,
+      customStrategyState: null,
     }
   }
 
@@ -788,7 +788,7 @@ export function getMetadataWithGridCellBounds(
   if (fromStartingMetadata?.specialSizeMeasurements.gridCellGlobalFrames != null) {
     return {
       metadata: fromStartingMetadata,
-      foundIn: 'startingMetadata',
+      customStrategyState: null,
     }
   }
 
@@ -796,7 +796,7 @@ export function getMetadataWithGridCellBounds(
   if (fromStrategyState != null) {
     return {
       metadata: fromStrategyState,
-      foundIn: 'strategyState',
+      customStrategyState: null,
     }
   }
 
@@ -804,12 +804,21 @@ export function getMetadataWithGridCellBounds(
   if (fromLatestMetadata?.specialSizeMeasurements.gridCellGlobalFrames != null) {
     return {
       metadata: fromLatestMetadata,
-      foundIn: 'latestMetadata',
+      customStrategyState: {
+        ...customStrategyState,
+        grid: {
+          ...customStrategyState.grid,
+          metadataCacheForGrids: {
+            ...customStrategyState.grid.metadataCacheForGrids,
+            [EP.toString(path)]: fromLatestMetadata,
+          },
+        },
+      },
     }
   }
 
   return {
     metadata: fromStartingMetadata,
-    foundIn: 'startingMetadata',
+    customStrategyState: null,
   }
 }
