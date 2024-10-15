3295b5bb808ca900a328c09329fffecfbf4b0cbb
Balazs Bajorics <2226774+balazsbajorics@users.noreply.github.com>
2024-10-15 10:17:06 +0200

    progress

--- editor/src/components/canvas/controls/select-mode/grid-gap-control-component-2.tsx
+++ editor/src/components/canvas/controls/select-mode/grid-gap-control-component-2.tsx
@@ -10,9 +10,10 @@ import {
 } from '../../../inspector/common/css-utils'
 import { CanvasOffsetWrapper } from '../canvas-offset-wrapper'
 import type { GridData } from '../grid-controls-for-strategies'
-import { useGridData } from '../grid-controls-for-strategies'
+import { getNullableAutoOrTemplateBaseString, useGridData } from '../grid-controls-for-strategies'
 import { getStyleMatchingTargetGrid } from '../grid-controls-helpers'
 import type { GridGapControlProps } from './grid-gap-control-component'
+import { NO_OP } from '../../../../core/shared/utils'
 
 export const GridGapControlComponent2 = React.memo<GridGapControlProps>((props) => {
   const { selectedElement, updatedGapValueRow, updatedGapValueColumn } = props
@@ -61,6 +62,15 @@ const GridPaddingOutlineForDimension = (props: {
           <GridRowHighlight
             key={index}
             hide={index === 0 || index === length - 1 || index % 2 === 0} // we only want to show the divs that fall in where the gaps are in the original grid
+            gapId={`${dimension}-${index}`}
+            onGapHover={NO_OP}
+            onHandleHoverEndInner={NO_OP}
+            axis={dimension === 'rows' ? 'row' : 'column'}
+            template={getNullableAutoOrTemplateBaseString(
+              dimension === 'rows' ? grid.gridTemplateColumns : grid.gridTemplateRows,
+            )}
+            numberOfHandles={dimension === 'rows' ? grid.columns : grid.rows}
+            gap={dimension === 'rows' ? grid.rowGap ?? grid.gap : grid.columnGap ?? grid.gap}
           />
         )
       })}
@@ -68,7 +78,19 @@ const GridPaddingOutlineForDimension = (props: {
   )
 }
 
-const GridRowHighlight = (props: { hide: boolean }) => {
+const GridRowHighlight = (props: {
+  gapId: string
+  onGapHover: () => void
+  onHandleHoverEndInner: () => void
+  hide: boolean
+  axis: 'row' | 'column'
+  template: string | undefined
+  numberOfHandles: number
+  gap: number | null
+}) => {
+  const { gapId, onGapHover, onHandleHoverEndInner, hide, axis, template, gap, numberOfHandles } =
+    props
+
   const colorTheme = useColorTheme()
   const canvasScale = useEditorState(
     Substores.canvas,
@@ -80,12 +102,30 @@ const GridRowHighlight = (props: { hide: boolean }) => {
 
   return (
     <div
+      key={gapId}
+      onMouseEnter={onGapHover}
+      onMouseLeave={onHandleHoverEndInner}
+      data-testid={`gap-control-segment-${gapId}`}
       style={{
+        display: 'grid',
         position: 'relative',
+        pointerEvents: 'all',
         boxShadow: `inset 0 0 0 ${lineWidth}px ${colorTheme.brandNeonOrange.value}`,
-        opacity: props.hide ? 0 : 1,
+        opacity: hide ? 0 : 1,
+
+        alignItems: 'center',
+        justifyContent: 'center',
+        placeItems: 'center',
+
+        gap: gap ?? 0,
+        gridTemplateColumns: axis === 'row' ? template : '1fr',
+        gridTemplateRows: axis === 'column' ? template : '1fr',
       }}
-    />
+    >
+      {createArrayWithLength(numberOfHandles, (i) => (
+        <div>{'<>'}</div>
+      ))}
+    </div>
   )
 }
 
