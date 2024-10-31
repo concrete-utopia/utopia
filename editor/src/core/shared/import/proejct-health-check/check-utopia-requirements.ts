36213b92f44a77eb14c66ffefbea1b2b13e0fa6e
Liad Yosef <liady@users.noreply.github.com>
2024-10-31 15:44:10 +0200

    feat(import): early break on error (#6604)
    This PR breaks early when there is a requirement check error (for
    example a TS project) - without fetching dependencies or dispatching the
    results to the project.
    For example a Next TS project - note the dependencies are not fetched
    and the project contents are not updated:
    <video
    src="https://github.com/user-attachments/assets/6ef4e75a-1659-4219-b7e0-d94471a88f57"></video>
    
    **Manual Tests:**
    I hereby swear that:
    
    - [X] I opened a hydrogen project and it loaded
    - [X] I could navigate to various routes in Play mode

--- editor/src/core/shared/import/proejct-health-check/check-utopia-requirements.ts
+++ editor/src/core/shared/import/proejct-health-check/check-utopia-requirements.ts
@@ -5,6 +5,7 @@ import type { EditorDispatch } from '../../../../components/editor/action-types'
 import CheckPackageJson from './requirements/requirement-package-json'
 import CheckLanguage from './requirements/requirement-language'
 import CheckReactVersion from './requirements/requirement-react'
+import { RequirementResolutionResult } from './utopia-requirements-types'
 import type { ProjectRequirement, RequirementCheck } from './utopia-requirements-types'
 import { notifyCheckingRequirement, notifyResolveRequirement } from './utopia-requirements-service'
 import CheckStoryboard from './requirements/requirement-storyboard'
@@ -12,7 +13,7 @@ import CheckStoryboard from './requirements/requirement-storyboard'
 export function checkAndFixUtopiaRequirements(
   dispatch: EditorDispatch,
   parsedProjectContents: ProjectContentTreeRoot,
-): ProjectContentTreeRoot {
+): { result: RequirementResolutionResult; fixedProjectContents: ProjectContentTreeRoot } {
   const checks: Record<ProjectRequirement, RequirementCheck> = {
     storyboard: new CheckStoryboard(),
     packageJsonEntries: new CheckPackageJson(),
@@ -20,11 +21,15 @@ export function checkAndFixUtopiaRequirements(
     reactVersion: new CheckReactVersion(),
   }
   let projectContents = parsedProjectContents
+  let result: RequirementResolutionResult = RequirementResolutionResult.Found
   // iterate over all checks, updating the project contents as we go
   for (const [name, check] of Object.entries(checks)) {
     const checkName = name as ProjectRequirement
     notifyCheckingRequirement(dispatch, checkName, check.getStartText())
     const checkResult = check.check(projectContents)
+    if (checkResult.resolution === RequirementResolutionResult.Critical) {
+      result = RequirementResolutionResult.Critical
+    }
     notifyResolveRequirement(
       dispatch,
       checkName,
@@ -34,7 +39,7 @@ export function checkAndFixUtopiaRequirements(
     )
     projectContents = checkResult.newProjectContents ?? projectContents
   }
-  return projectContents
+  return { result: result, fixedProjectContents: projectContents }
 }
 
 export function getPackageJson(
