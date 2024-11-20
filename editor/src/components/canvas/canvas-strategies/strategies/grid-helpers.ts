dca813ffd980b8b0d5d2674713887f3c56d4e96b
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2024-11-20 12:10:34 +0100

    Grid ruler markers (#6654)
    **Problem:**
    
    Selected grid items should show ruler markers on the borders of the grid
    to indicate their placement properties.
    
    **Fix:**
    
    Add the ruler markers to the grid controls for items.
    
    **Notes:**
    - markers currently target the cell bounds, it should change to the
    middle of the space between cells when we have offset indicator lines
    (future PR)
    
    
    
    https://github.com/user-attachments/assets/b0dd3450-70fa-47cc-b627-e9f792f13ad8
    
    
    
    **Manual Tests:**
    I hereby swear that:
    
    - [x] I opened a hydrogen project and it loaded
    - [x] I could navigate to various routes in Play mode
    
    Fixes #6656

--- editor/src/components/canvas/canvas-strategies/strategies/grid-helpers.ts
+++ editor/src/components/canvas/canvas-strategies/strategies/grid-helpers.ts
@@ -73,7 +73,7 @@ export function gridPositionToValue(
   return p.numericalPosition
 }
 
-export function isAutoGridPin(v: GridPositionOrSpan): boolean {
+export function isAutoGridPin(v: GridPositionOrSpan | null): boolean {
   return isCSSKeyword(v) && v.value === 'auto'
 }
 
@@ -498,6 +498,7 @@ export function getOriginalElementGridConfiguration(
   const draggingFromCellCoords = getClosestGridCellToPoint(
     gridCellGlobalFrames,
     interactionData.dragStart,
+    'exclusive',
   )?.gridCellCoordinates
   if (draggingFromCellCoords == null) {
     return null
