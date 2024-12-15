613d5d67820fbd298d904f0ca4b10945fb6bfa8a
Liad Yosef <liad.yosef@gmail.com>
2024-12-15 16:31:32 +0200

    add end-to-end tests for updating and deleting the correct classes

--- editor/src/components/canvas/plugins/tailwind-style-plugin.spec.ts
+++ editor/src/components/canvas/plugins/tailwind-style-plugin.spec.ts
@@ -9,6 +9,7 @@ import { TailwindPlugin } from './tailwind-style-plugin'
 import { createModifiedProject } from '../../../sample-projects/sample-project-utils.test-utils'
 import { TailwindConfigPath } from '../../../core/tailwind/tailwind-config'
 import { getTailwindConfigCached } from '../../../core/tailwind/tailwind-compilation'
+import * as StylePlugins from './style-plugins'
 
 const Project = createModifiedProject({
   [StoryboardFilePath]: `
@@ -30,7 +31,7 @@ export var storyboard = (
     >
       <div
         data-uid='div'
-        className='flex flex-row gap-[12px]'
+        className='flex flex-row gap-[12px] pt-[10px] lg:pt-[20px]'
       />
     </Scene>
   </Storyboard>
@@ -40,13 +41,16 @@ export var storyboard = (
   [TailwindConfigPath]: `
     const TailwindConfig = {
         content: [],
-        theme: { extend: { gap: { enormous: '222px' } } }
+        theme: { extend: { gap: { enormous: '222px' }, screens: { lg: '500px' } } }
     }
     export default TailwindConfig
 `,
 })
 
 describe('tailwind style plugin', () => {
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
   it('can set Tailwind class', async () => {
     const editor = await renderTestEditorWithModel(Project, 'await-first-dom-report')
     const target = EP.fromString('sb/scene/div')
@@ -64,7 +68,7 @@ describe('tailwind style plugin', () => {
       updatedEditor.editorStateWithChanges.projectContents,
     )!
     expect(formatJSXAttributes(normalizedElement.props)).toEqual({
-      className: 'flex flex-col gap-[222px]',
+      className: 'flex flex-col gap-[222px] pt-[10px] lg:pt-[20px]',
       'data-uid': 'div',
     })
   })
@@ -84,7 +88,47 @@ describe('tailwind style plugin', () => {
       updatedEditor.editorStateWithChanges.projectContents,
     )!
     expect(formatJSXAttributes(normalizedElement.props)).toEqual({
-      className: 'flex flex-col gap-enormous',
+      className: 'flex flex-col gap-enormous pt-[10px] lg:pt-[20px]',
+      'data-uid': 'div',
+    })
+  })
+
+  it('can set Tailwind class with size modifier and a custom config', async () => {
+    // this is done since we don't calculate the scene size in the test
+    jest.spyOn(StylePlugins, 'sceneSize').mockReturnValue({ type: 'scene', width: 700 })
+    const editor = await renderTestEditorWithModel(Project, 'await-first-dom-report')
+    const target = EP.fromString('sb/scene/div')
+    const updatedEditor = TailwindPlugin(
+      getTailwindConfigCached(editor.getEditorState().editor),
+    ).updateStyles(editor.getEditorState().editor, target, [
+      { type: 'set', property: 'paddingTop', value: '200px' },
+    ])
+    const normalizedElement = getJSXElementFromProjectContents(
+      target,
+      updatedEditor.editorStateWithChanges.projectContents,
+    )!
+    expect(formatJSXAttributes(normalizedElement.props)).toEqual({
+      className: 'flex flex-row gap-[12px] pt-[10px] lg:pt-[200px]',
+      'data-uid': 'div',
+    })
+  })
+
+  it('can remove Tailwind class with size modifier and a custom config', async () => {
+    // this is done since we don't calculate the scene size in the test
+    jest.spyOn(StylePlugins, 'sceneSize').mockReturnValue({ type: 'scene', width: 700 })
+    const editor = await renderTestEditorWithModel(Project, 'await-first-dom-report')
+    const target = EP.fromString('sb/scene/div')
+    const updatedEditor = TailwindPlugin(
+      getTailwindConfigCached(editor.getEditorState().editor),
+    ).updateStyles(editor.getEditorState().editor, target, [
+      { type: 'delete', property: 'paddingTop' },
+    ])
+    const normalizedElement = getJSXElementFromProjectContents(
+      target,
+      updatedEditor.editorStateWithChanges.projectContents,
+    )!
+    expect(formatJSXAttributes(normalizedElement.props)).toEqual({
+      className: 'flex flex-row gap-[12px] pt-[10px]',
       'data-uid': 'div',
     })
   })
